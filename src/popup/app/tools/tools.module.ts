5ae551ccb203c698b6d20dd694759ab095c7d46d
Hinton <hinton.oscar@gmail.com>
2017-11-07 19:48:50 +0100

    Convert password generator to typescript.

--- src/popup/app/tools/tools.module.ts
+++ src/popup/app/tools/tools.module.ts
@@ -1,7 +1,11 @@
 import * as angular from 'angular';
 import { ToolsComponent } from './tools.component';
+import { PasswordGeneratorComponent } from './password-generator.component';
 
 export default angular
     .module('bit.tools', ['ngAnimate', 'ngclipboard', 'toastr', 'oitozero.ngSweetAlert'])
+
     .component('tools', ToolsComponent)
+    .component('passwordGenerator', PasswordGeneratorComponent)
+
     .name;
