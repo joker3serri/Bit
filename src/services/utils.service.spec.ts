d6f134c0bd85282e3e395b614268caa9687aac40
Hinton <hinton.oscar@gmail.com>
2017-11-16 23:18:17 +0100

    Add test runner.

---
+++ src/services/utils.service.spec.ts
@@ -0,0 +1,8 @@
+describe('Utils Service', () => {
+
+    it('has a dummy spec to test 2 + 2', function() {
+        // An intentionally failing test. No code within expect() will never equal 4.
+        expect(4).toEqual(4);
+      });
+
+})
