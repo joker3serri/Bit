a615a2bb46204fb9e2b7477348f13e2fdf1fc18e
Hinton <hinton.oscar@gmail.com>
2017-11-26 00:52:45 +0100

    Re-write save feature settings.

--- src/popup/app/settings/features.component.ts
+++ src/popup/app/settings/features.component.ts
@@ -43,142 +43,64 @@ class FeaturesController {
             .getObjFromStorage<boolean>(this.constantsService.disableFaviconKey);
     }
 
-    updateGa() {
-        chrome.storage.local.get(this.constantsService.disableGaKey, (obj: any) => {
-            // Default for Firefox is disabled.
-            if ((this.utilsService.isFirefox() && obj[this.constantsService.disableGaKey] === undefined) ||
-                obj[this.constantsService.disableGaKey]) {
-                // enable
-                obj[this.constantsService.disableGaKey] = false;
-            } else {
-                // disable
-                this.$analytics.eventTrack('Disabled Analytics');
-                obj[this.constantsService.disableGaKey] = true;
-            }
+    callAnalytics(name: string, enabled: boolean) {
+        const status = enabled ? 'Enabled' : 'Disabled';
+        this.$analytics.eventTrack(`${status} ${name}`);
+    }
 
-            chrome.storage.local.set(obj, () => {
-                this.$timeout(() => {
-                    this.disableGa = obj[this.constantsService.disableGaKey];
-                });
-                if (!obj[this.constantsService.disableGaKey]) {
-                    this.$analytics.eventTrack('Enabled Analytics');
-                }
-            });
-        });
+    updateGa() {
+        this.utilsService.saveObjToStorage(
+            this.constantsService.disableGaKey,
+            this.disableGa,
+        );
+        this.callAnalytics('Analytics', !this.disableGa);
     }
 
     updateAddLoginNotification() {
-        chrome.storage.local.get(this.constantsService.disableAddLoginNotificationKey, (obj: any) => {
-            if (obj[this.constantsService.disableAddLoginNotificationKey]) {
-                // enable
-                obj[this.constantsService.disableAddLoginNotificationKey] = false;
-            } else {
-                // disable
-                this.$analytics.eventTrack('Disabled Add Login Notification');
-                obj[this.constantsService.disableAddLoginNotificationKey] = true;
-            }
-
-            chrome.storage.local.set(obj, () => {
-                this.$timeout(() => {
-                    this.disableAddLoginNotification = obj[this.constantsService.disableAddLoginNotificationKey];
-                });
-                if (!obj[this.constantsService.disableAddLoginNotificationKey]) {
-                    this.$analytics.eventTrack('Enabled Add Login Notification');
-                }
-            });
-        });
+        this.utilsService.saveObjToStorage(
+            this.constantsService.disableAddLoginNotificationKey,
+            this.disableAddLoginNotification,
+        );
+        this.callAnalytics('Add Login Notification', !this.disableAddLoginNotification);
     }
 
     updateDisableContextMenuItem() {
-        chrome.storage.local.get(this.constantsService.disableContextMenuItemKey, (obj: any) => {
-            if (obj[this.constantsService.disableContextMenuItemKey]) {
-                // enable
-                obj[this.constantsService.disableContextMenuItemKey] = false;
-            } else {
-                // disable
-                this.$analytics.eventTrack('Disabled Context Menu Item');
-                obj[this.constantsService.disableContextMenuItemKey] = true;
-            }
-
-            chrome.storage.local.set(obj, () => {
-                this.$timeout(() => {
-                    this.disableContextMenuItem = obj[this.constantsService.disableContextMenuItemKey];
-                });
-                if (!obj[this.constantsService.disableContextMenuItemKey]) {
-                    this.$analytics.eventTrack('Enabled Context Menu Item');
-                }
+        this.utilsService
+            .saveObjToStorage(
+                this.constantsService.disableContextMenuItemKey,
+                this.disableContextMenuItem,
+            )
+            .then(() => {
                 chrome.runtime.sendMessage({
                     command: 'bgUpdateContextMenu',
                 });
             });
-        });
+        this.callAnalytics('Context Menu Item', !this.disableContextMenuItem);
     }
 
     updateAutoTotpCopy() {
-        chrome.storage.local.get(this.constantsService.disableAutoTotpCopyKey, (obj: any) => {
-            if (obj[this.constantsService.disableAutoTotpCopyKey]) {
-                // enable
-                obj[this.constantsService.disableAutoTotpCopyKey] = false;
-            } else {
-                // disable
-                this.$analytics.eventTrack('Disabled Auto Copy TOTP');
-                obj[this.constantsService.disableAutoTotpCopyKey] = true;
-            }
-
-            chrome.storage.local.set(obj, () => {
-                this.$timeout(() => {
-                    this.disableAutoTotpCopy = obj[this.constantsService.disableAutoTotpCopyKey];
-                });
-                if (!obj[this.constantsService.disableAutoTotpCopyKey]) {
-                    this.$analytics.eventTrack('Enabled Auto Copy TOTP');
-                }
-            });
-        });
+        this.utilsService.saveObjToStorage(
+            this.constantsService.disableAutoTotpCopyKey,
+            this.disableAutoTotpCopy,
+        );
+        this.callAnalytics('Auto Copy TOTP', !this.disableAutoTotpCopy);
     }
 
     updateAutoFillOnPageLoad() {
-        chrome.storage.local.get(this.constantsService.enableAutoFillOnPageLoadKey, (obj: any) => {
-            if (obj[this.constantsService.enableAutoFillOnPageLoadKey]) {
-                // disable
-                obj[this.constantsService.enableAutoFillOnPageLoadKey] = false;
-            } else {
-                // enable
-                this.$analytics.eventTrack('Enable Auto-fill Page Load');
-                obj[this.constantsService.enableAutoFillOnPageLoadKey] = true;
-            }
-
-            chrome.storage.local.set(obj, () => {
-                this.$timeout(() => {
-                    this.enableAutoFillOnPageLoad = obj[this.constantsService.enableAutoFillOnPageLoadKey];
-                });
-                if (!obj[this.constantsService.enableAutoFillOnPageLoadKey]) {
-                    this.$analytics.eventTrack('Disable Auto-fill Page Load');
-                }
-            });
-        });
+        this.utilsService.saveObjToStorage(
+            this.constantsService.enableAutoFillOnPageLoadKey,
+            this.enableAutoFillOnPageLoad,
+        );
+        this.callAnalytics('Auto-fill Page Load', this.enableAutoFillOnPageLoad);
     }
 
     updateDisableFavicon() {
-        chrome.storage.local.get(this.constantsService.disableFaviconKey, (obj: any) => {
-            if (obj[this.constantsService.disableFaviconKey]) {
-                // enable
-                obj[this.constantsService.disableFaviconKey] = false;
-            } else {
-                // disable
-                this.$analytics.eventTrack('Disabled Favicon');
-                obj[this.constantsService.disableFaviconKey] = true;
-            }
-
-            chrome.storage.local.set(obj, () => {
-                this.$timeout(() => {
-                    this.disableFavicon = obj[this.constantsService.disableFaviconKey];
-                    this.stateService.saveState('faviconEnabled', !this.disableFavicon);
-                });
-                if (!obj[this.constantsService.disableFaviconKey]) {
-                    this.$analytics.eventTrack('Enabled Favicon');
-                }
-            });
-        });
+        this.utilsService.saveObjToStorage(
+            this.constantsService.disableFaviconKey,
+            this.disableFavicon,
+        );
+        this.stateService.saveState('faviconEnabled', !this.disableFavicon);
+        this.callAnalytics('Favicon', !this.disableFavicon);
     }
 }
 
