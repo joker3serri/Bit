3749c66bdc6b353ded90835d5fe24408d14c2070
Hinton <hinton.oscar@gmail.com>
2018-02-23 23:39:27 +0100

    Initial PoC of integration with haveibeenpwned.

--- src/popup/app/vault/vaultEditCipherController.js
+++ src/popup/app/vault/vaultEditCipherController.js
@@ -1,9 +1,11 @@
+import * as forge from 'node-forge';
+
 angular
     .module('bit.vault')
 
     .controller('vaultEditCipherController', function ($scope, $state, $stateParams, cipherService, folderService,
         cryptoService, toastr, SweetAlert, platformUtilsService, $analytics, i18nService, constantsService, $timeout,
-        popupUtilsService) {
+        popupUtilsService, $http) {
         $timeout(function () {
             popupUtilsService.initListSectionItemListeners(document, angular);
             document.getElementById('name').focus();
@@ -110,6 +112,36 @@ angular
         $scope.togglePassword = function () {
             $analytics.eventTrack('Toggled Password');
             $scope.showPassword = !$scope.showPassword;
+        };
+
+        $scope.checkPassword = () => {
+            $analytics.eventTrack('Check Password');
+
+            const md = forge.md.sha1.create();
+            md.update($scope.cipher.login.password);
+
+            const hash = md.digest().toHex().toUpperCase();
+            $http
+                .get('https://api.pwnedpasswords.com/range/' + hash.substr(0, 5))
+                .then((resp) => {
+                    const data = resp.data.split(/\r?\n/);
+                    const remaining = hash.substr(5);
+
+                    let matches = 0;
+                    data.forEach(element => {
+                        const d = element.split(':');
+                        if (d[0] == remaining) {
+                            console.log(d);
+                            matches = d[1];
+                        }
+                    });
+
+                    if (matches != 0) {
+                        toastr.error(i18nService.passwordExposed, i18nService.errorsOccurred);
+                    } else {
+                        toastr.success(i18nService.passwordSafe);
+                    }
+                });
         };
 
         $scope.addField = function (type) {
