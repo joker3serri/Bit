5f4b7bf164ec6638db0f8426fbb5b419411a6655
Gara64 <paul@cozycloud.cc>
2019-11-13 22:55:57 +0100

    feat: Reorganize vault display
    Change the vault organization:
    
    * Remove notes, collections and folders
    * Show all logins
    * Show cards and identities on the top

--- src/popup/vault/groupings.component.ts
+++ src/popup/vault/groupings.component.ts
@@ -18,8 +18,6 @@ import { BrowserApi } from '../../browser/browserApi';
 import { CipherType } from 'jslib/enums/cipherType';
 
 import { CipherView } from 'jslib/models/view/cipherView';
-import { CollectionView } from 'jslib/models/view/collectionView';
-import { FolderView } from 'jslib/models/view/folderView';
 
 import { CipherService } from 'jslib/abstractions/cipher.service';
 import { CollectionService } from 'jslib/abstractions/collection.service';
@@ -46,9 +44,6 @@ const ScopeStateId = ComponentId + 'Scope';
 })
 export class GroupingsComponent extends BaseGroupingsComponent implements OnInit, OnDestroy {
     ciphers: CipherView[];
-    noFolderCiphers: CipherView[];
-    folderCounts = new Map<string, number>();
-    collectionCounts = new Map<string, number>();
     typeCounts = new Map<CipherType, number>();
     searchText: string;
     state: any;
@@ -60,7 +55,6 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
     private loadedTimeout: number;
     private selectedTimeout: number;
     private preventSelected = false;
-    private noFolderListSize = 100;
     private searchTimeout: any = null;
     private hasSearched = false;
     private hasLoadedAllCiphers = false;
@@ -76,16 +70,6 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
         private platformUtilsService: PlatformUtilsService, private searchService: SearchService,
         private location: Location) {
         super(collectionService, folderService, storageService, userService);
-        this.noFolderListSize = platformUtilsService.isEdge() ? 25 : 100;
-    }
-
-    get showNoFolderCiphers(): boolean {
-        return this.noFolderCiphers != null && this.noFolderCiphers.length < this.noFolderListSize &&
-            this.collections.length === 0;
-    }
-
-    get folderCount(): number {
-        return this.nestedFolders.length - (this.showNoFolderCiphers ? 0 : 1);
     }
 
     async ngOnInit() {
@@ -153,60 +137,21 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
     async load() {
         await super.load(false);
         await this.loadCiphers();
-        if (this.showNoFolderCiphers && this.nestedFolders.length > 0) {
-            // Remove "No Folder" from folder listing
-            this.nestedFolders = this.nestedFolders.slice(0, this.nestedFolders.length - 1);
-        }
-
         super.loaded = true;
     }
 
     async loadCiphers() {
         this.allCiphers = await this.cipherService.getAllDecrypted();
-        if (!this.hasLoadedAllCiphers) {
-            this.hasLoadedAllCiphers = !this.searchService.isSearchable(this.searchText);
-        }
         await this.search(null);
-        let noFolderCiphers: CipherView[] = null;
-        const folderCounts = new Map<string, number>();
-        const collectionCounts = new Map<string, number>();
         const typeCounts = new Map<CipherType, number>();
-
         this.ciphers.forEach((c) => {
-            if (c.folderId == null) {
-                if (noFolderCiphers == null) {
-                    noFolderCiphers = [];
-                }
-                noFolderCiphers.push(c);
-            }
-
             if (typeCounts.has(c.type)) {
                 typeCounts.set(c.type, typeCounts.get(c.type) + 1);
             } else {
                 typeCounts.set(c.type, 1);
             }
-
-            if (folderCounts.has(c.folderId)) {
-                folderCounts.set(c.folderId, folderCounts.get(c.folderId) + 1);
-            } else {
-                folderCounts.set(c.folderId, 1);
-            }
-
-            if (c.collectionIds != null) {
-                c.collectionIds.forEach((colId) => {
-                    if (collectionCounts.has(colId)) {
-                        collectionCounts.set(colId, collectionCounts.get(colId) + 1);
-                    } else {
-                        collectionCounts.set(colId, 1);
-                    }
-                });
-            }
         });
-
-        this.noFolderCiphers = noFolderCiphers;
         this.typeCounts = typeCounts;
-        this.folderCounts = folderCounts;
-        this.collectionCounts = collectionCounts;
     }
 
     async search(timeout: number = null) {
@@ -231,19 +176,12 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
         }, timeout);
     }
 
-    async selectType(type: CipherType) {
-        super.selectType(type);
-        this.router.navigate(['/ciphers'], { queryParams: { type: type } });
+    ciphersByType(type: CipherType) {
+        return this.ciphers.filter((c) => c.type === type);
     }
 
-    async selectFolder(folder: FolderView) {
-        super.selectFolder(folder);
-        this.router.navigate(['/ciphers'], { queryParams: { folderId: folder.id || 'none' } });
-    }
-
-    async selectCollection(collection: CollectionView) {
-        super.selectCollection(collection);
-        this.router.navigate(['/ciphers'], { queryParams: { collectionId: collection.id } });
+    countByType(type: CipherType) {
+        return this.typeCounts.get(type);
     }
 
     async selectCipher(cipher: CipherView) {
@@ -287,13 +225,8 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
         await this.stateService.save(ComponentId, this.state);
 
         this.scopeState = {
-            noFolderCiphers: this.noFolderCiphers,
             ciphers: this.ciphers,
-            collectionCounts: this.collectionCounts,
-            folderCounts: this.folderCounts,
             typeCounts: this.typeCounts,
-            folders: this.folders,
-            collections: this.collections,
         };
         await this.stateService.save(ScopeStateId, this.scopeState);
     }
@@ -304,28 +237,12 @@ export class GroupingsComponent extends BaseGroupingsComponent implements OnInit
             return false;
         }
 
-        if (this.scopeState.noFolderCiphers != null) {
-            this.noFolderCiphers = this.scopeState.noFolderCiphers;
-        }
         if (this.scopeState.ciphers != null) {
             this.ciphers = this.scopeState.ciphers;
-        }
-        if (this.scopeState.collectionCounts != null) {
-            this.collectionCounts = this.scopeState.collectionCounts;
-        }
-        if (this.scopeState.folderCounts != null) {
-            this.folderCounts = this.scopeState.folderCounts;
         }
         if (this.scopeState.typeCounts != null) {
             this.typeCounts = this.scopeState.typeCounts;
         }
-        if (this.scopeState.folders != null) {
-            this.folders = this.scopeState.folders;
-        }
-        if (this.scopeState.collections != null) {
-            this.collections = this.scopeState.collections;
-        }
-
         return true;
     }
 }
