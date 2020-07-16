dbd3b929f7e3a54d532333a13a4aa239250a7c06
Ben <mail1@sonadresse.com>
2020-07-16 18:54:37 +0000

    build ok after merge, ready to test!

--- src/popup/services/sync.service.ts
+++ src/popup/services/sync.service.ts
@@ -1,20 +1,38 @@
+/* -----------------------------------------------------------------------------------
+
+    COZY DUPLICATE -
+    This file is duplicated from the JSlib file : jslib/abstractions/sync.service.ts
+    For more context, see commit f1956682454d00328dea38d37257ab32dc80129f
+    The copied file version is here :
+       https://github.com/bitwarden/jslib/blob/669f6ddf93bbfe8acd18a4834fff5e1c7f9c91ba/src/services/sync.service.ts
+
+   ----------------------------------------------------------------------------------- */
+
 import { ApiService } from 'jslib/abstractions/api.service';
 import { CipherService } from 'jslib/abstractions/cipher.service';
 import { CollectionService } from 'jslib/abstractions/collection.service';
 import { CryptoService } from 'jslib/abstractions/crypto.service';
 import { FolderService } from 'jslib/abstractions/folder.service';
 import { MessagingService } from 'jslib/abstractions/messaging.service';
+import { PolicyService } from 'jslib/abstractions/policy.service';
 import { SettingsService } from 'jslib/abstractions/settings.service';
 import { StorageService } from 'jslib/abstractions/storage.service';
 import { UserService } from 'jslib/abstractions/user.service';
 
-import { SyncService as BaseSyncService } from 'jslib/services/sync.service';
+// TODO BJA : should be imported ?
+// import { PolicyData } from 'jslib/models/data/policyData';
+// import { PolicyResponse } from '../models/response/policyResponse';
 
+/* start Cozy imports */
+import { SyncService as BaseSyncService } from 'jslib/services/sync.service';
 import { CozyClientService } from './cozyClient.service';
+/* end Cozy imports */
 
 const Keys = {
     lastSyncPrefix: 'lastSync_',
 };
+
+
 
 export class SyncService extends BaseSyncService {
     // We need to store these two service instances here because in the extended
@@ -35,6 +53,7 @@ export class SyncService extends BaseSyncService {
         collectionService: CollectionService,
         storageService: StorageService,
         messagingService: MessagingService,
+        policyService: PolicyService,
         logoutCallback: (expired: boolean) => Promise<void>,
         private cozyClientService: () => CozyClientService,
     ) {
@@ -48,6 +67,7 @@ export class SyncService extends BaseSyncService {
                 collectionService,
                 storageService,
                 messagingService,
+                policyService,
                 logoutCallback,
             );
 
@@ -61,4 +81,17 @@ export class SyncService extends BaseSyncService {
         const cozyClientService = this.cozyClientService();
         await cozyClientService.updateSynchronizedAt();
     }
+
+
+    // TODO BJA : should be added ?
+    // private async syncPolicies(response: PolicyResponse[]) {
+    //     const policies: { [id: string]: PolicyData; } = {};
+    //     if (response != null) {
+    //         response.forEach((p) => {
+    //             policies[p.id] = new PolicyData(p);
+    //         });
+    //     }
+    //     return await this.policyService.replace(policies);
+    // }
+
 }
