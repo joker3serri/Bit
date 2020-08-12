8b8936a2d095be4851bc58e1abde4b8d6114911e
Hinton <hinton.oscar@gmail.com>
2020-08-12 21:25:43 +0200

    Upgrade TypeScript

--- src/popup/accounts/login.component.ts
+++ src/popup/accounts/login.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from 'jslib/abstractions/auth.service';
+import { CryptoFunctionService } from 'jslib/abstractions/cryptoFunction.service';
+import { EnvironmentService } from 'jslib/abstractions/environment.service';
 import { I18nService } from 'jslib/abstractions/i18n.service';
 import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 import { StateService } from 'jslib/abstractions/state.service';
@@ -9,6 +11,7 @@ import { StorageService } from 'jslib/abstractions/storage.service';
 import { SyncService } from 'jslib/abstractions/sync.service';
 
 import { LoginComponent as BaseLoginComponent } from 'jslib/angular/components/login.component';
+import { PasswordGenerationService } from 'jslib/abstractions/passwordGeneration.service';
 
 @Component({
     selector: 'app-login',
@@ -18,8 +21,11 @@ export class LoginComponent extends BaseLoginComponent {
     constructor(authService: AuthService, router: Router,
         platformUtilsService: PlatformUtilsService, i18nService: I18nService,
         syncService: SyncService, storageService: StorageService,
-        stateService: StateService) {
-        super(authService, router, platformUtilsService, i18nService, storageService, stateService);
+        stateService: StateService, environmentService: EnvironmentService,
+        passwordGenerationService: PasswordGenerationService,
+        cryptoFunctionService: CryptoFunctionService) {
+        super(authService, router, platformUtilsService, i18nService, stateService, environmentService,
+            passwordGenerationService, cryptoFunctionService, storageService);
         super.onSuccessfulLogin = () => {
             return syncService.fullSync(true);
         };
