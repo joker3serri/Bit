280c7f5b5fa5f65a1905774b5dd9ecefdf5797cd
Thomas Rittson <trittson@bitwarden.com>
2021-09-16 13:54:34 +1000

    Move view custom fields to own component

---
+++ src/popup/vault/view-custom-fields.component.ts
@@ -0,0 +1,22 @@
+import {
+    Component,
+} from '@angular/core';
+
+import { EventService } from 'jslib-common/abstractions/event.service';
+import { I18nService } from 'jslib-common/abstractions/i18n.service';
+import { PasswordRepromptService } from 'jslib-common/abstractions/passwordReprompt.service';
+import { PlatformUtilsService } from 'jslib-common/abstractions/platformUtils.service';
+
+import {
+    ViewCustomFieldsComponent as BaseViewCustomFieldsComponent
+} from 'jslib-angular/components/view-custom-fields.component';
+
+@Component({
+    selector: 'app-vault-view-custom-fields',
+    templateUrl: 'view-custom-fields.component.html',
+})
+export class ViewCustomFieldsComponent extends BaseViewCustomFieldsComponent {
+    constructor(eventService: EventService) {
+        super(eventService);
+    }
+}
