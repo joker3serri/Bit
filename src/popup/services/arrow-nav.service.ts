567b9fadd691d4f12e65aa9b561ae5efc87d7d0e
Mat Schaffer <mat@elastic.co>
2022-04-05 09:35:08 +0900

    Fix indentation in added file

--- src/popup/services/arrow-nav.service.ts
+++ src/popup/services/arrow-nav.service.ts
@@ -1,75 +1,75 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ArrowNavService {
-    navType: string = null;
-    navIndex: number = -1;
+  navType: string = null;
+  navIndex: number = -1;
 
-    private allNavTypes: NavType[] = [];
-    private navTypeIndex: number = -1;
+  private allNavTypes: NavType[] = [];
+  private navTypeIndex: number = -1;
 
-    init(navTypes: NavType[]) {
-        this.allNavTypes = navTypes;
+  init(navTypes: NavType[]) {
+    this.allNavTypes = navTypes;
+  }
+
+  down() {
+    this.navIndex++;
+
+    if (!this.navType)
+      this.navType = this.getNextNavType();
+
+    while (this.navIndex >= this.getNavLength()) {
+      const navType = this.navType;
+      this.navType = this.getNextNavType();
+      this.navIndex = 0;
+      if (this.navType === navType)
+        return;
     }
+  }
 
-    down() {
-        this.navIndex++;
+  up() {
+    this.navIndex--;
 
-        if (!this.navType)
-            this.navType = this.getNextNavType();
+    if (!this.navType)
+      this.navType = this.getPrevNavType();
 
-        while (this.navIndex >= this.getNavLength()) {
-            const navType = this.navType;
-            this.navType = this.getNextNavType();
-            this.navIndex = 0;
-            if (this.navType === navType)
-                return;
-        }
+    while (this.navIndex < 0) {
+      const navType = this.navType;
+      this.navType = this.getPrevNavType();
+      this.navIndex = this.getNavLength() - 1;
+      if (this.navType === navType)
+        return;
     }
+  }
 
-    up() {
-        this.navIndex--;
+  reset() {
+    this.navType = null;
+    this.navIndex = -1;
+    this.navTypeIndex = -1;
+  }
 
-        if (!this.navType)
-            this.navType = this.getPrevNavType();
+  private getNextNavType() {
+    this.navTypeIndex++;
+    if (this.navTypeIndex >= this.allNavTypes.length)
+      this.navTypeIndex = 0;
 
-        while (this.navIndex < 0) {
-            const navType = this.navType;
-            this.navType = this.getPrevNavType();
-            this.navIndex = this.getNavLength() - 1;
-            if (this.navType === navType)
-                return;
-        }
-    }
+    return this.allNavTypes[this.navTypeIndex].name;
+  }
 
-    reset() {
-        this.navType = null;
-        this.navIndex = -1;
-        this.navTypeIndex = -1;
-    }
+  private getPrevNavType() {
+    this.navTypeIndex--;
+    if (this.navTypeIndex < 0)
+      this.navTypeIndex = this.allNavTypes.length - 1;
 
-    private getNextNavType() {
-        this.navTypeIndex++;
-        if (this.navTypeIndex >= this.allNavTypes.length)
-            this.navTypeIndex = 0;
+    return this.allNavTypes[this.navTypeIndex].name;
+  }
 
-        return this.allNavTypes[this.navTypeIndex].name;
-    }
-
-    private getPrevNavType() {
-        this.navTypeIndex--;
-        if (this.navTypeIndex < 0)
-            this.navTypeIndex = this.allNavTypes.length - 1;
-
-        return this.allNavTypes[this.navTypeIndex].name;
-    }
-
-    private getNavLength() {
-        return this.allNavTypes[this.navTypeIndex].length;
-    }
+  private getNavLength() {
+    return this.allNavTypes[this.navTypeIndex].length;
+  }
 }
 
 interface NavType {
-    name: string;
-    length: number;
+  name: string;
+  length: number;
 }
