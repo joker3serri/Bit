74438351a441b27ce18d29419df944c72268ef3c
Federico Andrés Maccaroni <fedemkr@gmail.com>
2022-06-08 16:54:24 -0300

    PS-589 Added Device Verification section for enable/disable the feature of 2FA email on new device login and the api calls for the new endpoint

---
+++ libs/common/src/models/request/deviceVerificationRequest.ts
@@ -0,0 +1,8 @@
+export class DeviceVerificationRequest {
+    unknownDeviceVerificationEnabled: boolean;
+  
+    constructor(unknownDeviceVerificationEnabled: boolean) {
+      this.unknownDeviceVerificationEnabled = unknownDeviceVerificationEnabled;
+    }
+  }
+  
