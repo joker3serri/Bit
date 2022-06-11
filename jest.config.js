a5de77014628bea8267690696092bfcce3eb10d8
Hinton <oscar@oscarhinton.com>
2022-06-11 21:12:03 +0100

    Add jest test to root, use shared tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 const { pathsToModuleNameMapper } = require("ts-jest");
 
-const { compilerOptions } = require("../shared/tsconfig.libs");
+const { compilerOptions } = require("./tsconfig");
 
 module.exports = {
-  name: "common",
-  displayName: "libs/common tests",
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  testMatch: ["**/+(*.)+(spec).+(ts)"],
-  setupFilesAfterEnv: ["<rootDir>/spec/test.ts"],
   collectCoverage: true,
   coverageReporters: ["html", "lcov"],
   coverageDirectory: "coverage",
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths || {}, {
     prefix: "<rootDir>/",
   }),
+  projects: [
+    "<rootDir>/libs/angular/jest.config.js",
+    "<rootDir>/libs/common/jest.config.js",
+    "<rootDir>/libs/electron/jest.config.js",
+    "<rootDir>/libs/node/jest.config.js",
+  ],
 };
