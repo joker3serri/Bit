695c2c28dcea2fd42488b12e701d73bccd450960
Hinton <hinton@users.noreply.github.com>
2022-06-17 15:34:02 +0200

    Add storybook to web

---
+++ apps/web/src/app/components/premium-badge.stories.ts
@@ -0,0 +1,47 @@
+import { Meta, moduleMetadata, Story } from "@storybook/angular";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
+import { MessagingService } from "@bitwarden/common/abstractions/messaging.service";
+import { BadgeModule, I18nMockService } from "@bitwarden/components";
+
+import { PremiumBadgeComponent } from "./premium-badge.component";
+
+class MockMessagingService implements MessagingService {
+  send(subscriber: string, arg?: any) {
+    alert("Clicked on badge");
+  }
+}
+
+export default {
+  title: "Web/Premium Badge",
+  component: PremiumBadgeComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [JslibModule, BadgeModule],
+      providers: [
+        {
+          provide: I18nService,
+          useFactory: () => {
+            return new I18nMockService({
+              premium: "Premium",
+            });
+          },
+        },
+        {
+          provide: MessagingService,
+          useFactory: () => {
+            return new MockMessagingService();
+          },
+        },
+      ],
+    }),
+  ],
+} as Meta;
+
+const Template: Story<PremiumBadgeComponent> = (args: PremiumBadgeComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {};
