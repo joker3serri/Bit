d86e6b4440cc0dd6f205fecbc773d18a8182f542
addison <addisonbeck1@gmail.com>
2022-06-23 13:57:10 -0400

    [fix] Move BrowserApi.saveFile logic into BrowserFileDownloadService

--- apps/browser/src/services/browserFileDownloadService.ts
+++ apps/browser/src/services/browserFileDownloadService.ts
@@ -1,13 +1,45 @@
 import { Injectable } from "@angular/core";
 
 import { FileDownloadService } from "@bitwarden/common/abstractions/fileDownload/fileDownload.service";
+import { FileDownloadBuilder } from "@bitwarden/common/abstractions/fileDownload/fileDownloadBuilder";
 import { FileDownloadRequest } from "@bitwarden/common/abstractions/fileDownload/fileDownloadRequest";
+import { Utils } from "@bitwarden/common/misc/utils";
 
 import { BrowserApi } from "../browser/browserApi";
+import { SafariApp } from "../browser/safariApp";
+
 
 @Injectable()
 export class BrowserFileDownloadService implements FileDownloadService {
   download(request: FileDownloadRequest): void {
-    BrowserApi.downloadFile(window, request.blobData, request.blobOptions, request.fileName);
+    const builder = new FileDownloadBuilder(request);
+    if (BrowserApi.isSafariApi) {
+      let data: string = null;
+      if (builder.blobOptions.type === "text/plain" && typeof request.blobData === "string") {
+        data = request.blobData;
+      } else {
+        data = Utils.fromBufferToB64(request.blobData);
+      }
+      SafariApp.sendMessageToApp(
+        "downloadFile",
+        JSON.stringify({
+          blobData: data,
+          blobOptions: request.blobOptions,
+          fileName: request.fileName,
+        }),
+        true
+      );
+    } else {
+      if (navigator.msSaveOrOpenBlob) {
+        navigator.msSaveBlob(builder.blob, request.fileName);
+      } else {
+        const a = window.document.createElement("a");
+        a.href = URL.createObjectURL(builder.blob);
+        a.download = request.fileName;
+        window.document.body.appendChild(a);
+        a.click();
+        window.document.body.removeChild(a);
+      }
+    }
   }
 }
