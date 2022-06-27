19df94c7c454e28bccccda99714be9267453b363
Hinton <hinton@users.noreply.github.com>
2022-06-27 14:44:56 +0200

    Add disabled when loading

--- libs/components/src/submit-button/submit-button.component.ts
+++ libs/components/src/submit-button/submit-button.component.ts
@@ -5,12 +5,12 @@ import { ButtonTypes } from "../button";
 @Component({
   selector: "bit-submit-button",
   template: `
-    <button bitButton type="submit" [buttonType]="buttonType">
+    <button bitButton type="submit" [buttonType]="buttonType" [disabled]="loading">
       <span class="tw-relative">
-        <span [ngClass]="{ invisible: loading }">
+        <span [ngClass]="{ 'tw-invisible': loading }">
           <ng-content></ng-content>
         </span>
-        <span class="tw-absolute tw-inset-0" [ngClass]="{ invisible: !loading }">
+        <span class="tw-absolute tw-inset-0" [ngClass]="{ 'tw-invisible': !loading }">
           <i class="bwi bwi-spinner bwi-lg bwi-spin tw-align-baseline" aria-hidden="true"></i>
         </span>
       </span>
