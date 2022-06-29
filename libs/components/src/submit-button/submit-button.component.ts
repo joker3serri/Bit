f791cc0d1a4ea6646dae49518853d7c73253b992
Hinton <hinton@users.noreply.github.com>
2022-06-29 14:25:35 +0200

    Move template to it's own file

--- libs/components/src/submit-button/submit-button.component.ts
+++ libs/components/src/submit-button/submit-button.component.ts
@@ -4,18 +4,7 @@ import { ButtonTypes } from "../button";
 
 @Component({
   selector: "bit-submit-button",
-  template: `
-    <button bitButton type="submit" [buttonType]="buttonType" [disabled]="loading || disabled">
-      <span class="tw-relative">
-        <span [ngClass]="{ 'tw-invisible': loading }">
-          <ng-content></ng-content>
-        </span>
-        <span class="tw-absolute tw-inset-0" [ngClass]="{ 'tw-invisible': !loading }">
-          <i class="bwi bwi-spinner bwi-lg bwi-spin tw-align-baseline" aria-hidden="true"></i>
-        </span>
-      </span>
-    </button>
-  `,
+  templateUrl: "./submit-button.component.html",
 })
 export class SubmitButtonComponent {
   @Input() buttonType: ButtonTypes = "primary";
