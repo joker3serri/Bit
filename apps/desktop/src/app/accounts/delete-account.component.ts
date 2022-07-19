70c68cd7960fc5217cb632a647992c3f4120e856
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-07-19 12:05:10 +0200

    [EC-317] feat: add new delete account modal

---
+++ apps/desktop/src/app/accounts/delete-account.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-delete-account",
+  templateUrl: "delete-account.component.html",
+})
+export class DeleteAccountComponent {
+  submit() {
+    // eslint-disable-next-line no-console
+    console.log("submit");
+  }
+}
