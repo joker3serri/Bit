50cfaea5951cc764c3aa4046f1fc69014ab35ada
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-07-20 15:49:42 +0200

    [EC-317] feat: inital delete account design without i18n

--- apps/desktop/src/app/accounts/delete-account.component.ts
+++ apps/desktop/src/app/accounts/delete-account.component.ts
@@ -5,8 +5,15 @@ import { Component } from "@angular/core";
   templateUrl: "delete-account.component.html",
 })
 export class DeleteAccountComponent {
+  showPassword: boolean;
+  masterPassword: string;
+
   submit() {
     // eslint-disable-next-line no-console
     console.log("submit");
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
   }
 }
