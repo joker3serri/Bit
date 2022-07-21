5ff65f634d7032711c5bd61a0443ab84a331a4a9
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-07-21 15:00:49 +0200

    [EC-317] feat: disabled but basic working delete functionality

--- apps/desktop/src/app/accounts/delete-account.component.ts
+++ apps/desktop/src/app/accounts/delete-account.component.ts
@@ -1,19 +1,54 @@
 import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
+import { LogService } from "@bitwarden/common/abstractions/log.service";
+import { MessagingService } from "@bitwarden/common/abstractions/messaging.service";
+import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
+import { UserVerificationService } from "@bitwarden/common/abstractions/userVerification.service";
+// import { Verification } from "@bitwarden/common/src/types/verification";
 
 @Component({
   selector: "app-delete-account",
   templateUrl: "delete-account.component.html",
 })
 export class DeleteAccountComponent {
-  showPassword: boolean;
-  masterPassword: string;
+  // showPassword: boolean;
+  // masterPassword: Verification;
 
-  submit() {
-    // eslint-disable-next-line no-console
-    console.log("submit");
-  }
+  deleteForm = this.formBuilder.group({
+    secret: [""],
+  });
 
-  togglePassword() {
-    this.showPassword = !this.showPassword;
+  constructor(
+    private apiService: ApiService,
+    private i18nService: I18nService,
+    private platformUtilsService: PlatformUtilsService,
+    private userVerificationService: UserVerificationService,
+    private messagingService: MessagingService,
+    private logService: LogService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  // togglePassword() {
+  //   this.showPassword = !this.showPassword;
+  // }
+
+  async submit() {
+    try {
+      const secret = this.deleteForm.get("secret").value;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const verificationRequest = await this.userVerificationService.buildRequest(secret);
+      // await this.apiService.deleteAccount(verificationRequest);
+      this.platformUtilsService.showToast(
+        "success",
+        this.i18nService.t("accountDeleted"),
+        this.i18nService.t("accountDeletedDesc")
+      );
+      // this.messagingService.send("logout");
+    } catch (e) {
+      this.logService.error(e);
+    }
   }
 }
