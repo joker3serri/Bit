e2825a18b6e0da676c68118df0b5b6450fac29bc
Thomas Rittson <trittson@bitwarden.com>
2022-07-25 12:02:53 +1000

    Fix constant variable names

--- libs/common/src/models/domain/encArrayBuffer.ts
+++ libs/common/src/models/domain/encArrayBuffer.ts
@@ -1,17 +1,16 @@
 import { EncryptionType } from "@bitwarden/common/enums/encryptionType";
 import { IEncrypted } from "@bitwarden/common/interfaces/IEncrypted";
 
-const encTypeLength = 1;
-const ivLength = 16;
-const macLength = 32;
-const minDataLength = 1;
+const ENC_TYPE_LENGTH = 1;
+const IV_LENGTH = 16;
+const MAC_LENGTH = 32;
+const MIN_DATA_LENGTH = 1;
 
 const decryptionError =
   "Error parsing encrypted ArrayBuffer: data is corrupted or has an invalid format.";
 
 export class EncArrayBuffer implements IEncrypted {
   readonly encryptionType: EncryptionType = null;
-
   readonly dataBytes: ArrayBuffer = null;
   readonly ivBytes: ArrayBuffer = null;
   readonly macBytes: ArrayBuffer = null;
@@ -23,27 +22,27 @@ export class EncArrayBuffer implements IEncrypted {
     switch (encType) {
       case EncryptionType.AesCbc128_HmacSha256_B64:
       case EncryptionType.AesCbc256_HmacSha256_B64: {
-        const minimumLength = encTypeLength + ivLength + macLength + minDataLength;
+        const minimumLength = ENC_TYPE_LENGTH + IV_LENGTH + MAC_LENGTH + MIN_DATA_LENGTH;
         if (encBytes.length < minimumLength) {
           throw new Error(decryptionError);
         }
 
-        this.ivBytes = encBytes.slice(encTypeLength, encTypeLength + ivLength).buffer;
+        this.ivBytes = encBytes.slice(ENC_TYPE_LENGTH, ENC_TYPE_LENGTH + IV_LENGTH).buffer;
         this.macBytes = encBytes.slice(
-          encTypeLength + ivLength,
-          encTypeLength + ivLength + macLength
+          ENC_TYPE_LENGTH + IV_LENGTH,
+          ENC_TYPE_LENGTH + IV_LENGTH + MAC_LENGTH
         ).buffer;
-        this.dataBytes = encBytes.slice(encTypeLength + ivLength + macLength).buffer;
+        this.dataBytes = encBytes.slice(ENC_TYPE_LENGTH + IV_LENGTH + MAC_LENGTH).buffer;
         break;
       }
       case EncryptionType.AesCbc256_B64: {
-        const minimumLength = encTypeLength + ivLength + minDataLength;
+        const minimumLength = ENC_TYPE_LENGTH + IV_LENGTH + MIN_DATA_LENGTH;
         if (encBytes.length < minimumLength) {
           throw new Error(decryptionError);
         }
 
-        this.ivBytes = encBytes.slice(encTypeLength, encTypeLength + ivLength).buffer;
-        this.dataBytes = encBytes.slice(encTypeLength + ivLength).buffer;
+        this.ivBytes = encBytes.slice(ENC_TYPE_LENGTH, ENC_TYPE_LENGTH + IV_LENGTH).buffer;
+        this.dataBytes = encBytes.slice(ENC_TYPE_LENGTH + IV_LENGTH).buffer;
         break;
       }
       default:
