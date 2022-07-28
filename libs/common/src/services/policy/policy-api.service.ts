8155a61e57408da4fc317b58c873c09e2dc300da
Rui Tome <rtome@bitwarden.com>
2022-07-28 17:03:29 +0100

    Added implementations for PolicyApiService and PolicyService

---
+++ libs/common/src/services/policy/policy-api.service.ts
@@ -0,0 +1,123 @@
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
+import { PolicyApiServiceAbstraction } from "@bitwarden/common/abstractions/policy/policy-api.service.abstraction";
+import { InternalPolicyService } from "@bitwarden/common/abstractions/policy/policy.service.abstraction";
+import { PolicyType } from "@bitwarden/common/enums/policyType";
+import { PolicyData } from "@bitwarden/common/models/data/policyData";
+import { MasterPasswordPolicyOptions } from "@bitwarden/common/models/domain/masterPasswordPolicyOptions";
+import { Policy } from "@bitwarden/common/models/domain/policy";
+import { PolicyRequest } from "@bitwarden/common/models/request/policyRequest";
+import { ListResponse } from "@bitwarden/common/models/response/listResponse";
+import { PolicyResponse } from "@bitwarden/common/models/response/policyResponse";
+
+import { OrganizationService } from "../organization.service";
+import { StateService } from "../state.service";
+
+export class PolicyApiService implements PolicyApiServiceAbstraction {
+  constructor(
+    private policyService: InternalPolicyService,
+    private apiService: ApiService,
+    private stateService: StateService,
+    private organizationService: OrganizationService
+  ) {}
+
+  async getPolicy(organizationId: string, type: PolicyType): Promise<PolicyResponse> {
+    const r = await this.apiService.send(
+      "GET",
+      "/organizations/" + organizationId + "/policies/" + type,
+      null,
+      true,
+      true
+    );
+    return new PolicyResponse(r);
+  }
+
+  async getPolicies(organizationId: string): Promise<ListResponse<PolicyResponse>> {
+    const r = await this.apiService.send(
+      "GET",
+      "/organizations/" + organizationId + "/policies",
+      null,
+      true,
+      true
+    );
+    return new ListResponse(r, PolicyResponse);
+  }
+
+  async getPoliciesByToken(
+    organizationId: string,
+    token: string,
+    email: string,
+    organizationUserId: string
+  ): Promise<ListResponse<PolicyResponse>> {
+    const r = await this.apiService.send(
+      "GET",
+      "/organizations/" +
+        organizationId +
+        "/policies/token?" +
+        "token=" +
+        encodeURIComponent(token) +
+        "&email=" +
+        encodeURIComponent(email) +
+        "&organizationUserId=" +
+        organizationUserId,
+      null,
+      false,
+      true
+    );
+    return new ListResponse(r, PolicyResponse);
+  }
+
+  async getPoliciesByInvitedUser(
+    organizationId: string,
+    userId: string
+  ): Promise<ListResponse<PolicyResponse>> {
+    const r = await this.apiService.send(
+      "GET",
+      "/organizations/" + organizationId + "/policies/invited-user?" + "userId=" + userId,
+      null,
+      false,
+      true
+    );
+    return new ListResponse(r, PolicyResponse);
+  }
+
+  async putPolicy(
+    organizationId: string,
+    type: PolicyType,
+    request: PolicyRequest
+  ): Promise<PolicyResponse> {
+    const r = await this.apiService.send(
+      "PUT",
+      "/organizations/" + organizationId + "/policies/" + type,
+      request,
+      true,
+      true
+    );
+    return new PolicyResponse(r);
+  }
+
+  async getPolicyForOrganization(policyType: PolicyType, organizationId: string): Promise<Policy> {
+    const org = await this.organizationService.get(organizationId);
+    if (org?.isProviderUser) {
+      const orgPolicies = await this.getPolicies(organizationId);
+      const policy = orgPolicies.data.find((p) => p.organizationId === organizationId);
+
+      if (policy == null) {
+        return null;
+      }
+
+      return new Policy(new PolicyData(policy));
+    }
+
+    const policies = await this.policyService.getAll(policyType);
+    return policies.find((p) => p.organizationId === organizationId);
+  }
+
+  async getMasterPasswordPoliciesForInvitedUsers(
+    orgId: string
+  ): Promise<MasterPasswordPolicyOptions> {
+    const userId = await this.stateService.getUserId();
+    const response = await this.getPoliciesByInvitedUser(orgId, userId);
+    const policies = await this.policyService.mapPoliciesFromToken(response);
+    return this.policyService.getMasterPasswordPolicyOptions(policies);
+  }
+}
