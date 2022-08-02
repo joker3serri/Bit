1d0ede6f70432c3dd409b9354ef6037bad9f9b42
addison <addisonbeck1@gmail.com>
2022-08-02 12:47:38 -0400

    Seperate the two VaultFilterComponents
    This commit seperate the `OrganizationVaultFilterComponent` from the `VaultFilerModule`, which is only used by the individual vault.
    A `VaultFilterSharedModule` was created to declare shared components and provide shared services between the two implementations.
    This was done to align with best practices for NgModules.

--- apps/web/src/app/organizations/vault/vault.module.ts
+++ apps/web/src/app/organizations/vault/vault.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 
 import { VaultSharedModule } from "../../vault/shared/vault-shared.module";
 
+import { VaultFilterModule } from "./vault-filter/vault-filter.module";
 import { VaultRoutingModule } from "./vault-routing.module";
 import { VaultComponent } from "./vault.component";
 
 @NgModule({
-  imports: [VaultSharedModule, VaultRoutingModule],
+  imports: [VaultSharedModule, VaultRoutingModule, VaultFilterModule],
   declarations: [VaultComponent],
   exports: [VaultComponent],
 })
--- apps/web/src/app/vault/vault-filter/vault-filter.module.ts
+++ apps/web/src/app/vault/vault-filter/vault-filter.module.ts
@@ -1,32 +1,19 @@
 import { NgModule } from "@angular/core";
 
-import { VaultFilterComponent as OrganizationVaultFilterComponent } from "../../organizations/vault/vault-filter/vault-filter.component";
-import { SharedModule } from "../../shared/shared.module";
-
 import { LinkSsoComponent } from "./organization-filter/link-sso.component";
 import { OrganizationFilterComponent } from "./organization-filter/organization-filter.component";
 import { OrganizationOptionsComponent } from "./organization-filter/organization-options.component";
-import { CollectionFilterComponent } from "./shared/collection-filter/collection-filter.component";
-import { FolderFilterComponent } from "./shared/folder-filter/folder-filter.component";
-import { StatusFilterComponent } from "./shared/status-filter/status-filter.component";
-import { TypeFilterComponent } from "./shared/type-filter/type-filter.component";
-import { VaultFilterService } from "./shared/vault-filter.service";
+import { VaultFilterSharedModule } from "./shared/vault-filter-shared.module";
 import { VaultFilterComponent } from "./vault-filter.component";
 
 @NgModule({
-  imports: [SharedModule],
+  imports: [VaultFilterSharedModule],
   declarations: [
     VaultFilterComponent,
-    CollectionFilterComponent,
-    FolderFilterComponent,
     OrganizationFilterComponent,
     OrganizationOptionsComponent,
-    StatusFilterComponent,
-    TypeFilterComponent,
-    OrganizationVaultFilterComponent,
     LinkSsoComponent,
   ],
-  exports: [VaultFilterComponent, OrganizationVaultFilterComponent],
-  providers: [VaultFilterService],
+  exports: [VaultFilterComponent],
 })
 export class VaultFilterModule {}
