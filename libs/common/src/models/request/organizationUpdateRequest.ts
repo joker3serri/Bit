8872ec06ef9dd66434d0c23ef16c1e4484ef094b
Shane Melton <smelton@bitwarden.com>
2022-08-03 10:16:37 -0700

    [EC-19] Remove SSO identfier from org info page

--- libs/common/src/models/request/organizationUpdateRequest.ts
+++ libs/common/src/models/request/organizationUpdateRequest.ts
@@ -2,6 +2,9 @@ import { OrganizationKeysRequest } from "./organizationKeysRequest";
 
 export class OrganizationUpdateRequest {
   name: string;
+  /**
+   * @deprecated 2022-08-03 Moved to OrganizationSsoRequest, left for backwards compatability
+   */
   identifier: string;
   businessName: string;
   billingEmail: string;
