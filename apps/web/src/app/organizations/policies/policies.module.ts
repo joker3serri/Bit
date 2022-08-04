5a32d8eae92e27447cde6717ccb94a8477c6cd95
Shane Melton <smelton@bitwarden.com>
2022-08-04 16:23:48 -0700

    [EC-19] Revert nested policy components folder

--- apps/web/src/app/organizations/policies/policies.module.ts
+++ apps/web/src/app/organizations/policies/policies.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from "@angular/core";
 
 import { LooseComponentsModule, SharedModule } from "../../shared";
 
-import { DisableSendPolicyComponent } from "./components/disable-send.component";
-import { MasterPasswordPolicyComponent } from "./components/master-password.component";
-import { PasswordGeneratorPolicyComponent } from "./components/password-generator.component";
-import { PersonalOwnershipPolicyComponent } from "./components/personal-ownership.component";
-import { RequireSsoPolicyComponent } from "./components/require-sso.component";
-import { ResetPasswordPolicyComponent } from "./components/reset-password.component";
-import { SendOptionsPolicyComponent } from "./components/send-options.component";
-import { SingleOrgPolicyComponent } from "./components/single-org.component";
-import { TwoFactorAuthenticationPolicyComponent } from "./components/two-factor-authentication.component";
+import { DisableSendPolicyComponent } from "./disable-send.component";
+import { MasterPasswordPolicyComponent } from "./master-password.component";
+import { PasswordGeneratorPolicyComponent } from "./password-generator.component";
+import { PersonalOwnershipPolicyComponent } from "./personal-ownership.component";
+import { RequireSsoPolicyComponent } from "./require-sso.component";
+import { ResetPasswordPolicyComponent } from "./reset-password.component";
+import { SendOptionsPolicyComponent } from "./send-options.component";
+import { SingleOrgPolicyComponent } from "./single-org.component";
+import { TwoFactorAuthenticationPolicyComponent } from "./two-factor-authentication.component";
 
 @NgModule({
   imports: [SharedModule, LooseComponentsModule],
