341d10bc15a295e8351fb002674d74abc44cdf73
Thomas Rittson <trittson@bitwarden.com>
2022-08-10 11:16:33 +1000

    Add maxWorkers to jest config

--- libs/shared/jest.config.base.js
+++ libs/shared/jest.config.base.js
@@ -3,4 +3,9 @@ module.exports = {
   collectCoverage: true,
   coverageReporters: ["html", "lcov"],
   coverageDirectory: "coverage",
+
+  // Workaround for a memory leak that crashes tests in CI:
+  // https://github.com/facebook/jest/issues/9430#issuecomment-1149882002
+  // Also anecdotally improves performance when run locally
+  maxWorkers: 3,
 };
