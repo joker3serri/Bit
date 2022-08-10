c9c9c006994b8d907b7541b2d2a21511c815da1d
Thomas Avery <tavery@bitwarden.com>
2022-08-10 10:00:44 -0500

    Fixing import statements

--- bitwarden_license/bit-web/src/app/sm/secrets/secrets.module.ts
+++ bitwarden_license/bit-web/src/app/sm/secrets/secrets.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 
-import { SharedModule } from "src/app/modules/shared.module";
+import { SharedModule } from "src/app/shared";
 
 import { FilterComponent } from "../layout/filter.component";
 import { HeaderComponent } from "../layout/header.component";
