f6024ef2775a1f8362423495df57ed247f6b568e
Thomas Rittson <trittson@bitwarden.com>
2022-08-11 06:14:11 +1000

    Add shared config to libs/common

--- libs/common/jest.config.js
+++ libs/common/jest.config.js
@@ -2,7 +2,10 @@ const { pathsToModuleNameMapper } = require("ts-jest");
 
 const { compilerOptions } = require("../shared/tsconfig.libs");
 
+const sharedConfig = require("../shared/jest.config.base");
+
 module.exports = {
+  ...sharedConfig,
   displayName: "libs/common tests",
   preset: "ts-jest",
   testEnvironment: "jsdom",
