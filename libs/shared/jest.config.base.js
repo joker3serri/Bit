1167d5b431784cce191821d4e25493b2c1f4a90a
Thomas Rittson <trittson@bitwarden.com>
2022-08-11 10:21:56 +1000

    Enable tsconfig.spec.json

--- libs/shared/jest.config.base.js
+++ libs/shared/jest.config.base.js
@@ -9,4 +9,11 @@ module.exports = {
   // https://github.com/facebook/jest/issues/9430#issuecomment-1149882002
   // Also anecdotally improves performance when run locally
   maxWorkers: 3,
+
+  // Jest does not use tsconfig.spec.json by default
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.spec.json",
+    },
+  },
 };
