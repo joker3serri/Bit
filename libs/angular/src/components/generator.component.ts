f1bcf37601dcc2b990d775588c6ce77942b4d4bc
Dattito <github@relay.datti.to>
2022-08-14 17:20:18 +0200

    added duckduckgo's email forwarding

--- libs/angular/src/components/generator.component.ts
+++ libs/angular/src/components/generator.component.ts
@@ -74,6 +74,7 @@ export class GeneratorComponent implements OnInit {
       { name: "SimpleLogin", value: "simplelogin" },
       { name: "AnonAddy", value: "anonaddy" },
       { name: "Firefox Relay", value: "firefoxrelay" },
+      { name: "Private Duck Address", value: "duckduckgo" },
       // { name: "FastMail", value: "fastmail" },
     ];
   }
