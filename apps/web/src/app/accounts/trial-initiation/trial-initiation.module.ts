26fb5d0eb4b8b480b512c0863508bbc5e96fdccb
rr-bw <rrichetto@bitwarden.com>
2022-08-18 14:17:05 -0700

    display content based on layout params

--- apps/web/src/app/accounts/trial-initiation/trial-initiation.module.ts
+++ apps/web/src/app/accounts/trial-initiation/trial-initiation.module.ts
@@ -9,10 +9,20 @@ import { LooseComponentsModule, SharedModule } from "../../shared";
 import { RegisterFormModule } from "../register-form/register-form.module";
 
 import { BillingComponent } from "./billing.component";
+import { CnetEnterpriseContentComponent } from "./cnet-enterprise-content.component";
+import { CnetIndividualContentComponent } from "./cnet-individual-content.component";
+import { CnetTeamsContentComponent } from "./cnet-teams-content.component";
 import { ConfirmationDetailsComponent } from "./confirmation-details.component";
+import { DefaultContentComponent } from "./default-content.component";
 import { EnterpriseContentComponent } from "./enterprise-content.component";
-import { FamiliesContentComponent } from "./families-content.component";
+import { Enterprise1ContentComponent } from "./enterprise1-content.component";
+import { Enterprise2ContentComponent } from "./enterprise2-content.component";
+import { LogoCnetComponent } from "./logo-cnet.component";
+import { LogoForbesComponent } from "./logo-forbes.component";
+import { LogoUSNewsComponent } from "./logo-us-news.component";
 import { TeamsContentComponent } from "./teams-content.component";
+import { Teams1ContentComponent } from "./teams1-content.component";
+import { Teams2ContentComponent } from "./teams2-content.component";
 import { TrialInitiationComponent } from "./trial-initiation.component";
 import { VerticalStepperModule } from "./vertical-stepper/vertical-stepper.module";
 
@@ -29,10 +39,22 @@ import { VerticalStepperModule } from "./vertical-stepper/vertical-stepper.modul
   declarations: [
     TrialInitiationComponent,
     EnterpriseContentComponent,
-    FamiliesContentComponent,
     TeamsContentComponent,
     ConfirmationDetailsComponent,
     BillingComponent,
+    DefaultContentComponent,
+    EnterpriseContentComponent,
+    Enterprise1ContentComponent,
+    Enterprise2ContentComponent,
+    TeamsContentComponent,
+    Teams1ContentComponent,
+    Teams2ContentComponent,
+    CnetEnterpriseContentComponent,
+    CnetIndividualContentComponent,
+    CnetTeamsContentComponent,
+    LogoCnetComponent,
+    LogoForbesComponent,
+    LogoUSNewsComponent,
   ],
   exports: [TrialInitiationComponent],
   providers: [TitleCasePipe],
