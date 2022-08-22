608484c6766b25662c040a8d8a7cc638df721614
Shane Melton <smelton@bitwarden.com>
2022-08-22 14:59:15 -0700

    [EC-19] Add explicit whitespace check for launch click directive

--- libs/angular/src/directives/launch-click.directive.ts
+++ libs/angular/src/directives/launch-click.directive.ts
@@ -1,6 +1,7 @@
 import { Directive, HostListener, Input } from "@angular/core";
 
 import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
+import { Utils } from "@bitwarden/common/misc/utils";
 
 @Directive({
   selector: "[appLaunchClick]",
@@ -11,7 +12,7 @@ export class LaunchClickDirective {
   @Input("appLaunchClick") uriToLaunch = "";
 
   @HostListener("click") onClick() {
-    if (this.uriToLaunch) {
+    if (!Utils.isNullOrWhitespace(this.uriToLaunch)) {
       this.platformUtilsService.launchUri(this.uriToLaunch);
     }
   }
