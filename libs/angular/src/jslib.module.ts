350876b89ef73f792dee8084f597342a9dcb0b4b
Shane Melton <smelton@bitwarden.com>
2022-08-22 14:53:44 -0700

    [EC-19] Fix import order linting

--- libs/angular/src/jslib.module.ts
+++ libs/angular/src/jslib.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/order */
 import { CommonModule, DatePipe } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
@@ -14,16 +13,16 @@ import { ApiActionDirective } from "./directives/api-action.directive";
 import { AutofocusDirective } from "./directives/autofocus.directive";
 import { BlurClickDirective } from "./directives/blur-click.directive";
 import { BoxRowDirective } from "./directives/box-row.directive";
+import { CopyClickDirective } from "./directives/copy-click.directive";
 import { FallbackSrcDirective } from "./directives/fallback-src.directive";
 import { InputStripSpacesDirective } from "./directives/input-strip-spaces.directive";
 import { InputVerbatimDirective } from "./directives/input-verbatim.directive";
+import { LaunchClickDirective } from "./directives/launch-click.directive";
 import { NotPremiumDirective } from "./directives/not-premium.directive";
 import { SelectCopyDirective } from "./directives/select-copy.directive";
 import { StopClickDirective } from "./directives/stop-click.directive";
 import { StopPropDirective } from "./directives/stop-prop.directive";
 import { TrueFalseValueDirective } from "./directives/true-false-value.directive";
-import { CopyClickDirective } from "./directives/copy-click.directive";
-import { LaunchClickDirective } from "./directives/launch-click.directive";
 import { ColorPasswordCountPipe } from "./pipes/color-password-count.pipe";
 import { ColorPasswordPipe } from "./pipes/color-password.pipe";
 import { CreditCardNumberPipe } from "./pipes/credit-card-number.pipe";
