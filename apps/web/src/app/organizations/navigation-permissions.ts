824d5eb0bb5ea43163cd7ffeb9b30cf7530302fd
Shane Melton <smelton@bitwarden.com>
2022-08-22 14:39:29 -0700

    [EC-451] Remove canAccessManageTab()

--- apps/web/src/app/organizations/navigation-permissions.ts
+++ apps/web/src/app/organizations/navigation-permissions.ts
@@ -20,29 +20,12 @@ export function canAccessSettingsTab(org: Organization): boolean {
   return org.isOwner;
 }
 
-export function canAccessManageTab(org: Organization): boolean {
-  return (
-    org.canCreateNewCollections ||
-    org.canEditAnyCollection ||
-    org.canDeleteAnyCollection ||
-    org.canEditAssignedCollections ||
-    org.canDeleteAssignedCollections ||
-    org.canAccessEventLogs ||
-    org.canManageGroups ||
-    org.canManageUsers ||
-    org.canManagePolicies ||
-    org.canManageSso ||
-    org.canManageScim
-  );
-}
-
 export function canAccessOrgAdmin(org: Organization): boolean {
   return (
     canAccessMembersTab(org) ||
     canAccessGroupsTab(org) ||
     canAccessReportingTab(org) ||
     canAccessBillingTab(org) ||
-    canAccessSettingsTab(org) ||
-    canAccessManageTab(org)
+    canAccessSettingsTab(org)
   );
 }
