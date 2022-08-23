b5080943117a6d6ea2f517c283b4b44b991cf408
Shane Melton <smelton@bitwarden.com>
2022-08-23 15:29:12 -0700

    [EC-19] Remove out of date comment

--- bitwarden_license/bit-web/src/app/organizations/organizations.module.ts
+++ bitwarden_license/bit-web/src/app/organizations/organizations.module.ts
@@ -7,8 +7,6 @@ import { ScimComponent } from "./manage/scim.component";
 import { SsoComponent } from "./manage/sso.component";
 import { OrganizationsRoutingModule } from "./organizations-routing.module";
 
-// Form components are for use in the SSO Configuration Form only and should not be exported for use elsewhere.
-// They will be deprecated by the Component Library.
 @NgModule({
   imports: [SharedModule, OrganizationsRoutingModule],
   declarations: [InputCheckboxComponent, SsoComponent, ScimComponent],
