42126fb655ef320b73e0ce55c0ec9bf3c846fccf
Colton Hurst <colton@coltonhurst.com>
2022-08-31 13:58:50 -0400

    Remove null checks from server-config.data.ts class

--- libs/common/src/models/data/server-config.data.ts
+++ libs/common/src/models/data/server-config.data.ts
@@ -21,10 +21,6 @@ export class ServerConfigData {
     this.environment = serverConfigReponse?.environment
       ? new EnvironmentServerConfigData(serverConfigReponse.environment)
       : null;
-
-    if (serverConfigReponse?.server?.name == null || serverConfigReponse?.server?.url == null) {
-      this.server = null;
-    }
   }
 }
 
