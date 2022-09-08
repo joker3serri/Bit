d63c3be2e148991cccbcc3e55acdbd4265eec94d
Shane Melton <smelton@bitwarden.com>
2022-09-08 14:54:38 -0700

    [EC-526] Default to the Events Logs page when navigating to the Reporting tab

--- apps/web/src/app/organizations/reporting/organization-reporting-routing.module.ts
+++ apps/web/src/app/organizations/reporting/organization-reporting-routing.module.ts
@@ -22,7 +22,7 @@ const routes: Routes = [
     canActivate: [OrganizationPermissionsGuard],
     data: { organizationPermissions: canAccessReportingTab },
     children: [
-      { path: "", pathMatch: "full", redirectTo: "reports" },
+      { path: "", pathMatch: "full", redirectTo: "events" },
       {
         path: "reports",
         component: ReportsHomeComponent,
