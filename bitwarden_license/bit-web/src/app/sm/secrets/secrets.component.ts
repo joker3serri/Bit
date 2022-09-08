ab1aa265ff7ff53f4c094a23b84c4d79e81f9be8
Thomas Avery <tavery@bitwarden.com>
2022-09-08 15:59:03 -0500

    DRY up code with combineLatestWith

--- bitwarden_license/bit-web/src/app/sm/secrets/secrets.component.ts
+++ bitwarden_license/bit-web/src/app/sm/secrets/secrets.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { switchMap } from "rxjs";
+import { combineLatestWith, startWith, switchMap } from "rxjs";
 
 import { SecretListView } from "@bitwarden/common/models/view/secretListView";
 import { DialogService } from "@bitwarden/components";
@@ -24,18 +24,12 @@ export class SecretsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.route.params
-      .pipe(
-        switchMap(async (params: any) => {
-          this.organizationId = params.organizationId;
-          return await this.getSecrets();
-        })
-      )
-      .subscribe((secrets: SecretListView[]) => (this.secrets = secrets));
-
     this.secretService.secret$
       .pipe(
-        switchMap(async (_) => {
+        startWith(null),
+        combineLatestWith(this.route.params),
+        switchMap(async ([_, params]) => {
+          this.organizationId = params.organizationId;
           return await this.getSecrets();
         })
       )
