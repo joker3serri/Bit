d830cb2e4d6c5bc7ed83fdbcb1d3c0a884aaf140
André Bispo <abispo@bitwarden.com>
2022-09-13 17:29:00 +0100

    [SG-659] Refactored condition.

--- libs/angular/src/components/view.component.ts
+++ libs/angular/src/components/view.component.ts
@@ -50,6 +50,7 @@ export class ViewComponent implements OnDestroy, OnInit {
   showCardNumber: boolean;
   showCardCode: boolean;
   canAccessPremium: boolean;
+  showPremiumRequiredTotp : boolean;
   totpCode: string;
   totpCodeFormatted: string;
   totpDash: number;
@@ -108,7 +109,10 @@ export class ViewComponent implements OnDestroy, OnInit {
     const cipher = await this.cipherService.get(this.cipherId);
     this.cipher = await cipher.decrypt();
     this.canAccessPremium = await this.stateService.getCanAccessPremium();
-
+    this.showPremiumRequiredTotp = this.cipher.login.totp && !this.canAccessPremium && 
+          ((!this.cipher.organizationId) ||
+            (this.cipher.organizationId && !this.cipher.organizationUseTotp) ||
+            (!this.cipher.organizationId && !this.cipher.organizationUseTotp));
     if (
       this.cipher.type === CipherType.Login &&
       this.cipher.login.totp &&
