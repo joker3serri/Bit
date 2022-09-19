d961e65a5766ceb84cd30ea5a59c72634bcb1992
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-09-19 14:35:12 +0200

    [EC-558] feat: implement bitAction directive

--- libs/components/src/async/bit-action.directive.ts
+++ libs/components/src/async/bit-action.directive.ts
@@ -1,11 +1,49 @@
-import { Directive, OnDestroy } from "@angular/core";
-import { Subject } from "rxjs";
+import { Directive, HostListener, Input, OnDestroy, Optional } from "@angular/core";
+import { finalize, Observable, Subject, takeUntil } from "rxjs";
+
+import { ButtonComponent } from "../button";
+import { functionToObservable } from "../utils/function-to-observable";
+
+export type BitActionHandler =
+  | (() => unknown)
+  | (() => Promise<unknown>)
+  | (() => Observable<unknown>);
 
 @Directive({
-  selector: "[formGroup][bitAction]",
+  selector: "[bitAction]",
 })
 export class BitActionDirective implements OnDestroy {
   private destroy$ = new Subject<void>();
+
+  @Input("bitAction") protected handler: BitActionHandler;
+
+  constructor(@Optional() private buttonComponent?: ButtonComponent) {}
+
+  get loading() {
+    return this.buttonComponent?.loading;
+  }
+
+  set loading(value: boolean) {
+    if (this.buttonComponent) {
+      this.buttonComponent.loading = value;
+    }
+  }
+
+  @HostListener("click")
+  protected async onClick() {
+    if (!this.handler) {
+      return;
+    }
+
+    this.loading = true;
+
+    functionToObservable(this.handler)
+      .pipe(
+        finalize(() => (this.loading = false)),
+        takeUntil(this.destroy$)
+      )
+      .subscribe();
+  }
 
   ngOnDestroy(): void {
     this.destroy$.next();
--- libs/components/src/async/bit-action.stories.ts
+++ libs/components/src/async/bit-action.stories.ts
@@ -1,34 +1,55 @@
+import { Component } from "@angular/core";
 import { Meta, moduleMetadata, Story } from "@storybook/angular";
+import { delay, of } from "rxjs";
 
 import { ButtonModule } from "../button";
 
 import { BitActionDirective } from "./bit-action.directive";
 
+@Component({
+  template: `<button bitButton buttonType="primary" [bitAction]="action">Perform action</button>`,
+  selector: "app-promise-example",
+})
+class PromiseExampleComponent {
+  action = async () => {
+    await new Promise<void>((resolve, reject) => {
+      setTimeout(resolve, 2000);
+    });
+  };
+}
+
+@Component({
+  template: `<button bitButton buttonType="primary" [bitAction]="action">Perform action</button>`,
+  selector: "app-observable-example",
+})
+class ObservableExampleComponent {
+  action = () => {
+    return of("fake observable").pipe(delay(2000));
+  };
+}
+
 export default {
   title: "Component Library/Async/Action",
-  component: BitActionDirective,
   decorators: [
     moduleMetadata({
-      declarations: [BitActionDirective],
+      declarations: [BitActionDirective, PromiseExampleComponent, ObservableExampleComponent],
       imports: [ButtonModule],
       providers: [],
     }),
   ],
 } as Meta;
 
-const Template: Story<BitActionDirective> = (args: BitActionDirective) => ({
-  template: `<button bitButton buttonType="primary" [bitAction]="action">Perform action</button>`,
+const PromiseTemplate: Story<PromiseExampleComponent> = (args: PromiseExampleComponent) => ({
+  props: args,
+  template: `<app-promise-example></app-promise-example>`,
 });
 
-export const UsingPromise = Template.bind({});
-UsingPromise.props = {
-  action: () => undefined,
-  // action: () => console.log("hej"),
-};
+export const UsingPromise = PromiseTemplate.bind({});
 
-// const ObservableTemplate: Story<PromiseExampleComponent> = (args: PromiseExampleComponent) => ({
-//   template: `<app-observable-example></app-observable-example>`,
-// });
+const ObservableTemplate: Story<ObservableExampleComponent> = (
+  args: ObservableExampleComponent
+) => ({
+  template: `<app-observable-example></app-observable-example>`,
+});
 
-// export const UsingObservable = ObservableTemplate.bind({});
-// UsingPromise.props = {};
+export const UsingObservable = ObservableTemplate.bind({});
