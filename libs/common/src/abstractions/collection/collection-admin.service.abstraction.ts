60c309ba1f3e9f8a5eb2823eb86538f1e9604cf0
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-09-26 14:16:47 +0200

    [EC-73] feat: implement deletion

--- libs/common/src/abstractions/collection/collection-admin.service.abstraction.ts
+++ libs/common/src/abstractions/collection/collection-admin.service.abstraction.ts
@@ -1,13 +1,7 @@
-import { CollectionRequest } from "@bitwarden/common/models/request/collection.request";
-import { CollectionResponse } from "@bitwarden/common/models/response/collection.response";
-import { CollectionAdminView } from "@bitwarden/common/models/view/collection-admin-view";
+import { CollectionAdminView } from "../../models/view/collection-admin-view";
 
 export class CollectionAdminService {
   getAll: (organizationId: string) => Promise<CollectionAdminView[]>;
-  decryptMany: (
-    organizationId: string,
-    collections: CollectionResponse[]
-  ) => Promise<CollectionAdminView[]>;
-  encrypt: (model: CollectionAdminView) => Promise<CollectionRequest>;
   save: (collection: CollectionAdminView) => Promise<unknown>;
+  remove: (organizationId: string, collectionId: string) => Promise<void>;
 }
