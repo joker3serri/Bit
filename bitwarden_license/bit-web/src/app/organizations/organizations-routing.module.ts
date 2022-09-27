57b11a6e4b29be823092a41f2ef7b6b0690e5efd
Hinton <hinton@users.noreply.github.com>
2022-09-27 12:08:30 +0200

    Rename web vault imports from src to web-vault

--- bitwarden_license/bit-web/src/app/organizations/organizations-routing.module.ts
+++ bitwarden_license/bit-web/src/app/organizations/organizations-routing.module.ts
@@ -3,11 +3,10 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { AuthGuard } from "@bitwarden/angular/guards/auth.guard";
 import { Organization } from "@bitwarden/common/models/domain/organization";
-
-import { OrganizationPermissionsGuard } from "src/app/organizations/guards/org-permissions.guard";
-import { OrganizationLayoutComponent } from "src/app/organizations/layouts/organization-layout.component";
-import { ManageComponent } from "src/app/organizations/manage/manage.component";
-import { canAccessManageTab } from "src/app/organizations/navigation-permissions";
+import { OrganizationPermissionsGuard } from "@bitwarden/web-vault/app/organizations/guards/org-permissions.guard";
+import { OrganizationLayoutComponent } from "@bitwarden/web-vault/app/organizations/layouts/organization-layout.component";
+import { ManageComponent } from "@bitwarden/web-vault/app/organizations/manage/manage.component";
+import { canAccessManageTab } from "@bitwarden/web-vault/app/organizations/navigation-permissions";
 
 import { ScimComponent } from "./manage/scim.component";
 import { SsoComponent } from "./manage/sso.component";
