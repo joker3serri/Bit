985e815be5a95e6d10d15a798ef6209cc1ba6860
Shane Melton <smelton@bitwarden.com>
2022-10-27 17:15:40 -0700

    [EC-593] Add alignContent input to bitRow directive

--- libs/components/src/table/row.directive.ts
+++ libs/components/src/table/row.directive.ts
@@ -1,9 +1,15 @@
-import { HostBinding, Directive } from "@angular/core";
+import { Directive, HostBinding, Input } from "@angular/core";
 
 @Directive({
   selector: "tr[bitRow]",
 })
 export class RowDirective {
+  @Input() alignContent: "top" | "middle" | "bottom" | "baseline" = "baseline";
+
+  get alignmentClass(): string {
+    return `tw-align-${this.alignContent}`;
+  }
+
   @HostBinding("class") get classList() {
     return [
       "tw-border-0",
@@ -12,6 +18,7 @@ export class RowDirective {
       "tw-border-solid",
       "hover:tw-bg-background-alt",
       "last:tw-border-0",
+      this.alignmentClass,
     ];
   }
 }
