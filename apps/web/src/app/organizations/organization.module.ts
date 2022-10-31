95aea8714def4dd39a56f56c3783df0e95c2ce27
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-10-31 16:12:33 +0100

    [EC-73] feat: create new shared org module

--- apps/web/src/app/organizations/organization.module.ts
+++ apps/web/src/app/organizations/organization.module.ts
@@ -5,21 +5,26 @@ import { ApiService as ApiServiceAbstraction } from "@bitwarden/common/abstracti
 import { SharedModule } from "../shared";
 
 import { AccessSelectorModule } from "./components/access-selector";
-import { CollectionAddEditComponent } from "./manage/collection-add-edit.component";
 import { GroupAddEditComponent } from "./manage/group-add-edit.component";
 import { GroupsComponent } from "./manage/groups.component";
 import { UserGroupsComponent } from "./manage/user-groups.component";
 import { CollectionDialogModule } from "./components/collection-dialog";
 import { OrganizationsRoutingModule } from "./organization-routing.module";
+import { SharedOrganizationModule } from "./shared";
 import { GroupServiceAbstraction } from "./services/abstractions/group";
 import { GroupService } from "./services/group/group.service";
 
 @NgModule({
-  imports: [SharedModule, AccessSelectorModule, CollectionDialogModule, OrganizationsRoutingModule],
+  imports: [
+    SharedModule,
+    AccessSelectorModule,
+    CollectionDialogModule,
+    OrganizationsRoutingModule,
+    SharedOrganizationModule,
+  ],
   declarations: [
     GroupsComponent,
     GroupAddEditComponent,
-    CollectionAddEditComponent,
     UserGroupsComponent,
   ],
   providers: [
