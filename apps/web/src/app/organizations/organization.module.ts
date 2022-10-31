41908ea92f612eaf30fa818584d7f5f541d31c7c
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-10-31 16:31:16 +0100

    [EC-73] feat: move collection dialog to shared

--- apps/web/src/app/organizations/organization.module.ts
+++ apps/web/src/app/organizations/organization.module.ts
@@ -8,7 +8,6 @@ import { AccessSelectorModule } from "./components/access-selector";
 import { GroupAddEditComponent } from "./manage/group-add-edit.component";
 import { GroupsComponent } from "./manage/groups.component";
 import { UserGroupsComponent } from "./manage/user-groups.component";
-import { CollectionDialogModule } from "./components/collection-dialog";
 import { OrganizationsRoutingModule } from "./organization-routing.module";
 import { SharedOrganizationModule } from "./shared";
 import { GroupServiceAbstraction } from "./services/abstractions/group";
@@ -17,10 +16,9 @@ import { GroupService } from "./services/group/group.service";
 @NgModule({
   imports: [
     SharedModule,
-    AccessSelectorModule,
-    CollectionDialogModule,
     OrganizationsRoutingModule,
     SharedOrganizationModule,
+    AccessSelectorModule,
   ],
   declarations: [
     GroupsComponent,
