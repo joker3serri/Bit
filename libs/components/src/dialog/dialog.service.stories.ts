259ed22dbaadf249e4e957b7981003e68529b9be
Shane Melton <smelton@bitwarden.com>
2022-11-15 16:04:57 -0800

    Revert "[CL-62] Add new tabbed dialog service story"
    This reverts commit e19216f0310fe0fdbb827be25db7aafc15d95f2f.

--- libs/components/src/dialog/dialog.service.stories.ts
+++ libs/components/src/dialog/dialog.service.stories.ts
@@ -1,17 +1,12 @@
 import { DIALOG_DATA, DialogModule, DialogRef } from "@angular/cdk/dialog";
-import { ComponentType } from "@angular/cdk/overlay";
-import { Component, Inject, Input } from "@angular/core";
-import { action } from "@storybook/addon-actions";
+import { Component, Inject } from "@angular/core";
 import { Meta, moduleMetadata, Story } from "@storybook/angular";
-import { firstValueFrom } from "rxjs";
 
 import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 
 import { ButtonModule } from "../button";
-import { FormFieldModule } from "../form-field";
 import { IconButtonModule } from "../icon-button";
 import { SharedModule } from "../shared";
-import { TabsModule } from "../tabs";
 import { I18nMockService } from "../utils/i18n-mock.service";
 
 import { DialogService } from "./dialog.service";
@@ -23,28 +18,19 @@ interface Animal {
   animal: string;
 }
 
-const actionsData = {
-  onDialogClosed: action("onDialogClosed"),
-};
-
 @Component({
   selector: "app-story-dialog",
-  template: `<button bitButton (click)="openDialog()">{{ buttonText }}</button>`,
+  template: `<button bitButton (click)="openDialog()">Open Dialog</button>`,
 })
 class StoryDialogComponent {
-  @Input() dialogContentComponent: ComponentType<unknown>;
-  @Input() buttonText: string;
-
   constructor(public dialogService: DialogService) {}
 
-  async openDialog() {
-    const ref = this.dialogService.open(this.dialogContentComponent, {
+  openDialog() {
+    this.dialogService.open(StoryDialogContentComponent, {
       data: {
         animal: "panda",
       },
     });
-    const result = await firstValueFrom(ref.closed);
-    actionsData.onDialogClosed(result);
   }
 }
 
@@ -73,41 +59,6 @@ class StoryDialogContentComponent {
   }
 }
 
-@Component({
-  selector: "story-tabbed-dialog-content",
-  template: `
-    <bit-dialog dialogSize="large" disablePadding>
-      <span bitDialogTitle>Tabbed Dialog Title</span>
-      <span bitDialogContent>
-        <bit-tab-group>
-          <bit-tab label="First Tab">
-            <p>
-              You can navigate through all tab labels, form inputs, and the dialog controls via the
-              keyboard.
-            </p>
-            <bit-form-field>
-              <bit-label>First Input</bit-label>
-              <input type="text" bitInput />
-            </bit-form-field>
-            <bit-form-field>
-              <bit-label>Second Input</bit-label>
-              <input type="text" bitInput />
-            </bit-form-field>
-          </bit-tab>
-          <bit-tab label="Second Tab">Second Tab Content</bit-tab>
-        </bit-tab-group>
-      </span>
-      <div bitDialogFooter class="tw-flex tw-flex-row tw-gap-2">
-        <button bitButton buttonType="primary" (click)="dialogRef.close()">Save</button>
-        <button bitButton buttonType="secondary" bitDialogClose>Cancel</button>
-      </div>
-    </bit-dialog>
-  `,
-})
-class StoryTabbedDialogContentComponent {
-  constructor(public dialogRef: DialogRef) {}
-}
-
 export default {
   title: "Component Library/Dialogs/Service",
   component: StoryDialogComponent,
@@ -118,16 +69,8 @@ export default {
         DialogComponent,
         DialogTitleContainerDirective,
         StoryDialogContentComponent,
-        StoryTabbedDialogContentComponent,
       ],
-      imports: [
-        SharedModule,
-        ButtonModule,
-        DialogModule,
-        IconButtonModule,
-        TabsModule,
-        FormFieldModule,
-      ],
+      imports: [SharedModule, ButtonModule, DialogModule, IconButtonModule],
       providers: [
         DialogService,
         {
@@ -150,21 +93,7 @@ export default {
 } as Meta;
 
 const Template: Story<StoryDialogComponent> = (args: StoryDialogComponent) => ({
-  props: {
-    ...args,
-    dialogContentComponent: StoryDialogContentComponent,
-    buttonText: "Open Dialog",
-  },
+  props: args,
 });
 
 export const Default = Template.bind({});
-
-const TabbedTemplate: Story<StoryDialogComponent> = (args: StoryDialogComponent) => ({
-  props: {
-    ...args,
-    dialogContentComponent: StoryTabbedDialogContentComponent,
-    buttonText: "Open Tabbed Dialog",
-  },
-});
-
-export const TabbedContent = TabbedTemplate.bind({});
