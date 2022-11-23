4b14c475ac27acf570229b22b254d5468a8049c4
Shane Melton <smelton@bitwarden.com>
2022-11-23 08:47:37 -0800

    Import/export SharedModule from SharedOrganizationModule

--- apps/web/src/app/organizations/shared/shared-organization.module.ts
+++ apps/web/src/app/organizations/shared/shared-organization.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 
+import { SharedModule } from "../../shared/shared.module";
+
 import { AccessSelectorModule } from "./components/access-selector";
 import { CollectionDialogModule } from "./components/collection-dialog";
 
 @NgModule({
-  imports: [CollectionDialogModule, AccessSelectorModule],
-  exports: [CollectionDialogModule, AccessSelectorModule],
+  imports: [SharedModule, CollectionDialogModule, AccessSelectorModule],
+  exports: [SharedModule, CollectionDialogModule, AccessSelectorModule],
 })
 export class SharedOrganizationModule {}
