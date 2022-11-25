aba5b5d3cdc7dd8ed7b3860482f13a37ff665bdb
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-11-25 14:49:45 +0100

    [CL-50] chore: clean up unused tailwind plugin

--- libs/components/tailwind.config.base.js
+++ libs/components/tailwind.config.base.js
@@ -90,19 +90,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [
-    plugin(function groupPeer({ addVariant }) {
-      // see: https://github.com/tailwindlabs/tailwindcss/discussions/8777
-      let pseudoVariants = ["checked", "disabled", "focus-visible"].map((variant) =>
-        Array.isArray(variant) ? variant : [variant, `&:${variant}`]
-      );
-
-      for (let [variantName, state] of pseudoVariants) {
-        addVariant(`group-peer-${variantName}`, (ctx) => {
-          let result = typeof state === "function" ? state(ctx) : state;
-          return result.replace(/&(\S+)/, ":merge(.peer)$1 ~ .group &");
-        });
-      }
-    }),
-  ],
+  plugins: [],
 };
