cfbeea88e8817ae3ea844437d813adb0551bdb4a
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-11-25 15:05:33 +0100

    [CL-50] chore: clean up unused icon

--- libs/components/src/checkbox/checkmark.icon.ts
+++
@@ -1,7 +0,0 @@
-import { svgIcon } from "../icon";
-
-export const Checkmark = svgIcon`
-  <svg class="svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="8" height="8" viewBox="0 0 10 10">
-    <path d="M0.5 6.2L2.9 8.6L9.5 1.4" fill="none" stroke="white" stroke-width="2"></path>
-  </svg>
-`;
