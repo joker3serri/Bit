005f8bb9a18e8976b82cb5ec207e81c16325ef10
Colton Hurst <colton@coltonhurst.com>
2022-11-29 17:50:38 -0500

    SM-99: Fix secrets tab redirect in project contents & make project singular

--- bitwarden_license/bit-web/src/app/secrets-manager/projects/projects-routing.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/projects/projects-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     children: [
       {
         path: "",
+        pathMatch: "full",
+        redirectTo: "secrets",
+      },
+      {
+        path: "secrets",
         component: ProjectSecretsComponent,
       },
     ],
