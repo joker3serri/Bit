bb98af15d5e3b94e519b28c41e9472a78a3ed57a
Thomas Avery <tavery@bitwarden.com>
2022-11-29 16:04:02 -0600

    Add support for never expiring access tokens

--- bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/access/access-list.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/access/access-list.component.ts
@@ -1,5 +1,8 @@
 import { SelectionModel } from "@angular/cdk/collections";
+import { DatePipe } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 
 import { AccessTokenView } from "../models/view/access-token.view";
 
@@ -22,6 +25,8 @@ export class AccessListComponent {
 
   protected selection = new SelectionModel<string>(true, []);
 
+  constructor(private i18nService: I18nService, private datePipe: DatePipe) {}
+
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.tokens.length;
@@ -36,5 +41,12 @@ export class AccessListComponent {
 
   protected permission(token: AccessTokenView) {
     return "canRead";
+  }
+
+  getExpiration(token: AccessTokenView) {
+    if (token.expireAt == null) {
+      return this.i18nService.t("never");
+    }
+    return this.datePipe.transform(token.expireAt, "medium");
   }
 }
