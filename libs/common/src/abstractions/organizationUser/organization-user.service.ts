00f806f0eceac33b92c32016e18fafd27b017c7e
Shane Melton <smelton@bitwarden.com>
2022-11-30 23:02:00 -0800

    [EC-784] Update abstraction method definitions to use abstract keyword

--- libs/common/src/abstractions/organizationUser/organization-user.service.ts
+++ libs/common/src/abstractions/organizationUser/organization-user.service.ts
@@ -25,52 +25,52 @@ export abstract class OrganizationUserService {
    * @param organizationId - Identifier for the user's organization
    * @param id - Organization user identifier
    */
-  getOrganizationUser: (
+  abstract getOrganizationUser(
     organizationId: string,
     id: string
-  ) => Promise<OrganizationUserDetailsResponse>;
+  ): Promise<OrganizationUserDetailsResponse>;
 
   /**
    * Retrieve a list of groups Ids the specified organization user belongs to
    * @param organizationId - Identifier for the user's organization
    * @param id - Organization user identifier
    */
-  getOrganizationUserGroups: (organizationId: string, id: string) => Promise<string[]>;
+  abstract getOrganizationUserGroups(organizationId: string, id: string): Promise<string[]>;
 
   /**
    * Retrieve a list of all users that belong to the specified organization
    * @param organizationId - Identifier for the organization
    */
-  getAllUsers: (
+  abstract getAllUsers(
     organizationId: string
-  ) => Promise<ListResponse<OrganizationUserUserDetailsResponse>>;
+  ): Promise<ListResponse<OrganizationUserUserDetailsResponse>>;
 
   /**
    * Retrieve reset password details for the specified organization user
    * @param organizationId - Identifier for the user's organization
    * @param id - Organization user identifier
    */
-  getOrganizationUserResetPasswordDetails: (
+  abstract getOrganizationUserResetPasswordDetails(
     organizationId: string,
     id: string
-  ) => Promise<OrganizationUserResetPasswordDetailsReponse>;
+  ): Promise<OrganizationUserResetPasswordDetailsReponse>;
 
   /**
    * Create new organization user invite(s) for the specified organization
    * @param organizationId - Identifier for the organization
    * @param request - New user invitation request details
    */
-  postOrganizationUserInvite: (
+  abstract postOrganizationUserInvite(
     organizationId: string,
     request: OrganizationUserInviteRequest
-  ) => Promise<void>;
+  ): Promise<void>;
 
   /**
    * Re-invite the specified organization user
    * @param organizationId - Identifier for the user's organization
    * @param id - Organization user identifier
    */
-  postOrganizationUserReinvite: (organizationId: string, id: string) => Promise<any>;
+  abstract postOrganizationUserReinvite(organizationId: string, id: string): Promise<any>;
 
   /**
    * Re-invite many organization users for the specified organization
@@ -78,10 +78,10 @@ export abstract class OrganizationUserService {
    * @param ids - A list of organization user identifiers
    * @return List of user ids, including both those that were successfully re-invited and those that had an error
    */
-  postManyOrganizationUserReinvite: (
+  abstract postManyOrganizationUserReinvite(
     organizationId: string,
     ids: string[]
-  ) => Promise<ListResponse<OrganizationUserBulkResponse>>;
+  ): Promise<ListResponse<OrganizationUserBulkResponse>>;
 
   /**
    * Accept an organization user invitation
@@ -89,11 +89,11 @@ export abstract class OrganizationUserService {
    * @param id - Organization user identifier
    * @param request - Request details for accepting the invitation
    */
-  postOrganizationUserAccept: (
+  abstract postOrganizationUserAccept(
     organizationId: string,
     id: string,
     request: OrganizationUserAcceptRequest
-  ) => Promise<void>;
+  ): Promise<void>;
 
   /**
    * Confirm an organization user that has accepted their invitation
@@ -101,31 +101,31 @@ export abstract class OrganizationUserService {
    * @param id - Organization user identifier
    * @param request - Request details for confirming the user
    */
-  postOrganizationUserConfirm: (
+  abstract postOrganizationUserConfirm(
     organizationId: string,
     id: string,
     request: OrganizationUserConfirmRequest
-  ) => Promise<void>;
+  ): Promise<void>;
 
   /**
    * Retrieve a list of the specified users' public keys
    * @param organizationId - Identifier for the organization to accept
    * @param ids - A list of organization user identifiers to retrieve public keys for
    */
-  postOrganizationUsersPublicKey: (
+  abstract postOrganizationUsersPublicKey(
     organizationId: string,
     ids: string[]
-  ) => Promise<ListResponse<OrganizationUserBulkPublicKeyResponse>>;
+  ): Promise<ListResponse<OrganizationUserBulkPublicKeyResponse>>;
 
   /**
    * Confirm many organization users that have accepted their invitations
    * @param organizationId - Identifier for the organization to confirm users
    * @param request - Bulk request details for confirming the user
    */
-  postOrganizationUserBulkConfirm: (
+  abstract postOrganizationUserBulkConfirm(
     organizationId: string,
     request: OrganizationUserBulkConfirmRequest
-  ) => Promise<ListResponse<OrganizationUserBulkResponse>>;
+  ): Promise<ListResponse<OrganizationUserBulkResponse>>;
 
   /**
    * Update an organization users
@@ -133,11 +133,11 @@ export abstract class OrganizationUserService {
    * @param id - Organization user identifier
    * @param request - Request details for updating the user
    */
-  putOrganizationUser: (
+  abstract putOrganizationUser(
     organizationId: string,
     id: string,
     request: OrganizationUserUpdateRequest
-  ) => Promise<void>;
+  ): Promise<void>;
 
   /**
    * Update an organization user's groups
