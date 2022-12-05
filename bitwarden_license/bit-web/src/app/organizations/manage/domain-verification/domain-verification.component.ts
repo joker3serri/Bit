15211c867db8c38a501f051ce5b4e27560563abb
Jared Snider <jsnider@bitwarden.com>
2022-12-05 17:22:52 -0500

    SG-680 - Moved domain verification comp into subfolder to add clarity in folder structure

---
+++ bitwarden_license/bit-web/src/app/organizations/manage/domain-verification/domain-verification.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
+import { concatMap, Observable, Subject, takeUntil } from "rxjs";
+
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
+import { OrgDomainApiServiceAbstraction } from "@bitwarden/common/abstractions/organization-domain/org-domain-api.service.abstraction";
+import { OrgDomainServiceAbstraction } from "@bitwarden/common/abstractions/organization-domain/org-domain.service.abstraction";
+import { OrganizationDomainResponse } from "@bitwarden/common/abstractions/organization-domain/responses/organization-domain.response";
+import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
+import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
+
+@Component({
+  selector: "app-org-manage-domain-verification",
+  templateUrl: "domain-verification.component.html",
+})
+export class DomainVerificationComponent implements OnInit, OnDestroy {
+  private componentDestroyed$ = new Subject<void>();
+
+  loading = true;
+
+  organizationId: string;
+  // organization: Organization;
+  orgDomains$: Observable<OrganizationDomainResponse[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private apiService: ApiService,
+    private platformUtilsService: PlatformUtilsService,
+    private i18nService: I18nService,
+    private organizationService: OrganizationService,
+    private orgDomainApiService: OrgDomainApiServiceAbstraction,
+    private orgDomainService: OrgDomainServiceAbstraction
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  async ngOnInit() {
+    this.orgDomains$ = this.orgDomainService.orgDomains$;
+
+    // Note: going to use concatMap as async subscribe blocks don't work as you expect and
+    // as such, ESLint rejects it
+    // ex: https://stackoverflow.com/a/71056380
+    this.route.params
+      .pipe(
+        concatMap(async (params: Params) => {
+          this.organizationId = params.organizationId;
+          await this.load();
+        }),
+        takeUntil(this.componentDestroyed$)
+      )
+      .subscribe();
+  }
+
+  async load() {
+    await this.orgDomainApiService.getAllByOrgId(this.organizationId);
+
+    this.loading = false;
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
+  }
+}
--- bitwarden_license/bit-web/src/app/organizations/organizations.module.ts
+++ bitwarden_license/bit-web/src/app/organizations/organizations.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "@bitwarden/web-vault/app/shared/shared.module";
 
 import { InputCheckboxComponent } from "./components/input-checkbox.component";
-import { DomainVerificationComponent } from "./manage/domain-verification.component";
+import { DomainVerificationComponent } from "./manage/domain-verification/domain-verification.component";
 import { ScimComponent } from "./manage/scim.component";
 import { SsoComponent } from "./manage/sso.component";
 import { OrganizationsRoutingModule } from "./organizations-routing.module";
