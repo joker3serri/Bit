35e6e6359e01b9058e79a579eaa46fae4347bdae
Andreas Coroiu <andreas.coroiu@gmail.com>
2022-12-07 15:20:25 +0100

    [EC-549] fix: old user group dialog not working

--- apps/web/src/app/organizations/manage/organization-manage.module.ts
+++ apps/web/src/app/organizations/manage/organization-manage.module.ts
@@ -1,7 +1,11 @@
 import { ScrollingModule } from "@angular/cdk/scrolling";
 import { NgModule } from "@angular/core";
 
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
+
 import { SharedModule } from "../../shared";
+import { GroupServiceAbstraction } from "../services/abstractions/group";
+import { GroupService } from "../services/group/group.service";
 
 import { EntityUsersComponent } from "./entity-users.component";
 import { UserDialogModule } from "./member-dialog";
@@ -10,5 +14,12 @@ import { UserDialogModule } from "./member-dialog";
   imports: [SharedModule, ScrollingModule, UserDialogModule],
   declarations: [EntityUsersComponent],
   exports: [EntityUsersComponent],
+  providers: [
+    {
+      provide: GroupServiceAbstraction,
+      useClass: GroupService,
+      deps: [ApiService],
+    },
+  ],
 })
 export class OrganizationManageModule {}
