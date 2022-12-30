df1f436f05ea34ad3582e2390904969e0c820b56
Brandon Maharaj <bmaharaj@bitwarden.com>
2022-12-30 12:45:13 -0500

    fix: undo

--- apps/web/src/app/settings/change-avatar.component.ts
+++ apps/web/src/app/settings/change-avatar.component.ts
@@ -73,7 +73,6 @@ export class ChangeAvatarComponent implements OnInit, OnDestroy {
         this.currentSelection = color;
       });
 
-    //grab changes
     this.setSelection(await this.accountUpdateService.loadColorFromState());
   }
 
