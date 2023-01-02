d46b185f6eb928b69a0101c7e888871d06d1e7d7
cyprain-okeke <onwudiweokeke@gmail.com>
2023-01-02 10:43:23 +0100

    Fixing error when exporting an organization vault

--- libs/common/src/models/response/organization-export.response.ts
+++ libs/common/src/models/response/organization-export.response.ts
@@ -9,12 +9,12 @@ export class OrganizationExportResponse extends BaseResponse {
   constructor(response: any) {
     super(response);
     const collections = this.getResponseProperty("Collections");
-    if (collections != null) {
-      this.collections = collections.map((c: any) => new CollectionResponse(c));
+    if (collections.data != null) {
+      this.collections = collections.data.map((c: any) => new CollectionResponse(c));
     }
     const ciphers = this.getResponseProperty("Ciphers");
-    if (ciphers != null) {
-      this.ciphers = ciphers.map((c: any) => new CipherResponse(c));
+    if (ciphers.data != null) {
+      this.ciphers = ciphers.data.map((c: any) => new CipherResponse(c));
     }
   }
 }
