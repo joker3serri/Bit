3e2a28fc582dcf91934550405d1ccdec9008e0c6
Daniel James Smith <djsmith85@users.noreply.github.com>
2023-01-11 18:20:42 +0100

    Add comment why Android is hidden on the MacAppStore release
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- apps/desktop/src/main/menu/menu.help.ts
+++ apps/desktop/src/main/menu/menu.help.ts
@@ -166,7 +166,7 @@ export class HelpMenu implements IMenubarMenu {
       {
         id: "android",
         label: "Android",
-        visible: !isMacAppStore(),
+        visible: !isMacAppStore(), // Apple Guideline 2.3.10 - Accurate Metadata
         click: () => {
           shell.openExternal(
             "https://play.google.com/store/apps/" + "details?id=com.x8bit.bitwarden"
