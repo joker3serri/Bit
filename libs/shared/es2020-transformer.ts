3c098619f564b0917c127fbed4157bb065d8ca23
Thomas Rittson <trittson@bitwarden.com>
2023-01-12 09:09:37 +1000

    Update comments per review feedback

--- libs/shared/es2020-transformer.ts
+++ libs/shared/es2020-transformer.ts
@@ -3,9 +3,12 @@ import * as ts from "typescript";
 // Custom Typescript AST transformer for use with ts-jest / jest-preset-angular
 // Removes specified ES2020 syntax from source code, as node does not support it yet
 // Reference: https://kulshekhar.github.io/ts-jest/docs/getting-started/options/astTransformers
-// Use this tool to understand how we identify and filter AST nodes:
-// https://ts-ast-viewer.com/#code/MYewdgzgLgBA7iATgawKaJgXhmVcYDqSaiAFLvgKoBKAMqQOSpjCICeADlAHQIrrcoEBgBoYASwC2HJD0mooAQ24BXRABsAlJqA
+// Use this tool to understand how we identify and filter AST nodes: https://ts-ast-viewer.com/
 
+/**
+ * Remember to increase the version whenever transformer's content is changed. This is to inform Jest to not reuse
+ * the previous cache which contains old transformer's content
+ */
 export const version = 1;
 export const name = "bit-es2020-transformer";
 
