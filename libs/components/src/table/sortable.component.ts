ac5ca24b0b1ff5ebecdac5f93af8a75f6cf27173
William Martin <contact@willmartian.com>
2023-01-11 18:58:06 -0500

    add hover and focus effects

--- libs/components/src/table/sortable.component.ts
+++ libs/components/src/table/sortable.component.ts
@@ -8,12 +8,13 @@ import { TableComponent } from "./table.component";
   selector: "th[bitSortable]",
   template: `
     <button
-      class="tw-flex tw-h-full tw-w-full tw-items-center tw-border-none tw-bg-transparent tw-p-0 tw-font-bold tw-text-muted"
+      class="tw-group"
+      [ngClass]="classList"
       [attr.aria-pressed]="isActive"
       (click)="setActive()"
     >
       <ng-content></ng-content>
-      <i class="bwi tw-ml-2 tw-w-0" [ngClass]="icon"></i>
+      <i class="bwi tw-ml-2" [ngClass]="icon"></i>
     </button>
   `,
 })
@@ -75,8 +76,50 @@ export class SortableComponent implements OnInit {
 
   get icon() {
     if (!this.isActive) {
-      return "";
+      return "bwi-chevron-up tw-opacity-0 group-hover:tw-opacity-100 group-focus-visible:tw-opacity-100";
     }
     return this.direction === "asc" ? "bwi-chevron-up" : "bwi-angle-down";
+  }
+
+  get classList() {
+    return [
+      // Offset to border and padding
+      "-tw-m-1.5",
+
+      // Below is copied from BitIconButtonComponent
+      "tw-font-semibold",
+      "tw-border",
+      "tw-border-solid",
+      "tw-rounded",
+      "tw-transition",
+      "hover:tw-no-underline",
+      "focus:tw-outline-none",
+
+      "tw-bg-transparent",
+      "!tw-text-muted",
+      "tw-border-transparent",
+      "hover:tw-bg-transparent-hover",
+      "hover:tw-border-primary-700",
+      "focus-visible:before:tw-ring-primary-700",
+      "disabled:tw-opacity-60",
+      "disabled:hover:tw-border-transparent",
+      "disabled:hover:tw-bg-transparent",
+
+      // Workaround for box-shadow with transparent offset issue:
+      // https://github.com/tailwindlabs/tailwindcss/issues/3595
+      // Remove `before:` and use regular `tw-ring` when browser no longer has bug, or better:
+      // switch to `outline` with `outline-offset` when Safari supports border radius on outline.
+      // Using `box-shadow` to create outlines is a hack and as such `outline` should be preferred.
+      "tw-relative",
+      "before:tw-content-['']",
+      "before:tw-block",
+      "before:tw-absolute",
+      "before:-tw-inset-[3px]",
+      "before:tw-rounded-md",
+      "before:tw-transition",
+      "before:tw-ring",
+      "before:tw-ring-transparent",
+      "focus-visible:tw-z-10",
+    ];
   }
 }
