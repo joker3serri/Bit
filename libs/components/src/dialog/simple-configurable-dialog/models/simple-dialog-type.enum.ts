94937b2c7b9b1a4b6f74814dc103dee1225a38d0
Jared Snider <116684653+JaredSnider-Bitwarden@users.noreply.github.com>
2023-01-12 10:24:19 -0500

    Update libs/components/src/dialog/simple-configurable-dialog/models/simple-dialog-type.enum.ts
    SG-878 - Remove early commentary about use of simple-dialog-type enum
    
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- libs/components/src/dialog/simple-configurable-dialog/models/simple-dialog-type.enum.ts
+++ libs/components/src/dialog/simple-configurable-dialog/models/simple-dialog-type.enum.ts
@@ -1,5 +1,3 @@
-// + of using enum is we can ensure no misspelling issues in simple dialog html
-// - of using enum is that comps have to import it in order to create SimpleDialogOptions & takes up more space.
 export enum SimpleDialogType {
   PRIMARY = "primary",
   SUCCESS = "success",
