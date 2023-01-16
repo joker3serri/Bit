1b1c7a29d79a3b9094f12b2ff1caabf8f8e3248c
Hermann Käser <hermann.kaser@gmail.com>
2023-01-16 14:47:14 -0500

    Fix small issue with notes import
    Notes field can be null, the ` + "\n"` coerces those to `"null"`.

--- libs/common/src/importers/keeper/keeper-csv-importer.ts
+++ libs/common/src/importers/keeper/keeper-csv-importer.ts
@@ -18,7 +18,12 @@ export class KeeperCsvImporter extends BaseImporter implements Importer {
 
       this.processFolder(result, value[0]);
       const cipher = this.initLoginCipher();
-      cipher.notes = this.getValueOrDefault(value[5]) + "\n";
+
+      const notes = this.getValueOrDefault(value[5]);
+      if (notes) {
+        cipher.notes = `${notes}\n`;
+      }
+
       cipher.name = this.getValueOrDefault(value[1], "--");
       cipher.login.username = this.getValueOrDefault(value[2]);
       cipher.login.password = this.getValueOrDefault(value[3]);
