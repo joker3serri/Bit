45ce9166a3fb5a27fd1e75492a7426546b9b0fcb
William Martin <contact@willmartian.com>
2023-01-23 23:01:23 -0500

    simplify access token logic

--- bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/access/access-tokens.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/access/access-tokens.component.ts
@@ -2,16 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { combineLatestWith, Observable, startWith, switchMap } from "rxjs";
 
-import { DialogService } from "@bitwarden/components";
-
-import { ServiceAccountView } from "../../models/view/service-account.view";
 import { AccessTokenView } from "../models/view/access-token.view";
 
 import { AccessService } from "./access.service";
-import {
-  AccessTokenOperation,
-  AccessTokenCreateDialogComponent,
-} from "./dialogs/access-token-create-dialog.component";
 
 @Component({
   selector: "sm-access-tokens",
@@ -20,42 +13,18 @@ import {
 export class AccessTokenComponent implements OnInit {
   accessTokens$: Observable<AccessTokenView[]>;
 
-  private serviceAccountId: string;
-  private organizationId: string;
-
-  constructor(
-    private route: ActivatedRoute,
-    private accessService: AccessService,
-    private dialogService: DialogService
-  ) {}
+  constructor(private route: ActivatedRoute, private accessService: AccessService) {}
 
   ngOnInit() {
     this.accessTokens$ = this.accessService.accessToken$.pipe(
       startWith(null),
       combineLatestWith(this.route.params),
       switchMap(async ([_, params]) => {
-        this.organizationId = params.organizationId;
-        this.serviceAccountId = params.serviceAccountId;
-        return await this.getAccessTokens();
+        return await this.accessService.getAccessTokens(
+          params.organizationId,
+          params.serviceAccountId
+        );
       })
     );
-  }
-
-  private async getAccessTokens(): Promise<AccessTokenView[]> {
-    return await this.accessService.getAccessTokens(this.organizationId, this.serviceAccountId);
-  }
-
-  async openNewAccessTokenDialog() {
-    // TODO once service account names are implemented in service account contents page pass in here.
-    const serviceAccountView = new ServiceAccountView();
-    serviceAccountView.id = this.serviceAccountId;
-    serviceAccountView.name = "placeholder";
-
-    this.dialogService.open<unknown, AccessTokenOperation>(AccessTokenCreateDialogComponent, {
-      data: {
-        organizationId: this.organizationId,
-        serviceAccountView: serviceAccountView,
-      },
-    });
   }
 }
