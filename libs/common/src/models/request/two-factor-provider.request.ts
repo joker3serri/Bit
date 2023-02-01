77b115eb613df8279524a1c8ba301ac3d060c270
Matt Gibson <mgibson@bitwarden.com>
2023-02-01 17:48:30 -0500

    Complete two factor provider request move

--- libs/common/src/models/request/two-factor-provider.request.ts
+++
@@ -1,6 +0,0 @@
-import { TwoFactorProviderType } from "../../auth/enums/two-factor-provider-type";
-import { SecretVerificationRequest } from "../../auth/models/request/secret-verification.request";
-
-export class TwoFactorProviderRequest extends SecretVerificationRequest {
-  type: TwoFactorProviderType;
-}
