f1722a00c6441dd9b9c9c82a63cab3edd744c734
Hinton <hinton@users.noreply.github.com>
2023-02-06 15:07:11 +0100

    Do a sync after enabling sm

--- apps/web/src/app/organizations/billing/secrets-manager/enroll.component.ts
+++ apps/web/src/app/organizations/billing/secrets-manager/enroll.component.ts
@@ -5,6 +5,7 @@ import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { OrganizationApiServiceAbstraction } from "@bitwarden/common/abstractions/organization/organization-api.service.abstraction";
 import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
 import { OrganizationEnrollSecretsManagerRequest } from "@bitwarden/common/models/request/organization/organization-enroll-secrets-manager.request";
+import { SyncService } from "@bitwarden/common/vault/abstractions/sync/sync.service.abstraction";
 
 import { flagEnabled } from "../../../../utils/flags";
 
@@ -26,7 +27,8 @@ export class SecretsManagerEnrollComponent implements OnInit {
     private formBuilder: FormBuilder,
     private organizationApiService: OrganizationApiServiceAbstraction,
     private platformUtilsService: PlatformUtilsService,
-    private i18nService: I18nService
+    private i18nService: I18nService,
+    private syncService: SyncService
   ) {
     this.showSecretsManager = flagEnabled("secretsManager");
   }
@@ -44,6 +46,7 @@ export class SecretsManagerEnrollComponent implements OnInit {
     request.enabled = this.formGroup.value.enabled;
 
     await this.organizationApiService.updateEnrollSecretsManager(this.organizationId, request);
+    await this.syncService.fullSync(true);
     this.platformUtilsService.showToast("success", null, this.i18nService.t("subscriptionUpdated"));
   };
 }
