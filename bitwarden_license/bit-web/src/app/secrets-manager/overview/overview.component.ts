86a510de136e1b7c244bf88dab29d18d848834ad
William Martin <contact@willmartian.com>
2023-02-09 22:05:17 -0500

    add header title

--- bitwarden_license/bit-web/src/app/secrets-manager/overview/overview.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/overview/overview.component.ts
@@ -1,7 +1,17 @@
 import { Component } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { map } from "rxjs";
+
+import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
 
 @Component({
   selector: "sm-overview",
   templateUrl: "./overview.component.html",
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+  protected orgName$ = this.route.params.pipe(
+    map((params) => params.organizationId && this.orgService.get(params.organizationId)?.name)
+  );
+
+  constructor(private route: ActivatedRoute, private orgService: OrganizationService) {}
+}
