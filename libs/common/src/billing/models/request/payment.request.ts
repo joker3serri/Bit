e233a21c579fbfd873e389a102d7ff33bb48ef7b
Vincent Salucci <vincesalucci21@gmail.com>
2023-02-13 16:54:36 -0600

    fix: update remaining imports for payment-request, refs EC-1011

--- libs/common/src/billing/models/request/payment.request.ts
+++ libs/common/src/billing/models/request/payment.request.ts
@@ -1,5 +1,6 @@
-import { PaymentMethodType } from "../../billing/enums/payment-method-type";
-import { OrganizationTaxInfoUpdateRequest } from "../../billing/models/request/organization-tax-info-update.request";
+import { PaymentMethodType } from "../../enums/payment-method-type";
+
+import { OrganizationTaxInfoUpdateRequest } from "./organization-tax-info-update.request";
 
 export class PaymentRequest extends OrganizationTaxInfoUpdateRequest {
   paymentMethodType: PaymentMethodType;
