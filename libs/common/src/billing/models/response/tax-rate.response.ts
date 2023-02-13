f7257532d0183e651779371728d73ed1fa319c75
Vincent Salucci <vincesalucci21@gmail.com>
2023-02-13 17:27:53 -0600

    fix: update remaining path to base response for tax-rate response, refs EC-1011

--- libs/common/src/billing/models/response/tax-rate.response.ts
+++ libs/common/src/billing/models/response/tax-rate.response.ts
@@ -1,4 +1,4 @@
-import { BaseResponse } from "./base.response";
+import { BaseResponse } from "../../../models/response/base.response";
 
 export class TaxRateResponse extends BaseResponse {
   id: string;
