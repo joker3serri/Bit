7d1cadaae9da7d518beaed06ad7762a39c587143
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-02-22 15:01:34 +0100

    [EC-974] feat: use cdk virtual scrolling

--- apps/web/src/vault/components/vault-items/vault-items.component.ts
+++ apps/web/src/vault/components/vault-items/vault-items.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 import { CollectionView } from "@bitwarden/common/src/models/view/collection.view";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
@@ -8,16 +8,34 @@ import { VaultItem } from "./vault-item";
 @Component({
   selector: "app-new-vault-items",
   templateUrl: "vault-items.component.html",
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  // TODO: Improve change detection, see: https://bitwarden.atlassian.net/browse/TDL-220
+  // changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VaultItemsComponent {
-  @Input() ciphers: CipherView[] = [];
-  @Input() collections: CollectionView[] = [];
+  private _ciphers: CipherView[] = [];
+  @Input() get ciphers(): CipherView[] {
+    return this._ciphers;
+  }
+  set ciphers(value: CipherView[]) {
+    this._ciphers = value;
+    this.refreshItems();
+  }
 
-  protected get items(): VaultItem[] {
+  private _collections: CollectionView[] = [];
+  @Input() get collections(): CollectionView[] {
+    return this._collections;
+  }
+  set collections(value: CollectionView[]) {
+    this._collections = value;
+    this.refreshItems();
+  }
+
+  protected items: VaultItem[] = [];
+
+  private refreshItems() {
     const collections: VaultItem[] = this.collections.map((collection) => ({ collection }));
     const ciphers: VaultItem[] = this.ciphers.map((cipher) => ({ cipher }));
 
-    return [].concat(collections).concat(ciphers);
+    this.items = [].concat(collections).concat(ciphers);
   }
 }
--- apps/web/src/vault/components/vault-items/vault-items.module.ts
+++ apps/web/src/vault/components/vault-items/vault-items.module.ts
@@ -1,3 +1,4 @@
+import { ScrollingModule } from "@angular/cdk/scrolling";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
@@ -12,7 +13,7 @@ import { VaultCollectionRowComponent } from "./vault-collection-row.component";
 import { VaultItemsComponent } from "./vault-items.component";
 
 @NgModule({
-  imports: [CommonModule, RouterModule, TableModule, SharedModule],
+  imports: [CommonModule, RouterModule, ScrollingModule, SharedModule, TableModule],
   declarations: [VaultItemsComponent, VaultCipherRowComponent, VaultCollectionRowComponent],
   exports: [VaultItemsComponent],
 })
