ca7052a4ced8b37b6dc270f9398e74d5031c4165
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-02-22 10:46:14 +0100

    [EC-974] feat: add basic mocked data to story

--- apps/web/src/vault/components/vault-items/vault-items.component.ts
+++ apps/web/src/vault/components/vault-items/vault-items.component.ts
@@ -1,7 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+
+import { CollectionView } from "@bitwarden/common/src/models/view/collection.view";
+import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 
 @Component({
   selector: "app-new-vault-items",
   templateUrl: "vault-items.component.html",
 })
-export class VaultItemsComponent {}
+export class VaultItemsComponent {
+  @Input() items: CipherView[];
+  @Input() collections: CollectionView[];
+}
--- apps/web/src/vault/components/vault-items/vault-items.stories.ts
+++ apps/web/src/vault/components/vault-items/vault-items.stories.ts
@@ -1,5 +1,8 @@
 import { Meta, moduleMetadata, Story } from "@storybook/angular";
 
+import { CollectionView } from "@bitwarden/common/models/view/collection.view";
+import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
+
 import { PreloadedEnglishI18nModule } from "../../../app/tests/preloaded-english-i18n.module";
 
 import { VaultItemsComponent } from "./vault-items.component";
@@ -13,6 +16,10 @@ export default {
       imports: [VaultItemsModule, PreloadedEnglishI18nModule],
     }),
   ],
+  args: {
+    items: [...Array(100).keys()].map(createCipherView),
+    collections: [...Array(5).keys()].map(createCollectionView),
+  },
 } as Meta;
 
 const Template: Story<VaultItemsComponent> = (args: VaultItemsComponent) => ({
@@ -21,3 +28,15 @@ const Template: Story<VaultItemsComponent> = (args: VaultItemsComponent) => ({
 
 export const Primary = Template.bind({});
 Primary.args = {};
+
+function createCipherView(i: number): CipherView {
+  const view = new CipherView();
+  view.name = `Vault item ${i}`;
+  return view;
+}
+
+function createCollectionView(i: number): CollectionView {
+  const view = new CollectionView();
+  view.name = `Colletion ${i}`;
+  return view;
+}
