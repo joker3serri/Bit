0e9e646a0e8abbd8f27160f69ff825c6b6997b73
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-02-22 13:14:16 +0100

    [EC-974] feat: add initial table version

--- apps/web/src/vault/components/vault-items/vault-items.module.ts
+++ apps/web/src/vault/components/vault-items/vault-items.module.ts
@@ -1,10 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+import { TableModule } from "@bitwarden/components";
+
+// eslint-disable-next-line no-restricted-imports
+import { SharedModule } from "../../../app/shared/shared.module";
 
 import { VaultItemsComponent } from "./vault-items.component";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, TableModule, SharedModule],
   declarations: [VaultItemsComponent],
   exports: [VaultItemsComponent],
 })
--- apps/web/src/vault/components/vault-items/vault-items.stories.ts
+++ apps/web/src/vault/components/vault-items/vault-items.stories.ts
@@ -1,6 +1,12 @@
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 import { Meta, moduleMetadata, Story } from "@storybook/angular";
+import { BehaviorSubject } from "rxjs";
 
+import { EnvironmentService } from "@bitwarden/common/abstractions/environment.service";
+import { StateService } from "@bitwarden/common/abstractions/state.service";
 import { CollectionView } from "@bitwarden/common/models/view/collection.view";
+import { CipherType } from "@bitwarden/common/src/vault/enums/cipher-type";
 import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
 
 import { PreloadedEnglishI18nModule } from "../../../app/tests/preloaded-english-i18n.module";
@@ -8,16 +14,45 @@ import { PreloadedEnglishI18nModule } from "../../../app/tests/preloaded-english
 import { VaultItemsComponent } from "./vault-items.component";
 import { VaultItemsModule } from "./vault-items.module";
 
+@Component({
+  template: "",
+})
+class EmptyComponent {}
+
 export default {
   title: "Web/Vault/Items",
   component: VaultItemsComponent,
   decorators: [
     moduleMetadata({
-      imports: [VaultItemsModule, PreloadedEnglishI18nModule],
+      imports: [
+        VaultItemsModule,
+        PreloadedEnglishI18nModule,
+        RouterModule.forRoot([{ path: "**", component: EmptyComponent }], { useHash: true }),
+      ],
+      providers: [
+        {
+          provide: EnvironmentService,
+          useValue: {
+            getIconsUrl() {
+              return "";
+            },
+          } as Partial<EnvironmentService>,
+        },
+        {
+          provide: StateService,
+          useValue: {
+            activeAccount$: new BehaviorSubject("1").asObservable(),
+            accounts$: new BehaviorSubject({ "1": { profile: { name: "Foo" } } }).asObservable(),
+            async getDisableFavicon() {
+              return false;
+            },
+          } as Partial<StateService>,
+        },
+      ],
     }),
   ],
   args: {
-    items: [...Array(100).keys()].map(createCipherView),
+    items: [...Array(200).keys()].map(createCipherView),
     collections: [...Array(5).keys()].map(createCollectionView),
   },
 } as Meta;
@@ -32,11 +67,12 @@ Primary.args = {};
 function createCipherView(i: number): CipherView {
   const view = new CipherView();
   view.name = `Vault item ${i}`;
+  view.type = CipherType.Login;
   return view;
 }
 
 function createCollectionView(i: number): CollectionView {
   const view = new CollectionView();
-  view.name = `Colletion ${i}`;
+  view.name = `Collection ${i}`;
   return view;
 }
