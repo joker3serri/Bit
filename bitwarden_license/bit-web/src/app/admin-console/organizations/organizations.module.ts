9d203b41c00da5709112cceb49b566ff58f06b48
Vincent Salucci <vincesalucci21@gmail.com>
2023-02-23 17:01:50 -0600

    fix: update leftover import statements for organizations.module, refs EC-1011

--- bitwarden_license/bit-web/src/app/admin-console/organizations/organizations.module.ts
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/organizations.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 
 import { SharedModule } from "@bitwarden/web-vault/app/shared/shared.module";
 
-import { InputCheckboxComponent } from "../admin-console/organizations/components/input-checkbox.component";
-import { ScimComponent } from "../admin-console/organizations/manage/scim.component";
-import { OrganizationsRoutingModule } from "../admin-console/organizations/organizations-routing.module";
-import { SsoComponent } from "../auth/sso/sso.component";
+import { SsoComponent } from "../../auth/sso/sso.component";
+import { DomainAddEditDialogComponent } from "../../organizations/manage/domain-verification/domain-add-edit-dialog/domain-add-edit-dialog.component";
+import { DomainVerificationComponent } from "../../organizations/manage/domain-verification/domain-verification.component";
 
-import { DomainAddEditDialogComponent } from "./manage/domain-verification/domain-add-edit-dialog/domain-add-edit-dialog.component";
-import { DomainVerificationComponent } from "./manage/domain-verification/domain-verification.component";
+import { InputCheckboxComponent } from "./components/input-checkbox.component";
+import { ScimComponent } from "./manage/scim.component";
+import { OrganizationsRoutingModule } from "./organizations-routing.module";
+
 
 @NgModule({
   imports: [SharedModule, OrganizationsRoutingModule],
