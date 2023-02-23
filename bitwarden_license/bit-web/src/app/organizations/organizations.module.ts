12f44078f9860f1c937f3c839f8799cb0b1af060
Vincent Salucci <vincesalucci21@gmail.com>
2023-02-23 11:00:19 -0600

    refactor: (web) move licensed components to admin-console, refs EC-1011

--- bitwarden_license/bit-web/src/app/organizations/organizations.module.ts
+++ bitwarden_license/bit-web/src/app/organizations/organizations.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from "@angular/core";
 
 import { SharedModule } from "@bitwarden/web-vault/app/shared/shared.module";
 
+import { InputCheckboxComponent } from "../admin-console/organizations/components/input-checkbox.component";
 import { ScimComponent } from "../admin-console/organizations/manage/scim.component";
 import { SsoComponent } from "../auth/sso/sso.component";
 
-import { InputCheckboxComponent } from "./components/input-checkbox.component";
 import { DomainAddEditDialogComponent } from "./manage/domain-verification/domain-add-edit-dialog/domain-add-edit-dialog.component";
 import { DomainVerificationComponent } from "./manage/domain-verification/domain-verification.component";
 import { OrganizationsRoutingModule } from "./organizations-routing.module";
