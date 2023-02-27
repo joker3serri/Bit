ba2194c3d886f0a11595de31dca0ce67eb95547d
Thomas Avery <tavery@bitwarden.com>
2023-02-27 15:01:14 -0600

    Hide tabs based on permission

--- bitwarden_license/bit-web/src/app/secrets-manager/projects/project/project.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/projects/project/project.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from "@angular/router";
 import { Observable, switchMap } from "rxjs";
 
 import { ProjectView } from "../../models/view/project.view";
+import { AccessPolicyService } from "../../shared/access-policies/access-policy.service";
 import { ProjectService } from "../project.service";
 
 @Component({
@@ -11,14 +12,41 @@ import { ProjectService } from "../project.service";
 })
 export class ProjectComponent implements OnInit {
   project$: Observable<ProjectView>;
+  userHasWriteAccess: boolean;
 
-  constructor(private route: ActivatedRoute, private projectService: ProjectService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private projectService: ProjectService,
+    private accessPolicyService: AccessPolicyService
+  ) {}
 
   ngOnInit(): void {
     this.project$ = this.route.params.pipe(
-      switchMap((params) => {
+      switchMap(async (params) => {
+        this.userHasWriteAccess = await this.userHasWriteAccessToProject(
+          params.organizationId,
+          params.projectId
+        );
         return this.projectService.getByProjectId(params.projectId);
       })
     );
+  }
+
+  private async userHasWriteAccessToProject(
+    organizationId: string,
+    projectId: string
+  ): Promise<boolean> {
+    try {
+      const projectAccessPolicies = await this.accessPolicyService.getProjectAccessPolicies(
+        organizationId,
+        projectId
+      );
+      if (projectAccessPolicies) {
+        return true;
+      }
+      return false;
+    } catch (ex) {
+      return false;
+    }
   }
 }
