b6ad30377ba699ec98bd1da86b9154a63bce7364
Colton Hurst <colton@coltonhurst.com>
2023-02-28 12:12:24 -0500

    SM-528: clean up navigation.component.ts

--- bitwarden_license/bit-web/src/app/secrets-manager/layout/navigation.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/layout/navigation.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { map, Observable } from "rxjs";
+import { map } from "rxjs";
 
 import { OrganizationService } from "@bitwarden/common/abstractions/organization/organization.service.abstraction";
 import { Organization } from "@bitwarden/common/models/domain/organization";
@@ -12,16 +12,13 @@ import { SecretsManagerLogo } from "./secrets-manager-logo";
   templateUrl: "./navigation.component.html",
 })
 export class NavigationComponent {
-  protected isAdmin$: Observable<boolean>;
+  protected isAdmin$ = this.route.params.pipe(
+    map((params) => this.organizationService.get(params.organizationId)?.isAdmin)
+  );
+
   protected readonly logo = SecretsManagerLogo;
 
-  constructor(private route: ActivatedRoute, private organizationService: OrganizationService) {}
-
-  async ngOnInit() {
-    this.isAdmin$ = this.route.params.pipe(
-      map((params) => this.organizationService.get(params.organizationId)?.isAdmin)
-    );
-  }
-
   protected orgFilter = (org: Organization) => org.canAccessSecretsManager;
+
+  constructor(private route: ActivatedRoute, private organizationService: OrganizationService) {}
 }
