debfe7d24a11d44fadef68f7b9da18b4b2f53a01
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2023-03-03 15:33:46 -0500

    Update bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/service-account.service.ts
    Co-authored-by: Oscar Hinton <Hinton@users.noreply.github.com>

--- bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/service-account.service.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/service-account.service.ts
@@ -47,7 +47,7 @@ export class ServiceAccountService {
     const orgKey = await this.getOrganizationKey(organizationId);
     const r = await this.apiService.send(
       "GET",
-      "/service-account/" + serviceAccountId + "/service-accounts",
+      "/service-account/" + serviceAccountId",
       null,
       true,
       true
