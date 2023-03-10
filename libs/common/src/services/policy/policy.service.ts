c38d8ec1c682054b762bf19f2fbb004e87461029
Shane Melton <smelton@bitwarden.com>
2023-03-09 16:22:11 -0800

    [AC-1070] Fix missing enforceOnLogin copy in policy service

--- libs/common/src/services/policy/policy.service.ts
+++ libs/common/src/services/policy/policy.service.ts
@@ -1,4 +1,4 @@
-import { of, concatMap, BehaviorSubject, Observable, map } from "rxjs";
+import { BehaviorSubject, concatMap, map, Observable, of } from "rxjs";
 
 import { OrganizationService } from "../../abstractions/organization/organization.service.abstraction";
 import { InternalPolicyService as InternalPolicyServiceAbstraction } from "../../abstractions/policy/policy.service.abstraction";
@@ -116,6 +116,10 @@ export class PolicyService implements InternalPolicyServiceAbstraction {
 
           if (currentPolicy.data.requireSpecial) {
             enforcedOptions.requireSpecial = true;
+          }
+
+          if (currentPolicy.data.enforceOnLogin) {
+            enforcedOptions.enforceOnLogin = true;
           }
         });
 
