fadc2787c34293b8fd7e6aded0d13137e6ddf050
SmithThe4th <gsmith@bitwarden.com>
2023-03-29 11:54:41 -0400

    [PM-1629] Update the upgrade dialog for users without billing rights (#5102)
    * Show dialog with description when user does not have access to the billing page
    
    * switched conditions to nested if to make the logic clearer

--- apps/web/src/app/admin-console/organizations/guards/is-paid-org.guard.ts
+++ apps/web/src/app/admin-console/organizations/guards/is-paid-org.guard.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
 
+import { I18nService } from "@bitwarden/common/abstractions/i18n.service";
 import { MessagingService } from "@bitwarden/common/abstractions/messaging.service";
+import { PlatformUtilsService } from "@bitwarden/common/abstractions/platformUtils.service";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 
 @Injectable({
@@ -11,17 +13,30 @@ export class IsPaidOrgGuard implements CanActivate {
   constructor(
     private router: Router,
     private organizationService: OrganizationService,
-    private messagingService: MessagingService
+    private platformUtilsService: PlatformUtilsService,
+    private messagingService: MessagingService,
+    private i18nService: I18nService
   ) {}
 
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const org = this.organizationService.get(route.params.organizationId);
+
     if (org == null) {
       return this.router.createUrlTree(["/"]);
     }
 
     if (org.isFreeOrg) {
-      this.messagingService.send("upgradeOrganization", { organizationId: org.id });
+      // Users without billing permission can't access billing
+      if (!org.canManageBilling) {
+        await this.platformUtilsService.showDialog(
+          this.i18nService.t("notAvailableForFreeOrganization"),
+          this.i18nService.t("upgradeOrganization"),
+          this.i18nService.t("ok")
+        );
+        return false;
+      } else {
+        this.messagingService.send("upgradeOrganization", { organizationId: org.id });
+      }
     }
 
     return !org.isFreeOrg;
