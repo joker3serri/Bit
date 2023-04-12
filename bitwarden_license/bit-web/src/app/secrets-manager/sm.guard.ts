bcf58f09b5d4c5e5ea3d84f9c42740488e97e6ee
William Martin <contact@willmartian.com>
2023-04-12 11:36:03 -0400

    remove deprecated CanActivate; use CanActivateFn; redirect to available org from SM root

--- bitwarden_license/bit-web/src/app/secrets-manager/sm.guard.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/sm.guard.ts
@@ -1,10 +1,24 @@
-import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate } from "@angular/router";
+import { inject } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  createUrlTreeFromSnapshot,
+  RouterStateSnapshot,
+} from "@angular/router";
 
-@Injectable()
-export class SMGuard implements CanActivate {
-  async canActivate(route: ActivatedRouteSnapshot) {
-    // TODO: Verify org
-    return true;
+import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+
+/**
+ * Redirects from root `/sm` to first organization with access to SM
+ */
+export const canActivateSM: CanActivateFn = async (
+  route: ActivatedRouteSnapshot,
+  _state: RouterStateSnapshot
+) => {
+  const orgs = await inject(OrganizationService).getAll();
+  const smOrg = orgs.find((o) => o.canAccessSecretsManager);
+  if (smOrg) {
+    return createUrlTreeFromSnapshot(route, ["sm", smOrg.id]);
   }
-}
+  return createUrlTreeFromSnapshot(route, ["/vault"]);
+};
