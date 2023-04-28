779e4ddde17ac4caad697ad3f4eb922e50d7299a
William Martin <contact@willmartian.com>
2023-04-28 15:58:19 -0400

    not working: redirect after login

--- bitwarden_license/bit-web/src/app/secrets-manager/sm.guard.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/sm.guard.ts
@@ -6,16 +6,27 @@ import {
   RouterStateSnapshot,
 } from "@angular/router";
 
+import { AuthGuard } from "@bitwarden/angular/auth/guards/auth.guard";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
+import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
 
 /**
  * Redirects from root `/sm` to first organization with access to SM
  */
 export const canActivateSM: CanActivateFn = async (
   route: ActivatedRouteSnapshot,
-  _state: RouterStateSnapshot
+  state: RouterStateSnapshot
 ) => {
-  const orgs = await inject(OrganizationService).getAll();
+  const authService = inject(AuthService);
+  const orgService = inject(OrganizationService);
+  const authGuard = inject(AuthGuard);
+
+  if ((await authService.getAuthStatus()) !== AuthenticationStatus.Unlocked) {
+    return authGuard.canActivate(route, state);
+  }
+
+  const orgs = await orgService.getAll();
   const smOrg = orgs.find((o) => o.canAccessSecretsManager);
   if (smOrg) {
     return createUrlTreeFromSnapshot(route, ["/sm", smOrg.id]);
