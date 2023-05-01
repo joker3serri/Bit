a78e4750dedca0db00702282f81078f15b2b8c85
Jacob Fink <jfink@bitwarden.com>
2023-05-01 11:51:37 -0400

    PM-1639 fix browser settings service imports

--- apps/browser/src/auth/background/service-factories/settings-service.factory.ts
+++ apps/browser/src/auth/background/service-factories/settings-service.factory.ts
@@ -1,9 +1,16 @@
 import { SettingsService as AbstractSettingsService } from "@bitwarden/common/abstractions/settings.service";
 
-import { BrowserSettingsService } from "../../services/browser-settings.service";
 
-import { FactoryOptions, CachedServices, factory } from "./factory-options";
-import { stateServiceFactory, StateServiceInitOptions } from "./state-service.factory";
+import {
+  FactoryOptions,
+  CachedServices,
+  factory,
+} from "../../../background/service_factories/factory-options";
+import {
+  stateServiceFactory,
+  StateServiceInitOptions,
+} from "../../../background/service_factories/state-service.factory";
+import { BrowserSettingsService } from "../../services/browser-settings.service";
 
 type SettingsServiceFactoryOptions = FactoryOptions;
 
