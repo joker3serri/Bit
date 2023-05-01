2d17b5c991df9707fed549127176d56c85b3385d
William Martin <contact@willmartian.com>
2023-05-01 09:33:54 -0400

    add sync service workaround

--- bitwarden_license/bit-web/src/app/secrets-manager/sm.guard.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/sm.guard.ts
@@ -10,6 +10,7 @@ import { AuthGuard } from "@bitwarden/angular/auth/guards/auth.guard";
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { AuthService } from "@bitwarden/common/auth/abstractions/auth.service";
 import { AuthenticationStatus } from "@bitwarden/common/auth/enums/authentication-status";
+import { SyncService } from "@bitwarden/common/vault/abstractions/sync/sync.service.abstraction";
 
 /**
  * Redirects from root `/sm` to first organization with access to SM
@@ -18,9 +19,15 @@ export const canActivateSM: CanActivateFn = async (
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot
 ) => {
+  const syncService = inject(SyncService);
   const authService = inject(AuthService);
   const orgService = inject(OrganizationService);
   const authGuard = inject(AuthGuard);
+
+  /** Workaround to avoid service initialization race condition. */
+  if ((await syncService.getLastSync()) == null) {
+    await syncService.fullSync(false);
+  }
 
   if ((await authService.getAuthStatus()) !== AuthenticationStatus.Unlocked) {
     return authGuard.canActivate(route, state);
