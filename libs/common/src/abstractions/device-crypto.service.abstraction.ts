d4eb69de91a6497e9590400bcaf4b73ec96bb284
Jared Snider <jsnider@bitwarden.com>
2023-05-24 15:41:20 -0400

    PM-1378 - Fix wrong return type for getDeviceKey on DeviceCryptoServiceAbstraction per PR feedback

--- libs/common/src/abstractions/device-crypto.service.abstraction.ts
+++ libs/common/src/abstractions/device-crypto.service.abstraction.ts
@@ -1,8 +1,8 @@
-import { SymmetricCryptoKey } from "../models/domain/symmetric-crypto-key";
+import { DeviceKey } from "../models/domain/symmetric-crypto-key";
 
 import { DeviceResponse } from "./devices/responses/device.response";
 
 export abstract class DeviceCryptoServiceAbstraction {
   trustDevice: () => Promise<DeviceResponse>;
-  getDeviceKey: () => Promise<SymmetricCryptoKey>;
+  getDeviceKey: () => Promise<DeviceKey>;
 }
