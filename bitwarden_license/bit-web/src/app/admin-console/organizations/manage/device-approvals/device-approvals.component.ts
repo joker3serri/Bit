9ec32bb85f41b96d2cc9fb555407f85e06171908
Shane Melton <smelton@bitwarden.com>
2023-05-24 10:24:31 -0700

    [AC-1192] Create initial device approvals component and route

---
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/manage/device-approvals/device-approvals.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-org-device-approvals",
+  templateUrl: "./device-approvals.component.html",
+})
+export class DeviceApprovalsComponent {}
--- bitwarden_license/bit-web/src/app/admin-console/organizations/organizations.module.ts
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/organizations.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from "@bitwarden/web-vault/app/shared/shared.module";
 
 import { SsoComponent } from "../../auth/sso/sso.component";
 
+import { DeviceApprovalsComponent } from "./manage/device-approvals/device-approvals.component";
 import { DomainAddEditDialogComponent } from "./manage/domain-verification/domain-add-edit-dialog/domain-add-edit-dialog.component";
 import { DomainVerificationComponent } from "./manage/domain-verification/domain-verification.component";
 import { ScimComponent } from "./manage/scim.component";
@@ -16,6 +17,7 @@ import { OrganizationsRoutingModule } from "./organizations-routing.module";
     ScimComponent,
     DomainVerificationComponent,
     DomainAddEditDialogComponent,
+    DeviceApprovalsComponent,
   ],
 })
 export class OrganizationsModule {}
