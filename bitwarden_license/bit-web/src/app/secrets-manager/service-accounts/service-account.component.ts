fb51158f855f13801e6b1951cbc533549ba5137d
William Martin <contact@willmartian.com>
2023-05-24 21:23:33 -0400

    redirect if SA isn't found

--- bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/service-account.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/service-accounts/service-account.component.ts
@@ -1,6 +1,6 @@
 import { Component } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { switchMap } from "rxjs";
+import { ActivatedRoute, Router } from "@angular/router";
+import { combineLatest, filter, startWith, switchMap, tap } from "rxjs";
 
 import { DialogServiceAbstraction } from "@bitwarden/angular/services/dialog";
 
@@ -15,24 +15,35 @@ export class ServiceAccountComponent {
   private organizationId: string;
   private serviceAccountId: string;
 
+  private onChange$ = this.serviceAccountService.serviceAccount$.pipe(
+    filter((sa) => sa?.id === this.serviceAccountId),
+    startWith(null)
+  );
+
   /**
    * TODO: remove when a server method is available that fetches a service account by ID
    */
-  protected serviceAccount$ = this.route.params.pipe(
-    switchMap((params) => {
+  protected serviceAccount$ = combineLatest([this.route.params, this.onChange$]).pipe(
+    switchMap(async ([params]) => {
       this.serviceAccountId = params.serviceAccountId;
       this.organizationId = params.organizationId;
 
       return this.serviceAccountService
         .getServiceAccounts(params.organizationId)
         .then((saList) => saList.find((sa) => sa.id === params.serviceAccountId));
+    }),
+    tap((sa) => {
+      if (!sa) {
+        this.router.navigate(["/sm", this.organizationId, "service-accounts"]);
+      }
     })
   );
 
   constructor(
     private route: ActivatedRoute,
     private serviceAccountService: ServiceAccountService,
-    private dialogService: DialogServiceAbstraction
+    private dialogService: DialogServiceAbstraction,
+    private router: Router
   ) {}
 
   protected openNewAccessTokenDialog() {
