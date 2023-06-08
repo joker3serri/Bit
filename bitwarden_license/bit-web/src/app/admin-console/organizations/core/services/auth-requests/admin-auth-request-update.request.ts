73e02f4e6e1bb9164798f07c1175662f46c868b7
Shane Melton <smelton@bitwarden.com>
2023-06-08 12:45:35 -0700

    [AC-1192] Rename key to encryptedUserKey to be more descriptive

--- bitwarden_license/bit-web/src/app/admin-console/organizations/core/services/auth-requests/admin-auth-request-update.request.ts
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/core/services/auth-requests/admin-auth-request-update.request.ts
@@ -2,10 +2,7 @@ export class AdminAuthRequestUpdateRequest {
   /**
    *
    * @param requestApproved - Whether the request was approved/denied. If true, the key must be provided.
-   * @param key The user's symmetric key that has been encrypted with a device public key if the request was approved.
+   * @param encryptedUserKey The user's symmetric key that has been encrypted with a device public key if the request was approved.
    */
-  constructor(public requestApproved: boolean, public key?: string) {
-    this.requestApproved = requestApproved;
-    this.key = key;
-  }
+  constructor(public requestApproved: boolean, public encryptedUserKey?: string) {}
 }
