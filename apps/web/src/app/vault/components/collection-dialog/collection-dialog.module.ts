7ec7d63c8f18c43d50f43e9ec8ead8ffe357d931
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-06-08 14:42:57 -0400

    Remove CoreOrganizationModule from CollectionDialogModule

--- apps/web/src/app/vault/components/collection-dialog/collection-dialog.module.ts
+++ apps/web/src/app/vault/components/collection-dialog/collection-dialog.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from "@angular/core";
 
 import { SelectModule } from "@bitwarden/components";
 
-import { CoreOrganizationModule } from "../../../admin-console/organizations/core";
 import { AccessSelectorModule } from "../../../admin-console/organizations/shared/components/access-selector/access-selector.module";
 import { SharedModule } from "../../../shared";
 
 import { CollectionDialogComponent } from "./collection-dialog.component";
 @NgModule({
-  imports: [SharedModule, AccessSelectorModule, SelectModule, CoreOrganizationModule],
+  imports: [SharedModule, AccessSelectorModule, SelectModule],
   declarations: [CollectionDialogComponent],
   exports: [CollectionDialogComponent],
 })
