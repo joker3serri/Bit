9edce0a485bfe68c804dc0c35b19b9f4e75fa57b
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-06-12 11:44:21 +0200

    fix: feature flags not working properly
    Due to race conditions the api url is not set during the first fetch och server config. This causes the config to be fetched from `api.bitwarden.com`. The config is then supposed to be re-fetched when the api url is set by the environment service, but due to a missing line this is not done when the urls are set from storage.

--- libs/common/src/platform/services/environment.service.ts
+++ libs/common/src/platform/services/environment.service.ts
@@ -139,6 +139,8 @@ export class EnvironmentService implements EnvironmentServiceAbstraction {
     this.eventsUrl = envUrls.events = urls.events;
     this.keyConnectorUrl = urls.keyConnector;
     // scimUrl is not saved to storage
+
+    this.urlsSubject.next(urls);
   }
 
   async setUrls(urls: Urls): Promise<Urls> {
