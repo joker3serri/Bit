bc71e5eafd6eb74fcdbd88cbe2260f1585b709da
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-06-13 08:13:11 -0400

    Add tests for enabled()

---
+++ libs/angular/src/vault/services/password-reprompt.service.spec.ts
@@ -0,0 +1,49 @@
+import { MockProxy, mock } from "jest-mock-extended";
+
+import { KeyConnectorService } from "@bitwarden/common/auth/abstractions/key-connector.service";
+import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
+
+import { ModalService } from "../../services/modal.service";
+
+import { PasswordRepromptService } from "./password-reprompt.service";
+
+describe("PasswordRepromptService", () => {
+  let passwordRepromptService: PasswordRepromptService;
+
+  let stateService: MockProxy<StateService>;
+  let modalService: MockProxy<ModalService>;
+  let keyConnectorService: MockProxy<KeyConnectorService>;
+
+  beforeEach(() => {
+    stateService = mock<StateService>();
+    modalService = mock<ModalService>();
+    keyConnectorService = mock<KeyConnectorService>();
+
+    passwordRepromptService = new PasswordRepromptService(
+      modalService,
+      keyConnectorService,
+      stateService
+    );
+  });
+
+  describe("enabled()", () => {
+    it("returns false if Key Connector is enabled", async () => {
+      keyConnectorService.getUsesKeyConnector.mockResolvedValue(true);
+      stateService.getAcctDecryptionOptions.mockResolvedValue({ hasMasterPassword: true });
+
+      expect(await passwordRepromptService.enabled()).toBe(false);
+    });
+    it("returns false if a user does not have a master password", async () => {
+      keyConnectorService.getUsesKeyConnector.mockResolvedValue(false);
+      stateService.getAcctDecryptionOptions.mockResolvedValue({ hasMasterPassword: false });
+
+      expect(await passwordRepromptService.enabled()).toBe(false);
+    });
+    it("returns true if Key Connector is not enabled and the user has a master password", async () => {
+      keyConnectorService.getUsesKeyConnector.mockResolvedValue(false);
+      stateService.getAcctDecryptionOptions.mockResolvedValue({ hasMasterPassword: true });
+
+      expect(await passwordRepromptService.enabled()).toBe(true);
+    });
+  });
+});
