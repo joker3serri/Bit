e420986aa81136c31d5e9d95e15e8adfdea1b7c0
André Bispo <abispo@bitwarden.com>
2023-06-17 17:13:20 +0100

    [PM-2676] Fix web set env urls after state init.

--- apps/web/src/app/core/init.service.ts
+++ apps/web/src/app/core/init.service.ts
@@ -38,11 +38,14 @@ export class InitService {
 
   init() {
     return async () => {
+      // Workaround to ignore stateService.activeAccount until process.env.URLS are set
+      this.environmentService.initialized = false;
       await this.stateService.init();
 
       const urls = process.env.URLS as Urls;
       urls.base ??= this.win.location.origin;
       this.environmentService.setUrls(urls);
+      this.environmentService.initialized = true;
 
       setTimeout(() => this.notificationsService.init(), 3000);
       (this.vaultTimeoutService as VaultTimeoutService).init(true);
