46f96ca4bc283944e445c36b2df108aabd85ef28
André Bispo <abispo@bitwarden.com>
2023-06-20 20:47:05 +0100

    [PM-1202] Add check if user has master password decryption option.

--- apps/web/src/app/settings/security.component.ts
+++ apps/web/src/app/settings/security.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 
 import { KeyConnectorService } from "@bitwarden/common/auth/abstractions/key-connector.service";
+import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
 
 @Component({
   selector: "app-security",
@@ -9,9 +10,16 @@ import { KeyConnectorService } from "@bitwarden/common/auth/abstractions/key-con
 export class SecurityComponent {
   showChangePassword = true;
 
-  constructor(private keyConnectorService: KeyConnectorService) {}
+  constructor(
+    private keyConnectorService: KeyConnectorService,
+    private stateService: StateService
+  ) {}
 
   async ngOnInit() {
+    const accountDecriptionOptions = await this.stateService.getAcctDecryptionOptions();
+    if (accountDecriptionOptions == null) {
+      this.showChangePassword = accountDecriptionOptions.hasMasterPassword;
+    }
     this.showChangePassword = !(await this.keyConnectorService.getUsesKeyConnector());
   }
 }
