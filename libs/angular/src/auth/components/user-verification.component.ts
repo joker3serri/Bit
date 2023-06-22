a7484590c0aedc9542fa5eaa150ca20c093dc37c
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-06-22 10:00:10 +0200

    [PM-1203] feat: add backwards compatibility for accounts/servers without decryption options

--- libs/angular/src/auth/components/user-verification.component.ts
+++ libs/angular/src/auth/components/user-verification.component.ts
@@ -69,7 +69,7 @@ export class UserVerificationComponent implements ControlValueAccessor, OnInit, 
   ) {}
 
   async ngOnInit() {
-    this.hasMasterPassword = await this.cryptoService.hasMasterPassword();
+    this.hasMasterPassword = await this.userVerificationService.hasMasterPassword();
     this.processChanges(this.secret.value);
 
     this.secret.valueChanges
