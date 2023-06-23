f9f67183f0bdab4cd8f7b752f5f76f9c56dd3ebb
Jared Snider <jsnider@bitwarden.com>
2023-06-23 13:36:19 -0400

    PM-1049 - Device Response & View models - remove keys which are going to be deprecated on the base model

--- libs/common/src/abstractions/devices/views/device.view.ts
+++ libs/common/src/abstractions/devices/views/device.view.ts
@@ -1,4 +1,3 @@
-// import { View } from "@bitwarden/common/models/view/view";
 import { DeviceType } from "../../../enums";
 import { View } from "../../../models/view/view";
 import { DeviceResponse } from "../responses/device.response";
@@ -11,14 +10,6 @@ export class DeviceView implements View {
   type: DeviceType;
   creationDate: string;
   revisionDate: string;
-
-  // TODO: how to handle encrypted data in a view?
-  // The Domain models lies at the center of our data model, and represents encrypted data.
-  // View models represent the decrypted state of the corresponding Domain model.
-  // They typically match the Domain model but contains a decrypted string for any EncString fields.
-  encryptedUserKey: string;
-  encryptedPublicKey: string;
-  encryptedPrivateKey: string;
 
   constructor(deviceResponse: DeviceResponse) {
     Object.assign(this, deviceResponse);
