b6e249e91bf80a6cbf6baa4e4eb1f524cb9b1de9
Jared Snider <jsnider@bitwarden.com>
2023-06-26 19:58:52 -0400

    PM-1049 - LoginDecryptionOptionsComp - Completely back away from template async pipe reactive approach as it caused massively increased complexity for little gain. Instead, just focus on reactively pulling asynchronously retrieved data and setting page loading state simply. This just works and is so much less overhead. + Add comments re flows of the component to be done later

--- libs/angular/src/auth/components/base-login-decryption-options.component.ts
+++ libs/angular/src/auth/components/base-login-decryption-options.component.ts
@@ -1,19 +1,7 @@
 import { Directive, OnDestroy, OnInit } from "@angular/core";
 import { FormBuilder } from "@angular/forms";
 import { Router } from "@angular/router";
-import {
-  BehaviorSubject,
-  Observable,
-  Subject,
-  catchError,
-  combineLatest,
-  from,
-  map,
-  shareReplay,
-  takeUntil,
-  tap,
-  throwError,
-} from "rxjs";
+import { Observable, Subject, catchError, forkJoin, from, of, finalize, takeUntil } from "rxjs";
 
 import { DevicesServiceAbstraction } from "@bitwarden/common/abstractions/devices/devices.service.abstraction";
 import { LoginService } from "@bitwarden/common/auth/abstractions/login.service";
@@ -31,12 +19,10 @@ import { AccountDecryptionOptions } from "@bitwarden/common/platform/models/doma
 export class BaseLoginDecryptionOptionsComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
 
-  showApproveFromOtherDeviceBtn$: Observable<boolean>;
-  showReqAdminApprovalBtn$: Observable<boolean>;
-  showApproveWithMasterPasswordBtn$: Observable<boolean>;
-
-  private userEmailSubject = new BehaviorSubject<string>(null);
-  userEmail$ = this.userEmailSubject.asObservable();
+  showApproveFromOtherDeviceBtn: boolean;
+  showReqAdminApprovalBtn: boolean;
+  showApproveWithMasterPasswordBtn: boolean;
+  userEmail: string;
 
   rememberDeviceForm = this.formBuilder.group({
     rememberDevice: [true],
@@ -55,81 +41,95 @@ export class BaseLoginDecryptionOptionsComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
-    const accountDecryptionOptions$: Observable<AccountDecryptionOptions> = from(
-      this.stateService.getAccountDecryptionOptions()
-    ).pipe(
-      // ShareReplay Context:
-      // from(promise) turns a promise into an a hot observable (i.e., data produced outside of the observable)
-      // However, the promise eagerly executes immediately once defined.
-      // Using shareReplay will ensure that late subscribers will receive the last emitted value
-      // and, if the promise is ever converted to an observable, it will only execute once still
-      shareReplay({ refCount: true, bufferSize: 1 })
-    );
+    // Note: this is probably not a comprehensive write up of all scenarios:
 
-    from(this.stateService.getEmail())
-      .pipe(
-        tap((email) => {
-          this.userEmailSubject.next(email);
-        }),
-        catchError((err: unknown) => {
-          this.validationService.showError(err);
-          return throwError(() => err);
-        }),
-        takeUntil(this.destroy$)
-      )
-      // Subscribe because we always want to get the email, even if the template doesn't subscribe to it via async pipe
-      .subscribe();
+    // If the TDE feature flag is enabled and TDE is configured for the org that the user is a member of,
+    // then new and existing users can be redirected here after completing the SSO flow (and 2FA if enabled).
 
-    // Show approve from other device btn if user has any mobile or desktop devices
+    // First we must determine user type (new or existing):
+
+    // New User
+    // - present user with option to remember the device or not (trust the device)
+    // - present a continue button to proceed to the vault
+    //  - loadNewUserData() --> will need to load enrollment status and user email address.
+
+    // Existing User
+    // - Determine if user is an admin with access to account recovery in admin console
+    //  - Determine if user has a MP or not, if not, they must be redirected to set one (see PM-1035)
+    // - Determine if device is trusted or not via device crypto service (method not yet written)
+    //  - If not trusted, present user with login decryption options (approve from other device, approve with master password, request admin approval)
+    //    - loadUntrustedDeviceData()
+
+    this.loadUntrustedDeviceData();
+  }
+
+  loadUntrustedDeviceData() {
+    this.loading = true;
+
     const mobileAndDesktopDeviceTypes: DeviceType[] = Array.from(MobileDeviceTypes).concat(
       Array.from(DesktopDeviceTypes)
     );
 
-    this.showApproveFromOtherDeviceBtn$ = this.devicesService
+    // Note: Each obs must handle error here and protect inner observable b/c we are using forkJoin below
+    // as per RxJs docs: if any given observable errors at some point, then
+    // forkJoin will error as well and immediately unsubscribe from the other observables.
+    const mobileOrDesktopDevicesExistence$ = this.devicesService
       .getDevicesExistenceByTypes$(mobileAndDesktopDeviceTypes)
       .pipe(
         catchError((err: unknown) => {
           this.validationService.showError(err);
-          return throwError(() => err);
+          return of(undefined);
         }),
         takeUntil(this.destroy$)
       );
 
-    // Show the admin approval btn if user has TDE enabled and the org admin approval policy is set
-    this.showReqAdminApprovalBtn$ = accountDecryptionOptions$.pipe(
+    const accountDecryptionOptions$: Observable<AccountDecryptionOptions> = from(
+      this.stateService.getAccountDecryptionOptions()
+    ).pipe(
       catchError((err: unknown) => {
         this.validationService.showError(err);
-        return throwError(() => err);
+        return of(undefined);
       }),
-      // !! is for case when trustedDeviceOption is undefined to achieve a boolean output always
-      map((acctDecryptionOptions) => !!acctDecryptionOptions.trustedDeviceOption?.hasAdminApproval),
       takeUntil(this.destroy$)
     );
 
-    this.showApproveWithMasterPasswordBtn$ = accountDecryptionOptions$.pipe(
+    const email$ = from(this.stateService.getEmail()).pipe(
       catchError((err: unknown) => {
         this.validationService.showError(err);
-        return throwError(() => err);
+        return of(undefined);
       }),
-      map((acctDecryptionOptions) => acctDecryptionOptions.hasMasterPassword),
       takeUntil(this.destroy$)
     );
 
-    // Set loading false once all observables have emitted at least once
-    combineLatest([
-      this.showApproveFromOtherDeviceBtn$,
-      this.showReqAdminApprovalBtn$,
-      this.showApproveWithMasterPasswordBtn$,
-    ])
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(() => (this.loading = false));
+    forkJoin({
+      mobileOrDesktopDevicesExistence: mobileOrDesktopDevicesExistence$,
+      accountDecryptionOptions: accountDecryptionOptions$,
+      email: email$,
+    })
+      .pipe(
+        takeUntil(this.destroy$),
+        finalize(() => {
+          this.loading = false;
+        })
+      )
+      .subscribe(({ mobileOrDesktopDevicesExistence, accountDecryptionOptions, email }) => {
+        this.showApproveFromOtherDeviceBtn = mobileOrDesktopDevicesExistence || false;
+
+        this.showReqAdminApprovalBtn =
+          !!accountDecryptionOptions?.trustedDeviceOption?.hasAdminApproval || false;
+
+        this.showApproveWithMasterPasswordBtn =
+          accountDecryptionOptions?.hasMasterPassword || false;
+
+        this.userEmail = email;
+      });
   }
 
   approveFromOtherDevice() {
     // TODO: plan is to re-use existing login-with-device component but rework it to have two flows
     // (1) Standard flow for unauthN user based on AuthService status
     // (2) New flow for authN user based on AuthService status b/c they have just authenticated w/ SSO
-    this.loginService.setEmail(this.userEmailSubject.value);
+    this.loginService.setEmail(this.userEmail);
     this.router.navigate(["/login-with-device"]);
   }
 
