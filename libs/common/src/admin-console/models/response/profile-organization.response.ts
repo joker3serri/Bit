fda85200925cad89c26dd960cbfd10d26799ddcf
William Martin <contact@willmartian.com>
2023-07-07 12:07:31 -0400

    Revert "remove beta field from profile org response"
    This reverts commit 6c6249e1ec3c54b08f21f0895f9e2c23fbd58e9c.

--- libs/common/src/admin-console/models/response/profile-organization.response.ts
+++ libs/common/src/admin-console/models/response/profile-organization.response.ts
@@ -48,6 +48,7 @@ export class ProfileOrganizationResponse extends BaseResponse {
   familySponsorshipValidUntil?: Date;
   familySponsorshipToDelete?: boolean;
   accessSecretsManager: boolean;
+  secretsManagerBeta: boolean;
 
   constructor(response: any) {
     super(response);
@@ -105,5 +106,6 @@ export class ProfileOrganizationResponse extends BaseResponse {
     }
     this.familySponsorshipToDelete = this.getResponseProperty("FamilySponsorshipToDelete");
     this.accessSecretsManager = this.getResponseProperty("AccessSecretsManager");
+    this.secretsManagerBeta = this.getResponseProperty("SecretsManagerBeta") ?? false;
   }
 }
