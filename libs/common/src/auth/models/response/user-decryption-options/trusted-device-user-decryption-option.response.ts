ed41579e9bc7940cb1598ff33e1daf9d491181c4
Jared Snider <jsnider@bitwarden.com>
2023-07-07 13:26:47 -0400

    PM-1260 - Add new encrypted keys to TrustedDeviceUserDecryptionOptionResponse

--- libs/common/src/auth/models/response/user-decryption-options/trusted-device-user-decryption-option.response.ts
+++ libs/common/src/auth/models/response/user-decryption-options/trusted-device-user-decryption-option.response.ts
@@ -1,10 +1,20 @@
 import { BaseResponse } from "../../../../models/response/base.response";
+import { EncString } from "../../../../platform/models/domain/enc-string";
 
 export class TrustedDeviceUserDecryptionOptionResponse extends BaseResponse {
   hasAdminApproval: boolean;
+  encryptedPrivateKey: EncString;
+  encryptedUserKey: EncString;
 
   constructor(response: any) {
     super(response);
     this.hasAdminApproval = this.getResponseProperty("HasAdminApproval");
+
+    if (response.EncryptedPrivateKey) {
+      this.encryptedPrivateKey = new EncString(this.getResponseProperty("EncryptedPrivateKey"));
+    }
+    if (response.EncryptedUserKey) {
+      this.encryptedUserKey = new EncString(this.getResponseProperty("EncryptedUserKey"));
+    }
   }
 }
