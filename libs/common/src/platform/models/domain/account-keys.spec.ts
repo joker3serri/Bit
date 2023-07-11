e78ef261f5f006699de9d8c262b94327cc915d33
Jared Snider <jsnider@bitwarden.com>
2023-07-11 16:55:30 -0400

    PM-1260 - AccountKeys tests - add tests for deviceKey persistence and deserialization

--- libs/common/src/platform/models/domain/account-keys.spec.ts
+++ libs/common/src/platform/models/domain/account-keys.spec.ts
@@ -1,10 +1,10 @@
 import { makeStaticByteArray } from "../../../../spec";
+import { CsprngArray } from "../../../types/csprng";
 import { Utils } from "../../misc/utils";
 
 import { AccountKeys, EncryptionPair } from "./account";
-import { SymmetricCryptoKey } from "./symmetric-crypto-key";
+import { DeviceKey, SymmetricCryptoKey } from "./symmetric-crypto-key";
 
-// TODO: add device key to tests
 describe("AccountKeys", () => {
   describe("toJSON", () => {
     it("should serialize itself", () => {
@@ -22,6 +22,23 @@ describe("AccountKeys", () => {
       keys.publicKey = Utils.fromByteStringToArray("hello").buffer;
       const json = JSON.stringify(keys);
       expect(json).toContain('"publicKey":"hello"');
+    });
+
+    // As the accountKeys.toJSON doesn't really serialize the device key
+    // this method just checks the persistence of the deviceKey
+    it("should persist deviceKey", () => {
+      // Arrange
+      const accountKeys = new AccountKeys();
+      const deviceKeyBytesLength = 64;
+      accountKeys.deviceKey = new SymmetricCryptoKey(
+        new Uint8Array(deviceKeyBytesLength).buffer as CsprngArray
+      ) as DeviceKey;
+
+      // Act
+      const serializedKeys = accountKeys.toJSON();
+
+      // Assert
+      expect(serializedKeys.deviceKey).toEqual(accountKeys.deviceKey);
     });
   });
 
@@ -57,6 +74,25 @@ describe("AccountKeys", () => {
         privateKey: { encrypted: "encrypted", decrypted: "decrypted" },
       } as any);
       expect(spy).toHaveBeenCalled();
+    });
+
+    it("should deserialize deviceKey", () => {
+      // Arrange
+      const expectedKeyB64 =
+        "ZJNnhx9BbJeb2EAq1hlMjqt6GFsg9G/GzoFf6SbPKsaiMhKGDcbHcwcyEg56Lh8lfilpZz4SRM6UA7oFCg+lSg==";
+
+      const symmetricCryptoKeyFromJsonSpy = jest.spyOn(SymmetricCryptoKey, "fromJSON");
+
+      // Act
+      const accountKeys = AccountKeys.fromJSON({
+        deviceKey: {
+          keyB64: expectedKeyB64,
+        },
+      } as any);
+
+      // Assert
+      expect(symmetricCryptoKeyFromJsonSpy).toHaveBeenCalled();
+      expect(accountKeys.deviceKey.keyB64).toEqual(expectedKeyB64);
     });
   });
 });
