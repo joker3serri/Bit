c09a046e659af187bfc4eaeef30f03192496892a
Robyn MacCallum <robyntmaccallum@gmail.com>
2023-07-18 14:39:18 -0400

    Use UserVerificationService to determine if a user has a master password

--- libs/angular/src/vault/services/password-reprompt.service.spec.ts
+++ libs/angular/src/vault/services/password-reprompt.service.spec.ts
@@ -1,7 +1,6 @@
 import { MockProxy, mock } from "jest-mock-extended";
 
-import { KeyConnectorService } from "@bitwarden/common/auth/abstractions/key-connector.service";
-import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
+import { UserVerificationService } from "@bitwarden/common/auth/abstractions/user-verification/user-verification.service.abstraction";
 
 import { ModalService } from "../../services/modal.service";
 
@@ -10,38 +9,24 @@ import { PasswordRepromptService } from "./password-reprompt.service";
 describe("PasswordRepromptService", () => {
   let passwordRepromptService: PasswordRepromptService;
 
-  let stateService: MockProxy<StateService>;
+  let userVerificationService: MockProxy<UserVerificationService>;
   let modalService: MockProxy<ModalService>;
-  let keyConnectorService: MockProxy<KeyConnectorService>;
 
   beforeEach(() => {
-    stateService = mock<StateService>();
     modalService = mock<ModalService>();
-    keyConnectorService = mock<KeyConnectorService>();
+    userVerificationService = mock<UserVerificationService>();
 
-    passwordRepromptService = new PasswordRepromptService(
-      modalService,
-      keyConnectorService,
-      stateService
-    );
+    passwordRepromptService = new PasswordRepromptService(modalService, userVerificationService);
   });
 
   describe("enabled()", () => {
-    it("returns false if Key Connector is enabled", async () => {
-      keyConnectorService.getUsesKeyConnector.mockResolvedValue(true);
-      stateService.getAccountDecryptionOptions.mockResolvedValue({ hasMasterPassword: true });
-
-      expect(await passwordRepromptService.enabled()).toBe(false);
-    });
     it("returns false if a user does not have a master password", async () => {
-      keyConnectorService.getUsesKeyConnector.mockResolvedValue(false);
-      stateService.getAccountDecryptionOptions.mockResolvedValue({ hasMasterPassword: false });
+      userVerificationService.hasMasterPassword.mockResolvedValue(false);
 
       expect(await passwordRepromptService.enabled()).toBe(false);
     });
-    it("returns true if Key Connector is not enabled and the user has a master password", async () => {
-      keyConnectorService.getUsesKeyConnector.mockResolvedValue(false);
-      stateService.getAccountDecryptionOptions.mockResolvedValue({ hasMasterPassword: true });
+    it("returns true if the user has a master password", async () => {
+      userVerificationService.hasMasterPassword.mockResolvedValue(true);
 
       expect(await passwordRepromptService.enabled()).toBe(true);
     });
