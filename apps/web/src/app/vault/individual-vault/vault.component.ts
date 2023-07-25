0087a08a36ce14ae1da9a26da0ae1edb6a6a982d
jng <jng@bitwarden.com>
2023-07-25 14:35:20 -0400

    added showPasswordReprompt logic to shareCipher method

--- apps/web/src/app/vault/individual-vault/vault.component.ts
+++ apps/web/src/app/vault/individual-vault/vault.component.ts
@@ -544,6 +544,12 @@ export class VaultComponent implements OnInit, OnDestroy {
   }
 
   async shareCipher(cipher: CipherView) {
+    if (cipher != null && cipher.reprompt != 0) {
+      if (!(await this.passwordRepromptService.showPasswordPrompt())) {
+        this.go({ cipherId: null, itemId: null });
+        return;
+      }
+    }
     const [modal] = await this.modalService.openViewRef(
       ShareComponent,
       this.shareModalRef,
