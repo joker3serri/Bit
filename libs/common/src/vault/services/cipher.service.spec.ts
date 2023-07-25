dfddf193fd182e9905aeb9b761f1e82ba9a56a27
jng <jng@bitwarden.com>
2023-07-25 13:21:44 -0400

    fixed mock return values inside the tests of cipher service spec

--- libs/common/src/vault/services/cipher.service.spec.ts
+++ libs/common/src/vault/services/cipher.service.spec.ts
@@ -4,13 +4,11 @@ import { mock, mockReset } from "jest-mock-extended";
 import { ApiService } from "../../abstractions/api.service";
 import { SearchService } from "../../abstractions/search.service";
 import { SettingsService } from "../../abstractions/settings.service";
-import { UriMatchType , FieldType } from "../../enums";
+import { UriMatchType, FieldType } from "../../enums";
 import { CryptoService } from "../../platform/abstractions/crypto.service";
 import { EncryptService } from "../../platform/abstractions/encrypt.service";
 import { I18nService } from "../../platform/abstractions/i18n.service";
 import { StateService } from "../../platform/abstractions/state.service";
-import { EncArrayBuffer } from "../../platform/models/domain/enc-array-buffer";
-import { EncString } from "../../platform/models/domain/enc-string";
 import { SymmetricCryptoKey } from "../../platform/models/domain/symmetric-crypto-key";
 import { CipherFileUploadService } from "../abstractions/file-upload/cipher-file-upload.service";
 import { CipherRepromptType } from "../enums/cipher-reprompt-type";
@@ -21,9 +19,6 @@ import { CipherCreateRequest } from "../models/request/cipher-create.request";
 import { CipherRequest } from "../models/request/cipher.request";
 
 import { CipherService } from "./cipher.service";
-
-const ENCRYPTED_TEXT = "This data has been encrypted";
-const ENCRYPTED_BYTES = mock<EncArrayBuffer>();
 
 const cipherData: CipherData = {
   id: "id",
@@ -106,9 +101,6 @@ describe("Cipher Service", () => {
     mockReset(searchService);
     mockReset(encryptService);
 
-    cryptoService.encryptToBytes.mockReturnValue(ENCRYPTED_BYTES);
-    cryptoService.encrypt.mockReturnValue(new EncString(ENCRYPTED_TEXT));
-
     cipherService = new CipherService(
       cryptoService,
       settingsService,
@@ -126,8 +118,12 @@ describe("Cipher Service", () => {
   it("should upload encrypted file contents with save attachments", async () => {
     const fileName = "filename";
     const fileData = new Uint8Array(10).buffer;
-    cryptoService.getOrgKey.mockReturnValue(new SymmetricCryptoKey(new Uint8Array(32).buffer));
-    cryptoService.makeEncKey.mockReturnValue(new SymmetricCryptoKey(new Uint8Array(32).buffer));
+    cryptoService.getOrgKey.mockReturnValue(
+      Promise.resolve<any>(new SymmetricCryptoKey(new Uint8Array(32).buffer))
+    );
+    cryptoService.makeEncKey.mockReturnValue(
+      Promise.resolve<any>(new SymmetricCryptoKey(new Uint8Array(32).buffer))
+    );
     const spy = jest.spyOn(cipherFileUploadService, "upload");
 
     await cipherService.saveAttachmentRawWithServer(new Cipher(), fileName, fileData);
@@ -136,7 +132,9 @@ describe("Cipher Service", () => {
   });
 
   it("should call apiService.postCipherAdmin when orgAdmin param is true", async () => {
-    const spy = jest.spyOn(apiService, "postCipherAdmin").mockImplementation(() => cipherObj);
+    const spy = jest
+      .spyOn(apiService, "postCipherAdmin")
+      .mockImplementation(() => Promise.resolve<any>(cipherObj));
     cipherService.createWithServer(cipherObj, true);
     const expectedObj = new CipherCreateRequest(cipherObj);
 
@@ -146,7 +144,9 @@ describe("Cipher Service", () => {
 
   it("should call apiService.postCipherCreate if collectionsIds != null", async () => {
     cipherObj.collectionIds = ["123"];
-    const spy = jest.spyOn(apiService, "postCipherCreate").mockImplementation(() => cipherObj);
+    const spy = jest
+      .spyOn(apiService, "postCipherCreate")
+      .mockImplementation(() => Promise.resolve<any>(cipherObj));
     cipherService.createWithServer(cipherObj);
     const expectedObj = new CipherCreateRequest(cipherObj);
 
@@ -155,7 +155,9 @@ describe("Cipher Service", () => {
   });
 
   it("should call apiService.postCipher when orgAdmin and collectionIds logic is false", async () => {
-    const spy = jest.spyOn(apiService, "postCipher").mockImplementation(() => cipherObj);
+    const spy = jest
+      .spyOn(apiService, "postCipher")
+      .mockImplementation(() => Promise.resolve<any>(cipherObj));
     cipherService.createWithServer(cipherObj);
     const expectedObj = new CipherRequest(cipherObj);
 
