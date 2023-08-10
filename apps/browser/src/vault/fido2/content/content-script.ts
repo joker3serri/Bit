de40fe04d81acae9947fc424239aea4905c7f607
Cesar Gonzalez <cgonzalez@bitwarden.com>
2023-08-10 03:07:33 -0500

    [PM-2907] Shopify Passkey Broken on Firefox When Extension is Installed

--- apps/browser/src/vault/fido2/content/content-script.ts
+++ apps/browser/src/vault/fido2/content/content-script.ts
@@ -1,65 +1,82 @@
 import { Message, MessageType } from "./messaging/message";
 import { Messenger } from "./messaging/messenger";
 
-const s = document.createElement("script");
-s.src = chrome.runtime.getURL("content/fido2/page-script.js");
-(document.head || document.documentElement).appendChild(s);
-
-const messenger = Messenger.forDOMCommunication(window);
-
-messenger.handler = async (message, abortController) => {
-  const abortHandler = () =>
-    chrome.runtime.sendMessage({
-      command: "fido2AbortRequest",
-      abortedRequestId: message.metadata.requestId,
-    });
-  abortController.signal.addEventListener("abort", abortHandler);
-
-  if (message.type === MessageType.CredentialCreationRequest) {
-    return new Promise((resolve, reject) => {
-      chrome.runtime.sendMessage(
-        {
-          command: "fido2RegisterCredentialRequest",
-          data: message.data,
-          requestId: message.metadata.requestId,
-        },
-        (response) => {
-          if (response.error !== undefined) {
-            return reject(response.error);
-          }
-
-          resolve({
-            type: MessageType.CredentialCreationResponse,
-            result: response.result,
-          });
-        }
-      );
-    });
+(function () {
+  function checkFido2FeatureEnabled() {
+    chrome.runtime.sendMessage(
+      { command: "checkFido2FeatureEnabled" },
+      (response: { result?: boolean }) => initializeFido2ContentScript(response.result)
+    );
   }
 
-  if (message.type === MessageType.CredentialGetRequest) {
-    return new Promise((resolve, reject) => {
-      chrome.runtime.sendMessage(
-        {
-          command: "fido2GetCredentialRequest",
-          data: message.data,
-          requestId: message.metadata.requestId,
-        },
-        (response) => {
-          if (response.error !== undefined) {
-            return reject(response.error);
-          }
+  function initializeFido2ContentScript(isFido2FeatureEnabled: boolean) {
+    if (isFido2FeatureEnabled !== true) {
+      return;
+    }
 
-          resolve({
-            type: MessageType.CredentialGetResponse,
-            result: response.result,
-          });
-        }
-      );
-    }).finally(() =>
-      abortController.signal.removeEventListener("abort", abortHandler)
-    ) as Promise<Message>;
+    const s = document.createElement("script");
+    s.src = chrome.runtime.getURL("content/fido2/page-script.js");
+    (document.head || document.documentElement).appendChild(s);
+
+    const messenger = Messenger.forDOMCommunication(window);
+
+    messenger.handler = async (message, abortController) => {
+      const abortHandler = () =>
+        chrome.runtime.sendMessage({
+          command: "fido2AbortRequest",
+          abortedRequestId: message.metadata.requestId,
+        });
+      abortController.signal.addEventListener("abort", abortHandler);
+
+      if (message.type === MessageType.CredentialCreationRequest) {
+        return new Promise((resolve, reject) => {
+          chrome.runtime.sendMessage(
+            {
+              command: "fido2RegisterCredentialRequest",
+              data: message.data,
+              requestId: message.metadata.requestId,
+            },
+            (response) => {
+              if (response.error !== undefined) {
+                return reject(response.error);
+              }
+
+              resolve({
+                type: MessageType.CredentialCreationResponse,
+                result: response.result,
+              });
+            }
+          );
+        });
+      }
+
+      if (message.type === MessageType.CredentialGetRequest) {
+        return new Promise((resolve, reject) => {
+          chrome.runtime.sendMessage(
+            {
+              command: "fido2GetCredentialRequest",
+              data: message.data,
+              requestId: message.metadata.requestId,
+            },
+            (response) => {
+              if (response.error !== undefined) {
+                return reject(response.error);
+              }
+
+              resolve({
+                type: MessageType.CredentialGetResponse,
+                result: response.result,
+              });
+            }
+          );
+        }).finally(() =>
+          abortController.signal.removeEventListener("abort", abortHandler)
+        ) as Promise<Message>;
+      }
+
+      return undefined;
+    };
   }
 
-  return undefined;
-};
+  checkFido2FeatureEnabled();
+})();
