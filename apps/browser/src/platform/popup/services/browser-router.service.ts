c204c70e6b611af33b8e1d37234bff8c65fe649a
gbubemismith <gsmithwalter@gmail.com>
2023-09-04 15:29:48 -0400

    Refactored fido2 popup to use auth guard when routing to component, added BrowserRouterService to track previous page and route using that

---
+++ apps/browser/src/platform/popup/services/browser-router.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd, Router } from "@angular/router";
+import { filter } from "rxjs";
+
+import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
+
+@Injectable({
+  providedIn: "root",
+})
+export class BrowserRouterService {
+  constructor(router: Router, private stateService: StateService) {
+    router.events
+      .pipe(filter((e) => e instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const state: ActivatedRouteSnapshot = router.routerState.snapshot.root;
+
+        let child = state.firstChild;
+        while (child.firstChild) {
+          child = child.firstChild;
+        }
+
+        const updateUrl = !child?.data?.doNotSaveUrl ?? true;
+
+        if (updateUrl) {
+          this.setPreviousUrl(event.url);
+        }
+      });
+  }
+
+  async getPreviousUrl() {
+    return this.stateService.getPreviousUrl();
+  }
+
+  // Check validity of previous url
+  async hasPreviousUrl() {
+    return (await this.getPreviousUrl()) != "/";
+  }
+
+  async setPreviousUrl(url: string) {
+    await this.stateService.setPreviousUrl(url);
+  }
+
+  private getDeepestChild(activatedRoute: ActivatedRoute): ActivatedRoute {
+    let child = activatedRoute;
+    while (child.firstChild) {
+      child = child.firstChild;
+    }
+    return child;
+  }
+}
