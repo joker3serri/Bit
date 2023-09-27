b346564d63d19865f4a0363b654806ada7dd7933
Todd Martin <106564991+trmartin4@users.noreply.github.com>
2023-09-27 09:29:58 -0400

    Updated vault export to keep immediate invocation and reformat for clarity.
    Co-authored-by: aj-rosado <109146700+aj-rosado@users.noreply.github.com>

--- libs/exporter/src/vault-export/services/vault-export.service.ts
+++ libs/exporter/src/vault-export/services/vault-export.service.ts
@@ -260,12 +260,14 @@ export class VaultExportService implements VaultExportServiceAbstraction {
               .filter((c) => c.deletedDate === null)
               .forEach(async (c) => {
                 const cipher = new Cipher(new CipherData(c));
-                exportPromises.push(async () => {
-                  const decCipher = await cipher.decrypt(
-                    await this.cipherService.getKeyForCipherKeyDecryption(cipher)
-                  );
-                  decCiphers.push(decCipher);
-                });
+               exportPromises.push(
+                 this.cipherService
+                   .getKeyForCipherKeyDecryption(cipher)
+                   .then((key) => cipher.decrypt(key))
+                   .then((decCipher) => {
+                     decCiphers.push(decCipher);
+                   })
+               );
               });
           }
         }
