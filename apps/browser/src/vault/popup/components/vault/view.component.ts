033b8b319c1d90a68f1c21fdbba1a006163a40a4
Jonathan Prusik <jprusik@classynemesis.com>
2023-09-28 13:16:40 -0400

    only check if target tab host+path changed during reprompt

--- apps/browser/src/vault/popup/components/vault/view.component.ts
+++ apps/browser/src/vault/popup/components/vault/view.component.ts
@@ -331,12 +331,19 @@ export class ViewComponent extends BaseViewComponent {
   }
 
   private async doAutofill() {
-    const originalTabURL = this.tab.url;
+    const originalTabURL = this.tab.url?.length && new URL(this.tab.url);
+
     if (!(await this.promptPassword())) {
       return false;
     }
 
-    const tabUrlChanged = !(originalTabURL && this.tab.url && originalTabURL === this.tab.url);
+    const currentTabURL = this.tab.url?.length && new URL(this.tab.url);
+
+    const originalTabHostPath =
+      originalTabURL && `${originalTabURL.origin}${originalTabURL.pathname}`;
+    const currentTabHostPath = currentTabURL && `${currentTabURL.origin}${currentTabURL.pathname}`;
+
+    const tabUrlChanged = originalTabHostPath !== currentTabHostPath;
 
     if (this.pageDetails == null || this.pageDetails.length === 0 || tabUrlChanged) {
       this.platformUtilsService.showToast("error", null, this.i18nService.t("autofillError"));
