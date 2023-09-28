ee81c9feef87fa2f6b169f6f08088d3a558d9b88
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2023-09-28 12:51:50 -0400

    Will's suggestions

--- bitwarden_license/bit-web/src/app/secrets-manager/shared/org-suspended.component.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/shared/org-suspended.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Subject, takeUntil } from "rxjs";
+import { map } from "rxjs";
 
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { Icon, Icons } from "@bitwarden/components";
@@ -8,21 +8,11 @@ import { Icon, Icons } from "@bitwarden/components";
 @Component({
   templateUrl: "./org-suspended.component.html",
 })
-export class OrgSuspendedComponent implements OnInit, OnDestroy {
+export class OrgSuspendedComponent {
   constructor(private organizationService: OrganizationService, private route: ActivatedRoute) {}
 
-  protected organizationName: string;
   protected NoAccess: Icon = Icons.NoAccess;
-  private destroy$ = new Subject<void>();
-
-  ngOnInit() {
-    this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
-      this.organizationName = this.organizationService.get(params.organizationId)?.name;
-    });
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+  protected organizationName$ = this.route.params.pipe(
+    map((params) => this.organizationService.get(params.organizationId)?.name)
+  );
 }
