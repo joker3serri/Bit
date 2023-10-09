2ecd710ab1a1c47e45dff984a8f8a441e55c646a
Andreas Coroiu <andreas.coroiu@gmail.com>
2023-10-09 16:49:04 +0200

    [PM-2014] fix: unawaited promise bug

--- apps/web/src/app/auth/settings/webauthn-login-settings/create-credential-dialog/create-credential-dialog.component.ts
+++ apps/web/src/app/auth/settings/webauthn-login-settings/create-credential-dialog/create-credential-dialog.component.ts
@@ -144,7 +144,7 @@ export class CreateCredentialDialogComponent implements OnInit {
       return;
     }
 
-    if (firstValueFrom(this.hasPasskeys$)) {
+    if (await firstValueFrom(this.hasPasskeys$)) {
       this.platformUtilsService.showToast(
         "success",
         null,
