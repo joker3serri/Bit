97247a91ec759eccc0a42d8b1b2b6dcad66f8830
Daniel James Smith <djsmith85@users.noreply.github.com>
2023-10-19 17:34:56 +0200

    Casing fixes from the original port of the code

--- libs/importer/src/importers/lastpass/access/models/user-type-context.ts
+++ libs/importer/src/importers/lastpass/access/models/user-type-context.ts
@@ -2,21 +2,21 @@ import { IdpProvider, LastpassLoginType } from "../enums";
 
 export class UserTypeContext {
   type: LastpassLoginType;
-  IdentityProviderGUID: string;
-  IdentityProviderURL: string;
-  OpenIDConnectAuthority: string;
-  OpenIDConnectClientId: string;
-  CompanyId: number;
-  Provider: IdpProvider;
-  PkceEnabled: boolean;
-  IsPasswordlessEnabled: boolean;
+  identityProviderGUID: string;
+  identityProviderURL: string;
+  openIDConnectAuthority: string;
+  openIDConnectClientId: string;
+  companyId: number;
+  provider: IdpProvider;
+  pkceEnabled: boolean;
+  isPasswordlessEnabled: boolean;
 
   isFederated(): boolean {
     return (
       this.type === LastpassLoginType.Federated &&
-      this.hasValue(this.IdentityProviderURL) &&
-      this.hasValue(this.OpenIDConnectAuthority) &&
-      this.hasValue(this.OpenIDConnectClientId)
+      this.hasValue(this.identityProviderURL) &&
+      this.hasValue(this.openIDConnectAuthority) &&
+      this.hasValue(this.openIDConnectClientId)
     );
   }
 
