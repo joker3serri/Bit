8f364860de24824e6fe376a1ae539921d908ed7a
Matt Gibson <mgibson@bitwarden.com>
2023-10-20 14:13:35 -0400

    Allow for disabling header theming for login page
    visual gitches exist for links and buttons, due to specifications futher down in the header, but those
    items shouldn't use the `no-theme` option. For now, it's just for the
    login screen

--- apps/browser/src/platform/popup/header.component.ts
+++ apps/browser/src/platform/popup/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Observable, map } from "rxjs";
 
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
@@ -11,6 +11,7 @@ import { flagEnabled } from "../flags";
   templateUrl: "header.component.html",
 })
 export class HeaderComponent {
+  @Input() noTheme = false;
   authedAccounts$: Observable<boolean>;
   constructor(accountService: AccountService) {
     this.authedAccounts$ = accountService.accounts$.pipe(
