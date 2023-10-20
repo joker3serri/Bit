8115f544017fef493323f3d7b0ca2021cca926c6
Kyle Spearrin <kyle.spearrin@gmail.com>
2023-10-20 15:31:59 -0400

    lastpassAuthResult for web sso connector

--- apps/browser/src/autofill/content/message_handler.ts
+++ apps/browser/src/autofill/content/message_handler.ts
@@ -6,12 +6,21 @@ window.addEventListener(
     }
 
     if (event.data.command && event.data.command === "authResult") {
-      chrome.runtime.sendMessage({
-        command: event.data.command,
-        code: event.data.code,
-        state: event.data.state,
-        referrer: event.source.location.hostname,
-      });
+      if (event.data.lastpass) {
+        chrome.runtime.sendMessage({
+          command: "lastpassAuthResult",
+          code: event.data.code,
+          state: event.data.state,
+          referrer: event.source.location.hostname,
+        });
+      } else {
+        chrome.runtime.sendMessage({
+          command: event.data.command,
+          code: event.data.code,
+          state: event.data.state,
+          referrer: event.source.location.hostname,
+        });
+      }
     }
 
     if (event.data.command && event.data.command === "webAuthnResult") {
