4cd4f24f119d4a1d636bcca0abc535e026ea7de6
William Martin <contact@willmartian.com>
2023-10-20 18:29:56 -0400

    update createClientInfo

--- libs/importer/src/components/lastpass/lastpass-direct-import.service.ts
+++ libs/importer/src/components/lastpass/lastpass-direct-import.service.ts
@@ -168,11 +168,10 @@ export class LastPassDirectImportService {
   }
 
   private async createClientInfo(email: string): Promise<ClientInfo> {
-    const clientInfo = ClientInfo.createClientInfo();
     const appId = await this.appIdService.getAppId();
     const id = appId + email;
     const idHash = await this.cryptoFunctionService.hash(id, "sha256");
-    clientInfo.id = Utils.fromBufferToHex(idHash);
+    const clientInfo = ClientInfo.createClientInfo(Utils.fromBufferToHex(idHash));
     return clientInfo;
   }
 }
