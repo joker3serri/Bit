13f5a28b1dafcba5e3472b78595a708cc4daec45
Jared Snider <jsnider@bitwarden.com>
2023-11-15 16:29:02 -0500

    PM-2401 - Per PR feedback, provide translated message to cover all invalid passkey scenarios.

--- libs/angular/src/auth/components/base-login-via-webauthn.component.ts
+++ libs/angular/src/auth/components/base-login-via-webauthn.component.ts
@@ -5,6 +5,7 @@ import { WebAuthnLoginServiceAbstraction } from "@bitwarden/common/auth/abstract
 import { ForceSetPasswordReason } from "@bitwarden/common/auth/models/domain/force-set-password-reason";
 import { WebAuthnLoginCredentialAssertionView } from "@bitwarden/common/auth/models/view/webauthn-login/webauthn-login-credential-assertion.view";
 import { ErrorResponse } from "@bitwarden/common/models/response/error.response";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { ValidationService } from "@bitwarden/common/platform/abstractions/validation.service";
 
@@ -22,7 +23,8 @@ export class BaseLoginViaWebAuthnComponent implements OnInit {
     private webAuthnLoginService: WebAuthnLoginServiceAbstraction,
     private router: Router,
     private logService: LogService,
-    private validationService: ValidationService
+    private validationService: ValidationService,
+    private i18nService: I18nService
   ) {}
 
   ngOnInit(): void {
@@ -59,7 +61,7 @@ export class BaseLoginViaWebAuthnComponent implements OnInit {
       }
     } catch (error) {
       if (error instanceof ErrorResponse) {
-        this.validationService.showError(error);
+        this.validationService.showError(this.i18nService.t("invalidPasskeyPleaseTryAgain"));
       }
       this.logService.error(error);
       this.currentState = "assertFailed";
