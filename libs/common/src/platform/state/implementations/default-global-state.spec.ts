c2a5915668d0a02175e92114bc561e4936aac393
Matt Gibson <mgibson@bitwarden.com>
2023-12-06 14:08:34 -0500

    Handle subscription race conditions

--- libs/common/src/platform/state/implementations/default-global-state.spec.ts
+++ libs/common/src/platform/state/implementations/default-global-state.spec.ts
@@ -3,7 +3,7 @@
  * @jest-environment ../shared/test.environment.ts
  */
 
-import { firstValueFrom, of } from "rxjs";
+import { firstValueFrom, of, timeout } from "rxjs";
 import { Jsonify } from "type-fest";
 
 import { trackEmissions, awaitAsync } from "../../../../spec";
@@ -79,6 +79,19 @@ describe("DefaultGlobalState", () => {
       expect(diskStorageService.mock.get).toHaveBeenCalledWith("global_fake_fake", undefined);
       expect(state).toBeTruthy();
     });
+
+    it("should not emit twice if there are two listeners", async () => {
+      const emissions = trackEmissions(globalState.state$);
+      const emissions2 = trackEmissions(globalState.state$);
+      await awaitAsync();
+
+      expect(emissions).toEqual([
+        null, // Initial value
+      ]);
+      expect(emissions2).toEqual([
+        null, // Initial value
+      ]);
+    });
   });
 
   describe("update", () => {
@@ -133,6 +146,7 @@ describe("DefaultGlobalState", () => {
 
     it("should not update if shouldUpdate returns false", async () => {
       const emissions = trackEmissions(globalState.state$);
+      await awaitAsync(); // storage updates are behind a promise
 
       const result = await globalState.update(
         (state) => {
@@ -196,6 +210,61 @@ describe("DefaultGlobalState", () => {
       expect(actual).toEqual(newState);
       expect(emissions).toHaveLength(2);
       expect(emissions).toEqual(expect.arrayContaining([initialState, newState]));
+    });
+  });
+
+  describe("update races", () => {
+    it("subscriptions during an update should not emit until update is complete", async () => {
+      // Seed with interesting data
+      const initialData = { date: new Date(2020, 1, 1) };
+      await globalState.update((state, dependencies) => {
+        return initialData;
+      });
+
+      await awaitAsync();
+
+      const emissions = trackEmissions(globalState.state$);
+      await awaitAsync();
+      expect(emissions).toEqual([initialData]);
+
+      const originalSave = diskStorageService.save.bind(diskStorageService);
+      diskStorageService.save = jest.fn().mockImplementation(async (key: string, obj: any) => {
+        await expect(() =>
+          firstValueFrom(globalState.state$.pipe(timeout(100))),
+        ).rejects.toThrowError();
+        await originalSave(key, obj);
+      });
+
+      const val = await globalState.update((state) => {
+        return newData;
+      });
+
+      await awaitAsync();
+
+      expect(val).toEqual(newData);
+      expect(emissions).toEqual([initialData, newData]);
+    });
+
+    it("updates should should wait until previous update is complete", async () => {
+      trackEmissions(globalState.state$);
+      await awaitAsync(); // storage updates are behind a promise
+
+      diskStorageService.save = jest.fn().mockImplementation(async (key: string, obj: any) => {
+        let resolved = false;
+        await Promise.race([
+          globalState.update(() => {
+            // deadlocks
+            resolved = true;
+            return newData;
+          }),
+          awaitAsync(100), // limit test to 100ms
+        ]);
+        expect(resolved).toBe(false);
+      });
+
+      await globalState.update((state) => {
+        return newData;
+      });
     });
   });
 });
