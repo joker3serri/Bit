dc9cb08bf4accbe29f103586870bd96787e5fd52
William Martin <contact@willmartian.com>
2023-12-14 00:15:21 -0500

    update route and focus logic

--- libs/components/src/layout/layout.component.ts
+++ libs/components/src/layout/layout.component.ts
@@ -1,16 +1,23 @@
 import { Component, Input } from "@angular/core";
-
-export type LayoutVariant = "primary" | "secondary";
+import { RouterModule } from "@angular/router";
 
 import { LinkModule } from "../link";
 import { SharedModule } from "../shared";
+
+export type LayoutVariant = "primary" | "secondary";
 
 @Component({
   selector: "bit-layout",
   templateUrl: "layout.component.html",
   standalone: true,
-  imports: [SharedModule, LinkModule],
+  imports: [SharedModule, LinkModule, RouterModule],
 })
 export class LayoutComponent {
+  protected mainContentId = "main-content";
+
   @Input() variant: LayoutVariant = "primary";
+
+  focusMainContent() {
+    document.getElementById(this.mainContentId)?.focus();
+  }
 }
