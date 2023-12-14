3a23e25fca6232a2e95e91fc5c9bed531501b2e0
William Martin <contact@willmartian.com>
2023-12-13 21:15:17 -0500

    add skip link to bit-layout; update i18n for all apps

--- libs/components/src/layout/layout.component.ts
+++ libs/components/src/layout/layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 
+import { LinkModule } from "../link";
+import { SharedModule } from "../shared";
+
 @Component({
   selector: "bit-layout",
   templateUrl: "layout.component.html",
   standalone: true,
-  imports: [],
+  imports: [SharedModule, LinkModule],
 })
 export class LayoutComponent {}
