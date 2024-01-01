51c1ccb85d8f5d994000d6ebf6952173fdcafab8
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-01-01 16:23:43 -0500

    Prettier

--- libs/common/src/platform/state/implementations/default-active-user-state.ts
+++ libs/common/src/platform/state/implementations/default-active-user-state.ts
@@ -90,7 +90,7 @@ export class DefaultActiveUserState<T> implements ActiveUserState<T> {
       filter(([storageUpdate, [_userId, fullKey]]) => storageUpdate.key === fullKey),
       switchMap(async ([storageUpdate, [userId, fullKey]]) => {
         // We can shortcut on updateType of "remove"
-        // and just emit null. 
+        // and just emit null.
         if (storageUpdate.updateType === "remove") {
           return [userId, null] as CombinedState<T>;
         }
