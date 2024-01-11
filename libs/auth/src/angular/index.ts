16f8560370cddf34137e7e98fb84ce0febf62f16
Jared Snider <jsnider@bitwarden.com>
2024-01-11 16:32:33 -0500

    PM-5242 - Add new user verification biometrics fingerprint icon with proper secondary fill so it displays properly on all themes.

--- libs/auth/src/angular/index.ts
+++ libs/auth/src/angular/index.ts
@@ -1,6 +1,10 @@
 /**
  * This barrel file should only contain Angular exports
  */
+
+// icons
+export * from "./icons";
+
 export * from "./fingerprint-dialog/fingerprint-dialog.component";
 export * from "./password-callout/password-callout.component";
 
