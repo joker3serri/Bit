fc857f3838b1ec257057339ef0838ad3e650791d
Matt Gibson <mgibson@bitwarden.com>
2024-01-15 13:12:38 -0500

    Require cryptoService
    This is temporary until cryptoService has an observable active user private key. We don't want promise-based values in derive functions

--- libs/common/src/platform/services/key-state/provider-keys.state.spec.ts
+++ libs/common/src/platform/services/key-state/provider-keys.state.spec.ts
@@ -1,12 +1,14 @@
 import { mock } from "jest-mock-extended";
 
 import { makeStaticByteArray } from "../../../../spec";
+import { ProviderId } from "../../../types/guid";
 import { ProviderKey, UserPrivateKey } from "../../../types/key";
 import { EncryptService } from "../../abstractions/encrypt.service";
 import { EncryptionType } from "../../enums";
 import { Utils } from "../../misc/utils";
-import { EncString } from "../../models/domain/enc-string";
+import { EncString, EncryptedString } from "../../models/domain/enc-string";
 import { SymmetricCryptoKey } from "../../models/domain/symmetric-crypto-key";
+import { CryptoService } from "../crypto.service";
 
 import { USER_ENCRYPTED_PROVIDER_KEYS, USER_PROVIDER_KEYS } from "./provider-keys.state";
 
@@ -32,6 +34,7 @@ describe("encrypted provider keys", () => {
 
 describe("derived decrypted provider keys", () => {
   const encryptService = mock<EncryptService>();
+  const cryptoService = mock<CryptoService>();
   const userPrivateKey = makeStaticByteArray(64, 0) as UserPrivateKey;
   const sut = USER_PROVIDER_KEYS;
 
@@ -63,9 +66,18 @@ describe("derived decrypted provider keys", () => {
 
     encryptService.rsaDecrypt.mockResolvedValueOnce(decryptedProviderKeys["provider-id-1"].key);
     encryptService.rsaDecrypt.mockResolvedValueOnce(decryptedProviderKeys["provider-id-2"].key);
+    cryptoService.getPrivateKey.mockResolvedValueOnce(userPrivateKey);
 
-    const result = await sut.derive(encryptedProviderKeys, { encryptService, userPrivateKey });
+    const result = await sut.derive(encryptedProviderKeys, { encryptService, cryptoService });
 
     expect(result).toEqual(decryptedProviderKeys);
+  });
+
+  it("should handle null input values", async () => {
+    const encryptedProviderKeys: Record<ProviderId, EncryptedString> = null;
+
+    const result = await sut.derive(encryptedProviderKeys, { encryptService, cryptoService });
+
+    expect(result).toEqual({});
   });
 });
--- libs/common/src/platform/services/key-state/provider-keys.state.ts
+++ libs/common/src/platform/services/key-state/provider-keys.state.ts
@@ -1,9 +1,10 @@
 import { ProviderId } from "../../../types/guid";
-import { ProviderKey, UserPrivateKey } from "../../../types/key";
+import { ProviderKey } from "../../../types/key";
 import { EncryptService } from "../../abstractions/encrypt.service";
 import { EncString, EncryptedString } from "../../models/domain/enc-string";
 import { SymmetricCryptoKey } from "../../models/domain/symmetric-crypto-key";
 import { KeyDefinition, CRYPTO_DISK, DeriveDefinition } from "../../state";
+import { CryptoService } from "../crypto.service";
 
 export const USER_ENCRYPTED_PROVIDER_KEYS = KeyDefinition.record<EncryptedString, ProviderId>(
   CRYPTO_DISK,
@@ -16,7 +17,7 @@ export const USER_ENCRYPTED_PROVIDER_KEYS = KeyDefinition.record<EncryptedString
 export const USER_PROVIDER_KEYS = DeriveDefinition.from<
   Record<ProviderId, EncryptedString>,
   Record<ProviderId, ProviderKey>,
-  { encryptService: EncryptService; userPrivateKey: UserPrivateKey }
+  { encryptService: EncryptService; cryptoService: CryptoService } // TODO: This should depend on an active user private key observable directly
 >(USER_ENCRYPTED_PROVIDER_KEYS, {
   deserializer: (obj) => {
     const result: Record<ProviderId, ProviderKey> = {};
@@ -25,14 +26,15 @@ export const USER_PROVIDER_KEYS = DeriveDefinition.from<
     }
     return result;
   },
-  derive: async (from, { encryptService, userPrivateKey }) => {
+  derive: async (from, { encryptService, cryptoService }) => {
     const result: Record<ProviderId, ProviderKey> = {};
     for (const providerId of Object.keys(from ?? {}) as ProviderId[]) {
       if (result[providerId] != null) {
         continue;
       }
       const encrypted = new EncString(from[providerId]);
-      const decrypted = await encryptService.rsaDecrypt(encrypted, userPrivateKey);
+      const privateKey = await cryptoService.getPrivateKey();
+      const decrypted = await encryptService.rsaDecrypt(encrypted, privateKey);
       const providerKey = new SymmetricCryptoKey(decrypted) as ProviderKey;
 
       result[providerId] = providerKey;
