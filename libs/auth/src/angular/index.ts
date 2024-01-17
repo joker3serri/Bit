0983b347a664263410fe4b2d56544e86407a92cc
Jared Snider <jsnider@bitwarden.com>
2024-01-17 18:06:59 -0500

    PM-5242 - Update auth index to export user-verification-dialog.types

--- libs/auth/src/angular/index.ts
+++ libs/auth/src/angular/index.ts
@@ -10,4 +10,5 @@ export * from "./password-callout/password-callout.component";
 
 // user verification
 export * from "./user-verification/user-verification-dialog.component";
+export * from "./user-verification/user-verification-dialog.types";
 export * from "./user-verification/user-verification-form-input.component";
