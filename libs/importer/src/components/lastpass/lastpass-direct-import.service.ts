6994ac7f2a8388e5c07b8f2d781b935470cfba4b
Tom <ttalty@bitwarden.com>
2024-01-18 10:17:38 -0500

    Initial implementation of simple dialog

--- libs/importer/src/components/lastpass/lastpass-direct-import.service.ts
+++ libs/importer/src/components/lastpass/lastpass-direct-import.service.ts
@@ -8,15 +8,15 @@ import { AppIdService } from "@bitwarden/common/platform/abstractions/app-id.ser
 import { BroadcasterService } from "@bitwarden/common/platform/abstractions/broadcaster.service";
 import { CryptoFunctionService } from "@bitwarden/common/platform/abstractions/crypto-function.service";
 import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { PasswordGenerationServiceAbstraction } from "@bitwarden/common/tools/generator/password";
 
-import { DialogService } from "../../../../components/src/dialog";
+import { DialogService, SimpleDialogOptions } from "../../../../components/src/dialog";
 import { ClientInfo, Vault } from "../../importers/lastpass/access";
 import { FederatedUserContext } from "../../importers/lastpass/access/models";
 
-import { LastPassAwaitSSODialogComponent } from "./dialog/lastpass-await-sso-dialog.component";
 import { LastPassPasswordPromptComponent } from "./dialog/lastpass-password-prompt.component";
 import { LastPassDirectImportUIService } from "./lastpass-direct-import-ui.service";
 
@@ -42,6 +42,7 @@ export class LastPassDirectImportService {
     private broadcasterService: BroadcasterService,
     private ngZone: NgZone,
     private dialogService: DialogService,
+    private i18nService: I18nService,
   ) {
     this.vault = new Vault(this.cryptoFunctionService, this.tokenService);
 
@@ -96,7 +97,13 @@ export class LastPassDirectImportService {
     const request = await this.createOidcSigninRequest(email);
     this.platformUtilsService.launchUri(request.url);
 
-    const cancelDialogRef = LastPassAwaitSSODialogComponent.open(this.dialogService);
+    const cancelDialogOpts: SimpleDialogOptions = {
+      title: this.i18nService.t("awaitingSSO"),
+      content: this.i18nService.t("awaitingSSODesc"),
+      type: "warning",
+      cancelButtonText: this.i18nService.t("cancel"),
+    };
+    const cancelDialogRef = this.dialogService.openSimpleDialogRef(cancelDialogOpts);
     const cancelled = firstValueFrom(cancelDialogRef.closed).then((_didCancel) => {
       throw Error("SSO auth cancelled");
     });
