be9ea10256780b69927e3227cd2a92ff328ed26a
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-01-24 19:01:05 -0500

    100% code coverage

--- libs/common/src/tools/generator/username/forwarders/forward-email.spec.ts
+++ libs/common/src/tools/generator/username/forwarders/forward-email.spec.ts
@@ -152,28 +152,31 @@ describe("ForwardEmail Forwarder", () => {
       );
     });
 
-    it("throws an unknown error if the request fails and no status is provided", async () => {
-      const apiService = mockApiService(500, {});
-      const i18nService = mockI18nService();
+    it.each([{}, null])(
+      "throws an unknown error if the request fails and no status (= %p) is provided",
+      async (json) => {
+        const apiService = mockApiService(500, json);
+        const i18nService = mockI18nService();
 
-      const forwarder = new ForwardEmailForwarder(apiService, i18nService);
+        const forwarder = new ForwardEmailForwarder(apiService, i18nService);
 
-      await expect(
-        async () =>
-          await forwarder.generate(null, {
-            token: "token",
-            domain: "example.com",
-          }),
-      ).rejects.toEqual("forwarderUnknownError");
+        await expect(
+          async () =>
+            await forwarder.generate(null, {
+              token: "token",
+              domain: "example.com",
+            }),
+        ).rejects.toEqual("forwarderUnknownError");
 
-      expect(apiService.nativeFetch).toHaveBeenCalledWith(expect.any(Request));
-      // counting instances is terribly flaky over changes, but jest doesn't have a better way to do this
-      expect(i18nService.t).toHaveBeenNthCalledWith(
-        2,
-        "forwarderUnknownError",
-        Forwarders.ForwardEmail.name,
-      );
-    });
+        expect(apiService.nativeFetch).toHaveBeenCalledWith(expect.any(Request));
+        // counting instances is terribly flaky over changes, but jest doesn't have a better way to do this
+        expect(i18nService.t).toHaveBeenNthCalledWith(
+          2,
+          "forwarderUnknownError",
+          Forwarders.ForwardEmail.name,
+        );
+      },
+    );
 
     it.each([
       [100, "Continue"],
