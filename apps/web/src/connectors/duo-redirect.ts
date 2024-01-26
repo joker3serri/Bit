56abc102cc7af06127a5024cf83fddf0c2df2dc4
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-01-25 16:59:57 -0800

    add countdown timer if a number is provided in duoHandOffMessage

--- apps/web/src/connectors/duo-redirect.ts
+++ apps/web/src/connectors/duo-redirect.ts
@@ -14,23 +14,58 @@ window.addEventListener("load", () => {
     channel.postMessage({ code: code });
     channel.close();
 
-    const handOffMessage = ("; " + document.cookie)
-      .split("; duoHandOffMessage=")
-      .pop()
-      .split(";")
-      .shift();
-
-    document.cookie = "duoHandOffMessage=;SameSite=strict;max-age=0";
-
-    const content = document.getElementById("content");
-    content.innerHTML = "";
-
-    const p = document.createElement("p");
-    p.className = "text-center";
-    p.innerText = handOffMessage;
-
-    content.appendChild(p);
+    handleMessage();
   } else if (client === "mobile" || client === "desktop") {
     document.location.replace(mobileDesktopCallback + "?code=" + encodeURIComponent(code));
   }
 });
+
+const handleMessage = () => {
+  const handOffMessage = ("; " + document.cookie)
+    .split("; duoHandOffMessage=")
+    .pop()
+    .split(";")
+    .shift();
+
+  document.cookie = "duoHandOffMessage=;SameSite=strict;max-age=0";
+
+  const content = document.getElementById("content");
+  content.className = "text-center";
+  content.innerHTML = "";
+
+  const h1 = document.createElement("h1");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+
+  h1.textContent = /<h1>(.*?)<\/h1>/g.exec(handOffMessage)[1]; // parse text from <h1> tag
+  p.textContent = /<p>(.*?)<\/p>/g.exec(handOffMessage)[1];
+  button.textContent = /<button>(.*?)<\/button>/g.exec(handOffMessage)[1];
+
+  h1.className = "font-weight-semibold";
+  p.className = "mb-4";
+  button.className = "bg-primary text-white border-0 rounded py-2 px-3";
+
+  button.addEventListener("click", () => {
+    window.close();
+  });
+
+  content.appendChild(h1);
+  content.appendChild(p);
+  content.appendChild(button);
+
+  let num = Number(p.textContent.match(/\d+/)[0]); // parse digit from string
+
+  // Countdown timer
+  if (num) {
+    setInterval(() => {
+      if (num > 1) {
+        p.textContent = p.textContent.replace(String(num), String(num - 1));
+        num--;
+      }
+    }, 1000);
+
+    setTimeout(() => {
+      window.close();
+    }, num * 1000);
+  }
+};
