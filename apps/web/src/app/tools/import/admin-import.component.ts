40aa22aa16ac42f4a8336251968cbb16713a664e
Andre Rosado <arosado@bitwarden.com>
2024-01-29 13:21:40 +0000

    Reverting AC-2095

--- apps/web/src/app/tools/import/admin-import.component.ts
+++ apps/web/src/app/tools/import/admin-import.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
 
-import { ApiService } from "@bitwarden/common/abstractions/api.service";
-import { CollectionService } from "@bitwarden/common/vault/abstractions/collection.service";
 import { ImportCollectionServiceAbstraction } from "@bitwarden/importer/core";
 import { ImportComponent } from "@bitwarden/importer/ui";
 
 import { SharedModule } from "../../shared";
+import { CollectionAdminService } from "../../vault/core/collection-admin.service";
 
 import { ImportCollectionAdminService } from "./import-collection-admin.service";
 import { ImportWebComponent } from "./import-web.component";
@@ -18,7 +17,7 @@ import { ImportWebComponent } from "./import-web.component";
     {
       provide: ImportCollectionServiceAbstraction,
       useClass: ImportCollectionAdminService,
-      deps: [CollectionService, ApiService],
+      deps: [CollectionAdminService],
     },
   ],
 })
--- apps/web/src/app/tools/import/import-collection-admin.service.ts
+++ apps/web/src/app/tools/import/import-collection-admin.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from "@angular/core";
 
-import { ApiService } from "@bitwarden/common/abstractions/api.service";
-import { CollectionService } from "@bitwarden/common/vault/abstractions/collection.service";
-import { CollectionData } from "@bitwarden/common/vault/models/data/collection.data";
-import { Collection } from "@bitwarden/common/vault/models/domain/collection";
-import { CollectionDetailsResponse } from "@bitwarden/common/vault/models/response/collection.response";
-import { CollectionView } from "@bitwarden/common/vault/models/view/collection.view";
-
 import { ImportCollectionServiceAbstraction } from "../../../../../../libs/importer/src/services/import-collection.service.abstraction";
+import { CollectionAdminService } from "../../vault/core/collection-admin.service";
+import { CollectionAdminView } from "../../vault/core/views/collection-admin.view";
 
 @Injectable()
 export class ImportCollectionAdminService implements ImportCollectionServiceAbstraction {
-  constructor(
-    private collectionService: CollectionService,
-    private apiService: ApiService,
-  ) {}
+  constructor(private collectionAdminService: CollectionAdminService) {}
 
-  async getAllAdminCollections(organizationId: string): Promise<CollectionView[]> {
-    const response = await this.apiService.getCollections(organizationId);
-    const collections = response.data
-      .filter((c) => c.organizationId === organizationId)
-      .map((r) => new Collection(new CollectionData(r as CollectionDetailsResponse)));
-    return await this.collectionService.decryptMany(collections);
+  async getAllAdminCollections(organizationId: string): Promise<CollectionAdminView[]> {
+    return await this.collectionAdminService.getAll(organizationId);
   }
 }
