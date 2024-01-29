7a4cf92d9b6dd6bf24747636b5d073c2111c098e
Jared Snider <jsnider@bitwarden.com>
2024-01-29 16:27:06 -0500

    PM-5368 - Base comp + web changes - (1)  Base component now has a setupDuoResultListener method for child classes to override (2) Web overrides setupDuoResultListener and cleans up broadcast channel once a duo result comes through.

--- libs/angular/src/auth/components/two-factor.component.ts
+++ libs/angular/src/auth/components/two-factor.component.ts
@@ -157,24 +157,8 @@ export class TwoFactorComponent extends CaptchaProtectedComponent implements OnI
           this.duoFrameless = true;
           // Setup listener for duo-redirect.ts connector to send back the code
 
-          // TODO: figure out how web is going to communicate with browser.
-          // BroadcastChannel allows communication ON THE SAME ORIGIN.
-          // https://developer.mozilla.org/en-US/docs/Web/API/Broadcast_Channel_API
-
-          // Web app:
-          // console.log(window.location.origin)
-          // VM629:1 https://localhost:8080
-
-          // Extension:
-          // console.log(window.location.origin)
-          // VM39:1 chrome-extension://ffjngkoaegfhikcaianndlcgafgejahp
-
-          // Probably will have to use the messaging API to communicate with the extension.
-
-          new BroadcastChannel("duoResult").addEventListener("message", async (e) => {
-            this.token = e.data.code;
-            await this.submit();
-          });
+          // setup client specific duo result listener
+          this.setupDuoResultListener();
 
           // flow must be launched by user so they can choose to remember the device or not.
           this.duoFramelessUrl = providerData.AuthUrl;
@@ -264,6 +248,9 @@ export class TwoFactorComponent extends CaptchaProtectedComponent implements OnI
     );
     return true;
   }
+
+  // Each client will have own implementation
+  protected setupDuoResultListener(): void {}
 
   private async handleLoginResponse(authResult: AuthResult) {
     if (this.handleCaptchaRequired(authResult)) {
