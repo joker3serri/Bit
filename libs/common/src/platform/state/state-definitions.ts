ccac7fd7d766dc9301d9449b3e70038a82ee03fd
gbubemismith <gsmithwalter@gmail.com>
2024-01-30 10:56:15 -0500

    renamed the state definition

--- libs/common/src/platform/state/state-definitions.ts
+++ libs/common/src/platform/state/state-definitions.ts
@@ -27,4 +27,4 @@ export const ENVIRONMENT_DISK = new StateDefinition("environment", "disk");
 export const GENERATOR_DISK = new StateDefinition("generator", "disk");
 export const GENERATOR_MEMORY = new StateDefinition("generator", "memory");
 
-export const PASSKEYS_DISK = new StateDefinition("passkeys", "disk");
+export const FIDO2_DISK = new StateDefinition("fido2", "disk");
