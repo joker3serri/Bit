faa656463f5ab51b4c67d0b8cb8a7ec7420f1f7c
Thomas Rittson <trittson@bitwarden.com>
2024-02-05 14:08:52 +1000

    Delete policies memory StateDefinition

--- libs/common/src/platform/state/state-definitions.ts
+++ libs/common/src/platform/state/state-definitions.ts
@@ -30,6 +30,5 @@ export const GENERATOR_MEMORY = new StateDefinition("generator", "memory");
 // Admin Console
 export const ORGANIZATIONS_DISK = new StateDefinition("organizations", "disk");
 export const POLICIES_DISK = new StateDefinition("policies", "disk");
-export const POLICIES_MEMORY = new StateDefinition("policies", "memory");
 export const PROVIDERS_DISK = new StateDefinition("providers", "disk");
 //
