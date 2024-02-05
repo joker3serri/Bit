383b304baf487fffcd79b13c331cf0e87a8bf8d4
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-02-05 02:20:17 -0800

    implement migrate() method of Migrator

---
+++ libs/common/src/state-migrations/migrations/14-move-remembered-email-to-state-providers.ts
@@ -0,0 +1,29 @@
+import { KeyDefinitionLike, MigrationHelper, StateDefinitionLike } from "../migration-helper";
+import { Migrator } from "../migrator";
+
+type ExpectedGlobalState = {
+  rememberedEmail: string;
+};
+
+const LOGIN_STATE: StateDefinitionLike = {
+  name: "login",
+};
+
+const REMEMBERED_EMAIL: KeyDefinitionLike = {
+  key: "rememberedEmail",
+  stateDefinition: LOGIN_STATE,
+};
+
+export class RememberedEmailMigrator extends Migrator<13, 14> {
+  async migrate(helper: MigrationHelper): Promise<void> {
+    const legacyGlobal = await helper.get<ExpectedGlobalState>("global");
+
+    if (legacyGlobal?.rememberedEmail != null) {
+      await helper.setToGlobal(REMEMBERED_EMAIL, legacyGlobal.rememberedEmail);
+    }
+  }
+
+  async rollback(helper: MigrationHelper): Promise<void> {
+    // TODO: implement method
+  }
+}
