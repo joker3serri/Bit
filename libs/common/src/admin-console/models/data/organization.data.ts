432ca295cd05d0daa241bee28242af062bc48066
Addison Beck <hello@addisonbeck.com>
2024-02-05 08:31:14 -0600

    Rework null check on OrganizationData

--- libs/common/src/admin-console/models/data/organization.data.ts
+++ libs/common/src/admin-console/models/data/organization.data.ts
@@ -56,64 +56,68 @@ export class OrganizationData {
   flexibleCollections: boolean;
 
   constructor(
-    response: Partial<ProfileOrganizationResponse>,
-    options: {
+    response?: ProfileOrganizationResponse,
+    options?: {
       isMember: boolean;
       isProviderUser: boolean;
     },
   ) {
-    this.id = response?.id;
-    this.name = response?.name;
-    this.status = response?.status;
-    this.type = response?.type;
-    this.enabled = response?.enabled;
-    this.usePolicies = response?.usePolicies;
-    this.useGroups = response?.useGroups;
-    this.useDirectory = response?.useDirectory;
-    this.useEvents = response?.useEvents;
-    this.useTotp = response?.useTotp;
-    this.use2fa = response?.use2fa;
-    this.useApi = response?.useApi;
-    this.useSso = response?.useSso;
-    this.useKeyConnector = response?.useKeyConnector;
-    this.useScim = response?.useScim;
-    this.useCustomPermissions = response?.useCustomPermissions;
-    this.useResetPassword = response?.useResetPassword;
-    this.useSecretsManager = response?.useSecretsManager;
-    this.usePasswordManager = response?.usePasswordManager;
-    this.useActivateAutofillPolicy = response?.useActivateAutofillPolicy;
-    this.selfHost = response?.selfHost;
-    this.usersGetPremium = response?.usersGetPremium;
-    this.seats = response?.seats;
-    this.maxCollections = response?.maxCollections;
-    this.maxStorageGb = response?.maxStorageGb;
-    this.ssoBound = response?.ssoBound;
-    this.identifier = response?.identifier;
-    this.permissions = response?.permissions;
-    this.resetPasswordEnrolled = response?.resetPasswordEnrolled;
-    this.userId = response?.userId;
-    this.hasPublicAndPrivateKeys = response?.hasPublicAndPrivateKeys;
-    this.providerId = response?.providerId;
-    this.providerName = response?.providerName;
-    this.providerType = response?.providerType;
-    this.familySponsorshipFriendlyName = response?.familySponsorshipFriendlyName;
-    this.familySponsorshipAvailable = response?.familySponsorshipAvailable;
-    this.planProductType = response?.planProductType;
-    this.keyConnectorEnabled = response?.keyConnectorEnabled;
-    this.keyConnectorUrl = response?.keyConnectorUrl;
-    this.familySponsorshipLastSyncDate = response?.familySponsorshipLastSyncDate;
-    this.familySponsorshipValidUntil = response?.familySponsorshipValidUntil;
-    this.familySponsorshipToDelete = response?.familySponsorshipToDelete;
-    this.accessSecretsManager = response?.accessSecretsManager;
-    this.limitCollectionCreationDeletion = response?.limitCollectionCreationDeletion;
+    if (response == null) {
+      return;
+    }
+
+    this.id = response.id;
+    this.name = response.name;
+    this.status = response.status;
+    this.type = response.type;
+    this.enabled = response.enabled;
+    this.usePolicies = response.usePolicies;
+    this.useGroups = response.useGroups;
+    this.useDirectory = response.useDirectory;
+    this.useEvents = response.useEvents;
+    this.useTotp = response.useTotp;
+    this.use2fa = response.use2fa;
+    this.useApi = response.useApi;
+    this.useSso = response.useSso;
+    this.useKeyConnector = response.useKeyConnector;
+    this.useScim = response.useScim;
+    this.useCustomPermissions = response.useCustomPermissions;
+    this.useResetPassword = response.useResetPassword;
+    this.useSecretsManager = response.useSecretsManager;
+    this.usePasswordManager = response.usePasswordManager;
+    this.useActivateAutofillPolicy = response.useActivateAutofillPolicy;
+    this.selfHost = response.selfHost;
+    this.usersGetPremium = response.usersGetPremium;
+    this.seats = response.seats;
+    this.maxCollections = response.maxCollections;
+    this.maxStorageGb = response.maxStorageGb;
+    this.ssoBound = response.ssoBound;
+    this.identifier = response.identifier;
+    this.permissions = response.permissions;
+    this.resetPasswordEnrolled = response.resetPasswordEnrolled;
+    this.userId = response.userId;
+    this.hasPublicAndPrivateKeys = response.hasPublicAndPrivateKeys;
+    this.providerId = response.providerId;
+    this.providerName = response.providerName;
+    this.providerType = response.providerType;
+    this.familySponsorshipFriendlyName = response.familySponsorshipFriendlyName;
+    this.familySponsorshipAvailable = response.familySponsorshipAvailable;
+    this.planProductType = response.planProductType;
+    this.keyConnectorEnabled = response.keyConnectorEnabled;
+    this.keyConnectorUrl = response.keyConnectorUrl;
+    this.familySponsorshipLastSyncDate = response.familySponsorshipLastSyncDate;
+    this.familySponsorshipValidUntil = response.familySponsorshipValidUntil;
+    this.familySponsorshipToDelete = response.familySponsorshipToDelete;
+    this.accessSecretsManager = response.accessSecretsManager;
+    this.limitCollectionCreationDeletion = response.limitCollectionCreationDeletion;
     this.allowAdminAccessToAllCollectionItems = response.allowAdminAccessToAllCollectionItems;
     this.flexibleCollections = response.flexibleCollections;
 
-    this.isMember = options?.isMember;
-    this.isProviderUser = options?.isProviderUser;
+    this.isMember = options.isMember;
+    this.isProviderUser = options.isProviderUser;
   }
 
   static fromJSON(obj: Jsonify<OrganizationData>) {
-    return Object.assign(new OrganizationData({}, null), obj);
+    return Object.assign(new OrganizationData(), obj);
   }
 }
