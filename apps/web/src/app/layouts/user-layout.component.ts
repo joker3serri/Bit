e1694c17363fdae64f939d341c070034bb5b3d93
Shane Melton <smelton@bitwarden.com>
2024-02-06 15:45:43 -0800

    Fix linting errors after merge with main

--- apps/web/src/app/layouts/user-layout.component.ts
+++ apps/web/src/app/layouts/user-layout.component.ts
@@ -44,10 +44,12 @@ export class UserLayoutComponent implements OnInit, OnDestroy {
     private apiService: ApiService,
   ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     document.body.classList.remove("layout_frontend");
 
     this.broadcasterService.subscribe(BroadcasterSubscriptionId, async (message: any) => {
+      // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
       this.ngZone.run(async () => {
         switch (message.command) {
           case "purchasedPremium":
@@ -58,7 +60,7 @@ export class UserLayoutComponent implements OnInit, OnDestroy {
       });
     });
 
-    this.load();
+    await this.load();
   }
 
   ngOnDestroy() {
