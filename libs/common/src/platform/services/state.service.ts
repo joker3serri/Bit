f4c692fbdcb812d7d236e52b02e5f1bbd70e4fca
Kai Naschinski <kai@naschinski.de>
2024-02-08 12:10:36 +0100

    Only GetGlobals when we actually need them
    Since we only need it as a backup if accountVaultTimeoutAction is
    null/undefined, there should be no need to call this function every time.
    
    While the overhead may seem trivial at first glance, it can add up to a
    massive increase in runtime when the function is called repeatedly in
    quick succession (e.g. when running `bw list items` where it is executed
    once for every item).
    
    In my concrete case this change leads to a 20x speedup.

--- libs/common/src/platform/services/state.service.ts
+++ libs/common/src/platform/services/state.service.ts
@@ -2525,10 +2525,14 @@ export class StateService<
     const accountVaultTimeoutAction = (
       await this.getAccount(this.reconcileOptions(options, await this.defaultOnDiskLocalOptions()))
     )?.settings?.vaultTimeoutAction;
-    const globalVaultTimeoutAction = (
-      await this.getGlobals(this.reconcileOptions(options, await this.defaultOnDiskLocalOptions()))
-    )?.vaultTimeoutAction;
-    return accountVaultTimeoutAction ?? globalVaultTimeoutAction;
+    return (
+      accountVaultTimeoutAction ??
+      (
+        await this.getGlobals(
+          this.reconcileOptions(options, await this.defaultOnDiskLocalOptions()),
+        )
+      )?.vaultTimeoutAction
+    );
   }
 
   async setVaultTimeoutAction(value: string, options?: StorageOptions): Promise<void> {
