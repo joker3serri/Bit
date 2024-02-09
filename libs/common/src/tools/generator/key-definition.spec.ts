adf96bfdacbca7fb73ac782d2af32e91c9a2ca11
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-02-09 15:12:40 -0500

    add test case

--- libs/common/src/tools/generator/key-definition.spec.ts
+++ libs/common/src/tools/generator/key-definition.spec.ts
@@ -1,6 +1,7 @@
 import {
   ENCRYPTED_HISTORY,
   EFF_USERNAME_SETTINGS,
+  CATCHALL_SETTINGS,
   PASSPHRASE_SETTINGS,
   PASSWORD_SETTINGS,
 } from "./key-definitions";
@@ -22,10 +23,18 @@ describe("Key definitions", () => {
     });
   });
 
-  describe("BASIC_LATIN_SETTINGS", () => {
+  describe("EFF_USERNAME_SETTINGS", () => {
     it("should pass through deserialization", () => {
       const value = {};
       const result = EFF_USERNAME_SETTINGS.deserializer(value);
+      expect(result).toBe(value);
+    });
+  });
+
+  describe("CATCHALL_SETTINGS", () => {
+    it("should pass through deserialization", () => {
+      const value = {};
+      const result = CATCHALL_SETTINGS.deserializer(value);
       expect(result).toBe(value);
     });
   });
