533d312d9c535cd2f0adaab4758479683866e57d
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-02-09 16:03:31 -0500

    Create MigrationRunner
    - Create MigrationRunner Service for running migrations in StateService
    - Create web override so that migrations also run against `localStorage`

--- libs/common/src/state-migrations/migrate.ts
+++ libs/common/src/state-migrations/migrate.ts
@@ -4,7 +4,6 @@ import { LogService } from "../platform/abstractions/log.service";
 import { AbstractStorageService } from "../platform/abstractions/storage.service";
 
 import { MigrationBuilder } from "./migration-builder";
-import { MigrationHelper } from "./migration-helper";
 import { EverHadUserKeyMigrator } from "./migrations/10-move-ever-had-user-key-to-state-providers";
 import { OrganizationKeyMigrator } from "./migrations/11-move-org-keys-to-state-providers";
 import { MoveEnvironmentStateToProviders } from "./migrations/12-move-environment-state-to-providers";
@@ -26,21 +25,8 @@ export const MIN_VERSION = 2;
 export const CURRENT_VERSION = 17;
 export type MinVersion = typeof MIN_VERSION;
 
-export async function migrate(
-  storageService: AbstractStorageService,
-  logService: LogService,
-): Promise<void> {
-  const migrationHelper = new MigrationHelper(
-    await currentVersion(storageService, logService),
-    storageService,
-    logService,
-  );
-  if (migrationHelper.currentVersion < 0) {
-    // Cannot determine state, assuming empty so we don't repeatedly apply a migration.
-    await storageService.save("stateVersion", CURRENT_VERSION);
-    return;
-  }
-  await MigrationBuilder.create()
+export function createMigrationBuilder() {
+  return MigrationBuilder.create()
     .with(MinVersionMigrator)
     .with(FixPremiumMigrator, 2, 3)
     .with(RemoveEverBeenUnlockedMigrator, 3, 4)
@@ -56,9 +42,7 @@ export async function migrate(
     .with(MoveBiometricClientKeyHalfToStateProviders, 13, 14)
     .with(FolderMigrator, 14, 15)
     .with(LastSyncMigrator, 15, 16)
-    .with(EnablePasskeysMigrator, 16, CURRENT_VERSION)
-
-    .migrate(migrationHelper);
+    .with(EnablePasskeysMigrator, 16, CURRENT_VERSION);
 }
 
 export async function currentVersion(
