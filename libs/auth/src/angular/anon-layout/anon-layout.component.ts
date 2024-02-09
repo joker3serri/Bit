1e128cdcee6e8c781e791f01e301acdac2d3bc72
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-02-09 11:53:42 -0800

    use svg directly

--- libs/auth/src/angular/anon-layout/anon-layout.component.ts
+++ libs/auth/src/angular/anon-layout/anon-layout.component.ts
@@ -3,13 +3,10 @@ import { Component, Input } from "@angular/core";
 
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 
-import { Icon, IconModule } from "../../../../components/src/icon";
+import { IconModule } from "../../../../components/src/icon";
 import { TypographyModule } from "../../../../components/src/typography";
 
 import { BitwardenLogo } from "./bitwarden-logo";
-import { IconLock } from "./icon-lock";
-
-type IconType = "lock";
 
 @Component({
   standalone: true,
@@ -20,24 +17,15 @@ type IconType = "lock";
 export class AnonLayoutComponent {
   @Input() title: string;
   @Input() subtitle: string;
-  @Input() icon: IconType;
+  @Input() lockIcon: boolean;
 
   protected logo = BitwardenLogo;
-  protected iconType: Icon | null;
   protected version: string;
   protected year = "2023";
 
   constructor(private platformUtilsService: PlatformUtilsService) {}
 
   async ngOnInit() {
-    switch (this.icon) {
-      case "lock":
-        this.iconType = IconLock;
-        break;
-      default:
-        this.iconType = null;
-    }
-
     this.year = new Date().getFullYear().toString();
     this.version = await this.platformUtilsService.getApplicationVersion();
   }
