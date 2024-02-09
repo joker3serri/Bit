412aff23282cfe6ae7aedf3919cf1a59f1a26ccb
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-02-09 15:10:12 -0500

    add catchall generation strategy

--- libs/common/src/tools/generator/username/index.ts
+++ libs/common/src/tools/generator/username/index.ts
@@ -1,4 +1,5 @@
 export { EffUsernameGeneratorStrategy } from "./eff-username-generator-strategy";
+export { CatchallGeneratorStrategy } from "./catchall-generator-strategy";
 export { UsernameGeneratorOptions } from "./username-generation-options";
 export { UsernameGenerationServiceAbstraction } from "./username-generation.service.abstraction";
 export { UsernameGenerationService } from "./username-generation.service";
