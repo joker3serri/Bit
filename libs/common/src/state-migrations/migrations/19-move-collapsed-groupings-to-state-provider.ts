4e82e9033ab6475922d9f2c2e248e728c4c647fe
gbubemismith <gsmithwalter@gmail.com>
2024-02-12 22:26:34 -0500

    created migration file

---
+++ libs/common/src/state-migrations/migrations/19-move-collapsed-groupings-to-state-provider.ts
@@ -0,0 +1,14 @@
+// import { KeyDefinitionLike } from "../migration-helper";
+
+// type ExpectedAccountType = {
+//   settings?: {
+//     collapsedGroupings?: string[];
+//   };
+// };
+
+// const COLLAPSED_GROUPINGS: KeyDefinitionLike = {
+//   key: "collapsedGroupings",
+//   stateDefinition: {
+//     name: "vaultFilter",
+//   },
+// };
