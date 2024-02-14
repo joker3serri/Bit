3b09c68676f22e8a98c32f8f5aa316b39eeb7c95
Thomas Rittson <trittson@bitwarden.com>
2024-02-15 09:38:12 +1000

    Update interface comments

--- libs/common/src/admin-console/abstractions/policy/policy.service.abstraction.ts
+++ libs/common/src/admin-console/abstractions/policy/policy.service.abstraction.ts
@@ -12,31 +12,31 @@ import { PolicyResponse } from "../../models/response/policy.response";
 export abstract class PolicyService {
   /**
    * All {@link Policy} objects for the active user (from sync data).
-   * May include policies that are disabled or otherwise do not apply to the user. You probably don't want this!
-   * @see {@link get$} or {@link applies$} if you want to know when a policy applies to a user.
+   * May include policies that are disabled or otherwise do not apply to the user.
+   * @see {@link get$} or {@link policyAppliesToActiveUser$} if you want to know when a policy applies to a user.
    */
   policies$: Observable<Policy[]>;
 
   /**
-   * @returns all policies of a given type that apply to the active user.
+   * @returns the first {@link Policy} found that applies to the active user.
    * A policy "applies" if it is enabled and the user is not exempt (e.g. because they are an Owner).
    * @param policyType the {@link PolicyType} to search for
+   * @see {@link getAll$} if you need all policies of a given type
    */
   get$: (policyType: PolicyType) => Observable<Policy>;
 
   /**
-   * @returns all policies of a given type that apply to the specified user.
+   * @returns all {@link Policy} objects of a given type that apply to the specified user (or the active user if not specified).
    * A policy "applies" if it is enabled and the user is not exempt (e.g. because they are an Owner).
-   * If you just want the active user, use {@link get$} instead.
    * @param policyType the {@link PolicyType} to search for
    */
   getAll$: (policyType: PolicyType, userId?: UserId) => Observable<Policy[]>;
 
   /**
-   * @returns true if the PolicyType applies to the active user, otherwise false.
+   * @returns true if a policy of the specified type applies to the active user, otherwise false.
    * A policy "applies" if it is enabled and the user is not exempt (e.g. because they are an Owner).
-   * This does not take into account the policy's configuration - if that is important, use {@link get$} to get the
-   * {@link Policy} object instad and then look at Policy.data.
+   * This does not take into account the policy's configuration - if that is important, use {@link getAll$} to get the
+   * {@link Policy} object instad and then filter by Policy.data.
    */
   policyAppliesToActiveUser$: (policyType: PolicyType) => Observable<boolean>;
 
