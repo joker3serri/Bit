0668cda10e5ac6227212402bf5e80264e420baf7
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-02-14 10:21:23 +0100

    [PM-4935] Change authenticator-attachment to platform

--- apps/browser/src/vault/fido2/webauthn-utils.ts
+++ apps/browser/src/vault/fido2/webauthn-utils.ts
@@ -57,7 +57,7 @@ export class WebauthnUtils {
       id: result.credentialId,
       rawId: Fido2Utils.stringToBuffer(result.credentialId),
       type: "public-key",
-      authenticatorAttachment: "cross-platform",
+      authenticatorAttachment: "platform",
       response: {
         clientDataJSON: Fido2Utils.stringToBuffer(result.clientDataJSON),
         attestationObject: Fido2Utils.stringToBuffer(result.attestationObject),
@@ -123,7 +123,7 @@ export class WebauthnUtils {
         userHandle: Fido2Utils.stringToBuffer(result.userHandle),
       } as AuthenticatorAssertionResponse,
       getClientExtensionResults: () => ({}),
-      authenticatorAttachment: "cross-platform",
+      authenticatorAttachment: "platform",
     } as PublicKeyCredential;
 
     // Modify prototype chains to fix `instanceof` calls.
