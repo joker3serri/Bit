e0c07b424a4163a6509f3c84053ec4ce1d3c259c
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-02-14 09:00:54 -0600

    [PM-6213] Fix Inline Autofill Menu Erasing Filled Form on Hilton.com

--- apps/browser/src/autofill/content/autofill-init.spec.ts
+++ apps/browser/src/autofill/content/autofill-init.spec.ts
@@ -208,7 +208,19 @@ describe("AutofillInit", () => {
           );
         });
 
-        it("updates the isCurrentlyFilling properties of the overlay and focus the recent field after filling", async () => {
+        it("removes the overlay when filling the form", async () => {
+          const blurAndRemoveOverlaySpy = jest.spyOn(autofillInit as any, "blurAndRemoveOverlay");
+          sendExtensionRuntimeMessage({
+            command: "fillForm",
+            fillScript,
+            pageDetailsUrl: window.location.href,
+          });
+          await flushPromises();
+
+          expect(blurAndRemoveOverlaySpy).toHaveBeenCalled();
+        });
+
+        it("updates the isCurrentlyFilling property of the overlay to true after filling", async () => {
           jest.useFakeTimers();
           jest.spyOn(autofillInit as any, "updateOverlayIsCurrentlyFilling");
           jest
