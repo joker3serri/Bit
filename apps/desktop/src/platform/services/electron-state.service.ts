e9b5bb01bd6e96c706b054bc92f253b8a0e7d2a1
Jared Snider <jsnider@bitwarden.com>
2024-02-15 17:05:43 -0500

    PM-5268 - Remove even more device key refs. *facepalm*

--- apps/desktop/src/platform/services/electron-state.service.ts
+++ apps/desktop/src/platform/services/electron-state.service.ts
@@ -1,9 +1,6 @@
-import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { GlobalState } from "@bitwarden/common/platform/models/domain/global-state";
 import { StorageOptions } from "@bitwarden/common/platform/models/domain/storage-options";
-import { SymmetricCryptoKey } from "@bitwarden/common/platform/models/domain/symmetric-crypto-key";
 import { StateService as BaseStateService } from "@bitwarden/common/platform/services/state.service";
-import { DeviceKey } from "@bitwarden/common/types/key";
 
 import { Account } from "../../models/account";
 
@@ -13,10 +10,6 @@ export class ElectronStateService
   extends BaseStateService<GlobalState, Account>
   implements ElectronStateServiceAbstraction
 {
-  private partialKeys = {
-    deviceKey: "_deviceKey",
-  };
-
   async addAccount(account: Account) {
     // Apply desktop overides to default account values
     account = new Account(account);
@@ -39,32 +32,5 @@ export class ElectronStateService
       account,
       this.reconcileOptions(options, await this.defaultOnDiskOptions()),
     );
-  }
-
-  override async getDeviceKey(options?: StorageOptions): Promise<DeviceKey | null> {
-    options = this.reconcileOptions(options, await this.defaultSecureStorageOptions());
-    if (options?.userId == null) {
-      return;
-    }
-
-    const b64DeviceKey = await this.secureStorageService.get<string>(
-      `${options.userId}${this.partialKeys.deviceKey}`,
-      options,
-    );
-
-    if (b64DeviceKey == null) {
-      return null;
-    }
-
-    return new SymmetricCryptoKey(Utils.fromB64ToArray(b64DeviceKey)) as DeviceKey;
-  }
-
-  override async setDeviceKey(value: DeviceKey, options?: StorageOptions): Promise<void> {
-    options = this.reconcileOptions(options, await this.defaultSecureStorageOptions());
-    if (options?.userId == null) {
-      return;
-    }
-
-    await this.saveSecureStorageKey(this.partialKeys.deviceKey, value.keyB64, options);
   }
 }
