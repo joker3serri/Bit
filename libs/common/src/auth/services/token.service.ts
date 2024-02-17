e34072b1b4cda94d50a2d09868bf37c69e7d1e1f
Jared Snider <jsnider@bitwarden.com>
2024-02-17 18:36:37 -0500

    PM-5263 - TokenSvc - Add TODO for figuring out how to store tokens in secure storage.

--- libs/common/src/auth/services/token.service.ts
+++ libs/common/src/auth/services/token.service.ts
@@ -19,6 +19,9 @@ import {
   TWO_FACTOR_TOKEN_DISK_LOCAL,
 } from "./token.state";
 
+// TODO: figure out how to store access and refresh tokens in secure storage.
+// see: https://github.com/bitwarden/clients/pull/7979/files#diff-9e557d79f4302c16b21dc8ca50352ce7394b3fba00dd004775ba74721c84c322
+
 export class TokenService implements TokenServiceAbstraction {
   static decodeAccessToken(token: string): Promise<any> {
     if (token == null) {
