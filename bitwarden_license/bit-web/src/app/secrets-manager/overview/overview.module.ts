262171a31811e09ae3b22f3825cb6155626988b8
Robyn MacCallum <robyntmaccallum@gmail.com>
2024-02-21 22:34:05 -0500

    Replace usage of stateService value with state new state provider

--- bitwarden_license/bit-web/src/app/secrets-manager/overview/overview.module.ts
+++ bitwarden_license/bit-web/src/app/secrets-manager/overview/overview.module.ts
@@ -6,10 +6,11 @@ import { SecretsManagerSharedModule } from "../shared/sm-shared.module";
 import { OverviewRoutingModule } from "./overview-routing.module";
 import { OverviewComponent } from "./overview.component";
 import { SectionComponent } from "./section.component";
+import { SMOnboardingTasksService } from "./sm-onboarding-tasks.service";
 
 @NgModule({
   imports: [SecretsManagerSharedModule, OverviewRoutingModule, OnboardingModule],
-  declarations: [OverviewComponent, SectionComponent],
+  declarations: [OverviewComponent, SectionComponent, SMOnboardingTasksService],
   providers: [],
 })
 export class OverviewModule {}
