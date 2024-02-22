d9a399e2edc2c1c51a7cb52d8e4e46cc01fb6034
Robyn MacCallum <robyntmaccallum@gmail.com>
2024-02-21 22:23:00 -0500

    Create state definition

--- libs/common/src/platform/state/state-definitions.ts
+++ libs/common/src/platform/state/state-definitions.ts
@@ -61,3 +61,8 @@ export const AUTOFILL_SETTINGS_DISK_LOCAL = new StateDefinition("autofillSetting
 export const VAULT_FILTER_DISK = new StateDefinition("vaultFilter", "disk", {
   web: "disk-local",
 });
+
+// Secrets Manager
+export const SM_ONBOARDING_DISK = new StateDefinition("smOnboarding", "disk", {
+  web: "disk-local",
+});
