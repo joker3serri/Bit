967ca8535fac7100ab57d2df7ac9ac140847ecff
Thomas Rittson <trittson@bitwarden.com>
2024-02-27 13:42:05 +1000

    Fix dependency

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -679,7 +679,7 @@ import { ModalService } from "./modal.service";
     {
       provide: PolicyApiServiceAbstraction,
       useClass: PolicyApiService,
-      deps: [PolicyServiceAbstraction, ApiServiceAbstraction],
+      deps: [InternalPolicyService, ApiServiceAbstraction],
     },
     {
       provide: KeyConnectorServiceAbstraction,
