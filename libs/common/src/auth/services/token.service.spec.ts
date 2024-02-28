db75c2d6a6f52542e716e6321e7520a3c956226e
Jared Snider <jsnider@bitwarden.com>
2024-02-28 17:40:17 -0500

    PM-5263 - TokenSvc tests - WIP - (1) Update TokenSvc instantiation to use new supportsSecureStorage (2) Test TwoFactorToken methods

--- libs/common/src/auth/services/token.service.spec.ts
+++ libs/common/src/auth/services/token.service.spec.ts
@@ -1,7 +1,6 @@
 import { mock } from "jest-mock-extended";
 
 import { FakeSingleUserStateProvider, FakeGlobalStateProvider } from "../../../spec";
-import { PlatformUtilsService } from "../../platform/abstractions/platform-utils.service";
 import { AbstractStorageService } from "../../platform/abstractions/storage.service";
 
 import { TokenService } from "./token.service";
@@ -13,7 +12,6 @@ describe("TokenService", () => {
   let singleUserStateProvider: FakeSingleUserStateProvider;
   let globalStateProvider: FakeGlobalStateProvider;
 
-  const platformUtilsService = mock<PlatformUtilsService>();
   const secureStorageService = mock<AbstractStorageService>();
 
   beforeEach(() => {
@@ -22,21 +20,16 @@ describe("TokenService", () => {
     singleUserStateProvider = new FakeSingleUserStateProvider();
     globalStateProvider = new FakeGlobalStateProvider();
 
-    tokenService = new TokenService(
-      singleUserStateProvider,
-      globalStateProvider,
-      platformUtilsService,
-      secureStorageService,
-    );
+    const supportsSecureStorage = false; // default to false; tests will override as needed
+    tokenService = createTokenService(supportsSecureStorage);
   });
 
   it("instantiates", () => {
     expect(tokenService).not.toBeFalsy();
   });
 
-  // TODO: tweak the names of the it blocks to be more descriptive around the fact that we are simply updating a single entry in a record
   describe("setTwoFactorToken", () => {
-    it("should set the two factor token when there hasn't been one set yet (initializing the record)", async () => {
+    it("should set the email and two factor token when there hasn't been a previous record (initializing the record)", async () => {
       // Arrange
       const email = "testUser@email.com";
       const twoFactorToken = "twoFactorTokenForTestUser";
@@ -48,7 +41,7 @@ describe("TokenService", () => {
       ).toHaveBeenCalledWith({ [email]: twoFactorToken });
     });
 
-    it("should set the two factor token when there is an initialized value already", async () => {
+    it("should set the email and two factor token when there is an initialized value already (updating the existing record)", async () => {
       // Arrange
       const email = "testUser@email.com";
       const twoFactorToken = "twoFactorTokenForTestUser";
@@ -62,10 +55,59 @@ describe("TokenService", () => {
 
       // Act
       await tokenService.setTwoFactorToken(email, twoFactorToken);
+
       // Assert
       expect(
         globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
       ).toHaveBeenCalledWith({ [email]: twoFactorToken, ...initialTwoFactorTokenRecord });
     });
   });
+
+  describe("getTwoFactorToken", () => {
+    it("should return the two factor token for the given email", async () => {
+      // Arrange
+      const email = "testUser";
+      const twoFactorToken = "twoFactorTokenForTestUser";
+      const initialTwoFactorTokenRecord: Record<string, string> = {
+        [email]: twoFactorToken,
+      };
+
+      globalStateProvider
+        .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
+        .stateSubject.next(initialTwoFactorTokenRecord);
+
+      // Act
+      const result = await tokenService.getTwoFactorToken(email);
+
+      // Assert
+      expect(result).toEqual(twoFactorToken);
+    });
+
+    it("should not return the two factor token for an email that doesn't exist", async () => {
+      // Arrange
+      const email = "testUser";
+      const initialTwoFactorTokenRecord: Record<string, string> = {
+        otherUser: "twoFactorTokenForOtherUser",
+      };
+
+      globalStateProvider
+        .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
+        .stateSubject.next(initialTwoFactorTokenRecord);
+
+      // Act
+      const result = await tokenService.getTwoFactorToken(email);
+
+      // Assert
+      expect(result).toEqual(undefined);
+    });
+  });
+
+  function createTokenService(supportsSecureStorage: boolean) {
+    return new TokenService(
+      singleUserStateProvider,
+      globalStateProvider,
+      supportsSecureStorage,
+      secureStorageService,
+    );
+  }
 });
