5e84e34c87c6e768dd6da3817738866b3a99d125
Jared Snider <jsnider@bitwarden.com>
2024-02-28 16:27:45 -0500

    PM-5263 - TokenService tests - WIP - 2FA token tests.

--- libs/common/src/auth/services/token.service.spec.ts
+++ libs/common/src/auth/services/token.service.spec.ts
@@ -5,6 +5,7 @@ import { PlatformUtilsService } from "../../platform/abstractions/platform-utils
 import { AbstractStorageService } from "../../platform/abstractions/storage.service";
 
 import { TokenService } from "./token.service";
+import { EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL } from "./token.state";
 
 describe("TokenService", () => {
   let tokenService: TokenService;
@@ -31,5 +32,40 @@ describe("TokenService", () => {
 
   it("instantiates", () => {
     expect(tokenService).not.toBeFalsy();
+  });
+
+  // TODO: tweak the names of the it blocks to be more descriptive around the fact that we are simply updating a single entry in a record
+  describe("setTwoFactorToken", () => {
+    it("should set the two factor token when there hasn't been one set yet (initializing the record)", async () => {
+      // Arrange
+      const email = "testUser@email.com";
+      const twoFactorToken = "twoFactorTokenForTestUser";
+      // Act
+      await tokenService.setTwoFactorToken(email, twoFactorToken);
+      // Assert
+      expect(
+        globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
+      ).toHaveBeenCalledWith({ [email]: twoFactorToken });
+    });
+
+    it("should set the two factor token when there is an initialized value already", async () => {
+      // Arrange
+      const email = "testUser@email.com";
+      const twoFactorToken = "twoFactorTokenForTestUser";
+      const initialTwoFactorTokenRecord: Record<string, string> = {
+        otherUser: "otherUserTwoFactorToken",
+      };
+
+      globalStateProvider
+        .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
+        .stateSubject.next(initialTwoFactorTokenRecord);
+
+      // Act
+      await tokenService.setTwoFactorToken(email, twoFactorToken);
+      // Assert
+      expect(
+        globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
+      ).toHaveBeenCalledWith({ [email]: twoFactorToken, ...initialTwoFactorTokenRecord });
+    });
   });
 });
