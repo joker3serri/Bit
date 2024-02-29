7e83e37579a5ff70b09980460d55ecd6e741c93c
Jared Snider <jsnider@bitwarden.com>
2024-02-29 17:48:09 -0500

    PM-5263 - TokenSvc Tests WIP

--- libs/common/src/auth/services/token.service.spec.ts
+++ libs/common/src/auth/services/token.service.spec.ts
@@ -1,10 +1,18 @@
 import { mock } from "jest-mock-extended";
 
 import { FakeSingleUserStateProvider, FakeGlobalStateProvider } from "../../../spec";
+import { VaultTimeoutAction } from "../../enums/vault-timeout-action.enum";
 import { AbstractStorageService } from "../../platform/abstractions/storage.service";
 
-import { TokenService } from "./token.service";
+import { TokenService, TokenStorageLocation } from "./token.service";
 import { EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL } from "./token.state";
+
+type VaultTimeoutSettings = Partial<{
+  [key in TokenStorageLocation]: {
+    vaultTimeoutAction: VaultTimeoutAction;
+    vaultTimeout: number | null;
+  };
+}>;
 
 describe("TokenService", () => {
   let tokenService: TokenService;
@@ -13,6 +21,17 @@ describe("TokenService", () => {
   let globalStateProvider: FakeGlobalStateProvider;
 
   const secureStorageService = mock<AbstractStorageService>();
+
+  const vaultTimeoutSettings: VaultTimeoutSettings = {
+    [TokenStorageLocation.Disk]: {
+      vaultTimeoutAction: VaultTimeoutAction.Lock,
+      vaultTimeout: null,
+    },
+    [TokenStorageLocation.Memory]: {
+      vaultTimeoutAction: VaultTimeoutAction.LogOut,
+      vaultTimeout: 30,
+    },
+  };
 
   beforeEach(() => {
     jest.clearAllMocks();
@@ -28,80 +47,122 @@ describe("TokenService", () => {
     expect(tokenService).not.toBeFalsy();
   });
 
-  describe("setTwoFactorToken", () => {
-    it("should set the email and two factor token when there hasn't been a previous record (initializing the record)", async () => {
-      // Arrange
-      const email = "testUser@email.com";
-      const twoFactorToken = "twoFactorTokenForTestUser";
-      // Act
-      await tokenService.setTwoFactorToken(email, twoFactorToken);
-      // Assert
-      expect(
-        globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
-      ).toHaveBeenCalledWith({ [email]: twoFactorToken });
+  // AccessToken logic
+  describe("AccessToken logic", () => {
+    // TODO: Need to break apart testing this into memory, disk, and secure storage tests
+
+    describe("Memory storage tests", () => {
+      const { vaultTimeoutAction, vaultTimeout } =
+        vaultTimeoutSettings[TokenStorageLocation.Memory];
+
+      // beforeEach(() => {
+      // })
+
+      it("should set the access token in memory", async () => {
+        // TODO: must create valid access token that can be decoded.
+        // Arrange
+        const accessToken = "accessTokenForTestUser";
+        // Act
+        await tokenService.setAccessToken(accessToken, vaultTimeoutAction, vaultTimeout);
+        // Assert
+        expect(
+          globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
+        ).toHaveBeenCalledWith({ accessToken });
+      });
     });
 
-    it("should set the email and two factor token when there is an initialized value already (updating the existing record)", async () => {
-      // Arrange
-      const email = "testUser@email.com";
-      const twoFactorToken = "twoFactorTokenForTestUser";
-      const initialTwoFactorTokenRecord: Record<string, string> = {
-        otherUser: "otherUserTwoFactorToken",
-      };
+    // describe("setAccessToken", () => {
+    //   it("should set the access token ", async () => {
+    //     // Arrange
+    //     const accessToken = "accessTokenForTestUser";
+    //     //
+    //     // Act
+    //     await tokenService.setAccessToken(accessToken);
+    //     // Assert
+    //     expect(
+    //       globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
+    //     ).toHaveBeenCalledWith({ accessToken });
+    //   });
+    // });
+  });
 
-      globalStateProvider
-        .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
-        .stateSubject.next(initialTwoFactorTokenRecord);
+  describe("TwoFactorToken logic", () => {
+    describe("setTwoFactorToken", () => {
+      it("should set the email and two factor token when there hasn't been a previous record (initializing the record)", async () => {
+        // Arrange
+        const email = "testUser@email.com";
+        const twoFactorToken = "twoFactorTokenForTestUser";
+        // Act
+        await tokenService.setTwoFactorToken(email, twoFactorToken);
+        // Assert
+        expect(
+          globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
+        ).toHaveBeenCalledWith({ [email]: twoFactorToken });
+      });
 
-      // Act
-      await tokenService.setTwoFactorToken(email, twoFactorToken);
+      it("should set the email and two factor token when there is an initialized value already (updating the existing record)", async () => {
+        // Arrange
+        const email = "testUser@email.com";
+        const twoFactorToken = "twoFactorTokenForTestUser";
+        const initialTwoFactorTokenRecord: Record<string, string> = {
+          otherUser: "otherUserTwoFactorToken",
+        };
 
-      // Assert
-      expect(
-        globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
-      ).toHaveBeenCalledWith({ [email]: twoFactorToken, ...initialTwoFactorTokenRecord });
+        globalStateProvider
+          .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
+          .stateSubject.next(initialTwoFactorTokenRecord);
+
+        // Act
+        await tokenService.setTwoFactorToken(email, twoFactorToken);
+
+        // Assert
+        expect(
+          globalStateProvider.getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL).nextMock,
+        ).toHaveBeenCalledWith({ [email]: twoFactorToken, ...initialTwoFactorTokenRecord });
+      });
+    });
+
+    describe("getTwoFactorToken", () => {
+      it("should return the two factor token for the given email", async () => {
+        // Arrange
+        const email = "testUser";
+        const twoFactorToken = "twoFactorTokenForTestUser";
+        const initialTwoFactorTokenRecord: Record<string, string> = {
+          [email]: twoFactorToken,
+        };
+
+        globalStateProvider
+          .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
+          .stateSubject.next(initialTwoFactorTokenRecord);
+
+        // Act
+        const result = await tokenService.getTwoFactorToken(email);
+
+        // Assert
+        expect(result).toEqual(twoFactorToken);
+      });
+
+      it("should not return the two factor token for an email that doesn't exist", async () => {
+        // Arrange
+        const email = "testUser";
+        const initialTwoFactorTokenRecord: Record<string, string> = {
+          otherUser: "twoFactorTokenForOtherUser",
+        };
+
+        globalStateProvider
+          .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
+          .stateSubject.next(initialTwoFactorTokenRecord);
+
+        // Act
+        const result = await tokenService.getTwoFactorToken(email);
+
+        // Assert
+        expect(result).toEqual(undefined);
+      });
     });
   });
 
-  describe("getTwoFactorToken", () => {
-    it("should return the two factor token for the given email", async () => {
-      // Arrange
-      const email = "testUser";
-      const twoFactorToken = "twoFactorTokenForTestUser";
-      const initialTwoFactorTokenRecord: Record<string, string> = {
-        [email]: twoFactorToken,
-      };
-
-      globalStateProvider
-        .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
-        .stateSubject.next(initialTwoFactorTokenRecord);
-
-      // Act
-      const result = await tokenService.getTwoFactorToken(email);
-
-      // Assert
-      expect(result).toEqual(twoFactorToken);
-    });
-
-    it("should not return the two factor token for an email that doesn't exist", async () => {
-      // Arrange
-      const email = "testUser";
-      const initialTwoFactorTokenRecord: Record<string, string> = {
-        otherUser: "twoFactorTokenForOtherUser",
-      };
-
-      globalStateProvider
-        .getFake(EMAIL_TWO_FACTOR_TOKEN_RECORD_DISK_LOCAL)
-        .stateSubject.next(initialTwoFactorTokenRecord);
-
-      // Act
-      const result = await tokenService.getTwoFactorToken(email);
-
-      // Assert
-      expect(result).toEqual(undefined);
-    });
-  });
-
+  // Helpers
   function createTokenService(supportsSecureStorage: boolean) {
     return new TokenService(
       singleUserStateProvider,
