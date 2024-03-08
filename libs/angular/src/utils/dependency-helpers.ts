cd90d6a1d5aec54c63c73253952fcc0d0945368f
Thomas Rittson <trittson@bitwarden.com>
2024-03-08 12:36:17 +1000

    Use AbstractConstructor and Constructor types

--- libs/angular/src/utils/dependency-helpers.ts
+++ libs/angular/src/utils/dependency-helpers.ts
@@ -1,20 +1,19 @@
+import { Constructor } from "type-fest";
+
 import { SafeInjectionToken } from "../services/injection-tokens";
 
-/**
- * Given a type, return a constructor that returns that type
- * This is used to resolve mismatches between types (generally representing an instantiated class) and constructor types (the non-instantiated class)
- */
-type ConstructorForType<T> = abstract new (...args: any) => T;
+// TODO: type-fest also provides a type like this when we upgrade >= 3.7.0
+type AbstractConstructor<T> = abstract new (...args: any) => T;
 
 type MapParametersToDeps<T> = {
-  [K in keyof T]: ConstructorForType<T[K]> | SafeInjectionToken<T[K]>;
+  [K in keyof T]: AbstractConstructor<T[K]> | SafeInjectionToken<T[K]>;
 };
 
 type SafeInjectionTokenType<T> = T extends SafeInjectionToken<infer J> ? J : never;
 
 export const useClass = <
-  A extends abstract new (...args: any) => InstanceType<A>, // A is an abstract class
-  I extends new (...args: any) => InstanceType<A>, // I is the implementation, it has a non-abstract ctor that returns a type that extends A
+  A extends AbstractConstructor<InstanceType<A>>, // A is an abstract class
+  I extends Constructor<InstanceType<A>>, // I is the implementation, it has a non-abstract ctor that returns a type that extends A
   D extends MapParametersToDeps<ConstructorParameters<I>>, // accept an array of constructor types OR injection tokens matching ctor parameters
 >(obj: {
   provide: A;
