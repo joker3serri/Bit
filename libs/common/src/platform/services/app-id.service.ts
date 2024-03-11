715feef07ed3981716c889bf8c21f5e572ac7722
Matt Gibson <mgibson@bitwarden.com>
2024-03-11 13:37:00 -0400

    Add reactive interface

--- libs/common/src/platform/services/app-id.service.ts
+++ libs/common/src/platform/services/app-id.service.ts
@@ -1,3 +1,5 @@
+import { Observable, filter, firstValueFrom, tap } from "rxjs";
+
 import { AppIdService as AppIdServiceAbstraction } from "../abstractions/app-id.service";
 import { Utils } from "../misc/utils";
 import { APPLICATION_ID_DISK, GlobalStateProvider, KeyDefinition } from "../state";
@@ -10,19 +12,35 @@ export const ANONYMOUS_APP_ID_KEY = new KeyDefinition(APPLICATION_ID_DISK, "anon
 });
 
 export class AppIdService implements AppIdServiceAbstraction {
-  private appIdState = this.globalStateProvider.get(APP_ID_KEY);
-  private anonymousAppIdState = this.globalStateProvider.get(ANONYMOUS_APP_ID_KEY);
-  constructor(private globalStateProvider: GlobalStateProvider) {}
+  appId$: Observable<string>;
+  anonymousAppId$: Observable<string>;
+
+  constructor(globalStateProvider: GlobalStateProvider) {
+    const appIdState = globalStateProvider.get(APP_ID_KEY);
+    const anonymousAppIdState = globalStateProvider.get(ANONYMOUS_APP_ID_KEY);
+    this.appId$ = appIdState.state$.pipe(
+      tap(async (appId) => {
+        if (!appId) {
+          await appIdState.update(() => Utils.newGuid());
+        }
+      }),
+      filter((appId) => !!appId),
+    );
+    this.anonymousAppId$ = anonymousAppIdState.state$.pipe(
+      tap(async (appId) => {
+        if (!appId) {
+          await anonymousAppIdState.update(() => Utils.newGuid());
+        }
+      }),
+      filter((appId) => !!appId),
+    );
+  }
 
   async getAppId(): Promise<string> {
-    return await this.appIdState.update(() => Utils.newGuid(), {
-      shouldUpdate: (value) => value == null,
-    });
+    return await firstValueFrom(this.appId$);
   }
 
   async getAnonymousAppId(): Promise<string> {
-    return await this.anonymousAppIdState.update(() => Utils.newGuid(), {
-      shouldUpdate: (value) => value == null,
-    });
+    return await firstValueFrom(this.anonymousAppId$);
   }
 }
