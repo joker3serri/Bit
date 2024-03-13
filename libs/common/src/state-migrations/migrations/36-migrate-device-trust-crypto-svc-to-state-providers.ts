630eae9c9e5d8b8b5706cd7a6c23879db2662998
Jared Snider <jsnider@bitwarden.com>
2024-03-13 19:03:37 -0400

    PM-5268 - bump migration versions

--- libs/common/src/state-migrations/migrations/36-migrate-device-trust-crypto-svc-to-state-providers.ts
+++ libs/common/src/state-migrations/migrations/36-migrate-device-trust-crypto-svc-to-state-providers.ts
@@ -29,7 +29,7 @@ export const SHOULD_TRUST_DEVICE: KeyDefinitionLike = {
   },
 };
 
-export class DeviceTrustCryptoServiceStateProviderMigrator extends Migrator<22, 23> {
+export class DeviceTrustCryptoServiceStateProviderMigrator extends Migrator<35, 36> {
   async migrate(helper: MigrationHelper): Promise<void> {
     const accounts = await helper.getAccounts<ExpectedAccountType>();
     async function migrateAccount(userId: string, account: ExpectedAccountType): Promise<void> {
