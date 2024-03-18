e35dd7423c9bdeff5267df850e299df01b49dbe1
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-03-18 12:34:00 -0400

    Update Tests

--- libs/common/src/state-migrations/migrations/40-move-desktop-settings.spec.ts
+++ libs/common/src/state-migrations/migrations/40-move-desktop-settings.spec.ts
@@ -27,17 +27,13 @@ describe("MoveDesktopSettings", () => {
         openAtLogin: true,
         alwaysShowDock: true,
       },
-      user1: {
-        settings: {
-          enableAlwaysOnTop: true,
-          minimizeOnCopyToClipboard: true,
-        },
-      },
     });
 
     expect(output).toEqual({
       authenticatedAccounts: ["user1"],
-      global: {},
+      global: {
+        enableAlwaysOnTop: true,
+      },
       global_desktopSettings_window: {
         width: 400,
         height: 400,
@@ -48,18 +44,12 @@ describe("MoveDesktopSettings", () => {
           y: 200,
         },
       },
-      global_desktopSettings_alwaysOnTop: true,
       global_desktopSettings_closeToTray: true,
       global_desktopSettings_minimizeToTray: true,
       global_desktopSettings_startToTray: true,
       global_desktopSettings_trayEnabled: true,
       global_desktopSettings_openAtLogin: true,
       global_desktopSettings_alwaysShowDock: true,
-      user1: {
-        settings: {},
-      },
-      user_user1_desktopSettings_alwaysOnTop: true,
-      user_user1_desktopSettings_minimizeOnCopy: true,
     });
   });
 
@@ -68,7 +58,6 @@ describe("MoveDesktopSettings", () => {
       authenticatedAccounts: ["user1"],
       global: {
         window: null,
-        enableAlwaysOnTop: false,
         enableCloseToTray: false,
         enableMinimizeToTray: false,
         enableStartToTray: false,
@@ -76,30 +65,35 @@ describe("MoveDesktopSettings", () => {
         openAtLogin: false,
         alwaysShowDock: false,
       },
-      user1: {
-        settings: {
-          enableAlwaysOnTop: false,
-          minimizeOnCopyToClipboard: false,
-        },
-      },
     });
 
     expect(output).toEqual({
       authenticatedAccounts: ["user1"],
       global: {},
       global_desktopSettings_window: null,
-      global_desktopSettings_alwaysOnTop: false,
       global_desktopSettings_closeToTray: false,
       global_desktopSettings_minimizeToTray: false,
       global_desktopSettings_startToTray: false,
       global_desktopSettings_trayEnabled: false,
       global_desktopSettings_openAtLogin: false,
       global_desktopSettings_alwaysShowDock: false,
-      user1: {
-        settings: {},
+    });
+  });
+
+  it("can migrate even if none of our values are found", async () => {
+    //
+    const output = await runMigrator(sut, {
+      authenticatedAccounts: ["user1"] as const,
+      global: {
+        anotherSetting: "",
       },
-      user_user1_desktopSettings_alwaysOnTop: false,
-      user_user1_desktopSettings_minimizeOnCopy: false,
+    });
+
+    expect(output).toEqual({
+      authenticatedAccounts: ["user1"] as const,
+      global: {
+        anotherSetting: "",
+      },
     });
   });
 });
