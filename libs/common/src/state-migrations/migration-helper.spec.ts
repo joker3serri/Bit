0ac54b99d4aa0ace7e690a321a7f82de0075775b
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-03-21 13:25:41 -0400

    Fix mockHelper.type

--- libs/common/src/state-migrations/migration-helper.spec.ts
+++ libs/common/src/state-migrations/migration-helper.spec.ts
@@ -176,7 +176,7 @@ export function mockMigrationHelper(
   );
   mockHelper.getAccounts.mockImplementation(() => helper.getAccounts());
 
-  Object.defineProperty(mockHelper, "type", { value: "general " });
+  Object.defineProperty(mockHelper, "type", { value: "general" });
 
   return mockHelper;
 }
