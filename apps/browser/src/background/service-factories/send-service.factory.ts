8b5dfb41be21d5349882913893a925887ab4aacb
Tom <ttalty@bitwarden.com>
2024-03-27 11:46:10 -0400

    Removing send service from services, removed browser send, and pointed to send services

--- apps/browser/src/background/service-factories/send-service.factory.ts
+++ apps/browser/src/background/service-factories/send-service.factory.ts
@@ -1,3 +1,4 @@
+import { SendService } from "@bitwarden/common/tools/send/services/send.service";
 import { InternalSendService } from "@bitwarden/common/tools/send/services/send.service.abstraction";
 
 import {
@@ -21,7 +22,7 @@ import {
   stateServiceFactory,
   StateServiceInitOptions,
 } from "../../platform/background/service-factories/state-service.factory";
-import { BrowserSendService } from "../../services/browser-send.service";
+
 
 type SendServiceFactoryOptions = FactoryOptions;
 
@@ -40,7 +41,7 @@ export function sendServiceFactory(
     "sendService",
     opts,
     async () =>
-      new BrowserSendService(
+      new SendService(
         await cryptoServiceFactory(cache, opts),
         await i18nServiceFactory(cache, opts),
         await keyGenerationServiceFactory(cache, opts),
