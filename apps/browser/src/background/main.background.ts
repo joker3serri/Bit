1cf22a526abd54693bb3b43a2afb240a8ff9c422
Addison Beck <hello@addisonbeck.com>
2024-04-01 06:48:55 -0500

    Update policy service to clear its own state

--- apps/browser/src/background/main.background.ts
+++ apps/browser/src/background/main.background.ts
@@ -1126,7 +1126,6 @@ export default class MainBackground {
       this.cipherService.clear(userId),
       this.folderService.clear(userId),
       this.collectionService.clear(userId),
-      this.policyService.clear(userId),
       this.passwordGenerationService.clear(userId),
       this.vaultTimeoutSettingsService.clear(userId),
       this.vaultFilterService.clear(),
