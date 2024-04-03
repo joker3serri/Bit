d8909f262e2b696ce580e689cb8f2d8321e94c83
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-04-03 11:34:18 -0400

    Fix Test

--- libs/common/src/auth/services/token.state.spec.ts
+++ libs/common/src/auth/services/token.state.spec.ts
@@ -1,4 +1,4 @@
-import { KeyDefinition } from "../../platform/state";
+import { KeyDefinition, UserKeyDefinition } from "../../platform/state";
 
 import {
   ACCESS_TOKEN_DISK,
@@ -28,8 +28,8 @@ describe.each([
   "deserializes state key definitions",
   (
     keyDefinition:
-      | KeyDefinition<string>
-      | KeyDefinition<boolean>
+      | UserKeyDefinition<string>
+      | UserKeyDefinition<boolean>
       | KeyDefinition<Record<string, string>>,
     state: string | boolean | Record<string, string>,
   ) => {
