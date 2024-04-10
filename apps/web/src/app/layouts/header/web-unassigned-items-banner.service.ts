6dbb6e9c031a7ed9dc44d89b06dfcc85c6326fb7
Thomas Rittson <trittson@bitwarden.com>
2024-04-10 10:22:04 +1000

    Add api call

--- apps/web/src/app/layouts/header/web-unassigned-items-banner.service.ts
+++ apps/web/src/app/layouts/header/web-unassigned-items-banner.service.ts
@@ -1,40 +1,58 @@
 import { Injectable } from "@angular/core";
-import { combineLatest, map } from "rxjs";
+import { EMPTY, combineLatest, concatMap } from "rxjs";
 
-import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { ApiService } from "@bitwarden/common/abstractions/api.service";
 import {
   GlobalStateProvider,
   KeyDefinition,
   UNASSIGNED_ITEMS_BANNER_DISK,
 } from "@bitwarden/common/platform/state";
 
-const HIDE_BANNER_KEY = new KeyDefinition<boolean>(UNASSIGNED_ITEMS_BANNER_DISK, "hideBanner", {
-  deserializer: (b) => {
-    if (b === null) {
-      return false;
-    }
-    return b;
+const DISMISS_BANNER_KEY = new KeyDefinition<boolean>(
+  UNASSIGNED_ITEMS_BANNER_DISK,
+  "dismissBanner",
+  {
+    deserializer: (b) => {
+      if (b === null) {
+        return false;
+      }
+      return b;
+    },
   },
-});
+);
+
+const SHOW_BANNER_KEY = new KeyDefinition<boolean | null>(
+  UNASSIGNED_ITEMS_BANNER_DISK,
+  "showBanner",
+  {
+    deserializer: (b) => b ?? null,
+  },
+);
 
 /** Displays a banner that tells users how to move their unassigned items into a collection. */
 @Injectable({ providedIn: "root" })
 export class WebUnassignedItemsBannerService {
-  private _hideBannerState = this.globalStateProvider.get(HIDE_BANNER_KEY);
-  private _adminOrganizations = this.organizationService.organizations$.pipe(
-    map((orgs) => orgs.filter((o) => o.isAdmin)),
-  );
+  private _dismissBannerState = this.globalStateProvider.get(DISMISS_BANNER_KEY);
+  private _showBannerState = this.globalStateProvider.get(SHOW_BANNER_KEY);
 
-  showBanner$ = combineLatest([this._hideBannerState.state$, this._adminOrganizations]).pipe(
-    map(([hideBanner, adminOrganizations]) => !hideBanner && adminOrganizations?.length > 1),
+  showBanner$ = combineLatest([this._dismissBannerState.state$, this._showBannerState.state$]).pipe(
+    concatMap(async ([dismissBanner, showBanner]) => {
+      if (!dismissBanner && showBanner == null) {
+        const showBannerResponse = await this.apiService.getShowUnassignedCiphersBanner();
+        await this._showBannerState.update(() => showBannerResponse);
+        return EMPTY; // to test, we could also just emit false and let the value update the next time around
+      }
+
+      return !dismissBanner && showBanner;
+    }),
   );
 
   constructor(
     private globalStateProvider: GlobalStateProvider,
-    private organizationService: OrganizationService,
+    private apiService: ApiService,
   ) {}
 
   async hideBanner() {
-    await this._hideBannerState.update(() => true);
+    await this._dismissBannerState.update(() => true);
   }
 }
