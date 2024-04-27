ca0982efbeb67afcfd361a18c6d2e951c6de317b
William Martin <contact@willmartian.com>
2024-04-27 12:18:01 -0400

    remove unused focusableElement directive

--- libs/components/src/shared/focusable-element.ts
+++ libs/components/src/shared/focusable-element.ts
@@ -1,21 +1,8 @@
-import { Directive, ElementRef } from "@angular/core";
-
 /**
- * Interface for implementing focusable components. Used by the AutofocusDirective.
+ * Interface for implementing focusable components.
+ *
+ * Used by the `AutofocusDirective` and `A11yGridDirective`.
  */
 export abstract class FocusableElement {
   getFocusTarget: () => HTMLElement;
-}
-
-@Directive({
-  selector: "[bitFocusableElement]",
-  standalone: true,
-  providers: [{ provide: FocusableElement, useExisting: FocusableElementDirective }],
-})
-export class FocusableElementDirective implements FocusableElement {
-  constructor(private elementRef: ElementRef) {}
-
-  getFocusTarget() {
-    return this.elementRef.nativeElement;
-  }
 }
