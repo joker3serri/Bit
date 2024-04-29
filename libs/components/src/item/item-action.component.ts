87e175a760ca4f2044c20259de18051171191939
William Martin <contact@willmartian.com>
2024-04-29 15:15:07 -0400

    code review changes

--- libs/components/src/item/item-action.component.ts
+++ libs/components/src/item/item-action.component.ts
@@ -1,4 +1,3 @@
-import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 import { A11yCellDirective } from "../a11y/a11y-cell.directive";
@@ -6,7 +5,7 @@ import { A11yCellDirective } from "../a11y/a11y-cell.directive";
 @Component({
   selector: "bit-item-action",
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   template: `<ng-content></ng-content>`,
   providers: [{ provide: A11yCellDirective, useExisting: ItemActionComponent }],
 })
