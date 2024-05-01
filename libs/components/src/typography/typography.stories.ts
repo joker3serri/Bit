36c14cff649341b2b70b1f9c064037ac35d86077
William Martin <contact@willmartian.com>
2024-05-01 16:26:15 -0400

    update styles, docs

--- libs/components/src/typography/typography.stories.ts
+++ libs/components/src/typography/typography.stories.ts
@@ -7,21 +7,22 @@ export default {
   component: TypographyDirective,
 } as Meta;
 
-type Story = StoryObj<TypographyDirective>;
-
-export const Default: Story = {
+export const Default: StoryObj<TypographyDirective & { text: string }> = {
   render: (args) => ({
     props: args,
     template: /*html*/ `
-      <div bitTypography="h1">h1</div>
-      <div bitTypography="h2">h2</div>
-      <div bitTypography="h3">h3</div>
-      <div bitTypography="h4">h4</div>
-      <div bitTypography="h5">h5</div>
-      <div bitTypography="h6">h6</div>
-      <div bitTypography="body1" class="tw-text-main">body1</div>
-      <div bitTypography="body2" class="tw-text-main">body2</div>
-      <div bitTypography="helper" class="tw-text-main">helper</div>
+      <div bitTypography="h1">h1 - {{ text }}</div>
+      <div bitTypography="h2">h2 - {{ text }}</div>
+      <div bitTypography="h3">h3 - {{ text }}</div>
+      <div bitTypography="h4">h4 - {{ text }}</div>
+      <div bitTypography="h5">h5 - {{ text }}</div>
+      <div bitTypography="h6">h6 - {{ text }}</div>
+      <div bitTypography="body1" class="tw-text-main">body1 - {{ text }}</div>
+      <div bitTypography="body2" class="tw-text-main">body2 - {{ text }}</div>
+      <div bitTypography="helper" class="tw-text-main">helper - {{ text }}</div>
     `,
   }),
+  args: {
+    text: `Sphinx of black quartz, judge my vow.`,
+  },
 };
