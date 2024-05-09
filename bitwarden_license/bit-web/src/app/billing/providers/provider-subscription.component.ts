963c41a72f398f4ecbcbbc5d4ea6abb0d05452e5
Cy Okeke <cokeke@bitwarden.com>
2024-05-09 18:54:25 +0100

    Make changes for provider billing details

--- bitwarden_license/bit-web/src/app/billing/providers/provider-subscription.component.ts
+++ bitwarden_license/bit-web/src/app/billing/providers/provider-subscription.component.ts
@@ -1,19 +1,18 @@
 import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { Observable, Subject, concatMap, firstValueFrom, takeUntil } from "rxjs";
+import { Observable, Subject, concatMap, takeUntil } from "rxjs";
 
-import { ApiService } from "@bitwarden/common/abstractions/api.service";
-import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+// import { ApiService } from "@bitwarden/common/abstractions/api.service";
+// import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
 import { BillingApiServiceAbstraction } from "@bitwarden/common/billing/abstractions/billilng-api.service.abstraction";
 import { ProviderSubscriptionResponse } from "@bitwarden/common/billing/models/response/provider-subscription-response";
 import { BillingSubscriptionItemResponse } from "@bitwarden/common/billing/models/response/subscription.response";
 import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
-import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
-import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
+// import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
+// import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { DialogService } from "@bitwarden/components";
-
-import { I18nService } from "src/app/core/i18n.service";
 
 @Component({
   selector: "app-provider-subscription",
@@ -28,17 +27,13 @@ export class ProviderSubscriptionComponent {
   locale: string;
   private destroy$ = new Subject<void>();
   showUpdatedSubscriptionStatusSection$: Observable<boolean>;
+  userOrg: Organization;
 
   protected enableConsolidatedBilling$ = this.configService.getFeatureFlag$(
     FeatureFlag.EnableConsolidatedBilling,
   );
 
   constructor(
-    private apiService: ApiService,
-    private platformUtilsService: PlatformUtilsService,
-    private i18nService: I18nService,
-    private logService: LogService,
-    private organizationService: OrganizationService,
     private billingApiService: BillingApiServiceAbstraction,
     private route: ActivatedRoute,
     private dialogService: DialogService,
@@ -67,9 +62,85 @@ export class ProviderSubscriptionComponent {
     );
   }
 
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
+  get subscriptionMarkedForCancel() {
+    return false;
+    // return (
+    //   this.subscription != null && !this.subscription.cancelled && this.subscription.cancelAtEndDate
+    // );
+  }
+
+  reinstate = async () => {
+    if (this.loading) {
+      return;
+    }
+
+    const confirmed = await this.dialogService.openSimpleDialog({
+      title: { key: "reinstateSubscription" },
+      content: { key: "reinstateConfirmation" },
+      type: "warning",
+    });
+
+    if (!confirmed) {
+      return;
+    }
+
+    // try {
+    //   await this.organizationApiService.reinstate(this.organizationId);
+    //   this.platformUtilsService.showToast("success", null, this.i18nService.t("reinstated"));
+    //   // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
+    //   // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    //   this.load();
+    // } catch (e) {
+    //   this.logService.error(e);
+    // }
+  };
+
+  get subscriptionLineItems() {
+    return this.lineItems.map((lineItem: BillingSubscriptionItemResponse) => ({
+      name: lineItem.name,
+      amount: this.discountPrice(lineItem.amount, lineItem.productId),
+      quantity: lineItem.quantity,
+      interval: lineItem.interval,
+      sponsoredSubscriptionItem: lineItem.sponsoredSubscriptionItem,
+      addonSubscriptionItem: lineItem.addonSubscriptionItem,
+      productName: lineItem.productName,
+      productId: lineItem.productId,
+    }));
+  }
+
+  discountPrice = (price: number, productId: string = null) => {
+    // const discount =
+    //   this.customerDiscount?.active &&
+    //   (!productId ||
+    //     !this.customerDiscount.appliesTo.length ||
+    //     this.customerDiscount.appliesTo.includes(productId))
+    //     ? price * (this.customerDiscount.percentOff / 100)
+    //     : 0;
+
+    // return price - discount;
+
+    return 5;
+  };
+
+  get isExpired() {
+    return false;
+    // const nextInvoice = this.nextInvoice;
+
+    // if (nextInvoice == null) {
+    //   return false;
+    // }
+
+    // return new Date(nextInvoice.date).getTime() < Date.now();
+  }
+
+  get nextInvoice() {
+    return "";
+    //return this.sub != null ? this.sub.upcomingInvoice : null;
+  }
+
+  get customerDiscount() {
+    // return this.sub != null ? this.sub.customerDiscount : null;
+    return 5;
   }
 
   async load() {
@@ -77,9 +148,13 @@ export class ProviderSubscriptionComponent {
       return;
     }
     this.loading = true;
-    this.locale = await firstValueFrom(this.i18nService.locale$);
     this.sub = await this.billingApiService.getProviderSubscription(this.providerId);
 
     this.loading = false;
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
