9b1325df750aa575621821680357a545bea533e0
Matt Gibson <mgibson@bitwarden.com>
2024-05-09 20:22:44 -0400

    Do not navigate in account switcher, main.background takes care of it

--- apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
+++ apps/browser/src/auth/popup/account-switching/account-switcher.component.ts
@@ -108,10 +108,6 @@ export class AccountSwitcherComponent implements OnInit, OnDestroy {
     if (confirmed) {
       this.messagingService.send("logout", { userId });
     }
-
-    // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.router.navigate(["home"]);
   }
 
   ngOnDestroy() {
