742277f1ac11bb0fd53626d315f0042d68cfa92d
Shane Melton <smelton@bitwarden.com>
2024-05-13 16:57:23 -0700

    [PM-6825] Add VaultListItems to VaultV2 component

--- apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
@@ -1,13 +1,26 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { CipherView } from "@bitwarden/common/vault/models/view/cipher.view";
+
+import { VaultPopupItemsService } from "../../services/vault-popup-items.service";
 
 @Component({
   selector: "app-vault",
   templateUrl: "vault-v2.component.html",
 })
 export class VaultV2Component implements OnInit, OnDestroy {
-  constructor() {}
+  autoFillCiphers$: Observable<CipherView[]>;
+  favoriteCiphers$: Observable<CipherView[]>;
+  remainingCiphers$: Observable<CipherView[]>;
 
-  ngOnInit(): void {}
+  constructor(private vaultPopupItemsService: VaultPopupItemsService) {}
+
+  ngOnInit(): void {
+    this.autoFillCiphers$ = this.vaultPopupItemsService.autoFillCiphers$;
+    this.favoriteCiphers$ = this.vaultPopupItemsService.favoriteCiphers$;
+    this.remainingCiphers$ = this.vaultPopupItemsService.remainingCiphers$;
+  }
 
   ngOnDestroy(): void {}
 }
