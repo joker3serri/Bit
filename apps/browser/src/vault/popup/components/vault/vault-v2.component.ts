5bd248e57deab05c311fe3f561d2a3e7fe3bd207
nick-livefront <nick@livefront.com>
2024-05-15 08:59:41 -0500

    Add V2 version of the vault header

--- apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
@@ -1,13 +1,32 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+
+import { VaultFilter } from "@bitwarden/angular/vault/vault-filter/models/vault-filter.model";
+
+import { VaultFilterService } from "../../../services/vault-filter.service";
 
 @Component({
   selector: "app-vault",
   templateUrl: "vault-v2.component.html",
 })
 export class VaultV2Component implements OnInit, OnDestroy {
-  constructor() {}
+  vaultFilter: VaultFilter;
+
+  constructor(
+    private router: Router,
+    private vaultFilterService: VaultFilterService,
+  ) {
+    this.vaultFilter = this.vaultFilterService.getVaultFilter();
+  }
 
   ngOnInit(): void {}
 
   ngOnDestroy(): void {}
+
+  addCipher() {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    this.router.navigate(["/add-cipher"], {
+      queryParams: { selectedVault: this.vaultFilter.selectedOrganizationId },
+    });
+  }
 }
