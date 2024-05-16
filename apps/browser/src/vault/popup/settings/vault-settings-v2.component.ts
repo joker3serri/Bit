55db865229d79380a8142017f1360b102b642572
Daniel James Smith <djsmith85@users.noreply.github.com>
2024-05-16 20:16:33 +0200

    Move sync functionality to vault-settings-v2

--- apps/browser/src/vault/popup/settings/vault-settings-v2.component.ts
+++ apps/browser/src/vault/popup/settings/vault-settings-v2.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Router, RouterModule } from "@angular/router";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { MessagingService } from "@bitwarden/common/platform/abstractions/messaging.service";
+import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
+import { SyncService } from "@bitwarden/common/vault/abstractions/sync/sync.service.abstraction";
 import { ItemModule } from "@bitwarden/components";
 
 import { BrowserApi } from "../../../platform/browser/browser-api";
@@ -27,16 +30,44 @@ import { PopupPageComponent } from "../../../platform/popup/layout/popup-page.co
     ItemModule,
   ],
 })
-export class VaultSettingsV2Component {
+export class VaultSettingsV2Component implements OnInit {
+  lastSync = "--";
+
   constructor(
     public messagingService: MessagingService,
     private router: Router,
+    private syncService: SyncService,
+    private platformUtilsService: PlatformUtilsService,
+    private i18nService: I18nService,
   ) {}
+
+  async ngOnInit() {
+    await this.setLastSync();
+  }
 
   async import() {
     await this.router.navigate(["/import"]);
     if (await BrowserApi.isPopupOpen()) {
       await BrowserPopupUtils.openCurrentPagePopout(window);
+    }
+  }
+
+  async sync() {
+    const success = await this.syncService.fullSync(true);
+    if (success) {
+      await this.setLastSync();
+      this.platformUtilsService.showToast("success", null, this.i18nService.t("syncingComplete"));
+    } else {
+      this.platformUtilsService.showToast("error", null, this.i18nService.t("syncingFailed"));
+    }
+  }
+
+  private async setLastSync() {
+    const last = await this.syncService.getLastSync();
+    if (last != null) {
+      this.lastSync = last.toLocaleDateString() + " " + last.toLocaleTimeString();
+    } else {
+      this.lastSync = this.i18nService.t("never");
     }
   }
 }
