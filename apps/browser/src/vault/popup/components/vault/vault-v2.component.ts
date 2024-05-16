3bcd7edc83684ae2813ed96ffb33fdd92afeb773
nick-livefront <nick@livefront.com>
2024-05-16 13:46:56 -0500

    remove VaultFilterService and add todo comment

--- apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
@@ -1,18 +1,10 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 
-import { VaultFilterService } from "../../../services/vault-filter.service";
-
 @Component({
   selector: "app-vault",
   templateUrl: "vault-v2.component.html",
 })
 export class VaultV2Component implements OnInit, OnDestroy {
-  selectedOrgId?: string;
-
-  constructor(private vaultFilterService: VaultFilterService) {
-    this.selectedOrgId = this.vaultFilterService.getVaultFilter().selectedOrganizationId;
-  }
-
   ngOnInit(): void {}
 
   ngOnDestroy(): void {}
