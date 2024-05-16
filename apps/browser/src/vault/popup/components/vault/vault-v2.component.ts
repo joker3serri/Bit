b406f82c5048da027dc8e82c124f5897ae9b3534
nick-livefront <nick@livefront.com>
2024-05-16 13:54:23 -0500

    bring back constructor

--- apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault/vault-v2.component.ts
@@ -5,6 +5,8 @@ import { Component, OnDestroy, OnInit } from "@angular/core";
   templateUrl: "vault-v2.component.html",
 })
 export class VaultV2Component implements OnInit, OnDestroy {
+  constructor() {}
+
   ngOnInit(): void {}
 
   ngOnDestroy(): void {}
