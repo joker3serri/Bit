957a2445c867f8eecc44b7f381ebb3b48ce32ee7
nick-livefront <nick@livefront.com>
2024-05-16 10:16:02 -0500

    use feature flag to drive pop out redesign

--- apps/browser/src/platform/popup/components/pop-out.component.ts
+++ apps/browser/src/platform/popup/components/pop-out.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from "@angular/common";
 import { Component, Input, OnInit } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { IconButtonModule } from "@bitwarden/components";
 
@@ -15,11 +17,16 @@ import BrowserPopupUtils from "../browser-popup-utils";
 })
 export class PopOutComponent implements OnInit {
   @Input() show = true;
-  @Input() useV2Variant = false;
+  useRefreshVariant = false;
 
-  constructor(private platformUtilsService: PlatformUtilsService) {}
+  constructor(
+    private platformUtilsService: PlatformUtilsService,
+    private configService: ConfigService,
+  ) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.useRefreshVariant = await this.configService.getFeatureFlag(FeatureFlag.ExtensionRefresh);
+
     if (this.show) {
       if (
         (BrowserPopupUtils.inSidebar(window) && this.platformUtilsService.isFirefox()) ||
