cdfed8029dc9f7537399470ad42a110441b4fe7c
Daniel James Smith <djsmith85@users.noreply.github.com>
2024-05-17 11:29:12 +0200

    Clean-up settings.component
    Some left-over clean-up from https://github.com/bitwarden/clients/pull/8840
    - Removing import and export methods

--- apps/browser/src/tools/popup/settings/settings.component.ts
+++ apps/browser/src/tools/popup/settings/settings.component.ts
@@ -1,54 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { Subject } from "rxjs";
-
-import { VaultTimeoutService } from "@bitwarden/common/abstractions/vault-timeout/vault-timeout.service";
-import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
-import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
-import { MessagingService } from "@bitwarden/common/platform/abstractions/messaging.service";
-import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
-import { DialogService } from "@bitwarden/components";
-
-import { BrowserApi } from "../../../platform/browser/browser-api";
-import BrowserPopupUtils from "../../../platform/popup/browser-popup-utils";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "tools-settings",
   templateUrl: "settings.component.html",
 })
-// eslint-disable-next-line rxjs-angular/prefer-takeuntil
-export class SettingsComponent implements OnInit {
-  private destroy$ = new Subject<void>();
-
-  constructor(
-    private platformUtilsService: PlatformUtilsService,
-    private i18nService: I18nService,
-    private vaultTimeoutService: VaultTimeoutService,
-    public messagingService: MessagingService,
-    private router: Router,
-    private environmentService: EnvironmentService,
-    private dialogService: DialogService,
-  ) {}
-
-  async ngOnInit() {}
-
-  async import() {
-    await this.router.navigate(["/import"]);
-    if (await BrowserApi.isPopupOpen()) {
-      // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      BrowserPopupUtils.openCurrentPagePopout(window);
-    }
-  }
-
-  export() {
-    // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.router.navigate(["/export"]);
-  }
-
-  ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+export class SettingsComponent {
+  constructor() {}
 }
