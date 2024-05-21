cf22c1489656f78e23ce84c333f20b08facb26ed
Jared Snider <jsnider@bitwarden.com>
2024-05-21 16:19:43 -0400

    PM-5086 - WIP first draft of registration env selector; name might change to differentiate it from existing env selector.

--- libs/auth/src/angular/registration/environment-selector/environment-selector.component.ts
+++ libs/auth/src/angular/registration/environment-selector/environment-selector.component.ts
@@ -1,24 +1,28 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
-import { firstValueFrom, map } from "rxjs";
+import { EMPTY, Subject, firstValueFrom, from, map, of, switchMap, takeUntil } from "rxjs";
 
+import { JslibModule } from "@bitwarden/angular/jslib.module";
 import {
   Environment,
   EnvironmentService,
   Region,
   RegionConfig,
 } from "@bitwarden/common/platform/abstractions/environment.service";
+import { FormFieldModule } from "@bitwarden/components";
 
 @Component({
   standalone: true,
   selector: "auth-environment-selector",
-  template: "./environment-selector.component.html",
-  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: "environment-selector.component.html",
+  imports: [CommonModule, JslibModule, ReactiveFormsModule, FormFieldModule],
 })
 export class EnvironmentSelectorComponent implements OnInit, OnDestroy {
+  @Output() onOpenSelfHostedSettings = new EventEmitter();
+
   formGroup = this.formBuilder.group({
-    selectedRegion: [null],
+    selectedRegion: [null, Validators.required],
   });
 
   get selectedRegion(): FormControl {
@@ -27,18 +31,19 @@ export class EnvironmentSelectorComponent implements OnInit, OnDestroy {
 
   availableRegionConfigs: RegionConfig[] = this.environmentService.availableRegions();
 
+  private destroy$ = new Subject<void>();
+
   constructor(
     private formBuilder: FormBuilder,
     private environmentService: EnvironmentService,
   ) {}
 
   async ngOnInit() {
-    await this.initializeSelectedRegion();
+    await this.initializeSelectedRegionValue();
+    this.listenForSelectedRegionChanges();
   }
 
-  private async initializeSelectedRegion() {
-    this.selectedRegion.setValidators(Validators.required);
-
+  private async initializeSelectedRegionValue() {
     // TODO: figure out if observable or promise is better here
     // this.environmentService.environment$
     //   .pipe(
@@ -68,5 +73,28 @@ export class EnvironmentSelectorComponent implements OnInit, OnDestroy {
     this.selectedRegion.setValue(selectedRegionConfig);
   }
 
-  ngOnDestroy() {}
+  private listenForSelectedRegionChanges() {
+    this.selectedRegion.valueChanges
+      .pipe(
+        switchMap((selectedRegionConfig: RegionConfig | null) => {
+          if (selectedRegionConfig === null) {
+            return of(null);
+          }
+
+          if (selectedRegionConfig.key === Region.SelfHosted) {
+            this.onOpenSelfHostedSettings.emit();
+            return EMPTY;
+          }
+
+          return from(this.environmentService.setEnvironment(selectedRegionConfig.key));
+        }),
+        takeUntil(this.destroy$),
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
