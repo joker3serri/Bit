9e5111d4a90620da8ff4c9050a9a5f177cd29f3a
KiruthigaManivannan <kiruthiga.manivannan@kovanlabs.com>
2024-05-21 11:49:17 +0530

    PM-2165 Added invalidSecret to form input

--- apps/web/src/app/auth/settings/account/delete-account-dialog.component.ts
+++ apps/web/src/app/auth/settings/account/delete-account-dialog.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder } from "@angular/forms";
 
 import { AccountApiService } from "@bitwarden/common/auth/abstractions/account-api.service";
 import { Verification } from "@bitwarden/common/auth/types/verification";
+import { ErrorResponse } from "@bitwarden/common/models/response/error.response";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { DialogService } from "@bitwarden/components";
@@ -15,6 +16,7 @@ export class DeleteAccountDialogComponent {
   deleteForm = this.formBuilder.group({
     verification: undefined as Verification | undefined,
   });
+  invalidSecret: boolean = false;
 
   constructor(
     private i18nService: I18nService,
@@ -25,14 +27,21 @@ export class DeleteAccountDialogComponent {
   ) {}
 
   submit = async () => {
-    const verification = this.deleteForm.get("verification").value;
-    await this.accountApiService.deleteAccount(verification);
-    this.dialogRef.close();
-    this.platformUtilsService.showToast(
-      "success",
-      this.i18nService.t("accountDeleted"),
-      this.i18nService.t("accountDeletedDesc"),
-    );
+    try {
+      const verification = this.deleteForm.get("verification").value;
+      await this.accountApiService.deleteAccount(verification);
+      this.dialogRef.close();
+      this.platformUtilsService.showToast(
+        "success",
+        this.i18nService.t("accountDeleted"),
+        this.i18nService.t("accountDeletedDesc"),
+      );
+    } catch (e) {
+      if (e instanceof ErrorResponse && e.statusCode === 400) {
+        this.invalidSecret = true;
+      }
+      throw e;
+    }
   };
 
   static open(dialogService: DialogService) {
