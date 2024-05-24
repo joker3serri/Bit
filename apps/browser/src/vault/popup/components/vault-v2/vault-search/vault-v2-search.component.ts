89342c9cb7a9ca7785e35e2ebb7931f815708ae2
jng <jng@bitwarden.com>
2024-05-24 12:01:38 -0400

    adding debounce to new vault v2 search component

--- apps/browser/src/vault/popup/components/vault-v2/vault-search/vault-v2-search.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/vault-search/vault-v2-search.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component, Output, EventEmitter } from "@angular/core";
+import { Component, Output, EventEmitter, OnDestroy } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { Subject, debounceTime, takeUntil } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { SearchModule } from "@bitwarden/components";
+
+const SearchTextDebounceInterval = 200;
 
 @Component({
   imports: [CommonModule, SearchModule, JslibModule, FormsModule],
@@ -11,12 +14,27 @@ import { SearchModule } from "@bitwarden/components";
   selector: "app-vault-v2-search",
   templateUrl: "vault-v2-search.component.html",
 })
-export class VaultV2SearchComponent {
+export class VaultV2SearchComponent implements OnDestroy {
   searchText: string;
   @Output() searchTextChanged = new EventEmitter<string>();
 
-  onSearchTextChanged(t: string) {
-    this.searchText = t;
-    this.searchTextChanged.emit(t);
+  private searchText$ = new Subject<string>();
+  private destroy$ = new Subject<void>();
+
+  constructor() {
+    this.searchText$
+      .pipe(debounceTime(SearchTextDebounceInterval), takeUntil(this.destroy$))
+      .subscribe((data) => {
+        this.searchTextChanged.emit(data);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onSearchTextChanged() {
+    this.searchText$.next(this.searchText);
   }
 }
