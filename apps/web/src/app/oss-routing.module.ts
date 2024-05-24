0712648dd4dc31ac8f1b6d8a8f5f83a5ac1ca55a
KiruthigaManivannan <kiruthiga.manivannan@kovanlabs.com>
2024-05-24 15:21:12 +0530

    PM-1943 Anon layout changes done

--- apps/web/src/app/oss-routing.module.ts
+++ apps/web/src/app/oss-routing.module.ts
@@ -7,7 +7,9 @@ import {
   redirectGuard,
   tdeDecryptionRequiredGuard,
   UnauthGuard,
+  unauthGuardFn,
 } from "@bitwarden/angular/auth/guards";
+import { AnonLayoutWrapperComponent } from "@bitwarden/auth/angular";
 
 import { flagEnabled, Flags } from "../utils/flags";
 
@@ -18,7 +20,6 @@ import { VerifyRecoverDeleteProviderComponent } from "./admin-console/providers/
 import { CreateOrganizationComponent } from "./admin-console/settings/create-organization.component";
 import { SponsoredFamiliesComponent } from "./admin-console/settings/sponsored-families.component";
 import { AcceptOrganizationComponent } from "./auth/accept-organization.component";
-import { AnonLayoutWrapperComponent } from "./auth/anon-layout-wrapper.component";
 import { deepLinkGuard } from "./auth/guards/deep-link.guard";
 import { HintComponent } from "./auth/hint.component";
 import { LockComponent } from "./auth/lock.component";
@@ -304,9 +305,10 @@ const routes: Routes = [
       {
         path: "recover-delete",
         component: RecoverDeleteComponent,
-        canActivate: [UnauthGuard],
+        canActivate: [unauthGuardFn()],
         data: {
           pageTitle: "deleteAccount",
+          titleId: "deleteAccount",
         },
       },
     ],
