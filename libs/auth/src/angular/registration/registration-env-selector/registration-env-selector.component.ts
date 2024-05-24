6e9c7a0e0823b51182e53061c8bb4a4e09cbc619
Jared Snider <jsnider@bitwarden.com>
2024-05-24 10:32:11 -0400

    PM-8367 - Majority of new registration self hosted env config dialog working

--- libs/auth/src/angular/registration/registration-env-selector/registration-env-selector.component.ts
+++ libs/auth/src/angular/registration/registration-env-selector/registration-env-selector.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from "@angular/forms";
-import { EMPTY, Subject, from, map, of, switchMap, takeUntil, tap } from "rxjs";
+import { Subject, from, map, of, pairwise, startWith, switchMap, takeUntil, tap } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import {
@@ -10,7 +10,9 @@ import {
   Region,
   RegionConfig,
 } from "@bitwarden/common/platform/abstractions/environment.service";
-import { FormFieldModule, SelectModule } from "@bitwarden/components";
+import { DialogService, FormFieldModule, SelectModule } from "@bitwarden/components";
+
+import { RegistrationSelfHostedEnvConfigDialogComponent } from "./registration-self-hosted-env-config-dialog.component";
 
 @Component({
   standalone: true,
@@ -19,8 +21,6 @@ import { FormFieldModule, SelectModule } from "@bitwarden/components";
   imports: [CommonModule, JslibModule, ReactiveFormsModule, FormFieldModule, SelectModule],
 })
 export class RegistrationEnvSelectorComponent implements OnInit, OnDestroy {
-  @Output() onOpenSelfHostedSettings = new EventEmitter();
-
   ServerEnvironmentType = Region;
 
   formGroup = this.formBuilder.group({
@@ -33,11 +33,14 @@ export class RegistrationEnvSelectorComponent implements OnInit, OnDestroy {
 
   availableRegionConfigs: RegionConfig[] = this.environmentService.availableRegions();
 
+  private selectedRegionFromEnv: RegionConfig | Region.SelfHosted;
+
   private destroy$ = new Subject<void>();
 
   constructor(
     private formBuilder: FormBuilder,
     private environmentService: EnvironmentService,
+    private dialogService: DialogService,
   ) {}
 
   async ngOnInit() {
@@ -61,13 +64,17 @@ export class RegistrationEnvSelectorComponent implements OnInit, OnDestroy {
 
           return regionConfig;
         }),
-        tap((selectedRegionInitialValue: RegionConfig | Region.SelfHosted) => {
-          // This inits the form control with the selected region, but
-          // it also sets the value to self hosted if the self hosted settings are saved successfully
-          // in the client specific implementation managed by the parent component.
-          // It also resets the value to the previously selected region if the self hosted
-          // settings are closed without saving. We don't emit the event to avoid a loop.
-          this.selectedRegion.setValue(selectedRegionInitialValue, { emitEvent: false });
+        tap((selectedRegionFromEnv: RegionConfig | Region.SelfHosted) => {
+          // Only set the value if it is different from the current value.
+          if (selectedRegionFromEnv !== this.selectedRegion.value) {
+            // Don't emit to avoid triggering the selectedRegion valueChanges subscription
+            // which could loop back to this code.
+            this.selectedRegion.setValue(selectedRegionFromEnv, { emitEvent: false });
+          }
+
+          // Save this off so we can reset the value to the previously selected region
+          // if the self hosted settings are closed without saving.
+          this.selectedRegionFromEnv = selectedRegionFromEnv;
         }),
         takeUntil(this.destroy$),
       )
@@ -77,18 +84,37 @@ export class RegistrationEnvSelectorComponent implements OnInit, OnDestroy {
   private listenForSelectedRegionChanges() {
     this.selectedRegion.valueChanges
       .pipe(
-        switchMap((selectedRegionConfig: RegionConfig | Region.SelfHosted | null) => {
-          if (selectedRegionConfig === null) {
-            return of(null);
-          }
+        startWith(null), // required so that first user choice is not ignored
+        pairwise(),
+        switchMap(
+          ([prevSelectedRegion, selectedRegion]: [
+            RegionConfig | Region.SelfHosted | null,
+            RegionConfig | Region.SelfHosted | null,
+          ]) => {
+            if (selectedRegion === null) {
+              return of(null);
+            }
 
-          if (selectedRegionConfig === Region.SelfHosted) {
-            this.onOpenSelfHostedSettings.emit();
-            return EMPTY;
-          }
+            if (selectedRegion === Region.SelfHosted) {
+              return from(
+                RegistrationSelfHostedEnvConfigDialogComponent.open(this.dialogService),
+              ).pipe(
+                tap((result: boolean | undefined) => {
+                  // Reset the value to the previously selected region or the current env setting
+                  // if the self hosted env settings dialog is closed without saving.
+                  if ((result === false || result === undefined) && prevSelectedRegion !== null) {
+                    this.selectedRegion.setValue(prevSelectedRegion, { emitEvent: false });
+                  } else {
+                    this.selectedRegion.setValue(this.selectedRegionFromEnv, { emitEvent: false });
+                  }
+                }),
+              );
+            }
 
-          return from(this.environmentService.setEnvironment(selectedRegionConfig.key));
-        }),
+            // You have access to previousValue here if needed
+            return from(this.environmentService.setEnvironment(selectedRegion.key));
+          },
+        ),
         takeUntil(this.destroy$),
       )
       .subscribe();
--- libs/auth/src/angular/registration/registration-env-selector/registration-self-hosted-env-config-dialog.component.ts
+++ libs/auth/src/angular/registration/registration-env-selector/registration-self-hosted-env-config-dialog.component.ts
@@ -1,31 +1,112 @@
+import { DialogRef } from "@angular/cdk/dialog";
 import { CommonModule } from "@angular/common";
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
-import { Subject } from "rxjs";
+import { FormBuilder, FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Subject, firstValueFrom } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
-import { FormFieldModule } from "@bitwarden/components";
+import {
+  EnvironmentService,
+  Region,
+} from "@bitwarden/common/platform/abstractions/environment.service";
+import {
+  AsyncActionsModule,
+  ButtonModule,
+  DialogModule,
+  DialogService,
+  FormFieldModule,
+  LinkModule,
+  TypographyModule,
+} from "@bitwarden/components";
 
 @Component({
   standalone: true,
   selector: "auth-registration-self-hosted-env-config-dialog",
   templateUrl: "registration-self-hosted-env-config-dialog.component.html",
-  imports: [CommonModule, JslibModule, ReactiveFormsModule, FormFieldModule],
+  imports: [
+    CommonModule,
+    JslibModule,
+    DialogModule,
+    ButtonModule,
+    LinkModule,
+    TypographyModule,
+    ReactiveFormsModule,
+    FormFieldModule,
+    AsyncActionsModule,
+  ],
 })
 export class RegistrationSelfHostedEnvConfigDialogComponent implements OnInit, OnDestroy {
+  static async open(dialogService: DialogService): Promise<boolean> {
+    const dialogRef = dialogService.open<boolean>(RegistrationSelfHostedEnvConfigDialogComponent, {
+      disableClose: false,
+    });
+
+    const dialogResult = await firstValueFrom(dialogRef.closed);
+
+    return dialogResult;
+  }
+
   formGroup = this.formBuilder.group({
-    // selectedRegion: [null, Validators.required],
+    baseUrl: [null],
+    webVaultUrl: [null],
+    apiUrl: [null],
+    identityUrl: [null],
+    iconsUrl: [null],
+    notificationsUrl: [null],
   });
+
+  get baseUrl(): FormControl {
+    return this.formGroup.get("baseUrl") as FormControl;
+  }
+
+  get webVaultUrl(): FormControl {
+    return this.formGroup.get("webVaultUrl") as FormControl;
+  }
+
+  get apiUrl(): FormControl {
+    return this.formGroup.get("apiUrl") as FormControl;
+  }
+
+  get identityUrl(): FormControl {
+    return this.formGroup.get("identityUrl") as FormControl;
+  }
+
+  get iconsUrl(): FormControl {
+    return this.formGroup.get("iconsUrl") as FormControl;
+  }
+
+  get notificationsUrl(): FormControl {
+    return this.formGroup.get("notificationsUrl") as FormControl;
+  }
+
+  showCustomEnv = false;
 
   private destroy$ = new Subject<void>();
 
   constructor(
+    private dialogRef: DialogRef<boolean>,
     private formBuilder: FormBuilder,
     private environmentService: EnvironmentService,
   ) {}
 
   ngOnInit() {}
+
+  submit = async () => {
+    await this.environmentService.setEnvironment(Region.SelfHosted, {
+      base: this.baseUrl.value,
+      api: this.apiUrl.value,
+      identity: this.identityUrl.value,
+      webVault: this.webVaultUrl.value,
+      icons: this.iconsUrl.value,
+      notifications: this.notificationsUrl.value,
+    });
+
+    this.dialogRef.close(true);
+  };
+
+  async cancel() {
+    this.dialogRef.close(false);
+  }
 
   ngOnDestroy() {
     this.destroy$.next();
