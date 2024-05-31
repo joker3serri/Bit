e7220282e46b1614b6254ba4b5194671cc5db2c4
nick-livefront <nick@livefront.com>
2024-05-31 13:39:50 -0500

    initial add of assign collections component

--- apps/browser/src/popup/app-routing.module.ts
+++ apps/browser/src/popup/app-routing.module.ts
@@ -72,6 +72,7 @@ import { ViewComponent } from "../vault/popup/components/vault/view.component";
 import { AddEditV2Component } from "../vault/popup/components/vault-v2/add-edit/add-edit-v2.component";
 import { AttachmentsV2Component } from "../vault/popup/components/vault-v2/attachments/attachments-v2.component";
 import { ViewV2Component } from "../vault/popup/components/vault-v2/view-v2/view-v2.component";
+import { AssignCollections } from "../vault/popup/components/vault-v2/assign-collections/assign-collections.component";
 import { AppearanceComponent } from "../vault/popup/settings/appearance.component";
 import { FolderAddEditComponent } from "../vault/popup/settings/folder-add-edit.component";
 import { FoldersComponent } from "../vault/popup/settings/folders.component";
@@ -363,6 +364,12 @@ const routes: Routes = [
     data: { state: "edit-send" },
   },
   {
+    path: "assign-collections",
+    component: AssignCollections,
+    canActivate: [canAccessFeature(FeatureFlag.ExtensionRefresh, true, "/")],
+    data: { state: "assign-collections" },
+  },
+  {
     path: "update-temp-password",
     component: UpdateTempPasswordComponent,
     canActivate: [AuthGuard],
@@ -500,4 +507,4 @@ export class NoRouteReuseStrategy implements RouteReuseStrategy {
   exports: [RouterModule],
   providers: [{ provide: RouteReuseStrategy, useClass: NoRouteReuseStrategy }],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
