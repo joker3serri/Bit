c106a1bd3ccc7d62d4efceff40ab04a49c73ca71
KiruthigaManivannan <kiruthiga.manivannan@kovanlabs.com>
2024-06-03 19:34:48 +0530

    PM-8482 Change KDF Confirmation no longer has browser validation

--- apps/web/src/app/auth/settings/security/change-kdf/change-kdf-confirmation.component.ts
+++ apps/web/src/app/auth/settings/security/change-kdf/change-kdf-confirmation.component.ts
@@ -41,6 +41,9 @@ export class ChangeKdfConfirmationComponent {
   }
 
   submit = async () => {
+    if (this.form.value.masterPassword === null) {
+      return;
+    }
     this.loading = true;
     await this.makeKeyAndSaveAsync();
     this.platformUtilsService.showToast(
