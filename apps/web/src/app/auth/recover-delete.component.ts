e633bd2e6d5161d0ede125a98b41ffc858b67372
Jared Snider <jsnider@bitwarden.com>
2024-06-04 12:23:15 -0400

    PM-1943 - await navigate

--- apps/web/src/app/auth/recover-delete.component.ts
+++ apps/web/src/app/auth/recover-delete.component.ts
@@ -32,8 +32,7 @@ export class RecoverDeleteComponent {
       null,
       this.i18nService.t("deleteRecoverEmailSent"),
     );
-    // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    this.router.navigate(["/"]);
+
+    await this.router.navigate(["/"]);
   };
 }
