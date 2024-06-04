5b8821d3dddf3e977394a37260b311d9e73a4017
nick-livefront <nick@livefront.com>
2024-06-04 16:18:51 -0500

    use singular verbiage for cipher type

--- apps/browser/src/vault/popup/services/vault-popup-list-filters.service.ts
+++ apps/browser/src/vault/popup/services/vault-popup-list-filters.service.ts
@@ -139,22 +139,22 @@ export class VaultPopupListFiltersService {
   readonly cipherTypes: ChipSelectOption<CipherType>[] = [
     {
       value: CipherType.Login,
-      label: this.i18nService.t("logins"),
+      label: this.i18nService.t("loginOneWord"),
       icon: "bwi-globe",
     },
     {
       value: CipherType.Card,
-      label: this.i18nService.t("cards"),
+      label: this.i18nService.t("card"),
       icon: "bwi-credit-card",
     },
     {
       value: CipherType.Identity,
-      label: this.i18nService.t("identities"),
+      label: this.i18nService.t("identity"),
       icon: "bwi-id-card",
     },
     {
       value: CipherType.SecureNote,
-      label: this.i18nService.t("notes"),
+      label: this.i18nService.t("note"),
       icon: "bwi-sticky-note",
     },
   ];
