fba3121bbed4d697bb2dce47cba798ef99363042
Jared Snider <jsnider@bitwarden.com>
2024-06-07 17:49:45 -0400

    PM-7324 - Add registerSendVerificationEmail logic in API service layer.

--- libs/common/src/auth/abstractions/account-api.service.ts
+++ libs/common/src/auth/abstractions/account-api.service.ts
@@ -1,5 +1,9 @@
+import { RegisterSendVerificationEmailRequest } from "../models/request/registration/register-send-verification-email.request";
 import { Verification } from "../types/verification";
 
 export abstract class AccountApiService {
   abstract deleteAccount(verification: Verification): Promise<void>;
+  abstract registerSendVerificationEmail(
+    request: RegisterSendVerificationEmailRequest,
+  ): Promise<null | string>;
 }
