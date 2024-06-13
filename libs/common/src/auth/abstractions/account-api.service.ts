0371b62564b83a987844e671a53de61cc0f2fdc2
Jared Snider <jsnider@bitwarden.com>
2024-06-13 17:43:20 -0400

    PM-7324 - PR feedback - jsdoc tweaks

--- libs/common/src/auth/abstractions/account-api.service.ts
+++ libs/common/src/auth/abstractions/account-api.service.ts
@@ -3,22 +3,23 @@ import { Verification } from "../types/verification";
 
 export abstract class AccountApiService {
   /**
-   * Deletes an account based on the provided verification information.
+   * Deletes an account that has confirmed the operation is authorized
    *
-   * @param {Verification} verification - The verification object containing information
-   * required to authorize the account deletion.
-   * @returns {Promise<void>} A promise that resolves when the account is successfully deleted.
+   * @param verification - authorizes the account deletion operation.
+   * @returns A promise that resolves when the account is
+   * successfully deleted.
    */
   abstract deleteAccount(verification: Verification): Promise<void>;
 
   /**
    * Sends a verification email as part of the registration process.
    *
-   * @param {RegisterSendVerificationEmailRequest} request - The request object containing
+   * @param request - The request object containing
    * information needed to send the verification email, such as the user's email address.
-   * @returns {Promise<null | string>} A promise that resolves to a string token
-   * containing the user's encrypted information which must be submitted to complete registration
-   * or `null` if email verification is enabled (users must get the token by clicking a link in the email that will be sent to them).
+   * @returns A promise that resolves to a string tokencontaining the user's encrypted
+   * information which must be submitted to complete registration or `null` if
+   * email verification is enabled (users must get the token by clicking a
+   * link in the email that will be sent to them).
    */
   abstract registerSendVerificationEmail(
     request: RegisterSendVerificationEmailRequest,
