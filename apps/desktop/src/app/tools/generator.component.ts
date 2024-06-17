e8ccef0c88b8c6aebd5602111f5c55a2795f71a1
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-06-17 09:46:18 -0400

    fix angular DI

--- apps/desktop/src/app/tools/generator.component.ts
+++ apps/desktop/src/app/tools/generator.component.ts
@@ -6,7 +6,10 @@ import { AccountService } from "@bitwarden/common/auth/abstractions/account.serv
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
-import { legacyPassword, legacyUsername } from "@bitwarden/generator-extensions";
+import {
+  PasswordGenerationServiceAbstraction,
+  UsernameGenerationServiceAbstraction,
+} from "@bitwarden/generator-extensions";
 
 @Component({
   selector: "app-generator",
@@ -14,8 +17,8 @@ import { legacyPassword, legacyUsername } from "@bitwarden/generator-extensions"
 })
 export class GeneratorComponent extends BaseGeneratorComponent {
   constructor(
-    passwordGenerationService: legacyPassword.PasswordGenerationServiceAbstraction,
-    usernameGenerationService: legacyUsername.UsernameGenerationServiceAbstraction,
+    passwordGenerationService: PasswordGenerationServiceAbstraction,
+    usernameGenerationService: UsernameGenerationServiceAbstraction,
     accountService: AccountService,
     platformUtilsService: PlatformUtilsService,
     i18nService: I18nService,
