d86ff65eab81a191645d53d8f7c33e966c1faa48
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-07-04 11:22:58 +0200

    feat: scaffold desktop_objc

--- apps/desktop/desktop_native/core/src/lib.rs
+++ apps/desktop/desktop_native/core/src/lib.rs
@@ -4,13 +4,13 @@ pub mod biometric;
 pub mod clipboard;
 pub mod crypto;
 pub mod error;
+pub mod fido2;
 pub mod ipc;
 #[cfg(feature = "sys")]
 pub mod password;
 #[cfg(feature = "sys")]
-pub mod process_isolation;
-#[cfg(feature = "sys")]
 pub mod powermonitor;
 #[cfg(feature = "sys")]
-
+pub mod process_isolation;
+#[cfg(feature = "sys")]
 pub mod ssh_agent;
