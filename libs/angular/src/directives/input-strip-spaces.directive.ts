0d4bf012c31744d54b3e86db34f5f12d9d4bee45
Conner Turnbull <cturnbull@bitwarden.com>
2024-07-08 14:51:53 -0400

    Updated appInputStripSpaces to update both the element and the control value

--- libs/angular/src/directives/input-strip-spaces.directive.ts
+++ libs/angular/src/directives/input-strip-spaces.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, ElementRef, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Renderer2, Self } from "@angular/core";
+import { NgControl } from "@angular/forms";
 
 @Directive({
   selector: "input[appInputStripSpaces]",
 })
 export class InputStripSpacesDirective {
-  constructor(private el: ElementRef<HTMLInputElement>) {}
+  constructor(
+    private el: ElementRef<HTMLInputElement>,
+    private renderer: Renderer2,
+    @Self() private ngControl: NgControl,
+  ) {}
 
   @HostListener("input") onInput() {
-    this.el.nativeElement.value = this.el.nativeElement.value.replace(/ /g, "");
+    const value = this.el.nativeElement.value.replace(/\s+/g, "");
+    this.renderer.setProperty(this.el.nativeElement, "value", value);
+    this.ngControl.control.setValue(value, { emitEvent: false });
   }
 }
