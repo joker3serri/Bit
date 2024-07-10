4b81cb49c1d84cd153de1d08b910798412fa56dd
Conner Turnbull <cturnbull@bitwarden.com>
2024-07-10 12:00:50 -0400

    Removed the call to Renderer2 as it wasn't needed

--- libs/angular/src/directives/input-strip-spaces.directive.ts
+++ libs/angular/src/directives/input-strip-spaces.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener, Renderer2, Self } from "@angular/core";
+import { Directive, ElementRef, HostListener, Self } from "@angular/core";
 import { NgControl } from "@angular/forms";
 
 @Directive({
@@ -7,13 +7,11 @@ import { NgControl } from "@angular/forms";
 export class InputStripSpacesDirective {
   constructor(
     private el: ElementRef<HTMLInputElement>,
-    private renderer: Renderer2,
     @Self() private ngControl: NgControl,
   ) {}
 
   @HostListener("input") onInput() {
     const value = this.el.nativeElement.value.replace(/\s+/g, "");
-    this.renderer.setProperty(this.el.nativeElement, "value", value);
-    this.ngControl.control.setValue(value, { emitEvent: false });
+    this.ngControl.control.setValue(value);
   }
 }
