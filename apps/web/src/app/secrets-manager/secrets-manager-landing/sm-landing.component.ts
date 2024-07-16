de6109a7917dabf15fe63142b9fab7915f09c76b
cd-bitwarden <106776772+cd-bitwarden@users.noreply.github.com>
2024-07-16 17:00:53 -0400

    New awesome changes, to include the scenario where a Provider user is logged in, and to handle if an admin needs instructions to enable SM for themselves

--- apps/web/src/app/secrets-manager/secrets-manager-landing/sm-landing.component.ts
+++ apps/web/src/app/secrets-manager/secrets-manager-landing/sm-landing.component.ts
@@ -1,5 +1,4 @@
 import { Component } from "@angular/core";
-import { Router } from "@angular/router";
 
 import { OrganizationService } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
 import { Organization } from "@bitwarden/common/admin-console/models/domain/organization";
@@ -15,30 +14,61 @@ import { SharedModule } from "../../shared/shared.module";
   templateUrl: "sm-landing.component.html",
 })
 export class SMLandingComponent {
-  constructor(
-    private router: Router,
-    private organizationService: OrganizationService,
-  ) {}
-
   tryItNowUrl: string;
+  learnMoreUrl: string = "https://bitwarden.com/help/secrets-manager-overview/";
+  imageSrc: string = "../images/sm.webp";
+  showSecretsManagerInformation: boolean = true;
+  showGiveMembersAccessInstructions: boolean = false;
+
+  constructor(private organizationService: OrganizationService) {}
 
   async ngOnInit() {
-    const enabledOrgs = (await this.organizationService.getAll()).filter((e) => e.enabled);
+    const organizations = await this.organizationService.getAll();
 
-    if (enabledOrgs?.length > 0) {
-      const orgsUserIsAdminOf: Organization[] = enabledOrgs.filter((o) => o.isAdmin);
-      //User is a part of at least one org and user is an admin of at least one
-      if (orgsUserIsAdminOf.length > 0) {
-        const organizationId = orgsUserIsAdminOf[0].id;
-
-        this.tryItNowUrl = `/organizations/${organizationId}/billing/subscription`;
-      } else {
-        //User is not an admin but there are orgs
-        this.tryItNowUrl = "/request-sm-access";
-      }
+    if (organizations.length > 0) {
+      this.handleEnabledOrganizations(organizations);
     } else {
-      //User needs to create an org to sign up for secrets manager
+      // Person is not part of any orgs they need to be in an organization in order to use SM
       this.tryItNowUrl = "/create-organization";
     }
+  }
+
+  private handleEnabledOrganizations(enabledOrgs: Organization[]) {
+    // People get to this page because SM (Secrets Manager) isn't enabled for them (or the Organization they are a part of)
+    // 1 - SM is enabled for the Organization but not that user
+    //1a - person is Admin+ (Admin or higher) and just needs instructions on how to enable it for themselves
+    //1b - person is beneath admin status and needs to request SM access from Administrators/Owners
+    // 2 - SM is not enabled for the organization yet
+    //2a - person is Owner/Provider - Direct them to the subscription/billing page
+    //2b - person is Admin - Direct them to request access page where an email is sent to owner/admins
+    //2c - person is user - Direct them to request access page where an email is sent to owner/admins
+
+    // We use useSecretsManager because we want to get the first org the person is a part of where SM is enabled but they don't have access enabled yet
+    const adminPlusNeedsInstructionsToEnableSM = enabledOrgs.filter(
+      (o) => o.isAdmin && o.useSecretsManager,
+    );
+    const ownerNeedsToEnableSM = enabledOrgs.filter((o) => o.isOwner && !o.useSecretsManager);
+
+    // 1a If Organization has SM Enabled, but this logged in person does not have it enabled, but they are admin+ then give them instructions to enable.
+    if (adminPlusNeedsInstructionsToEnableSM.length > 0) {
+      this.ShowHowToEnableSMForMembers(adminPlusNeedsInstructionsToEnableSM[0].id);
+    }
+    // 2a Owners can enable SM in the subscription area of Admin Console.
+    else if (ownerNeedsToEnableSM.length > 0) {
+      this.tryItNowUrl = `/organizations/${ownerNeedsToEnableSM[0].id}/billing/subscription`;
+    }
+    // 1b and 2b 2c, they must be lower than an Owner, and they need access, or want their org to have access to SM.
+    else {
+      this.tryItNowUrl = "/request-sm-access";
+    }
+  }
+
+  private ShowHowToEnableSMForMembers(orgId: string) {
+    this.showGiveMembersAccessInstructions = true;
+    this.showSecretsManagerInformation = false;
+    this.learnMoreUrl =
+      "https://bitwarden.com/help/secrets-manager-quick-start/#give-members-access";
+    this.imageSrc = "../images/sm-give-access.png";
+    this.tryItNowUrl = `/organizations/${orgId}/members`;
   }
 }
