0dcc59a133ff0032fed0aaa7bf7b0018bb9fbc13
Vicki League <vleague@bitwarden.com>
2024-07-16 09:45:11 -0400

    [CL-305] Allow margin to be disabled for elements at the bottom of a page

--- libs/components/src/section/section.component.ts
+++ libs/components/src/section/section.component.ts
@@ -1,14 +1,25 @@
+import { coerceBooleanProperty } from "@angular/cdk/coercion";
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "bit-section",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <section class="tw-mb-6 md:tw-mb-12">
+    <section [ngClass]="sectionClass">
       <ng-content></ng-content>
     </section>
   `,
 })
-export class SectionComponent {}
+export class SectionComponent {
+  @Input({ transform: coerceBooleanProperty }) disableMargin = false;
+
+  get sectionClass() {
+    if (this.disableMargin) {
+      return [];
+    }
+
+    return ["tw-mb-6", "md:tw-mb-12"];
+  }
+}
