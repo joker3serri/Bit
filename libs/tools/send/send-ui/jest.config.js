e7198ec860ddb34e4ad157b05dc3978dcfaa5231
jaasen-livefront <jaasen@livefront.com>
2024-07-16 17:59:49 -0700

    undo changes to jest config

--- libs/tools/send/send-ui/jest.config.js
+++ libs/tools/send/send-ui/jest.config.js
@@ -5,10 +5,6 @@ const { compilerOptions } = require("../../../shared/tsconfig.libs");
 /** @type {import('jest').Config} */
 module.exports = {
   testMatch: ["**/+(*.)+(spec).+(ts)"],
-  roots: ["<rootDir>/libs/tools/send/send-ui/src"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths || {}, {
