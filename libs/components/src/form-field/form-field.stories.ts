daa3e6123cde15b00b093d78c832c88283b0a7a8
William Martin <contact@willmartian.com>
2024-07-18 15:25:54 -0400

    update label styles; update stories

--- libs/components/src/form-field/form-field.stories.ts
+++ libs/components/src/form-field/form-field.stories.ts
@@ -1,3 +1,4 @@
+import { TextFieldModule } from "@angular/cdk/text-field";
 import {
   AbstractControl,
   UntypedFormBuilder,
@@ -12,6 +13,7 @@ import { Meta, StoryObj, moduleMetadata } from "@storybook/angular";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 
 import { AsyncActionsModule } from "../async-actions";
+import { BadgeModule } from "../badge";
 import { ButtonModule } from "../button";
 import { CardComponent } from "../card";
 import { CheckboxModule } from "../checkbox";
@@ -19,6 +21,7 @@ import { IconButtonModule } from "../icon-button";
 import { InputModule } from "../input/input.module";
 import { LinkModule } from "../link";
 import { RadioButtonModule } from "../radio-button";
+import { SectionComponent } from "../section";
 import { SelectModule } from "../select";
 import { I18nMockService } from "../utils/i18n-mock.service";
 
@@ -43,6 +46,9 @@ export default {
         SelectModule,
         LinkModule,
         CardComponent,
+        SectionComponent,
+        TextFieldModule,
+        BadgeModule,
       ],
       providers: [
         {
@@ -229,7 +235,8 @@ export const Readonly: Story = {
         <textarea bitInput rows="4" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
       </bit-form-field>
 
-      <div class="tw-p-4 tw-mt-6 tw-bg-background-alt">
+      <div class="tw-p-4 tw-mt-10 tw-border-2 tw-border-solid tw-border-black tw-bg-background-alt">
+        <h2 bitTypography="h2">Inside card</h2>
         <bit-section>
           <bit-card>
             <bit-form-field>
@@ -245,12 +252,14 @@ export const Readonly: Story = {
             </bit-form-field>
 
             <bit-form-field>
-              <bit-label>Textarea</bit-label>
-              <textarea bitInput rows="4" readonly>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+              <bit-label>Textarea <span bitBadge variant="success">Premium</span></bit-label>
+              <textarea bitInput rows="3" readonly class="tw-resize-none">Row1
+Row2 
+Row3</textarea>
             </bit-form-field>
 
             <bit-form-field disableMargin disableReadOnlyBorder>
-              <bit-label>Input</bit-label>
+              <bit-label>Sans margin & border</bit-label>
               <input bitInput value="Foobar" readonly />
             </bit-form-field>
           </bit-card>
