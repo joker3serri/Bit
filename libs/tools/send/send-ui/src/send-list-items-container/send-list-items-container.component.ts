a18d509409731039708c02ec11cf524c5c60a541
jaasen-livefront <jaasen@livefront.com>
2024-07-18 16:54:22 -0700

    update send list items container

--- libs/tools/send/send-ui/src/send-list-items-container/send-list-items-container.component.ts
+++ libs/tools/send/send-ui/src/send-list-items-container/send-list-items-container.component.ts
@@ -1,21 +1,27 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { firstValueFrom } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { EnvironmentService } from "@bitwarden/common/platform/abstractions/environment.service";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
+import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { SendType } from "@bitwarden/common/tools/send/enums/send-type";
 import { SendView } from "@bitwarden/common/tools/send/models/view/send.view";
+import { SendApiService } from "@bitwarden/common/tools/send/services/send-api.service.abstraction";
 import {
   BadgeModule,
   ButtonModule,
+  DialogService,
   IconButtonModule,
   ItemModule,
   SectionComponent,
   SectionHeaderComponent,
+  ToastService,
   TypographyModule,
 } from "@bitwarden/components";
-
-import { ContainerComponent } from "../../../../../components/src/container/container.component";
 
 @Component({
   imports: [
@@ -29,7 +35,6 @@ import { ContainerComponent } from "../../../../../components/src/container/cont
     JslibModule,
     SectionHeaderComponent,
     RouterLink,
-    ContainerComponent,
   ],
   selector: "app-send-list-items-container",
   templateUrl: "send-list-items-container.component.html",
@@ -42,4 +47,47 @@ export class SendListItemsContainerComponent {
    */
   @Input()
   sends: SendView[] = [];
+
+  constructor(
+    protected dialogService: DialogService,
+    protected environmentService: EnvironmentService,
+    protected i18nService: I18nService,
+    protected logService: LogService,
+    protected platformUtilsService: PlatformUtilsService,
+    protected sendApiService: SendApiService,
+    protected toastService: ToastService,
+  ) {}
+
+  async deleteSend(s: SendView): Promise<boolean> {
+    const confirmed = await this.dialogService.openSimpleDialog({
+      title: { key: "deleteSend" },
+      content: { key: "deleteSendConfirmation" },
+      type: "warning",
+    });
+
+    if (!confirmed) {
+      return false;
+    }
+
+    try {
+      this.toastService.showToast({
+        variant: "success",
+        title: null,
+        message: this.i18nService.t("deletedSend"),
+      });
+    } catch (e) {
+      this.logService.error(e);
+    }
+  }
+
+  async copy(s: SendView) {
+    const env = await firstValueFrom(this.environmentService.environment$);
+    const link = env.getSendUrl() + s.accessId + "/" + s.urlB64Key;
+    this.platformUtilsService.copyToClipboard(link);
+    this.toastService.showToast({
+      variant: "success",
+      title: null,
+      message: this.i18nService.t("valueCopied", this.i18nService.t("sendLink")),
+    });
+  }
 }
