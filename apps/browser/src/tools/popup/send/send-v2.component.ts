81e9860c254904700c28985b07b40b36923ca312
jaasen-livefront <jaasen@livefront.com>
2024-07-19 11:26:50 -0700

    Revert "Merge branch 'PM-9853' into PM-9852"
    This reverts commit 9f65ded13f1dfd38c80da6e0ce4f1d0c48eb8b59, reversing
    changes made to 63f95600e847afa28ef0e200543c3dd90d5ac233.

--- apps/browser/src/tools/popup/send/send-v2.component.ts
+++ apps/browser/src/tools/popup/send/send-v2.component.ts
@@ -1,23 +1,20 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { mergeMap, Subject, takeUntil } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { SendType } from "@bitwarden/common/tools/send/enums/send-type";
-import { SendView } from "@bitwarden/common/tools/send/models/view/send.view";
-import { SendService } from "@bitwarden/common/tools/send/services/send.service.abstraction";
 import { ButtonModule, NoItemsModule } from "@bitwarden/components";
-import {
-  NoSendsIcon,
-  NewSendDropdownComponent,
-  SendListItemsContainerComponent,
-} from "@bitwarden/send-ui";
+import { NoSendsIcon, NewSendDropdownComponent } from "@bitwarden/send-ui";
 
 import { CurrentAccountComponent } from "../../../auth/popup/account-switching/current-account.component";
 import { PopOutComponent } from "../../../platform/popup/components/pop-out.component";
 import { PopupHeaderComponent } from "../../../platform/popup/layout/popup-header.component";
 import { PopupPageComponent } from "../../../platform/popup/layout/popup-page.component";
+
+enum SendsListState {
+  Empty,
+}
 
 @Component({
   templateUrl: "send-v2.component.html",
@@ -33,30 +30,23 @@ import { PopupPageComponent } from "../../../platform/popup/layout/popup-page.co
     ButtonModule,
     RouterLink,
     NewSendDropdownComponent,
-    SendListItemsContainerComponent,
   ],
 })
 export class SendV2Component implements OnInit, OnDestroy {
   sendType = SendType;
 
-  protected sends: SendView[] = [];
-
-  private destroy$ = new Subject<void>();
+  /** Visual state of the Sends list */
+  protected sendsListState: SendsListState | null = null;
 
   protected noItemIcon = NoSendsIcon;
 
-  constructor(protected sendService: SendService) {}
+  protected SendsListStateEnum = SendsListState;
 
-  async ngOnInit() {
-    this.sendService.sendViews$
-      .pipe(
-        mergeMap(async (sends) => {
-          this.sends = sends.sort((a, b) => a.name.localeCompare(b.name));
-        }),
-        takeUntil(this.destroy$),
-      )
-      .subscribe();
+  constructor() {
+    this.sendsListState = SendsListState.Empty;
   }
+
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {}
 }
--- libs/tools/send/send-ui/src/index.ts
+++ libs/tools/send/send-ui/src/index.ts
@@ -1,3 +1,2 @@
 export * from "./icons";
 export { NewSendDropdownComponent } from "./new-send-dropdown/new-send-dropdown.component";
-export { SendListItemsContainerComponent } from "./send-list-items-container/send-list-items-container.component";
