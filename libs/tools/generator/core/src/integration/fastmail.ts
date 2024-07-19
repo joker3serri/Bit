de4137cd6df2a983df4cd4bf68660987d0fa05b1
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-07-19 15:54:49 -0400

    another attempt at fixing the typecheck

--- libs/tools/generator/core/src/integration/fastmail.ts
+++ libs/tools/generator/core/src/integration/fastmail.ts
@@ -8,12 +8,13 @@ import {
   ForwarderContext,
   EmailDomainSettings,
   AccountRequest,
+  EmailPrefixSettings,
 } from "../engine";
 import { CreateForwardingEmailRpcDef, GetAccountIdRpcDef } from "../engine/forwarder-configuration";
 import { ApiOptions, EmailPrefixOptions } from "../types";
 
 // integration types
-export type FastmailSettings = ApiSettings & EmailDomainSettings;
+export type FastmailSettings = ApiSettings & EmailPrefixSettings & EmailDomainSettings;
 export type FastmailOptions = ApiOptions & EmailPrefixOptions & AccountRequest;
 export type FastmailRequest = IntegrationRequest & AccountRequest;
 export type FastmailConfiguration = ForwarderConfiguration<FastmailSettings, FastmailRequest>;
