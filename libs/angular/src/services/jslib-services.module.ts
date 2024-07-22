a6086ec9c8821e33fd62ef72f631c1a6c5e046f7
Alex Morask <amorask@bitwarden.com>
2024-07-22 13:50:38 -0400

    Update client side error handling and remove add account credit from sub

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -1195,7 +1195,7 @@ const safeProviders: SafeProvider[] = [
   safeProvider({
     provide: BillingApiServiceAbstraction,
     useClass: BillingApiService,
-    deps: [ApiServiceAbstraction],
+    deps: [ApiServiceAbstraction, LogService, ToastService],
   }),
   safeProvider({
     provide: PaymentMethodWarningsServiceAbstraction,
