dfa66a50e5ed449bb9bad92d4147e435db605a79
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-07-30 12:48:19 -0500

    [PM-10155] Implementing jest tests for the AutoSubmitLogin content script

--- apps/browser/src/autofill/content/auto-submit-login.spec.ts
+++ apps/browser/src/autofill/content/auto-submit-login.spec.ts
@@ -97,7 +97,7 @@ describe("AutoSubmitLogin content script", () => {
         ];
         fillScriptMock = createAutofillScriptMock(
           {
-            autosubmit: [null, null],
+            autosubmit: [null],
           },
           { "name-field": "name-value", "password-field": "password-value" },
         );
@@ -155,7 +155,7 @@ describe("AutoSubmitLogin content script", () => {
         expect(submitButton.click).toHaveBeenCalled();
       });
 
-      it("triggers the submit action when the field is within a shadowDOM", async () => {
+      it("triggers the submit action when the field is within a shadow root", async () => {
         createFormlessShadowRootFields();
         const submitButton = document.querySelector("input[type=submit]") as HTMLInputElement;
         jest.spyOn(submitButton, "click");
@@ -171,7 +171,123 @@ describe("AutoSubmitLogin content script", () => {
       });
     });
 
-    describe("triggering auto-submit on a form", () => {});
+    describe("triggering auto-submit on a form", () => {
+      beforeEach(async () => {
+        pageDetailsMock.fields = [
+          createAutofillFieldMock({
+            htmlID: "username",
+            formOpid: "__form0__",
+            opid: "name-field",
+          }),
+          createAutofillFieldMock({
+            htmlID: "password",
+            type: "password",
+            formOpid: "__form0__",
+            opid: "password-field",
+          }),
+        ];
+        fillScriptMock = createAutofillScriptMock(
+          {
+            autosubmit: ["__form0__"],
+          },
+          { "name-field": "name-value", "password-field": "password-value" },
+        );
+        document.body.innerHTML = `
+          <form>
+            <div>
+              <div>
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
+              </div>
+              <div>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+              </div>
+            </div>
+            <div class="submit-container">
+              <input type="submit" value="Submit">
+            </div>
+          </form>
+        `;
+        const formElement = document.querySelector("form") as HTMLFormElement;
+        (formElement as any).opid = "__form0__";
+        formElement.addEventListener("submit", (e) => e.preventDefault());
+        const passwordElement = document.getElementById("password") as HTMLInputElement;
+        (passwordElement as any).opid = "password-field";
+        await initAutoSubmitWorkflow();
+      });
+
+      it("attempts to trigger submission of the element as a formless field if the form cannot be found by opid", async () => {
+        const formElement = document.querySelector("form") as HTMLFormElement;
+        (formElement as any).opid = "__form1__";
+        const submitButton = document.querySelector(
+          ".submit-container input[type=submit]",
+        ) as HTMLInputElement;
+        jest.spyOn(submitButton, "click");
+
+        sendMockExtensionMessage({
+          command: "triggerAutoSubmitLogin",
+          fillScript: fillScriptMock,
+          pageDetailsUrl: globalThis.location.href,
+        });
+        await flushPromises();
+
+        expect(submitButton.click).toHaveBeenCalled();
+      });
+
+      it("triggers the submit action on an element that contains a type=Submit attribute", async () => {
+        const submitButton = document.querySelector(
+          ".submit-container input[type=submit]",
+        ) as HTMLInputElement;
+        jest.spyOn(submitButton, "click");
+
+        sendMockExtensionMessage({
+          command: "triggerAutoSubmitLogin",
+          fillScript: fillScriptMock,
+          pageDetailsUrl: globalThis.location.href,
+        });
+        await flushPromises();
+
+        expect(submitButton.click).toHaveBeenCalled();
+      });
+
+      it("triggers the form's requestSubmit method when the form does not contain an button to allow submission", async () => {
+        const submitButton = document.querySelector(
+          ".submit-container input[type=submit]",
+        ) as HTMLInputElement;
+        submitButton.remove();
+        const formElement = document.querySelector("form") as HTMLFormElement;
+        jest.spyOn(formElement, "requestSubmit").mockImplementation();
+
+        sendMockExtensionMessage({
+          command: "triggerAutoSubmitLogin",
+          fillScript: fillScriptMock,
+          pageDetailsUrl: globalThis.location.href,
+        });
+        await flushPromises();
+
+        expect(formElement.requestSubmit).toHaveBeenCalled();
+      });
+
+      it("triggers the form's submit method when the requestSubmit method is not available", async () => {
+        const submitButton = document.querySelector(
+          ".submit-container input[type=submit]",
+        ) as HTMLInputElement;
+        submitButton.remove();
+        const formElement = document.querySelector("form") as HTMLFormElement;
+        formElement.requestSubmit = undefined;
+        jest.spyOn(formElement, "submit").mockImplementation();
+
+        sendMockExtensionMessage({
+          command: "triggerAutoSubmitLogin",
+          fillScript: fillScriptMock,
+          pageDetailsUrl: globalThis.location.href,
+        });
+        await flushPromises();
+
+        expect(formElement.submit).toHaveBeenCalled();
+      });
+    });
   });
 });
 
