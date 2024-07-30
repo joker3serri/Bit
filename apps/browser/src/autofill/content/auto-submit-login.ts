c06461a3379f60851fa8c056b7552cfd655e77d6
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-07-30 09:58:26 -0500

    [PM-10155] Refactoring the auto-submit-login.ts content script and adding documentation to the methods of the script

--- apps/browser/src/autofill/content/auto-submit-login.ts
+++ apps/browser/src/autofill/content/auto-submit-login.ts
@@ -23,11 +23,17 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     "sign-in",
     "submit",
     "continue",
+    "next",
   ];
-  let autoSubmitLoginFlowTimeout: number | NodeJS.Timeout;
+  let autoSubmitLoginTimeout: number | NodeJS.Timeout;
 
+  init();
+
+  /**
+   * Initializes the auto-submit workflow with a delay to ensure that all page content is loaded.
+   */
   function init() {
-    const triggerOnPageLoad = () => setAutoSubmitLoginFlowTimeout(250);
+    const triggerOnPageLoad = () => setAutoSubmitLoginTimeout(250);
     if (globalContext.document.readyState === "complete") {
       triggerOnPageLoad();
       return;
@@ -36,11 +42,14 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     globalContext.document.addEventListener("DOMContentLoaded", triggerOnPageLoad);
   }
 
-  async function startAutoSubmitLoginFlow() {
+  /**
+   * Collects the autofill page details and triggers the auto-submit login workflow.
+   * If no details are found, we exit the auto-submit workflow.
+   */
+  async function startAutoSubmitLoginWorkflow() {
     const pageDetails: AutofillPageDetails = await collectAutofillContentService.getPageDetails();
-    if (!pageDetails?.fields?.length) {
-      clearAutoSubmitLoginFlowTimeout();
-      updateIsFieldCurrentlyFilling(false);
+    if (!pageDetails?.fields.length) {
+      endUpAutoSubmitLoginWorkflow();
       return;
     }
 
@@ -48,6 +57,21 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     await sendExtensionMessage("triggerAutoSubmitLogin", { pageDetails });
   }
 
+  /**
+   * Ends the auto-submit login workflow.
+   */
+  function endUpAutoSubmitLoginWorkflow() {
+    clearAutoSubmitLoginTimeout();
+    updateIsFieldCurrentlyFilling(false);
+  }
+
+  /**
+   * Handles the extension message used to trigger the auto-submit login action.
+   *
+   * @param command - The command to execute
+   * @param fillScript - The autofill script to use
+   * @param pageDetailsUrl - The URL of the page details
+   */
   async function handleExtensionMessage({
     command,
     fillScript,
@@ -67,19 +91,33 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     await triggerAutoSubmitLogin(fillScript);
   }
 
+  /**
+   * Fills the fields set within the autofill script and triggers the auto-submit action. Will
+   * also set up a subsequent auto-submit action to continue the workflow on any multistep
+   * login forms.
+   *
+   * @param fillScript - The autofill script to use
+   */
   async function triggerAutoSubmitLogin(fillScript: AutofillScript) {
-    if (!fillScript?.autosubmit.length) {
-      clearAutoSubmitLoginFlowTimeout();
-      await updateIsFieldCurrentlyFilling(false);
+    if (!fillScript?.autosubmit?.length) {
+      endUpAutoSubmitLoginWorkflow();
       throw new Error("Unable to auto-submit form, no autosubmit reference found.");
     }
 
-    await updateIsFieldCurrentlyFilling(true);
+    updateIsFieldCurrentlyFilling(true);
     await insertAutofillContentService.fillForm(fillScript);
-    setAutoSubmitLoginFlowTimeout(400);
+    setAutoSubmitLoginTimeout(400);
     triggerAutoSubmitOnForm(fillScript);
   }
 
+  /**
+   * Triggers the auto-submit action on the form element. Will attempt to click an existing
+   * submit button, and if none are found, will attempt to submit the form directly. Note
+   * only the first matching field will be used to trigger the submit action. We will not
+   * attempt to trigger the submit action on multiple forms that might exist on a page.
+   *
+   * @param fillScript - The autofill script to use
+   */
   function triggerAutoSubmitOnForm(fillScript: AutofillScript) {
     const formOpid = fillScript.autosubmit[0];
 
@@ -94,24 +132,8 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
       return;
     }
 
-    const genericSubmitElement = collectAutofillContentService.deepQueryElements<HTMLButtonElement>(
-      formElement,
-      "[type='submit']",
-    );
-    if (genericSubmitElement[0]) {
-      genericSubmitElement[0].click();
+    if (submitElementFoundAndClicked(formElement)) {
       return;
-    }
-
-    const buttons = collectAutofillContentService.deepQueryElements<HTMLButtonElement>(
-      formElement,
-      "button",
-    );
-    for (let i = 0; i < buttons.length; i++) {
-      if (isLoginButton(buttons[i])) {
-        buttons[i].click();
-        return;
-      }
     }
 
     if (formElement.requestSubmit) {
@@ -122,42 +144,22 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     formElement.submit();
   }
 
+  /**
+   * Triggers the auto-submit action on formless fields. This is done by iterating up the DOM
+   * tree, and attempting to find a submit button or form element to trigger the submit action.
+   *
+   * @param fillScript - The autofill script to use
+   */
   function triggerAutoSubmitOnFormlessFields(fillScript: AutofillScript) {
     let currentElement = collectAutofillContentService.getAutofillFieldElementByOpid(
       fillScript.script[fillScript.script.length - 1][1],
     );
-    const currentElementIsPassword =
+    const lastFieldIsPasswordInput =
       elementIsInputElement(currentElement) && currentElement.type === "password";
 
     while (currentElement && currentElement.tagName !== "BODY") {
-      const genericSubmitElement = collectAutofillContentService.deepQueryElements<HTMLElement>(
-        currentElement,
-        "[type='submit']",
-      );
-      if (genericSubmitElement[0]) {
-        if (currentElementIsPassword) {
-          clearAutoSubmitLoginFlowTimeout();
-          sendMultiStepAutoSubmitLoginComplete();
-          updateIsFieldCurrentlyFilling(false);
-        }
-        genericSubmitElement[0].click();
+      if (submitElementFoundAndClicked(currentElement, lastFieldIsPasswordInput)) {
         return;
-      }
-
-      const buttons = collectAutofillContentService.deepQueryElements<HTMLButtonElement>(
-        currentElement,
-        "button",
-      );
-      for (let i = 0; i < buttons.length; i++) {
-        if (isLoginButton(buttons[i])) {
-          if (currentElementIsPassword) {
-            clearAutoSubmitLoginFlowTimeout();
-            sendMultiStepAutoSubmitLoginComplete();
-            updateIsFieldCurrentlyFilling(false);
-          }
-          buttons[i].click();
-          return;
-        }
       }
 
       if (!currentElement.parentElement && currentElement.getRootNode() instanceof ShadowRoot) {
@@ -167,8 +169,68 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
 
       currentElement = currentElement.parentElement;
     }
+
+    if (!currentElement || currentElement.tagName === "BODY") {
+      throw new Error("Unable to auto-submit form, no submit button or form element found.");
+    }
   }
 
+  /**
+   * Queries the element for an element of type="submit" or a button element with a keyword
+   * that matches a login action. If found, the element is clicked and the submit action is
+   * triggered.
+   *
+   * @param element - The element to query for a submit action
+   * @param lastFieldIsPasswordInput - Whether the last field is a password input
+   */
+  function submitElementFoundAndClicked(
+    element: HTMLElement,
+    lastFieldIsPasswordInput = false,
+  ): boolean {
+    const genericSubmitElement = collectAutofillContentService.deepQueryElements<HTMLButtonElement>(
+      element,
+      "[type='submit']",
+    );
+    if (genericSubmitElement[0]) {
+      clickSubmitElement(genericSubmitElement[0], lastFieldIsPasswordInput);
+      return true;
+    }
+
+    const buttons = collectAutofillContentService.deepQueryElements<HTMLButtonElement>(
+      element,
+      "button",
+    );
+    for (let i = 0; i < buttons.length; i++) {
+      if (isLoginButton(buttons[i])) {
+        clickSubmitElement(buttons[i], lastFieldIsPasswordInput);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  /**
+   * Handles clicking the submit element and optionally triggering
+   * a completion action for multistep login forms.
+   *
+   * @param element - The element to click
+   * @param lastFieldIsPasswordInput - Whether the last field is a password input
+   */
+  function clickSubmitElement(element: HTMLElement, lastFieldIsPasswordInput = false) {
+    if (lastFieldIsPasswordInput) {
+      triggerMultiStepAutoSubmitLoginComplete();
+    }
+
+    element.click();
+  }
+
+  /**
+   * Gathers attributes from the element and checks if any of the values match the login
+   * keywords. This is used to determine if the element is a login button.
+   *
+   * @param element - The element to check
+   */
   function isLoginButton(element: HTMLElement) {
     const keywordValues = [
       element.textContent,
@@ -187,6 +249,11 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     return loginKeywords.some((keyword) => keywordValues.includes(keyword));
   }
 
+  /**
+   * Retrieves a form element by its opid attribute.
+   *
+   * @param opid - The opid to search for
+   */
   function getAutofillFormElementByOpid(opid: string): HTMLFormElement | null {
     const cachedFormElements = Array.from(
       collectAutofillContentService.autofillFormElements.keys(),
@@ -198,6 +265,9 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     return formElements.find((formElement) => formElement.opid === opid) || null;
   }
 
+  /**
+   * Gets all form elements on the page.
+   */
   function getAutofillFormElements(): HTMLFormElement[] {
     const formElements: HTMLFormElement[] = [];
     collectAutofillContentService.queryAllTreeWalkerNodes(
@@ -215,24 +285,40 @@ import { elementIsInputElement, nodeIsFormElement, sendExtensionMessage } from "
     return formElements;
   }
 
-  function setAutoSubmitLoginFlowTimeout(delay: number) {
-    clearAutoSubmitLoginFlowTimeout();
-    autoSubmitLoginFlowTimeout = globalContext.setTimeout(() => startAutoSubmitLoginFlow(), delay);
+  /**
+   * Sets a timeout to trigger the auto-submit login workflow.
+   *
+   * @param delay - The delay to wait before triggering the workflow
+   */
+  function setAutoSubmitLoginTimeout(delay: number) {
+    clearAutoSubmitLoginTimeout();
+    autoSubmitLoginTimeout = globalContext.setTimeout(() => startAutoSubmitLoginWorkflow(), delay);
   }
 
-  function clearAutoSubmitLoginFlowTimeout() {
-    if (autoSubmitLoginFlowTimeout) {
-      clearInterval(autoSubmitLoginFlowTimeout);
+  /**
+   * Clears the auto-submit login timeout.
+   */
+  function clearAutoSubmitLoginTimeout() {
+    if (autoSubmitLoginTimeout) {
+      clearInterval(autoSubmitLoginTimeout);
     }
   }
 
-  function sendMultiStepAutoSubmitLoginComplete() {
+  /**
+   * Triggers a completion action for multistep login forms.
+   */
+  function triggerMultiStepAutoSubmitLoginComplete() {
+    endUpAutoSubmitLoginWorkflow();
     void sendExtensionMessage("multiStepAutoSubmitLoginComplete");
   }
 
+  /**
+   * Updates the state of whether a field is currently being filled. This ensures that
+   * the inline menu is not displayed when a field is being filled.
+   *
+   * @param isFieldCurrentlyFilling - Whether a field is currently being filled
+   */
   function updateIsFieldCurrentlyFilling(isFieldCurrentlyFilling: boolean) {
     void sendExtensionMessage("updateIsFieldCurrentlyFilling", { isFieldCurrentlyFilling });
   }
-
-  init();
 })(globalThis);
