92c56f9db6157d0615e32dbd7dba989dc37161e4
Shane Melton <smelton@bitwarden.com>
2024-07-31 12:49:38 -0700

    [PM-9675] Cleanup vault generator dialog spec file

--- apps/browser/src/vault/popup/components/vault-v2/vault-generator-dialog/vault-generator-dialog.component.spec.ts
+++ apps/browser/src/vault/popup/components/vault-v2/vault-generator-dialog/vault-generator-dialog.component.spec.ts
@@ -1,17 +1,11 @@
 import { DIALOG_DATA, DialogRef } from "@angular/cdk/dialog";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
 import { mock, MockProxy } from "jest-mock-extended";
-import { BehaviorSubject } from "rxjs";
 
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
-import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
-import {
-  PasswordGenerationServiceAbstraction,
-  PasswordGeneratorOptions,
-  UsernameGenerationServiceAbstraction,
-  UsernameGeneratorOptions,
-} from "@bitwarden/generator-legacy";
+import { CipherFormGeneratorComponent } from "@bitwarden/vault";
 
 import {
   GeneratorDialogParams,
@@ -19,41 +13,24 @@ import {
   VaultGeneratorDialogComponent,
 } from "./vault-generator-dialog.component";
 
+@Component({
+  selector: "vault-cipher-form-generator",
+  template: "",
+  standalone: true,
+})
+class MockCipherFormGenerator {
+  @Input() type: "password" | "username";
+  @Output() valueGenerated = new EventEmitter<string>();
+}
+
 describe("VaultGeneratorDialogComponent", () => {
   let component: VaultGeneratorDialogComponent;
   let fixture: ComponentFixture<VaultGeneratorDialogComponent>;
-
   let mockDialogRef: MockProxy<DialogRef<GeneratorDialogResult>>;
-  let mockLegacyPasswordGenerationService: MockProxy<PasswordGenerationServiceAbstraction>;
-  let mockLegacyUsernameGenerationService: MockProxy<UsernameGenerationServiceAbstraction>;
-  let mockPlatformUtilsService: MockProxy<PlatformUtilsService>;
-
   let dialogData: GeneratorDialogParams;
 
-  let passwordOptions$: BehaviorSubject<any>;
-  let usernameOptions$: BehaviorSubject<any>;
-
   beforeEach(async () => {
-    passwordOptions$ = new BehaviorSubject([
-      {
-        type: "password",
-      },
-    ] as [PasswordGeneratorOptions]);
-    usernameOptions$ = new BehaviorSubject([
-      {
-        type: "word",
-      },
-    ] as [UsernameGeneratorOptions]);
-
     mockDialogRef = mock<DialogRef<GeneratorDialogResult>>();
-    mockPlatformUtilsService = mock<PlatformUtilsService>();
-
-    mockLegacyPasswordGenerationService = mock<PasswordGenerationServiceAbstraction>();
-    mockLegacyPasswordGenerationService.getOptions$.mockReturnValue(passwordOptions$);
-
-    mockLegacyUsernameGenerationService = mock<UsernameGenerationServiceAbstraction>();
-    mockLegacyUsernameGenerationService.getOptions$.mockReturnValue(usernameOptions$);
-
     dialogData = { type: "password" };
 
     await TestBed.configureTestingModule({
@@ -62,17 +39,13 @@ describe("VaultGeneratorDialogComponent", () => {
         { provide: I18nService, useValue: { t: (key: string) => key } },
         { provide: DIALOG_DATA, useValue: dialogData },
         { provide: DialogRef, useValue: mockDialogRef },
-        {
-          provide: PasswordGenerationServiceAbstraction,
-          useValue: mockLegacyPasswordGenerationService,
-        },
-        {
-          provide: UsernameGenerationServiceAbstraction,
-          useValue: mockLegacyUsernameGenerationService,
-        },
-        { provide: PlatformUtilsService, useValue: mockPlatformUtilsService },
       ],
-    }).compileComponents();
+    })
+      .overrideComponent(VaultGeneratorDialogComponent, {
+        remove: { imports: [CipherFormGeneratorComponent] },
+        add: { imports: [MockCipherFormGenerator] },
+      })
+      .compileComponents();
 
     fixture = TestBed.createComponent(VaultGeneratorDialogComponent);
     component = fixture.componentInstance;
