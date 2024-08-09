747da83be6b400362740bc4c1d36dbf214de5a69
gbubemismith <gsmithwalter@gmail.com>
2024-08-09 12:18:52 -0400

    Added missing feature flag

--- apps/browser/src/background/runtime.background.ts
+++ apps/browser/src/background/runtime.background.ts
@@ -228,7 +228,10 @@ export default class RuntimeBackground {
         // `getAllDecryptedForUrl` and is anticipated to be refactored
         await this.main.refreshBadge();
         await this.main.refreshMenu(false);
-        await this.autofillService.setAutoFillOnPageLoadOrgPolicy();
+
+        if (await this.configService.getFeatureFlag(FeatureFlag.ExtensionRefresh)) {
+          await this.autofillService.setAutoFillOnPageLoadOrgPolicy();
+        }
         break;
       }
       case "addToLockedVaultPendingNotifications":
@@ -248,7 +251,10 @@ export default class RuntimeBackground {
           }, 2000);
           await this.configService.ensureConfigFetched();
           await this.main.updateOverlayCiphers();
-          await this.autofillService.setAutoFillOnPageLoadOrgPolicy();
+
+          if (await this.configService.getFeatureFlag(FeatureFlag.ExtensionRefresh)) {
+            await this.autofillService.setAutoFillOnPageLoadOrgPolicy();
+          }
         }
         break;
       case "openPopup":
