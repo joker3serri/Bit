4ab49b3b53429f0b96433df19e7ac11e17d01e3d
Alec Rippberger <alec@livefront.com>
2024-08-08 23:41:54 -0500

    Adjust import order.

--- apps/web/src/app/vault/org-vault/vault.module.ts
+++ apps/web/src/app/vault/org-vault/vault.module.ts
@@ -4,12 +4,12 @@ import { LooseComponentsModule } from "../../shared/loose-components.module";
 import { SharedModule } from "../../shared/shared.module";
 import { OrganizationBadgeModule } from "../../vault/individual-vault/organization-badge/organization-badge.module";
 import { CollectionDialogModule } from "../components/collection-dialog";
+import { ViewComponent } from "../individual-vault/view.component";
 
 import { CollectionBadgeModule } from "./collection-badge/collection-badge.module";
 import { GroupBadgeModule } from "./group-badge/group-badge.module";
 import { VaultRoutingModule } from "./vault-routing.module";
 import { VaultComponent } from "./vault.component";
-import { ViewComponent } from "../individual-vault/view.component";
 
 @NgModule({
   imports: [
