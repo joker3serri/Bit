e81dcb9e8019e45793add790a09df77b3769fa51
Jonathan Prusik <jprusik@classynemesis.com>
2024-08-09 15:16:55 -0400

    add card expiry warning to the v2 Card Details component

--- libs/vault/src/cipher-view/card-details/card-details-view.component.ts
+++ libs/vault/src/cipher-view/card-details/card-details-view.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { firstValueFrom } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { UserNotificationSettingsServiceAbstraction } from "@bitwarden/common/autofill/services/user-notification-settings.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { CardView } from "@bitwarden/common/vault/models/view/card.view";
 import {
+  BannerModule,
   CardComponent,
   SectionComponent,
   SectionHeaderComponent,
@@ -18,6 +21,7 @@ import {
   templateUrl: "card-details-view.component.html",
   standalone: true,
   imports: [
+    BannerModule,
     CommonModule,
     JslibModule,
     CardComponent,
@@ -28,15 +32,48 @@ import {
     IconButtonModule,
   ],
 })
-export class CardDetailsComponent {
+export class CardDetailsComponent implements OnInit {
   @Input() card: CardView;
+  cardIsExpired: boolean = false;
+  enableExpiredPaymentCipherWarning = true;
 
-  constructor(private i18nService: I18nService) {}
+  constructor(
+    private i18nService: I18nService,
+    private userNotificationSettingsService: UserNotificationSettingsServiceAbstraction,
+  ) {}
+
+  async ngOnInit() {
+    this.cardIsExpired = this.isCardExpiryInThePast();
+
+    this.enableExpiredPaymentCipherWarning = await firstValueFrom(
+      this.userNotificationSettingsService.enableExpiredPaymentCipherWarning$,
+    );
+  }
 
   get setSectionTitle() {
     if (this.card.brand && this.card.brand !== "Other") {
       return this.i18nService.t("cardBrandDetails", this.card.brand);
     }
     return this.i18nService.t("cardDetails");
+  }
+
+  isCardExpiryInThePast() {
+    if (this.card) {
+      const { expMonth, expYear }: CardView = this.card;
+
+      if (expYear && expMonth) {
+        // `Date` months are zero-indexed
+        const parsedMonth = parseInt(expMonth) - 1;
+        const parsedYear = parseInt(expYear);
+
+        // First day of the next month minus one, to get last day of the card month
+        const cardExpiry = new Date(parsedYear, parsedMonth + 1, 0);
+        const now = new Date();
+
+        return cardExpiry < now;
+      }
+    }
+
+    return false;
   }
 }
