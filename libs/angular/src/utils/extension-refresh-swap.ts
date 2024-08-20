1e20f5199e5b1ef0f4d692af30f8a72946c5b04c
Jared Snider <jsnider@bitwarden.com>
2024-08-20 17:02:08 -0400

    Update extensionRefreshSwap docs

--- libs/angular/src/utils/extension-refresh-swap.ts
+++ libs/angular/src/utils/extension-refresh-swap.ts
@@ -10,7 +10,8 @@ import { componentRouteSwap } from "./component-route-swap";
  * Helper function to swap between two components based on the ExtensionRefresh feature flag.
  * @param defaultComponent - The current non-refreshed component to render.
  * @param refreshedComponent - The new refreshed component to render.
- * @param options - The shared route options to apply to both components.
+ * @param options - The shared route options to apply to the default component, and to the alt component if altOptions is not provided.
+ * @param altOptions - The alt route options to apply to the alt component.
  */
 export function extensionRefreshSwap(
   defaultComponent: Type<any>,
