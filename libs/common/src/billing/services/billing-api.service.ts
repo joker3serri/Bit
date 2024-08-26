675fad5cce4aae2cd5ee26502cb5ef292dd4ba4a
Alex Morask <amorask@bitwarden.com>
2024-08-26 14:30:13 -0400

    (Cleanup) Remove payment method components and API methods from provider portal
    Product team decided not to have a payment method page in the provider portal for consolidated billing. This just removes all the unused components and API methods.

--- libs/common/src/billing/services/billing-api.service.ts
+++ libs/common/src/billing/services/billing-api.service.ts
@@ -9,10 +9,7 @@ import { BillingApiServiceAbstraction } from "../../billing/abstractions";
 import { PaymentMethodType } from "../../billing/enums";
 import { ExpandedTaxInfoUpdateRequest } from "../../billing/models/request/expanded-tax-info-update.request";
 import { SubscriptionCancellationRequest } from "../../billing/models/request/subscription-cancellation.request";
-import { TokenizedPaymentMethodRequest } from "../../billing/models/request/tokenized-payment-method.request";
-import { VerifyBankAccountRequest } from "../../billing/models/request/verify-bank-account.request";
 import { OrganizationBillingMetadataResponse } from "../../billing/models/response/organization-billing-metadata.response";
-import { PaymentInformationResponse } from "../../billing/models/response/payment-information.response";
 import { PlanResponse } from "../../billing/models/response/plan.response";
 import { ListResponse } from "../../models/response/list.response";
 import { CreateClientOrganizationRequest } from "../models/request/create-client-organization.request";
@@ -123,19 +120,6 @@ export class BillingApiService implements BillingApiServiceAbstraction {
     return new InvoicesResponse(response);
   }
 
-  async getProviderPaymentInformation(providerId: string): Promise<PaymentInformationResponse> {
-    const response = await this.execute(() =>
-      this.apiService.send(
-        "GET",
-        "/providers/" + providerId + "/billing/payment-information",
-        null,
-        true,
-        true,
-      ),
-    );
-    return new PaymentInformationResponse(response);
-  }
-
   async getProviderSubscription(providerId: string): Promise<ProviderSubscriptionResponse> {
     const response = await this.execute(() =>
       this.apiService.send(
@@ -163,33 +147,10 @@ export class BillingApiService implements BillingApiServiceAbstraction {
     );
   }
 
-  async updateProviderPaymentMethod(
-    providerId: string,
-    request: TokenizedPaymentMethodRequest,
-  ): Promise<void> {
-    return await this.apiService.send(
-      "PUT",
-      "/providers/" + providerId + "/billing/payment-method",
-      request,
-      true,
-      false,
-    );
-  }
-
   async updateProviderTaxInformation(providerId: string, request: ExpandedTaxInfoUpdateRequest) {
     return await this.apiService.send(
       "PUT",
       "/providers/" + providerId + "/billing/tax-information",
-      request,
-      true,
-      false,
-    );
-  }
-
-  async verifyProviderBankAccount(providerId: string, request: VerifyBankAccountRequest) {
-    return await this.apiService.send(
-      "POST",
-      "/providers/" + providerId + "/billing/payment-method/verify-bank-account",
       request,
       true,
       false,
