2d90a368744de868b66c3eadf260dc49eba67a61
Nick Krantz <nick@livefront.com>
2024-09-16 13:53:43 -0500

    rework observable handling

--- apps/browser/src/platform/services/popup-view-cache-background.service.ts
+++ apps/browser/src/platform/services/popup-view-cache-background.service.ts
@@ -1,4 +1,4 @@
-import { switchMap, merge, delay, filter, concatMap, map, firstValueFrom } from "rxjs";
+import { switchMap, merge, delay, filter, concatMap, map, first, of, from } from "rxjs";
 
 import { CommandDefinition, MessageListener } from "@bitwarden/common/platform/messaging";
 import {
@@ -61,18 +61,16 @@ export class PopupViewCacheBackgroundService {
     merge(
       // on tab changed, excluding extension tabs
       fromChromeEvent(chrome.tabs.onActivated).pipe(
-        switchMap(async (tabs) => {
-          const tab = await BrowserApi.getTab(tabs[0].tabId);
-
+        switchMap((tabs) => BrowserApi.getTab(tabs[0].tabId)),
+        switchMap((tab) => {
           // FireFox sets the `url` to "about:blank" and won't populate the `url` until the `onUpdated` event
           if (tab.url !== "about:blank") {
-            return tab;
+            return of(tab);
           }
 
-          return firstValueFrom(
-            fromChromeEvent(chrome.tabs.onUpdated).pipe(
-              switchMap(([tabId]) => BrowserApi.getTab(tabId)),
-            ),
+          return fromChromeEvent(chrome.tabs.onUpdated).pipe(
+            first(),
+            switchMap(([tabId]) => from(BrowserApi.getTab(tabId))),
           );
         }),
         map((tab) => tab.url || tab.pendingUrl),
