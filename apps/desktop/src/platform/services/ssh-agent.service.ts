072c699ff7a023d251a500822778879674effcf3
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-09-19 12:34:10 +0200

    feat: rewrite sshagent.signrequest as purely observable

--- apps/desktop/src/platform/services/ssh-agent.service.ts
+++ apps/desktop/src/platform/services/ssh-agent.service.ts
@@ -1,17 +1,20 @@
 import { Injectable, OnDestroy } from "@angular/core";
 import {
+  catchError,
   combineLatest,
   concatMap,
-  delay,
   filter,
-  firstValueFrom,
+  from,
   map,
   of,
-  race,
   Subject,
   switchMap,
   takeUntil,
+  tap,
+  timeout,
+  TimeoutError,
   timer,
+  withLatestFrom,
 } from "rxjs";
 
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
@@ -28,18 +31,18 @@ import { ApproveSshRequestComponent } from "../components/approve-ssh-request";
 
 import { DesktopSettingsService } from "./desktop-settings.service";
 
-type SshRequest = {
-  cipherId: string;
-  requestId: number;
-  timeout: boolean;
-};
+// type SshRequest = {
+//   cipherId: string;
+//   requestId: number;
+//   timeout: boolean;
+// };
 
 @Injectable({
   providedIn: "root",
 })
 export class SshAgentService implements OnDestroy {
   SSH_REFRESH_INTERVAL = 1000;
-  SSH_VAULT_UNLOCK_REQUEST_TIMEOUT = 1000 * 60 * 5;
+  SSH_VAULT_UNLOCK_REQUEST_TIMEOUT = 1000 * 10;
   SSH_REQUEST_UNLOCK_POLLING_INTERVAL = 100;
 
   private destroy$ = new Subject<void>();
@@ -58,69 +61,133 @@ export class SshAgentService implements OnDestroy {
     this.messageListener
       .messages$(new CommandDefinition("sshagent.signrequest"))
       .pipe(
-        concatMap(async (message: any) => {
+        withLatestFrom(this.authService.activeAccountStatus$),
+        // switchMap will stop waiting for an unlock if a new request comes in
+        switchMap(([message, status]) => {
           ipc.platform.focusWindow();
-          if (
-            (await firstValueFrom(this.authService.activeAccountStatus$)) !==
-            AuthenticationStatus.Unlocked
-          ) {
+          if (status !== AuthenticationStatus.Unlocked) {
             this.toastService.showToast({
               variant: "info",
               title: null,
               message: this.i18nService.t("sshAgentUnlockRequired"),
             });
-          }
-          return message;
-        }),
-        switchMap(async (message: any) => {
-          const cipherId = message.cipherId;
-          const requestId = message.requestId;
-
-          const ret = race([
-            of({ cipherId, requestId, timeout: true } as SshRequest).pipe(
-              delay(this.SSH_VAULT_UNLOCK_REQUEST_TIMEOUT),
-            ),
-            this.authService.activeAccountStatus$.pipe(
-              map((status) => {
-                return status;
-              }),
+            return this.authService.activeAccountStatus$.pipe(
               filter((status) => status === AuthenticationStatus.Unlocked),
-              map(() => {
-                return {
-                  cipherId,
-                  requestId,
-                  timeout: false,
-                } as SshRequest;
+              timeout(this.SSH_VAULT_UNLOCK_REQUEST_TIMEOUT),
+              catchError((error: unknown) => {
+                if (error instanceof TimeoutError) {
+                  this.toastService.showToast({
+                    variant: "error",
+                    title: null,
+                    message: this.i18nService.t("sshAgentUnlockTimeout"),
+                  });
+                  return of();
+                }
+
+                throw error;
               }),
-            ),
-          ]);
-          return await firstValueFrom(ret);
-        }),
-        concatMap(async (request: SshRequest) => {
-          if (request.timeout) {
-            this.toastService.showToast({
-              variant: "error",
-              title: null,
-              message: this.i18nService.t("sshAgentUnlockTimeout"),
-            });
-          } else {
-            const decryptedCiphers = await this.cipherService.getAllDecrypted();
-            const cipher = decryptedCiphers.find((cipher) => cipher.id == request.cipherId);
-
-            const dialogRef = ApproveSshRequestComponent.open(
-              this.dialogService,
-              cipher.name,
-              this.i18nService.t("unknownApplication"),
+              map(() => message),
             );
-
-            const result = await firstValueFrom(dialogRef.closed);
-            await ipc.platform.sshAgent.signRequestResponse(request.requestId, result);
-            ipc.platform.hideWindow();
           }
+
+          return of(message);
+        }),
+        switchMap((message) =>
+          from(this.cipherService.getAllDecrypted()).pipe(
+            map((ciphers) => [message, ciphers] as const),
+          ),
+        ),
+        concatMap(([message, decryptedCiphers]) => {
+          const cipherId = message.cipherId as string;
+          const requestId = message.requestId as number;
+          const cipher = decryptedCiphers.find((cipher) => cipher.id == cipherId);
+
+          const dialogRef = ApproveSshRequestComponent.open(
+            this.dialogService,
+            cipher.name,
+            this.i18nService.t("unknownApplication"),
+          );
+
+          return dialogRef.closed.pipe(
+            switchMap((result) => ipc.platform.sshAgent.signRequestResponse(requestId, result)),
+            tap(() => ipc.platform.hideWindow()),
+          );
         }),
         takeUntil(this.destroy$),
       )
-      .subscribe();
+      .subscribe({
+        // Please not that any error here will have cause the whole stream to
+        error: (error: unknown) => {
+          this.logService.error("Error processing sshagent.signrequest", error);
+        },
+      });
+
+    // this.messageListener
+    //   .messages$(new CommandDefinition("sshagent.signrequest"))
+    //   .pipe(
+    //     tap(() => ipc.platform.focusWindow()),
+    //     concatMap(async (message: any) => {
+    //       if (
+    //         (await firstValueFrom(this.authService.activeAccountStatus$)) !==
+    //         AuthenticationStatus.Unlocked
+    //       ) {
+    //         this.toastService.showToast({
+    //           variant: "info",
+    //           title: null,
+    //           message: this.i18nService.t("sshAgentUnlockRequired"),
+    //         });
+    //       }
+    //       return message;
+    //     }),
+    //     switchMap(async (message: any) => {
+    //       const cipherId = message.cipherId;
+    //       const requestId = message.requestId;
+
+    //       const ret = race([
+    //         of({ cipherId, requestId, timeout: true } as SshRequest).pipe(
+    //           delay(this.SSH_VAULT_UNLOCK_REQUEST_TIMEOUT),
+    //         ),
+    //         this.authService.activeAccountStatus$.pipe(
+    //           map((status) => {
+    //             return status;
+    //           }),
+    //           filter((status) => status === AuthenticationStatus.Unlocked),
+    //           map(() => {
+    //             return {
+    //               cipherId,
+    //               requestId,
+    //               timeout: false,
+    //             } as SshRequest;
+    //           }),
+    //         ),
+    //       ]);
+    //       return await firstValueFrom(ret);
+    //     }),
+    //     concatMap(async (request: SshRequest) => {
+    //       if (request.timeout) {
+    //         this.toastService.showToast({
+    //           variant: "error",
+    //           title: null,
+    //           message: this.i18nService.t("sshAgentUnlockTimeout"),
+    //         });
+    //       } else {
+    //         const decryptedCiphers = await this.cipherService.getAllDecrypted();
+    //         const cipher = decryptedCiphers.find((cipher) => cipher.id == request.cipherId);
+
+    //         const dialogRef = ApproveSshRequestComponent.open(
+    //           this.dialogService,
+    //           cipher.name,
+    //           this.i18nService.t("unknownApplication"),
+    //         );
+
+    //         const result = await firstValueFrom(dialogRef.closed);
+    //         await ipc.platform.sshAgent.signRequestResponse(request.requestId, result);
+    //         ipc.platform.hideWindow();
+    //       }
+    //     }),
+    //     takeUntil(this.destroy$),
+    //   )
+    //   .subscribe();
 
     combineLatest([
       timer(0, this.SSH_REFRESH_INTERVAL),
