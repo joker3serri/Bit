80af4e75c2e2814ab7aeec58e15168216b8114ed
Hinton <hinton@users.noreply.github.com>
2024-09-30 15:09:14 +0200

    Document the client factories

--- apps/cli/src/platform/services/node-sdk-client-factory.ts
+++ apps/cli/src/platform/services/node-sdk-client-factory.ts
@@ -1,6 +1,9 @@
 import { SdkClientFactory } from "@bitwarden/common/platform/abstractions/sdk/sdk-client-factory";
 import * as sdk from "@bitwarden/sdk-internal";
 
+/**
+ * The sdk-internal package exposes a non ESM module with a slightly different initialization.
+ */
 export class NodeSdkClientFactory implements SdkClientFactory {
   async createSdkClient(
     ...args: ConstructorParameters<typeof sdk.BitwardenClient>
--- apps/web/src/app/platform/web-sdk-client-factory.ts
+++ apps/web/src/app/platform/web-sdk-client-factory.ts
@@ -1,6 +1,9 @@
 import { SdkClientFactory } from "@bitwarden/common/platform/abstractions/sdk/sdk-client-factory";
 import * as sdk from "@bitwarden/sdk-internal";
 
+/**
+ * SDK client factory with a js fallback for when WASM is not supported.
+ */
 export class WebSdkClientFactory implements SdkClientFactory {
   async createSdkClient(
     ...args: ConstructorParameters<typeof sdk.BitwardenClient>
