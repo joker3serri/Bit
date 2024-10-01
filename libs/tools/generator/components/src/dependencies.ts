abf7a5146f647ae7e23eddddcb584b52d925cb88
Nick Krantz <nick@livefront.com>
2024-10-01 15:14:55 -0500

    remove JSLibServicesModule from generator dependencies

--- libs/tools/generator/components/src/dependencies.ts
+++ libs/tools/generator/components/src/dependencies.ts
@@ -5,7 +5,6 @@ import { ReactiveFormsModule } from "@angular/forms";
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { safeProvider } from "@bitwarden/angular/platform/utils/safe-provider";
 import { SafeInjectionToken } from "@bitwarden/angular/services/injection-tokens";
-import { JslibServicesModule } from "@bitwarden/angular/services/jslib-services.module";
 import { PolicyService } from "@bitwarden/common/admin-console/abstractions/policy/policy.service.abstraction";
 import { CryptoService } from "@bitwarden/common/platform/abstractions/crypto.service";
 import { StateProvider } from "@bitwarden/common/platform/state";
@@ -34,6 +33,7 @@ const RANDOMIZER = new SafeInjectionToken<Randomizer>("Randomizer");
 @NgModule({
   imports: [CardComponent, SectionComponent, SectionHeaderComponent],
   exports: [
+    JslibModule,
     CardComponent,
     CheckboxModule,
     CommonModule,
@@ -42,8 +42,6 @@ const RANDOMIZER = new SafeInjectionToken<Randomizer>("Randomizer");
     IconButtonModule,
     InputModule,
     ItemModule,
-    JslibModule,
-    JslibServicesModule,
     ReactiveFormsModule,
     SectionComponent,
     SectionHeaderComponent,
