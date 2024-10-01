8178598b16def2f136db9abfaafbe8f8f060ffe5
Jared Snider <jsnider@bitwarden.com>
2024-10-01 16:54:59 -0400

    PM-8112 - Export icons from libs/auth

--- libs/auth/src/angular/icons/index.ts
+++ libs/auth/src/angular/icons/index.ts
@@ -3,3 +3,5 @@ export * from "./bitwarden-shield.icon";
 export * from "./lock.icon";
 export * from "./user-lock.icon";
 export * from "./user-verification-biometrics-fingerprint.icon";
+export * from "./registration-user-add.icon";
+export * from "./registration-lock-alt.icon";
