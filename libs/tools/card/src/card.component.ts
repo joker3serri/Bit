86407f32358a7ddec86be98e528ad7043c1a2389
jaasen-livefront <jaasen@livefront.com>
2024-10-03 09:34:02 -0700

    fix tw classes in card component

--- libs/tools/card/src/card.component.ts
+++ libs/tools/card/src/card.component.ts
@@ -10,7 +10,7 @@ import { TypographyModule } from "@bitwarden/components";
   imports: [CommonModule, TypographyModule],
   host: {
     class:
-      "tw-box-border tw-block tw-text-main tw-border-solid tw-border-1 tw-border-secondary-300 tw-border tw-rounded-lg tw-p-6",
+      "tw-box-border tw-bg-background tw-block tw-text-main tw-border-solid tw-border tw-border-secondary-300 tw-border [&:not(bit-layout_*)]:tw-rounded-lg tw-p-6",
   },
 })
 export class CardComponent {
