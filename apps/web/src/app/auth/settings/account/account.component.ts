ebfcf3a9ca8baac923183c4d6702c9195d99a623
Rui Tome <rtome@bitwarden.com>
2024-10-04 16:00:09 +0100

    Update AccountComponent to conditionally show the purgeVault button based on a feature flag and if the user is managed by an organization

--- apps/web/src/app/auth/settings/account/account.component.ts
+++ apps/web/src/app/auth/settings/account/account.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ViewChild, ViewContainerRef } from "@angular/core";
-import { lastValueFrom } from "rxjs";
+import { lastValueFrom, map, Observable, of, switchMap } from "rxjs";
 
 import { ModalService } from "@bitwarden/angular/services/modal.service";
+import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { UserVerificationService } from "@bitwarden/common/auth/abstractions/user-verification/user-verification.service.abstraction";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { DialogService } from "@bitwarden/components";
 
 import { PurgeVaultComponent } from "../../../vault/settings/purge-vault.component";
@@ -19,15 +22,29 @@ export class AccountComponent implements OnInit {
   deauthModalRef: ViewContainerRef;
 
   showChangeEmail = true;
+  showPurgeVault$: Observable<boolean>;
 
   constructor(
     private modalService: ModalService,
     private dialogService: DialogService,
     private userVerificationService: UserVerificationService,
+    private accountService: AccountService,
+    private configService: ConfigService,
   ) {}
 
   async ngOnInit() {
     this.showChangeEmail = await this.userVerificationService.hasMasterPassword();
+    this.showPurgeVault$ = this.configService
+      .getFeatureFlag$(FeatureFlag.AccountDeprovisioning)
+      .pipe(
+        switchMap((isAccountDeprovisioningEnabled) =>
+          isAccountDeprovisioningEnabled
+            ? this.accountService.activeAccount$.pipe(
+                map((account) => account?.managedByOrganizationId === null),
+              )
+            : of(true),
+        ),
+      );
   }
 
   async deauthorizeSessions() {
