389fe5f2085e4499d16339f36059ca008c608ad0
jaasen-livefront <jaasen@livefront.com>
2024-10-08 13:37:07 -0700

    only show when feature flag enabled

--- apps/web/src/app/admin-console/organizations/access-intelligence/access-intelligence-routing.module.ts
+++ apps/web/src/app/admin-console/organizations/access-intelligence/access-intelligence-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { unauthGuardFn } from "@bitwarden/angular/auth/guards";
+import { canAccessFeature } from "@bitwarden/angular/platform/guard/feature-flag.guard";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+
 import { AccessIntelligenceComponent } from "./access-intelligence.component";
 
 const routes: Routes = [
   {
     path: "",
     component: AccessIntelligenceComponent,
-    // TODO - Implement canAccessAccessIntelligenceTab
+    canActivate: [canAccessFeature(FeatureFlag.AccessIntelligence), unauthGuardFn()],
     data: {
       titleId: "accessIntelligence",
     },
