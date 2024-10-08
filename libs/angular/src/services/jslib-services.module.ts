e3326702c25760e470578daf162ce12b7ccf0ea2
Andreas Coroiu <andreas.coroiu@gmail.com>
2024-10-08 10:10:50 +0200

    feat: add uninitialized user client with cache

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -1334,6 +1334,8 @@ const safeProviders: SafeProvider[] = [
       SdkClientFactory,
       EnvironmentService,
       PlatformUtilsServiceAbstraction,
+      AccountServiceAbstraction,
+      TokenServiceAbstraction,
       ApiServiceAbstraction,
     ],
   }),
--- libs/common/src/platform/services/sdk/default-sdk.service.ts
+++ libs/common/src/platform/services/sdk/default-sdk.service.ts
@@ -1,27 +1,43 @@
-import { concatMap, firstValueFrom, shareReplay } from "rxjs";
+import {
+  combineLatest,
+  concatMap,
+  firstValueFrom,
+  Observable,
+  shareReplay,
+  tap,
+  map,
+  filter,
+} from "rxjs";
 
-import { LogLevel, DeviceType as SdkDeviceType } from "@bitwarden/sdk-internal";
+import {
+  BitwardenClient,
+  ClientSettings,
+  LogLevel,
+  DeviceType as SdkDeviceType,
+} from "@bitwarden/sdk-internal";
 
 import { ApiService } from "../../../abstractions/api.service";
+import { AccountService } from "../../../auth/abstractions/account.service";
+import { TokenService } from "../../../auth/abstractions/token.service";
 import { DeviceType } from "../../../enums/device-type.enum";
-import { EnvironmentService } from "../../abstractions/environment.service";
+import { UserId } from "../../../types/guid";
+import { Environment, EnvironmentService } from "../../abstractions/environment.service";
 import { PlatformUtilsService } from "../../abstractions/platform-utils.service";
 import { SdkClientFactory } from "../../abstractions/sdk/sdk-client-factory";
 import { SdkService } from "../../abstractions/sdk/sdk.service";
 
 export class DefaultSdkService implements SdkService {
+  private sdkClientCache = new Map<UserId, Observable<BitwardenClient>>();
+
   client$ = this.environmentService.environment$.pipe(
     concatMap(async (env) => {
-      const settings = {
-        apiUrl: env.getApiUrl(),
-        identityUrl: env.getIdentityUrl(),
-        deviceType: this.toDevice(this.platformUtilsService.getDevice()),
-        userAgent: this.userAgent ?? navigator.userAgent,
-      };
-
+      const settings = this.toSettings(env);
       return await this.sdkClientFactory.createSdkClient(settings, LogLevel.Info);
     }),
     shareReplay({ refCount: true, bufferSize: 1 }),
+    tap((client) => {
+      (window as any).client = client;
+    }),
   );
 
   supported$ = this.client$.pipe(
@@ -34,9 +50,38 @@ export class DefaultSdkService implements SdkService {
     private sdkClientFactory: SdkClientFactory,
     private environmentService: EnvironmentService,
     private platformUtilsService: PlatformUtilsService,
+    private accountService: AccountService,
+    private tokenService: TokenService,
     private apiService: ApiService, // Yes we shouldn't import ApiService, but it's temporary
     private userAgent: string = null,
   ) {}
+
+  userClient$(userId: UserId): Observable<BitwardenClient> {
+    // TODO: Figure out what happens when the user logs out
+    if (this.sdkClientCache.has(userId)) {
+      return this.sdkClientCache.get(userId);
+    }
+
+    const account$ = this.accountService.accounts$.pipe(map((accounts) => accounts[userId]));
+    const token$ = this.tokenService.hasAccessToken$(userId).pipe(
+      filter((hasToken) => hasToken),
+      concatMap(() => this.tokenService.getAccessToken(userId)),
+    );
+
+    const client$ = combineLatest([this.environmentService.environment$, account$, token$]).pipe(
+      concatMap(async ([env, account, token]) => {
+        const settings = this.toSettings(env);
+        const client = await this.sdkClientFactory.createSdkClient(settings, LogLevel.Info);
+
+        // TODO: Init client crypto
+
+        return client;
+      }),
+    );
+
+    this.sdkClientCache.set(userId, client$);
+    return client$;
+  }
 
   async failedToInitialize(): Promise<void> {
     // Only log on cloud instances
@@ -50,6 +95,15 @@ export class DefaultSdkService implements SdkService {
     return this.apiService.send("POST", "/wasm-debug", null, false, false, null, (headers) => {
       headers.append("SDK-Version", "1.0.0");
     });
+  }
+
+  private toSettings(env: Environment): ClientSettings {
+    return {
+      apiUrl: env.getApiUrl(),
+      identityUrl: env.getIdentityUrl(),
+      deviceType: this.toDevice(this.platformUtilsService.getDevice()),
+      userAgent: this.userAgent ?? navigator.userAgent,
+    };
   }
 
   private toDevice(device: DeviceType): SdkDeviceType {
