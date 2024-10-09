4461a541b853226519548f0b9bc32758e87fb071
jaasen-livefront <jaasen@livefront.com>
2024-10-09 11:22:22 -0700

    add RTL support.

--- libs/tools/card/src/card.component.ts
+++ libs/tools/card/src/card.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
 
+import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { TypographyModule } from "@bitwarden/components";
 
 @Component({
   selector: "tools-card",
   templateUrl: "./card.component.html",
   standalone: true,
-  imports: [CommonModule, TypographyModule],
+  imports: [CommonModule, TypographyModule, JslibModule],
   host: {
     class:
       "tw-box-border tw-bg-background tw-block tw-text-main tw-border-solid tw-border tw-border-secondary-300 tw-border [&:not(bit-layout_*)]:tw-rounded-lg tw-p-6",
@@ -15,6 +16,6 @@ import { TypographyModule } from "@bitwarden/components";
 })
 export class CardComponent {
   @Input() title: string;
-  @Input() mainText: string;
-  @Input() subText: string;
+  @Input() value: number;
+  @Input() maxValue: number;
 }
--- libs/tools/card/src/card.stories.ts
+++ libs/tools/card/src/card.stories.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { Meta, StoryObj, moduleMetadata } from "@storybook/angular";
 
-import { TypographyModule } from "@bitwarden/components";
+import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { I18nMockService, TypographyModule } from "@bitwarden/components";
 
 import { CardComponent } from "./card.component";
 
@@ -11,6 +12,15 @@ export default {
   decorators: [
     moduleMetadata({
       imports: [CardComponent, CommonModule, TypographyModule],
+      providers: [
+        {
+          provide: I18nService,
+          useFactory: () =>
+            new I18nMockService({
+              cardMetrics: (value) => `out of ${value}`,
+            }),
+        },
+      ],
     }),
   ],
 } as Meta;
@@ -21,6 +31,6 @@ export const Default: Story = {
   render: (args) => ({
     props: args,
     template: /*html*/ `
-      <tools-card [title]="'Unsecured Members'" [mainText]="'38'" [subText]="'out of 157'"></tools-card>`,
+      <tools-card [title]="'Unsecured Members'" [value]="'38'" [maxValue]="'157'"></tools-card>`,
   }),
 };
