2b267ef3e00b91bd03a084f8079c23710bd23983
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-10-22 15:34:48 -0400

    Add More Docs to Matrix

--- libs/common/spec/matrix.ts
+++ libs/common/spec/matrix.ts
@@ -13,10 +13,46 @@ export class Matrix<TKeys extends unknown[], TValue> {
   private map: Map<PickFirst<TKeys>, MatrixOrValue<RemoveFirst<TKeys>, TValue>> = new Map();
 
   /**
+   * This is especially useful for methods on a service that take inputs but return Observables.
+   * Generally when interacting with observables in tests, you want to use a simple SubjectLike
+   * type to back it instead, so that you can easily `next` values to simulate an emission.
    *
-   * @param mockFunction
-   * @param creator
-   * @returns
+   * @param mockFunction The function to have a Matrix based implementation added to it.
+   * @param creator The function to use to create the underlying value to return for the given arguments.
+   * @returns A "getter" function that allows you to retrieve the backing value that is used for the given arguments.
+   *
+   * @example
+   * ```ts
+   * interface MyService {
+   *   event$(userId: UserId) => Observable<UserEvent>
+   * }
+   *
+   * // Test
+   * const myService = mock<MyService>();
+   * const eventGetter = Matrix.autoMockMethod(myService.event$, (userId) => BehaviorSubject<UserEvent>());
+   *
+   * eventGetter("userOne").next(new UserEvent());
+   * eventGetter("userTwo").next(new UserEvent());
+   * ```
+   *
+   * This replaces a more manual way of doing things like:
+   *
+   * ```ts
+   * const myService = mock<MyService>();
+   * const userOneSubject = new BehaviorSubject<UserEvent>();
+   * const userTwoSubject = new BehaviorSubject<UserEvent>();
+   * myService.event$.mockImplementation((userId) => {
+   *    if (userId === "userOne") {
+   *      return userOneSubject;
+   *    } else if (userId === "userTwo") {
+   *      return userTwoSubject;
+   *    }
+   *    return new BehaviorSubject<UserEvent>();
+   * });
+   *
+   * userOneSubject.next(new UserEvent());
+   * userTwoSubject.next(new UserEvent());
+   * ```
    */
   static autoMockMethod<TReturn, TArgs extends unknown[], TActualReturn extends TReturn>(
     mockFunction: jest.Mock<TReturn, TArgs>,
@@ -33,6 +69,16 @@ export class Matrix<TKeys extends unknown[], TValue> {
     return getter;
   }
 
+  /**
+   * Gives the ability to get or create an entry in the matrix via the given args.
+   *
+   * @note The args are evaulated using Javascript equality so primivites work best.
+   *
+   * @param args The arguments to use to evaluate if an entry in the matrix exists already,
+   *   or a value should be created and stored with those arguments.
+   * @param creator The function to call with the arguments to build a value.
+   * @returns The existing entry if one already exists or a new value created with the creator param.
+   */
   getOrCreateEntry(args: TKeys, creator: (args: TKeys) => TValue): TValue {
     if (args.length === 0) {
       throw new Error("Matrix is not for you.");
