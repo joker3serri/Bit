b91b7f366bd90a1ed9ccc650934880fc50294184
jaasen-livefront <jaasen@livefront.com>
2024-10-23 09:45:07 -0700

    fix import

--- apps/web/src/app/tools/access-intelligence/password-health-members.component.ts
+++ apps/web/src/app/tools/access-intelligence/password-health-members.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute } from "@angular/router";
 import { map } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+// eslint-disable-next-line no-restricted-imports
+import { PasswordHealthService } from "@bitwarden/bit-common/tools/reports/access-intelligence";
 import { AuditService } from "@bitwarden/common/abstractions/audit.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { PasswordStrengthServiceAbstraction } from "@bitwarden/common/tools/password-strength";
@@ -24,8 +26,6 @@ import { HeaderModule } from "../../layouts/header/header.module";
 import { OrganizationBadgeModule } from "../../vault/individual-vault/organization-badge/organization-badge.module";
 // eslint-disable-next-line no-restricted-imports
 import { PipesModule } from "../../vault/individual-vault/pipes/pipes.module";
-
-import { PasswordHealthService } from "./password-health.service";
 
 @Component({
   standalone: true,
