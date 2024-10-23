fbb1cc35e6db19ca452100831c69d14110f300e9
Daniel García <dani-garcia@users.noreply.github.com>
2024-10-23 18:50:54 +0200

    [PM-13650] Support transpiled JS SDK

--- libs/common/src/platform/services/sdk/default-sdk-client-factory.ts
+++ libs/common/src/platform/services/sdk/default-sdk-client-factory.ts
@@ -1,5 +1,4 @@
 import * as sdk from "@bitwarden/sdk-internal";
-import * as module from "@bitwarden/sdk-internal/bitwarden_wasm_internal_bg.wasm";
 
 import { SdkClientFactory } from "../../abstractions/sdk/sdk-client-factory";
 
@@ -12,7 +11,7 @@ export class DefaultSdkClientFactory implements SdkClientFactory {
   async createSdkClient(
     ...args: ConstructorParameters<typeof sdk.BitwardenClient>
   ): Promise<sdk.BitwardenClient> {
-    (sdk as any).init(module);
+    await sdk.init();
 
     return Promise.resolve(new sdk.BitwardenClient(...args));
   }
