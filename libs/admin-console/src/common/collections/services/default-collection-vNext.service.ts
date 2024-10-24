ea23acc988add92f6c944be2fa4e16692e01aad1
Thomas Rittson <trittson@bitwarden.com>
2024-10-24 11:59:07 +1000

    Move private helper methods to bottom

--- libs/admin-console/src/common/collections/services/default-collection-vNext.service.ts
+++ libs/admin-console/src/common/collections/services/default-collection-vNext.service.ts
@@ -101,32 +101,6 @@ export class DefaultCollectionvNextService implements CollectionvNextService {
     return userId$.pipe(switchMap((userId) => this.decryptedCollectionState(userId).state$));
   }
 
-  /**
-   * @returns a SingleUserState for encrypted collection data.
-   */
-  private encryptedCollectionState(userId: UserId) {
-    return this.stateProvider.getUser(userId, ENCRYPTED_COLLECTION_DATA_KEY);
-  }
-
-  /**
-   * @returns a SingleUserState for decrypted collection data.
-   */
-  private decryptedCollectionState(userId: UserId): DerivedState<CollectionView[]> {
-    const encryptedCollectionsWithKeys = this.encryptedCollectionState(userId).combinedState$.pipe(
-      switchMap(([userId, collectionData]) =>
-        combineLatest([of(collectionData), this.cryptoService.orgKeys$(userId)]),
-      ),
-    );
-
-    return this.stateProvider.getDerived(
-      encryptedCollectionsWithKeys,
-      DECRYPTED_COLLECTION_DATA_KEY,
-      {
-        collectionService: this,
-      },
-    );
-  }
-
   async encrypt(model: CollectionView): Promise<Collection> {
     if (model.organizationId == null) {
       throw new Error("Collection has no organization id.");
@@ -249,5 +223,31 @@ export class DefaultCollectionvNextService implements CollectionvNextService {
       }
       return collections;
     });
+  }
+
+  /**
+   * @returns a SingleUserState for encrypted collection data.
+   */
+  private encryptedCollectionState(userId: UserId) {
+    return this.stateProvider.getUser(userId, ENCRYPTED_COLLECTION_DATA_KEY);
+  }
+
+  /**
+   * @returns a SingleUserState for decrypted collection data.
+   */
+  private decryptedCollectionState(userId: UserId): DerivedState<CollectionView[]> {
+    const encryptedCollectionsWithKeys = this.encryptedCollectionState(userId).combinedState$.pipe(
+      switchMap(([userId, collectionData]) =>
+        combineLatest([of(collectionData), this.cryptoService.orgKeys$(userId)]),
+      ),
+    );
+
+    return this.stateProvider.getDerived(
+      encryptedCollectionsWithKeys,
+      DECRYPTED_COLLECTION_DATA_KEY,
+      {
+        collectionService: this,
+      },
+    );
   }
 }
