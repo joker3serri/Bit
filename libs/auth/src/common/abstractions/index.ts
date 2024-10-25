c5abdc3f4849979f0c6dece210398a43b24e9346
Bernd Schoolmann <mail@quexten.com>
2024-10-25 13:23:57 +0200

    Move userkey rotation data provider abstraction to km ownership

--- libs/auth/src/common/abstractions/index.ts
+++ libs/auth/src/common/abstractions/index.ts
@@ -3,4 +3,4 @@ export * from "./login-email.service";
 export * from "./login-strategy.service";
 export * from "./user-decryption-options.service.abstraction";
 export * from "./auth-request.service.abstraction";
-export * from "./user-key-rotation-data-provider.abstraction";
+export * from "../../../../key-management/src/abstractions/user-key-rotation-data-provider.abstraction";
