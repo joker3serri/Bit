85c0bd3bd7ece85025781e626b1d222e7cbc2aa0
Bernd Schoolmann <mail@quexten.com>
2024-10-25 15:19:45 +0200

    Remove incorrect export

--- libs/auth/src/common/abstractions/index.ts
+++ libs/auth/src/common/abstractions/index.ts
@@ -3,4 +3,3 @@ export * from "./login-email.service";
 export * from "./login-strategy.service";
 export * from "./user-decryption-options.service.abstraction";
 export * from "./auth-request.service.abstraction";
-export * from "../../../../key-management/src/abstractions/user-key-rotation-data-provider.abstraction";
