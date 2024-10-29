d8927de4c9543134958144c2f528c883dd60fa5a
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-10-29 14:25:28 -0700

    add template

--- libs/auth/src/angular/login-decryption-options/login-decryption-options.component.ts
+++ libs/auth/src/angular/login-decryption-options/login-decryption-options.component.ts
@@ -1,8 +1,38 @@
-import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import {
+  AsyncActionsModule,
+  ButtonModule,
+  CheckboxModule,
+  FormFieldModule,
+} from "@bitwarden/components";
 
 @Component({
   standalone: true,
   templateUrl: "./login-decryption-options.component.html",
-  imports: [],
+  imports: [
+    AsyncActionsModule,
+    ButtonModule,
+    CheckboxModule,
+    CommonModule,
+    FormFieldModule,
+    JslibModule,
+    ReactiveFormsModule,
+  ],
 })
-export class LoginDecryptionOptionsComponent {}
+export class LoginDecryptionOptionsComponent implements OnInit {
+  loading = false;
+
+  formGroup = this.formBuilder.group({
+    rememberDevice: [true],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  submit = () => {};
+}
