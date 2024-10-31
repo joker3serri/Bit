7a35d66a9a3c3b80940ef67813568497de1e0008
Cy Okeke <cokeke@bitwarden.com>
2024-10-31 22:06:52 +0100

    Resolve the disable icon issue

--- apps/web/src/app/billing/shared/adjust-payment-dialog/adjust-payment-dialog.component.ts
+++ apps/web/src/app/billing/shared/adjust-payment-dialog/adjust-payment-dialog.component.ts
@@ -74,7 +74,7 @@ export class AdjustPaymentDialogComponent {
       }
     });
     await response;
-    await new Promise((resolve) => setTimeout(resolve, 5000));
+    await new Promise((resolve) => setTimeout(resolve, 7000));
     this.toastService.showToast({
       variant: "success",
       title: null,
