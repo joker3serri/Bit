03fdefb2861e35895b70467f171a6e4a97f3757c
Jonathan Prusik <jprusik@classynemesis.com>
2024-10-31 13:23:20 -0400

    update extension autofill concerns to use theme object and type over enum

--- libs/common/src/platform/enums/theme-type.enum.ts
+++ libs/common/src/platform/enums/theme-type.enum.ts
@@ -1,3 +1,4 @@
+// Deprecated; prefer the constants/types below over unsafe enum types
 export enum ThemeType {
   System = "system",
   Light = "light",
@@ -5,3 +6,13 @@ export enum ThemeType {
   Nord = "nord",
   SolarizedDark = "solarizedDark",
 }
+
+export const ThemeTypes = {
+  System: "system",
+  Light: "light",
+  Dark: "dark",
+  Nord: "nord",
+  SolarizedDark: "solarizedDark",
+} as const;
+
+export type Theme = (typeof ThemeTypes)[keyof typeof ThemeTypes];
