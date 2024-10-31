335958e096d705726c88d89b960ab87854e2d557
rr-bw <102181210+rr-bw@users.noreply.github.com>
2024-10-31 15:31:16 -0700

    migrate loadUntrustedDeviceData() and requestAdminApproval() methods

--- libs/auth/src/angular/login-decryption-options/login-decryption-options.component.ts
+++ libs/auth/src/angular/login-decryption-options/login-decryption-options.component.ts
@@ -6,23 +6,43 @@ import { Router } from "@angular/router";
 import { defer, firstValueFrom, map, switchMap } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import {
+  LoginEmailServiceAbstraction,
+  UserDecryptionOptions,
+  UserDecryptionOptionsServiceAbstraction,
+} from "@bitwarden/auth/common";
 import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { DeviceTrustServiceAbstraction } from "@bitwarden/common/auth/abstractions/device-trust.service.abstraction";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
+import { ValidationService } from "@bitwarden/common/platform/abstractions/validation.service";
 import { UserId } from "@bitwarden/common/types/guid";
-import {
-  AsyncActionsModule,
-  ButtonModule,
-  CheckboxModule,
-  FormFieldModule,
-  ToastService,
-} from "@bitwarden/components";
+import { ButtonModule, CheckboxModule, FormFieldModule, ToastService } from "@bitwarden/components";
+
+enum State {
+  NewUser,
+  ExistingUserUntrustedDevice,
+}
+
+type NewUserData = {
+  readonly state: State.NewUser;
+  readonly organizationId: string;
+  readonly userEmail: string;
+};
+
+type ExistingUserUntrustedDeviceData = {
+  readonly state: State.ExistingUserUntrustedDevice;
+  readonly showApproveFromOtherDeviceBtn: boolean;
+  readonly showReqAdminApprovalBtn: boolean;
+  readonly showApproveWithMasterPasswordBtn: boolean;
+  readonly userEmail: string;
+};
+
+type Data = NewUserData | ExistingUserUntrustedDeviceData;
 
 @Component({
   standalone: true,
   templateUrl: "./login-decryption-options.component.html",
   imports: [
-    AsyncActionsModule,
     ButtonModule,
     CheckboxModule,
     CommonModule,
@@ -35,11 +55,13 @@ export class LoginDecryptionOptionsComponent implements OnInit {
   private activeAccountId: UserId;
   private email: string;
 
-  loading = false;
+  protected data?: Data;
+  protected loading = false;
+  protected State = State;
 
-  // Remember device means for the user to trust the device
-  formGroup = this.formBuilder.group({
-    rememberDevice: [false], // TODO-rr-bw: change to true by default after testing
+  protected formGroup = this.formBuilder.group({
+    // TODO-rr-bw: change to true by default after testing
+    rememberDevice: [false], // Remember device means for the user to trust the device
   });
 
   get rememberDeviceControl(): FormControl<boolean> {
@@ -52,8 +74,11 @@ export class LoginDecryptionOptionsComponent implements OnInit {
     private deviceTrustService: DeviceTrustServiceAbstraction,
     private formBuilder: FormBuilder,
     private i18nService: I18nService,
+    private loginEmailService: LoginEmailServiceAbstraction,
     private router: Router,
     private toastService: ToastService,
+    private userDecryptionOptionsService: UserDecryptionOptionsServiceAbstraction,
+    private validationService: ValidationService,
   ) {}
 
   async ngOnInit(): Promise<void> {
@@ -80,6 +105,23 @@ export class LoginDecryptionOptionsComponent implements OnInit {
 
     // Persist user choice from state if it exists
     await this.setRememberDeviceDefaultValue();
+
+    try {
+      const userDecryptionOptions = await firstValueFrom(
+        this.userDecryptionOptionsService.userDecryptionOptions$,
+      );
+
+      if (
+        !userDecryptionOptions?.trustedDeviceOption?.hasAdminApproval &&
+        !userDecryptionOptions?.hasMasterPassword
+      ) {
+        // this.loadNewUserData();
+      } else {
+        this.loadUntrustedDeviceData(userDecryptionOptions);
+      }
+    } catch (err) {
+      this.validationService.showError(err);
+    }
   }
 
   private observeAndPersistRememberDeviceValueChanges(): void {
@@ -103,5 +145,26 @@ export class LoginDecryptionOptionsComponent implements OnInit {
     this.rememberDeviceControl.setValue(rememberDevice);
   }
 
-  submit = () => {};
+  private loadUntrustedDeviceData(userDecryptionOptions: UserDecryptionOptions) {
+    const showApproveFromOtherDeviceBtn =
+      userDecryptionOptions?.trustedDeviceOption?.hasLoginApprovingDevice || false;
+
+    const showReqAdminApprovalBtn =
+      !!userDecryptionOptions?.trustedDeviceOption?.hasAdminApproval || false;
+
+    const showApproveWithMasterPasswordBtn = userDecryptionOptions?.hasMasterPassword || false;
+
+    this.data = {
+      state: State.ExistingUserUntrustedDevice,
+      showApproveFromOtherDeviceBtn,
+      showReqAdminApprovalBtn,
+      showApproveWithMasterPasswordBtn,
+      userEmail: this.email,
+    };
+  }
+
+  protected async requestAdminApproval() {
+    this.loginEmailService.setLoginEmail(this.data.userEmail);
+    await this.router.navigate(["/admin-approval-requested"]);
+  }
 }
