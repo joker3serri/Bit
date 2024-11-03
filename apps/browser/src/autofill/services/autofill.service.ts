e4f76a820f182cc62432707c6093ff414265b030
Brandon <36675220+BrandonTreston@users.noreply.github.com>
2024-11-02 23:59:09 -0400

    Remove comment

--- apps/browser/src/autofill/services/autofill.service.ts
+++ apps/browser/src/autofill/services/autofill.service.ts
@@ -370,8 +370,6 @@ export default class AutofillService implements AutofillServiceInterface {
 
         didAutofill = true;
         if (!options.skipLastUsed) {
-          // FIXME: Verify that this floating promise is intentional. If it is, add an explanatory comment and ensure there is proper error handling.
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
           await this.cipherService.updateLastUsedDate(options.cipher.id);
         }
 
