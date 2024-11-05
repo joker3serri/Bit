6589aa161247e1d89b8a954608cc8b0ee61e7f37
jng <jng@bitwarden.com>
2024-11-05 09:08:52 -0500

    update error message for cli edit collections

--- apps/cli/src/commands/edit.command.ts
+++ apps/cli/src/commands/edit.command.ts
@@ -129,6 +129,11 @@ export class EditCommand {
       const res = new CipherResponse(decCipher);
       return Response.success(res);
     } catch (e) {
+      // If a Resource not found error is returned here, the user did not have ViewPassword permissions
+      // Show No Edit Permission message
+      if (e.message === "Resource not found.") {
+        return Response.noEditPermission();
+      }
       return Response.error(e);
     }
   }
