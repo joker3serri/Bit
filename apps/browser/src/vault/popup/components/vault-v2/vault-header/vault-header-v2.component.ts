b2c50ffc4ef42ecffdaf56b2e9509230794440c0
Nick Krantz <nick@livefront.com>
2024-11-07 13:53:51 -0600

    integrate disclosure trigger to hide vault filters

--- apps/browser/src/vault/popup/components/vault-v2/vault-header/vault-header-v2.component.ts
+++ apps/browser/src/vault/popup/components/vault-v2/vault-header/vault-header-v2.component.ts
@@ -1,5 +1,9 @@
+import { NgClass } from "@angular/common";
 import { Component } from "@angular/core";
 
+import { DisclosureTriggerForDirective, IconButtonModule } from "@bitwarden/components";
+
+import { DisclosureComponent } from "../../../../../../../../libs/components/src/disclosure/disclosure.component";
 import { VaultListFiltersComponent } from "../vault-list-filters/vault-list-filters.component";
 import { VaultV2SearchComponent } from "../vault-search/vault-v2-search.component";
 
@@ -7,6 +11,13 @@ import { VaultV2SearchComponent } from "../vault-search/vault-v2-search.componen
   selector: "app-vault-header-v2",
   templateUrl: "vault-header-v2.component.html",
   standalone: true,
-  imports: [VaultV2SearchComponent, VaultListFiltersComponent],
+  imports: [
+    VaultV2SearchComponent,
+    VaultListFiltersComponent,
+    DisclosureComponent,
+    IconButtonModule,
+    DisclosureTriggerForDirective,
+    NgClass,
+  ],
 })
 export class VaultHeaderV2Component {}
