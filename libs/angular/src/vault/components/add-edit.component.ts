5e39aa0b5295f9b8f0429992def6cd1981961447
Shane Melton <smelton@bitwarden.com>
2024-11-07 16:58:16 -0800

    [PM-5718] Fix totp generation for free orgs in old add-edit component

--- libs/angular/src/vault/components/add-edit.component.ts
+++ libs/angular/src/vault/components/add-edit.component.ts
@@ -120,7 +120,7 @@ export class AddEditComponent implements OnInit, OnDestroy {
     protected policyService: PolicyService,
     protected logService: LogService,
     protected passwordRepromptService: PasswordRepromptService,
-    private organizationService: OrganizationService,
+    protected organizationService: OrganizationService,
     protected sendApiService: SendApiService,
     protected dialogService: DialogService,
     protected win: Window,
