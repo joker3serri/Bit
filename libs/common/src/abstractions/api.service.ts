01ec4b9ffce6cf60c54492f113adcfeab370a098
Cy Okeke <cokeke@bitwarden.com>
2024-11-13 17:15:22 +0100

    Resolve the lint issue

--- libs/common/src/abstractions/api.service.ts
+++ libs/common/src/abstractions/api.service.ts
@@ -24,7 +24,7 @@ import {
 } from "../admin-console/models/response/organization-connection.response";
 import { OrganizationExportResponse } from "../admin-console/models/response/organization-export.response";
 import { OrganizationSponsorshipSyncStatusResponse } from "../admin-console/models/response/organization-sponsorship-sync-status.response";
-import { PreValidateSponsorshipResponse } from "../admin-console/models/response/preValidate-sponsorship.response";
+import { PreValidateSponsorshipResponse } from "../admin-console/models/response/prevalidate-sponsorship.response";
 import {
   ProviderOrganizationOrganizationDetailsResponse,
   ProviderOrganizationResponse,
