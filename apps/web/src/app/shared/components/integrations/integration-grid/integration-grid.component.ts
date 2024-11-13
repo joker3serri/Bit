02f0392b27d03d9ff7539388a584c1fd402f720c
Brandon <btreston@bitwarden.com>
2024-11-13 13:45:07 -0500

    refactor modules and imports. Remove hyperlink text from integration card template

--- apps/web/src/app/shared/components/integrations/integration-grid/integration-grid.component.ts
+++ apps/web/src/app/shared/components/integrations/integration-grid/integration-grid.component.ts
@@ -2,16 +2,15 @@ import { Component, Input } from "@angular/core";
 
 import { IntegrationType } from "@bitwarden/common/enums";
 
-import { Integration } from "../models";
-import { IntegrationCardComponent } from "../integration-card/integration-card.component";
 import { SharedModule } from "../../../shared.module";
-import { CommonModule } from "@angular/common";
+import { IntegrationCardComponent } from "../integration-card/integration-card.component";
+import { Integration } from "../models";
 
 @Component({
   selector: "app-integration-grid",
   templateUrl: "./integration-grid.component.html",
   standalone: true,
-  imports: [IntegrationCardComponent, SharedModule, CommonModule],
+  imports: [IntegrationCardComponent, SharedModule],
 })
 export class IntegrationGridComponent {
   @Input() integrations: Integration[];
