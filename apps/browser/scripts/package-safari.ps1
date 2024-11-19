45bc304e1293ca04a52d8bd49852840f0bd42a67
Hinton <hinton@users.noreply.github.com>
2024-11-19 11:38:43 +0100

    Try including quotations

--- apps/browser/scripts/package-safari.ps1
+++ apps/browser/scripts/package-safari.ps1
@@ -44,7 +44,7 @@ foreach ($subBuildPath in $subBuildPaths) {
                 "--verbose",
                 "--force",
                 "--sign",
-                "3rd Party Mac Developer Application: Bitwarden Inc",
+                '"3rd Party Mac Developer Application: Bitwarden Inc"',
                 "--entitlements",
                 $entitlementsPath
             )
@@ -66,7 +66,7 @@ foreach ($subBuildPath in $subBuildPaths) {
                 "-o",
                 "runtime",
                 "--sign",
-                "Developer ID Application: 8bit Solutions LLC",
+                '"Developer ID Application: 8bit Solutions LLC"',
                 "--entitlements",
                 $entitlementsPath
             )
