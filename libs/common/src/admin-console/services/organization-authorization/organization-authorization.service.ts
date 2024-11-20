942c8d340088ea1d9d7c4735f9f28799d07cd25e
Thomas Rittson <trittson@bitwarden.com>
2024-11-20 13:51:00 +1000

    Revert "Add org authz service"
    This reverts commit 0cf1f232b623c31629c88625501dc1cfd5b6d5bf.

--- libs/common/src/admin-console/services/organization-authorization/organization-authorization.service.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from "rxjs";
-
-import { Organization } from "../../models/domain/organization";
-
-export abstract class OrganizationAuthorizationService {
-  canExport: (org: Organization) => Observable<boolean>;
-  canImport: (org: Organization) => boolean;
-}
