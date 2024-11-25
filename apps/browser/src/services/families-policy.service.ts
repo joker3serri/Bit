1e1e4123fec1a7aab32dcf44ed4e7307f02aeb94
Cy Okeke <cokeke@bitwarden.com>
2024-11-25 16:33:56 +0100

    Add some test and documentations to service

--- apps/browser/src/services/families-policy.service.ts
+++ apps/browser/src/services/families-policy.service.ts
@@ -13,35 +13,41 @@ export class FamiliesPolicyService {
   ) {}
 
   hasSingleEnterpriseOrg$(): Observable<boolean> {
-    return this.organizationService
-      .getAll$()
-      .pipe(
-        map(
-          (organizations) => organizations.filter((org) => org.canManageSponsorships).length === 1,
-        ),
-      );
+    // Retrieve all organizations the user is part of
+    return this.organizationService.getAll$().pipe(
+      map((organizations) => {
+        // Filter to only those organizations that can manage sponsorships
+        const sponsorshipOrgs = organizations.filter((org) => org.canManageSponsorships);
+
+        // Check if there is exactly one organization that can manage sponsorships.
+        // This is important because users that are part of multiple organizations
+        // may always access free bitwarden family menu. We want to restrict access
+        // to the policy only when there is a single enterprise organization and the free family policy is turn.
+        return sponsorshipOrgs.length === 1;
+      }),
+    );
   }
 
   isFreeFamilyPolicyEnabled$(): Observable<boolean> {
     return this.hasSingleEnterpriseOrg$().pipe(
       switchMap((hasSingleEnterpriseOrg) => {
-        if (hasSingleEnterpriseOrg) {
-          return this.organizationService.getAll$().pipe(
-            map((organizations) => organizations.find((org) => org.canManageSponsorships)?.id),
-            switchMap((enterpriseOrgId) =>
-              this.policyService
-                .getAll$(PolicyType.FreeFamiliesSponsorshipPolicy)
-                .pipe(
-                  map(
-                    (policies) =>
-                      policies.find((policy) => policy.organizationId === enterpriseOrgId)
-                        ?.enabled ?? false,
-                  ),
-                ),
-            ),
-          );
+        if (!hasSingleEnterpriseOrg) {
+          return of(false);
         }
-        return of(false);
+        return this.organizationService.getAll$().pipe(
+          map((organizations) => organizations.find((org) => org.canManageSponsorships)?.id),
+          switchMap((enterpriseOrgId) =>
+            this.policyService
+              .getAll$(PolicyType.FreeFamiliesSponsorshipPolicy)
+              .pipe(
+                map(
+                  (policies) =>
+                    policies.find((policy) => policy.organizationId === enterpriseOrgId)?.enabled ??
+                    false,
+                ),
+              ),
+          ),
+        );
       }),
     );
   }
