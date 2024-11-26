027dbd33d0fd0276488173baddd8ee2f0b79e7b9
Bernd Schoolmann <mail@quexten.com>
2024-11-26 18:44:36 +0100

    Make type be compared with ===

--- apps/web/src/app/vault/individual-vault/add-edit.component.ts
+++ apps/web/src/app/vault/individual-vault/add-edit.component.ts
@@ -103,7 +103,7 @@ export class AddEditComponent extends BaseAddEditComponent implements OnInit, On
 
     // https://bitwarden.atlassian.net/browse/PM-10413
     // cannot generate ssh keys so block creation
-    if (this.type == CipherType.SshKey && this.cipherId === undefined) {
+    if (this.type === CipherType.SshKey && this.cipherId == null) {
       this.type = CipherType.Login;
       this.cipher.type = CipherType.Login;
     }
