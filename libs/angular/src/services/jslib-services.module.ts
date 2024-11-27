8abba01c5fea6c53f20c3217b29f374287762d62
Daniel James Smith <djsmith85@users.noreply.github.com>
2024-11-27 15:00:52 +0100

    Fix dependency on ApiService within jslib-services.module

--- libs/angular/src/services/jslib-services.module.ts
+++ libs/angular/src/services/jslib-services.module.ts
@@ -445,7 +445,7 @@ const safeProviders: SafeProvider[] = [
   safeProvider({
     provide: FileUploadServiceAbstraction,
     useClass: FileUploadService,
-    deps: [LogService],
+    deps: [LogService, ApiServiceAbstraction],
   }),
   safeProvider({
     provide: CipherFileUploadServiceAbstraction,
