8847b72fb6c1e4178ca0c4a9fd8de415eb97216c
Alec Rippberger <alec@livefront.com>
2024-12-01 20:27:34 -0600

    Clean up TwoFactorTimeoutComponent

--- libs/angular/src/auth/components/two-factor-auth/two-factor-auth-expired.component.ts
+++ libs/angular/src/auth/components/two-factor-auth/two-factor-auth-expired.component.ts
@@ -3,19 +3,17 @@ import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
-import { I18nPipe } from "@bitwarden/angular/platform/pipes/i18n.pipe";
 import { ButtonModule } from "@bitwarden/components";
 
 @Component({
   selector: "app-two-factor-expired",
   standalone: true,
   imports: [CommonModule, JslibModule, ButtonModule],
-  providers: [I18nPipe],
   template: `
     <p class="tw-text-center">
       {{ "authenticationSessionTimedOut" | i18n }}
     </p>
-    <button bitButton block (click)="logIn()" buttonType="primary">
+    <button bitButton block (click)="navigateToLogin()" buttonType="primary">
       {{ "logIn" | i18n }}
     </button>
   `,
@@ -23,7 +21,7 @@ import { ButtonModule } from "@bitwarden/components";
 export class TwoFactorTimeoutComponent {
   constructor(private router: Router) {}
 
-  async logIn() {
+  async navigateToLogin() {
     await this.router.navigate(["login"]);
   }
 }
