dc574bd918465a2361a4f9524eeea5e9c5902ebc
Vicki League <vleague@bitwarden.com>
2024-12-03 14:14:27 -0500

    switch to infiniteScrollDirective now that module is deprecated

--- bitwarden_license/bit-web/src/app/app.module.ts
+++ bitwarden_license/bit-web/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule } from "@angular/router";
-import { InfiniteScrollModule } from "ngx-infinite-scroll";
+import { InfiniteScrollDirective } from "ngx-infinite-scroll";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { CoreModule } from "@bitwarden/web-vault/app/core";
@@ -36,7 +36,7 @@ import { FreeFamiliesSponsorshipPolicyComponent } from "./billing/policies/free-
     FormsModule,
     ReactiveFormsModule,
     CoreModule,
-    InfiniteScrollModule,
+    InfiniteScrollDirective,
     DragDropModule,
     AppRoutingModule,
     OssRoutingModule,
