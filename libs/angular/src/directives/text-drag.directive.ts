04ac39eca61ecfa00d289deec318a0d2a6099fe4
--global <>
2024-12-10 16:02:50 -0500

    handle strict nulls

--- libs/angular/src/directives/text-drag.directive.ts
+++ libs/angular/src/directives/text-drag.directive.ts
@@ -13,10 +13,14 @@ export class TextDragDirective {
     alias: "appTextDrag",
     required: true,
   })
-  data = "";
+  @Input()
+  data: string | null = "";
 
   @HostListener("dragstart", ["$event"])
   onDragStart(event: DragEvent) {
+    if (this.data === null || this.data === undefined) {
+      this.data = "";
+    }
     event.dataTransfer.setData("text", this.data);
   }
 }
