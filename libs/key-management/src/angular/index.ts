bc8bdc84b128f1c636f1830f2f3159bc03d74c89
Thomas Avery <tavery@bitwarden.com>
2024-12-13 16:18:54 -0600

    Move lock component v2 to KM

---
+++ libs/key-management/src/angular/index.ts
@@ -0,0 +1,10 @@
+/**
+ * This barrel file should only contain Angular exports
+ */
+
+export { LockV2Component } from "./lock/components/lock.component";
+export {
+  LockComponentService,
+  BiometricsDisableReason,
+  UnlockOptions,
+} from "./lock/services/lock-component.service";
