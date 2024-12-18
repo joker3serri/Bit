049f4c03e0f9fbd5c24b1fb261947dc7eb6024f7
Shane Melton <smelton@bitwarden.com>
2024-12-18 12:00:14 -0800

    [PM-16098] Add CS link to decryption failure dialog

--- libs/vault/src/components/decryption-failure-dialog/decryption-failure-dialog.component.ts
+++ libs/vault/src/components/decryption-failure-dialog/decryption-failure-dialog.component.ts
@@ -3,8 +3,10 @@ import { CommonModule } from "@angular/common";
 import { Component, inject } from "@angular/core";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
 import { CipherId } from "@bitwarden/common/types/guid";
 import {
+  AnchorLinkDirective,
   AsyncActionsModule,
   ButtonModule,
   DialogModule,
@@ -27,11 +29,13 @@ export type DecryptionFailureDialogParams = {
     JslibModule,
     AsyncActionsModule,
     ButtonModule,
+    AnchorLinkDirective,
   ],
 })
 export class DecryptionFailureDialogComponent {
   protected dialogRef = inject(DialogRef);
   protected params = inject<DecryptionFailureDialogParams>(DIALOG_DATA);
+  protected platformUtilsService = inject(PlatformUtilsService);
 
   selectText(element: HTMLElement) {
     const selection = window.getSelection();
@@ -42,6 +46,11 @@ export class DecryptionFailureDialogComponent {
     const range = document.createRange();
     range.selectNodeContents(element);
     selection.addRange(range);
+  }
+
+  openContactSupport(event: Event) {
+    event.preventDefault();
+    this.platformUtilsService.launchUri("https://bitwarden.com/contact");
   }
 
   static open(dialogService: DialogService, params: DecryptionFailureDialogParams) {
