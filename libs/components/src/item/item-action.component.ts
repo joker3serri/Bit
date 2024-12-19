fc4c0eb9804f34ab73c2496ae30f28e809e9410a
Vicki League <vleague@bitwarden.com>
2024-12-19 13:48:18 -0500

    update :before height and update story

--- libs/components/src/item/item-action.component.ts
+++ libs/components/src/item/item-action.component.ts
@@ -10,7 +10,11 @@ import { A11yCellDirective } from "../a11y/a11y-cell.directive";
   providers: [{ provide: A11yCellDirective, useExisting: ItemActionComponent }],
   host: {
     class:
-      "[&>button]:tw-relative [&>button:not([bit-item-content])]:before:tw-content-[''] [&>button]:before:tw-absolute [&>button]:before:tw-block [&>button]:before:tw-top-[-0.75rem] [&>button]:before:tw-bottom-[-0.75rem] [&>button]:before:tw-right-[-0.25rem] [&>button]:before:tw-left-[-0.25rem]",
+      /**
+       * `top` and `bottom` units should be kept in sync with `item-content.component.ts`'s y-axis padding.
+       * we want this `:before` element to be the same height as the `item-content`
+       */
+      "[&>button]:tw-relative [&>button:not([bit-item-content])]:before:tw-content-[''] [&>button]:before:tw-absolute [&>button]:before:tw-block bit-compact:[&>button]:before:tw-top-[-0.8rem] bit-compact:[&>button]:before:tw-bottom-[-0.8rem] [&>button]:before:tw-top-[-0.85rem] [&>button]:before:tw-bottom-[-0.85rem] [&>button]:before:tw-right-[-0.25rem] [&>button]:before:tw-left-[-0.25rem]",
   },
 })
 export class ItemActionComponent extends A11yCellDirective {}
