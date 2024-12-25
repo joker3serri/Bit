5d82ddf9672374c12ba62769347915ca96733355
Bernd Schoolmann <mail@quexten.com>
2024-12-25 12:51:17 +0100

    Fix import

--- apps/browser/src/background/runtime.background.ts
+++ apps/browser/src/background/runtime.background.ts
@@ -8,13 +8,13 @@ import { AccountService } from "@bitwarden/common/auth/abstractions/account.serv
 import { AutofillOverlayVisibility, ExtensionCommand } from "@bitwarden/common/autofill/constants";
 import { AutofillSettingsServiceAbstraction } from "@bitwarden/common/autofill/services/autofill-settings.service";
 import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
-import { ProcessReloadServiceAbstraction } from "@bitwarden/common/key-management/abstractions/process-reload.service";
 import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { MessagingService } from "@bitwarden/common/platform/abstractions/messaging.service";
 import { devFlagEnabled } from "@bitwarden/common/platform/misc/flags";
 import { Utils } from "@bitwarden/common/platform/misc/utils";
 import { CipherType } from "@bitwarden/common/vault/enums";
+import { ProcessReloadServiceAbstraction } from "@bitwarden/key-management";
 
 import { MessageListener, isExternalMessage } from "../../../../libs/common/src/platform/messaging";
 import {
